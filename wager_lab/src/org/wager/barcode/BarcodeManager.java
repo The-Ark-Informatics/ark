6c886409fb167b01b550ea6c0858fa85b074dec4
cwilliams <cwilliams@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2009-08-10 08:33:44 +0000

    Abstraction of Barcode Engine subsystem
    git-svn-id: svn://localhost@279 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- wager_lab/src/org/wager/barcode/BarcodeManager.java
+++ wager_lab/src/org/wager/barcode/BarcodeManager.java
@@ -1,6 +1,12 @@
 package org.wager.barcode;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 
 import neuragenix.security.AuthToken;
 
@@ -12,46 +18,79 @@ public class BarcodeManager {
 	static final int SINGLE_ADMISSION = 3;
 	static final int SINGLE_PLATE = 4;
 
-	public static InputStream generateBarcode(AuthToken authToken,
-			ChannelRuntimeData runtimeData) {
-		int mode = -1;
-		String domain = runtimeData.getParameter("domain").toUpperCase();
-		int patientkey = -1;
-		int biospecimenkey = -1;
-		if (domain.equals("BIOSPECIMEN")) {
-			String str_patientkey = runtimeData
-			.getParameter("PATIENT_intInternalPatientID");
-			String str_biospecimenkey = runtimeData.getParameter("BIOSPECIMEN_intBiospecimenID");
-			if (!str_patientkey.equals(null)) {
-				try {
-					patientkey = Integer.parseInt(str_patientkey);
-					mode = BULK_BIOSPECIMEN;
-
-				} catch (NumberFormatException ne) {
-				}
-			}
-			else if (!str_biospecimenkey.equals(null)) {
-				try {
-					biospecimenkey = Integer.parseInt(str_biospecimenkey);
-					mode = SINGLE_BIOSPECIMEN;
-
-				} catch (NumberFormatException ne) {
-				}
-			}
+	public static String barcodeSupported(Vector<Integer> siteList, String domain,
+			int studyKey) {
+		Connection conn;
+		
+		try {
+			
+			conn = DriverManager.getConnection("jdbc:poolman://oracle");
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT CLASS from IX_BARCODEENGINE where  SITEKEY in " + getSiteIDSQLString(siteList) + " and DOMAIN=? and (STUDYKEY=? or STUDYKEY=0) order by STUDYKEY DESC, SITEKEY DESC");
+			
+			ps.setString(1, domain.toUpperCase());
+			ps.setInt(2, studyKey);
+			ResultSet rs = ps.executeQuery();
+			rs.next(); // Grab the first.
+			String className = rs.getString("CLASS");
+			return className;
 			
 
-		} else if (domain.equals("ADMISSION")) {
-
-		} else if (domain.equals("INVENTORY")) {
-
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
 		}
 
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-		try {
-			BarcodeEngine e = (BarcodeEngine) Class.forName(
-					"org.wager.barcode.WAFSSBio").newInstance();
+	}
+	
+	 public static String getSiteIDSQLString (Vector<Integer> sitekeys){
+		  // System.err.println("Studykeys size: " + sitekeys.size());
+	       StringBuffer in_expr = new StringBuffer();
+	       in_expr.append("(");
+	       try {
+	       if (sitekeys.size() != 0) {
+	       for (int i=0; i < sitekeys.size(); i++) {
+	    	   Integer j = sitekeys.get(i);
+	       	in_expr.append(j.toString());
+	       if (i < sitekeys.size() - 1)
+	       	in_expr.append(",");
+	       }
+	       }
+	       else 
+	       	in_expr.append("null");
+	       
+	       in_expr.append(")");
+	       } catch (ClassCastException nfe) {
+	    	  in_expr = new StringBuffer("(null)");
+	       }
+	   	return in_expr.toString();
+	   	
+	   }
 
-			return e.getBarcode(runtimeData, authToken);
+	public static InputStream generateBarcode(AuthToken authToken,
+			ChannelRuntimeData runtimeData) {
+		String domain = runtimeData.getParameter("domain");
+		String studyKeystr = runtimeData.getParameter("studykey");
+		String domainKeyStr = runtimeData.getParameter("domainkey");
+		if (domain == null || studyKeystr == null || domainKeyStr == null)
+			 return null;
+		int studyKey = Integer.parseInt(studyKeystr);
+		int domainKey = Integer.parseInt(domainKeyStr);
+		Vector<Integer> siteList = authToken.getSiteList();
+		if (!siteList.contains(new Integer(0))) {
+			siteList.add(new Integer(0));
+		}
+		String classStr = barcodeSupported(siteList,domain,studyKey);
+		System.out.println("Loading class: " + classStr );
+		//ClassLoader cl = ClassLoader.getSystemClassLoader();
+		try {
+			Class<?> c =  Class.forName(
+					classStr);
+			
+			BarcodeEngine e = (BarcodeEngine) c.newInstance();
+
+			return e.getBarcode(domain,domainKey, authToken);
 
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
@@ -62,8 +101,13 @@ public class BarcodeManager {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (NumberFormatException ne) {
+			ne.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 		return null;
 	}
-
 }
--- wager_lab/src/org/wager/barcode/WAFSSAdmissionBarcode.java
+++ wager_lab/src/org/wager/barcode/WAFSSAdmissionBarcode.java
@@ -1,22 +1,26 @@
 package org.wager.barcode;
 
 import java.io.*;
-import java.util.Hashtable;
 
-import org.jasig.portal.ChannelRuntimeData;
 import org.jasig.portal.RDBMServices;
+
 import java.sql.*;
 
-import neuragenix.dao.*;
 import neuragenix.security.AuthToken;
 
-public class WAFSSBio implements BarcodeEngine {
-
-	public InputStream getBarcode(ChannelRuntimeData runtimeData, AuthToken authtoken) {
+public class WAFSSAdmissionBarcode extends AbstractBarcodeEngine {
+	
+	
+	@Override
+	public InputStream getBarcode(String strDomain, int domainKey,
+			AuthToken authtoken) {
+		// TODO Auto-generated method stub
 		 StringBuffer sb = new StringBuffer();
+		 if (!strDomain.equals("ADMISSIONS")) 
+			 return null;
 		 
-		 String patientkey = (String) runtimeData.getParameter("PATIENT_intInternalPatientID");
-         String admissionid = (String) runtimeData.getParameter("BIOSPECIMEN_strEncounter");
+		 int admissionkey = domainKey;
+				
          try{
          Connection con = RDBMServices.getConnection();
          Statement stmt = con.createStatement();
@@ -24,9 +28,8 @@ public class WAFSSBio implements BarcodeEngine {
                  "select patientid, to_char(dob, 'dd/mm/yyyy'), to_char(admissiondate, 'dd/mm/yyyy'), familyid, otherid, ref_doctor, sex " +
                  "from ix_patientv2 p,ix_admissions adm " +
                  "where p.patientkey = adm.patientkey " +
-                 "and adm.admissionid = '" + admissionid + "'" +
-                 "and adm.deleted = 0" +
-                 "and p.patientkey = " + patientkey);
+                 "and adm.admissionkey = '" + admissionkey + "'" +
+                 "and adm.deleted = 0");
          while (rset.next()) {
          String patientid = rset.getString(1);
          String dob = rset.getString(2);
@@ -35,10 +38,40 @@ public class WAFSSBio implements BarcodeEngine {
          String asrbno = rset.getString(5);
          String researcher = rset.getString(6);
          String sex = rset.getString(7);
-
-        
+         BarcodeData b = new BarcodeData();
+         b.setValue("PATIENTID", patientid);
+         b.setValue(DATE_OF_BIRTH,dob);
+         b.setValue("ADMISSIONDATE",admissiondate);
+         b.setValue("ASBRNO",asrbno);
+         b.setValue("FAMILYID",familyid);
+         b.setValue("RESEARCHER",researcher);
+         b.setValue("SEX",sex);
+         printBarcode(b,sb);
+         rset.close();
+         stmt.close();
+         con.close();
+         }
+         }catch(SQLException se) {
+        	 se.printStackTrace(System.err);
+         }
+		// TODO Auto-generated method stub
+       
          
-         sb.append("\n");
+		return new ByteArrayInputStream(sb.toString().getBytes());
+		
+	}
+
+	@Override
+	public void printBarcode(BarcodeData b, StringBuffer sb) {
+		
+		String patientid = b.getValue("PATIENTID");
+		String familyid = b.getValue("FAMILYID");
+		String asrbno = b.getValue("ASRBNO");
+		String admissiondate = b.getValue("ADMISSIONDATE");
+		String researcher = b.getValue("RESEARCHER");
+		String dob = b.getValue(DATE_OF_BIRTH);
+		String sex = b.getValue("SEX");
+		 sb.append("\n");
          sb.append("D15\n");
          sb.append("N\n");
          sb.append("A240,10,1,2,1,1,N,\"ID: " + patientid + " Family ID:" + familyid + "\"" + "\n");
@@ -47,13 +80,8 @@ public class WAFSSBio implements BarcodeEngine {
          sb.append("A180,10,1,2,1,1,N,\"Researcher: " + researcher + "\"" + "\n");
          sb.append("A160,10,1,2,1,1,N,\"DOB: " + dob + " " + "Sex: " + sex + "\"" + "\n");
          sb.append("P1\n");
-         }
-         }catch(SQLException se) {
-        	 se.printStackTrace(System.err);
-         }
-		// TODO Auto-generated method stub
-		return new ByteArrayInputStream(sb.toString().getBytes());
-		
 	}
 
-}
+	}	
+
+
