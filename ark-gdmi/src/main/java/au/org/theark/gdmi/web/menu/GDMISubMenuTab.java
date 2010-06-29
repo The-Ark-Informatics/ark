aa84b1998273e1c34b0878382d93cdb1676382f7
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-06-29 08:11:41 +0000

    Initial version checkin.
    git-svn-id: svn://localhost@1122 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-gdmi/src/main/java/au/org/theark/gdmi/web/menu/GDMISubMenuTab.java
@@ -0,0 +1,12 @@
+package au.org.theark.gdmi.web.menu;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class GDMISubMenuTab extends Panel {
+
+	public GDMISubMenuTab(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
