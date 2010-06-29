eb09f538bf6a53dd223744459652a5b14a317d7b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-06-29 06:27:54 +0000

    Modified the call to the service layer to use a light weight version of getUser that returns only the details of the user not the modules and roles he has. This allows the logged in user to change his personal details including his password.
    git-svn-id: svn://localhost@1108 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/user/MyDetailsContainer.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/user/MyDetailsContainer.java
@@ -1,11 +1,7 @@
 package au.org.theark.study.web.component.user;
 
-import java.util.List;
-
 import org.apache.shiro.subject.Subject;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +19,7 @@ import au.org.theark.study.web.Constants;
 public class MyDetailsContainer extends Panel{
 
 	private transient Logger log = LoggerFactory.getLogger(MyDetailsContainer.class);
-
-	private UserDetailsPanel userDetailsPanel;
+	
 	/**
 	 * The spring injected reference to a UserService implementation
 	 */
@@ -42,35 +37,12 @@ public class MyDetailsContainer extends Panel{
 		//Create a Form instance and send in the currently logged in  user details
 		userVO.setUserName(subject.getPrincipal().toString());
 		try{
-			List<EtaUserVO> userList = userService.searchUser(userVO);
-			if(userList.size() > 0){
-				userVO = userList.get(0);
-			}
+			userVO = userService.getCurrentUser(userVO.getUserName());
 		}catch(ArkSystemException ine){
 			log.error("Exception occured :" + ine.getMessage());
 		}
-		
-		MyDetailsForm form = new MyDetailsForm("myDetailsContainerForm",userVO);
-		add(form);
-	}
-	
-	
-	/**
-	 * A Form class that this container will use to add child components.
-	 */
-	private class MyDetailsForm extends Form {
-
-		/**
-		 * Construct the Form object and set the current user details 
-		 * @param id
-		 * @param userVO
-		 */
-		public MyDetailsForm(String id, EtaUserVO userVO) {
-			super(id, new CompoundPropertyModel<EtaUserVO>(userVO));
-			userVO.setMode(Constants.MODE_EDIT);
-			add( new UserDetailsPanel("userDetailsPanel", userVO));
-			
-		}
+		//Add the details panel into the container
+		add(new MyDetailsPanel(Constants.MY_DETAILS_PANEL, userVO));
 	}
 
 }
