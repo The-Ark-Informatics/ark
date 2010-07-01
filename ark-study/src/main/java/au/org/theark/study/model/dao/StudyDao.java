0711be50a2aa42941081ad42f725d8c9e631e5b0
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-01 08:01:05 +0000

    git-svn-id: svn://localhost@1143 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/model/dao/StudyDao.java
+++ ark-study/src/main/java/au/org/theark/study/model/dao/StudyDao.java
@@ -3,8 +3,8 @@ package au.org.theark.study.model.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
-import org.hibernate.criterion.Example;
-import org.hibernate.criterion.Order;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import au.org.theark.core.dao.HibernateSessionDao;
@@ -14,14 +14,17 @@ import au.org.theark.study.web.Constants;
 @Repository(Constants.STUDY_DAO)
 public class StudyDao extends HibernateSessionDao implements IStudyDao {
 
-	public List<Study> getStudy(Study study){
+	public List<Study> getStudy(Study study)
+	{
 		
-		Example example = Example.create(study);
-		example.ignoreCase().enableLike();
-		Criteria studyCritera =  getSession().createCriteria(Study.class).
-												addOrder(Order.asc("studyKey")).
-												addOrder(Order.asc("name" )).
-												add(example);
+		Criteria studyCritera =  getSession().createCriteria(Study.class);
+		
+		studyCritera.add(Restrictions.isNotNull("studyKey"));
+		studyCritera.add(Restrictions.eq("studyKey",study.getStudyKey()));
+		studyCritera.add(Restrictions.ilike("name", study.getName(), MatchMode.ANYWHERE));
+		
+		//studyCritera.add(Restrictions.ilike("name",study.getName(),MatchMode.ANYWHERE));
+		//studyCritera.addOrder(Order.asc("name"));
 		List<Study> studyList  = studyCritera.list();
 		return studyList;
 	}
