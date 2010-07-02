a51ccb257cd0f9df31933b9ef460452af5188acb
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-02 05:17:37 +0000

    Added Constants class, containing header image names
    git-svn-id: svn://localhost@1158 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-container/src/main/java/au/org/theark/web/pages/Constants.java
@@ -0,0 +1,7 @@
+package au.org.theark.web.pages;
+
+public class Constants
+{
+	public static final String HOSTED_BY_IMAGE = "hosted_by_image.gif";
+	public static final String PRODUCT_IMAGE = "product_image.gif";
+}
