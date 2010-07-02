2d8441a4a8f8d94df8e0d2e7c422a4fa000e7065
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-02 10:09:01 +0000

    Moved the my details container and related files into mydetails package. This essentially part of user component but for clarity have moved it here.
    git-svn-id: svn://localhost@1186 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-study/src/main/java/au/org/theark/study/web/component/mydetails/MyDetailsContainer.java
@@ -0,0 +1,48 @@
+package au.org.theark.study.web.component.mydetails;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.vo.EtaUserVO;
+import au.org.theark.study.service.IUserService;
+import au.org.theark.study.web.Constants;
+
+/**
+ * A class that will contain Details component.
+ * @author nivedann
+ *
+ */
+public class MyDetailsContainer extends Panel{
+
+	private transient Logger log = LoggerFactory.getLogger(MyDetailsContainer.class);
+	
+	/**
+	 * The spring injected reference to a UserService implementation
+	 */
+	@SpringBean( name = "userService")
+	private IUserService userService;
+	
+	/**
+	 * Construct the panel that will contain the User Details
+	 * @param id
+	 * @param userVO
+	 * @param subject
+	 */
+	public MyDetailsContainer(String id, EtaUserVO userVO, Subject subject) {
+		super(id);
+		//Create a Form instance and send in the currently logged in  user details
+		userVO.setUserName(subject.getPrincipal().toString());
+		try{
+			userVO = userService.getCurrentUser(userVO.getUserName());
+		}catch(ArkSystemException ine){
+			log.error("Exception occured :" + ine.getMessage());
+		}
+		//Add the details panel into the container
+		add(new MyDetails(Constants.MY_DETAILS_PANEL, userVO));
+	}
+
+}
