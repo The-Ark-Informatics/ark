d79ae91ed3021fc9691ab033ba548a5d1f9ca788
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-02 10:16:43 +0000

    Reverted change to UserContainerPanel. Having it as Container causes conflict with other Containers. E.g if we created instances of StudyContainer and User Container. We have to explicity state the package but will be cumbersome in code. So prefixing the component name before container would be the way to go.
    git-svn-id: svn://localhost@1190 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/user/UserContainer.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/user/UserContainer.java
@@ -19,10 +19,10 @@ import au.org.theark.study.service.IUserService;
  * @author nivedann
  *
  */
-public class Container extends Panel{
+public class UserContainer extends Panel{
 	
 	private static final long serialVersionUID = 1L;
-	private transient Logger log = LoggerFactory.getLogger(Container.class);
+	private transient Logger log = LoggerFactory.getLogger(UserContainer.class);
 
 	/**
 	 * Child components
@@ -55,7 +55,7 @@ public class Container extends Panel{
 	 * Constructor
 	 * @param id
 	 */
-	public Container(String id) {
+	public UserContainer(String id) {
 		super(id);
 		searchUserPanel = new Search("searchUserPanel");
 		add(searchUserPanel);
@@ -66,7 +66,7 @@ public class Container extends Panel{
 	 * @param id
 	 * @param userVO
 	 */
-	public Container(String id, EtaUserVO userVO) {
+	public UserContainer(String id, EtaUserVO userVO) {
 		
 		super(id);
 		searchUserPanel = new Search("searchUserPanel");
