dbe0d1089f93f57e4d0e73eb760a2f37c983c8fc
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-06 07:58:26 +0000

    Added an implementation to get a list of study status'.
    git-svn-id: svn://localhost@1211 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/model/dao/StudyDao.java
+++ ark-study/src/main/java/au/org/theark/study/model/dao/StudyDao.java
@@ -3,6 +3,7 @@ package au.org.theark.study.model.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import au.org.theark.core.dao.HibernateSessionDao;
 import au.org.theark.study.model.entity.Study;
+import au.org.theark.study.model.entity.StudyStatus;
 import au.org.theark.study.web.Constants;
 
 @Repository(Constants.STUDY_DAO)
@@ -18,39 +20,52 @@ public class StudyDao extends HibernateSessionDao implements IStudyDao {
 	public List<Study> getStudy(Study study)
 	{
 		
-		Criteria studyCritera =  getSession().createCriteria(Study.class);
+		Criteria studyCriteria =  getSession().createCriteria(Study.class);
 		
 		if(study.getStudyKey() != null){
-			studyCritera.add(Restrictions.eq("studyKey",study.getStudyKey()));	
+			studyCriteria.add(Restrictions.eq("studyKey",study.getStudyKey()));	
 		}
 		
 		if(study.getName() != null){
-			studyCritera.add(Restrictions.ilike("name", study.getName(), MatchMode.ANYWHERE));	
+			studyCriteria.add(Restrictions.ilike("name", study.getName(), MatchMode.ANYWHERE));	
 		}
 		
 		if(study.getDateOfApplication() != null){
-			studyCritera.add(Restrictions.eq("dateOfApplication", study.getDateOfApplication()));
+			studyCriteria.add(Restrictions.eq("dateOfApplication", study.getDateOfApplication()));
 		}
 		
 		if(study.getEstimatedYearOfCompletion() != null){
-			studyCritera.add(Restrictions.eq("estimatedYearOfCompletion", study.getEstimatedYearOfCompletion()));
+			studyCriteria.add(Restrictions.eq("estimatedYearOfCompletion", study.getEstimatedYearOfCompletion()));
 		}
 
 		if(study.getChiefInvestigator() != null){
-			studyCritera.add(Restrictions.ilike("chiefInvestigator", study.getChiefInvestigator(),MatchMode.ANYWHERE));
+			studyCriteria.add(Restrictions.ilike("chiefInvestigator", study.getChiefInvestigator(),MatchMode.ANYWHERE));
 		}
 
 		if(study.getContactPerson() != null){
-			studyCritera.add(Restrictions.ilike("contactPerson", study.getContactPerson(), MatchMode.ANYWHERE));
+			studyCriteria.add(Restrictions.ilike("contactPerson", study.getContactPerson(), MatchMode.ANYWHERE));
+		}
+		
+		if(study.getStudyStatus() != null){
+			studyCriteria.add(Restrictions.eq("studyStatus", study.getStudyStatus()));
 		}
 
-		studyCritera.addOrder(Order.asc("name"));
-		List<Study> studyList  = studyCritera.list();
+		studyCriteria.addOrder(Order.asc("name"));
+		List<Study> studyList  = studyCriteria.list();
 		return studyList;
 	}
 
 	public void create(Study study) {
 		getSession().save(study);
 	}
+
+	public List<StudyStatus> getListOfStudyStatus() {
+
+		Example studyStatus = Example.create(new StudyStatus());
+		Criteria studyStatusCriteria = getSession().createCriteria(StudyStatus.class).add(studyStatus);
+		return   studyStatusCriteria.list();
+		
+	}
+	
 	
 }
