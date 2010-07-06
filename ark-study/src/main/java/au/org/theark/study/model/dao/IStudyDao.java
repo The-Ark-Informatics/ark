9287900f1f0991eb3d0bbb1737720b6df21ac03c
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-06 07:58:11 +0000

    Added an interface to get a list of study status'.
    git-svn-id: svn://localhost@1210 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/model/dao/IStudyDao.java
+++ ark-study/src/main/java/au/org/theark/study/model/dao/IStudyDao.java
@@ -3,10 +3,19 @@ package au.org.theark.study.model.dao;
 import java.util.List;
 
 import au.org.theark.study.model.entity.Study;
+import au.org.theark.study.model.entity.StudyStatus;
 
 public interface IStudyDao {
 
 	public void create(Study study);
 	
 	public List<Study> getStudy(Study study);
+	
+	/**
+	 * Interface to get a list of Study Status reference data from the backend.
+	 * These study status' are no associated with a study as such but can be used for
+	 * displaying a list of options for a particular study.
+	 * @return
+	 */
+	public List<StudyStatus> getListOfStudyStatus();
 }
