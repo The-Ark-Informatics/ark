684ea8f576fa4c865a1c274604b692f1c0f1177b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-08 09:19:29 +0000

    renamed textField component fields.
    git-svn-id: svn://localhost@1236 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
@@ -1,5 +1,6 @@
 package au.org.theark.study.web.form;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -27,70 +29,31 @@ public class StudyForm extends Form<Study>{
 	@SpringBean( name = Constants.STUDY_SERVICE)
 	private IStudyService studyService;
 	
-	TextField<String> studyIdTxtField =new TextField<String>(Constants.STUDY_KEY);
-	TextField<String> studyNameTxtField = new TextField<String>(Constants.STUDY_NAME);
-	TextArea<String> studyDescriptionTxtField = new TextArea<String>(Constants.STUDY_DESCRIPTION);
-	TextField<String> yearOfCompletionTxtField = new TextField<String>(Constants.STUDY_ESTIMATED_YEAR_OF_COMPLETION);
+	TextField<String> studyIdTxtFld =new TextField<String>(Constants.STUDY_KEY);
+	TextField<String> studyNameTxtFld = new TextField<String>(Constants.STUDY_NAME);
+	TextArea<String> studyDescriptionTxtArea = new TextArea<String>(Constants.STUDY_DESCRIPTION);
+	TextField<String> estYearOfCompletionTxtFld = new TextField<String>(Constants.STUDY_ESTIMATED_YEAR_OF_COMPLETION);
 	DropDownChoice<StudyStatus> studyStatusDpChoices;
 	DatePicker<Date> dateOfApplicationDp = new DatePicker<Date>(Constants.STUDY_DATE_OF_APPLICATION);
-	TextField<String> principalContactTxtField = new TextField<String>(Constants.STUDY_CONTACT);
-	TextField<String> principalContactPhone = new TextField<String>(Constants.STUDY_CONTACT_PHONE);
-	TextField<String> chiefInvestigator = new TextField<String>(Constants.STUDY_CHIEF_INVESTIGATOR);
+	TextField<String> principalContactTxtFld = new TextField<String>(Constants.STUDY_CONTACT);
+	TextField<String> principalContactPhoneTxtFld = new TextField<String>(Constants.STUDY_CONTACT_PHONE);
+	TextField<String> chiefInvestigatorTxtFld = new TextField<String>(Constants.STUDY_CHIEF_INVESTIGATOR);
+	TextField<String> coInvestigatorTxtFld = new TextField<String>(Constants.STUDY_CO_INVESTIGATOR);
+	RadioChoice<String> autoGenSubIdRdChoice;
+	TextField<String> subjectIdPrefixTxtFld = new TextField<String>(Constants.SUBJECT_ID_PREFIX);
 	Button saveButton;
 	Button cancelButton;
 	Button deleteButton;
+
+	public TextField<String> getStudyIdTxtFld() {
+		return studyIdTxtFld;
+	}
 	
 	protected  void onSave(Study study){}
 	protected  void onCancel(){}
 	protected void  onDelete(Study study){}
 	
-	private void initFormFields(){
-		
-	}
-	
-	private void decorateComponents(){
-		ThemeUiHelper.componentRounded(studyIdTxtField);
-		ThemeUiHelper.componentRounded(studyNameTxtField);
-		ThemeUiHelper.componentRounded(studyDescriptionTxtField);
-		ThemeUiHelper.componentRounded(yearOfCompletionTxtField);
-		//ThemeUiHelper.componentRounded(studyStatusDpChoices);
-		ThemeUiHelper.componentRounded(dateOfApplicationDp);
-		ThemeUiHelper.componentRounded(principalContactPhone);
-		ThemeUiHelper.componentRounded(principalContactTxtField);
-		ThemeUiHelper.componentRounded(chiefInvestigator);
-		ThemeUiHelper.buttonRounded(saveButton);
-		ThemeUiHelper.buttonRounded(cancelButton);
-		ThemeUiHelper.buttonRounded(deleteButton);
-	}
-	
-	private void addComponents(){
-		add(studyIdTxtField);
-		add(studyNameTxtField);
-		add(studyDescriptionTxtField);
-		add(yearOfCompletionTxtField);
-		//add(studyStatusDpChoices);
-		add(dateOfApplicationDp);
-		//add(principalContactPhone);
-		//add(principalContactTxtField);
-		add(chiefInvestigator);
-		add(saveButton);
-		add(cancelButton);
-		add(deleteButton);
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void initStudyStatusDropDown(Study study){
-		List<StudyStatus>  studyStatusList = studyService.getListOfStudyStatus();
-		ChoiceRenderer defaultChoiceRenderer = new ChoiceRenderer("name", "studyStatusKey");
-		PropertyModel propertyModel = new PropertyModel(study, "studyStatus");
-		studyStatusDpChoices = new DropDownChoice("studyChoice",propertyModel,studyStatusList,defaultChoiceRenderer);
-	}
-	
-	public StudyForm(String id, Study study) {
-		
-		super(id, new CompoundPropertyModel<Study>(study));
-		
-		initFormFields();
+	private void initFormFields(Study study){
 		
 		saveButton = new Button(Constants.SAVE, new StringResourceModel("saveKey", this, null))
 		{
@@ -121,6 +84,66 @@ public class StudyForm extends Form<Study>{
 		};
 		
 		initStudyStatusDropDown(study);
+		initRadioButtonChoice(study);
+		
+	}
+	
+	private void decorateComponents(){
+		ThemeUiHelper.componentRounded(studyIdTxtFld);
+		ThemeUiHelper.componentRounded(studyNameTxtFld);
+		ThemeUiHelper.componentRounded(studyDescriptionTxtArea);
+		ThemeUiHelper.componentRounded(estYearOfCompletionTxtFld);
+		ThemeUiHelper.componentRounded(studyStatusDpChoices);
+		ThemeUiHelper.componentRounded(dateOfApplicationDp);
+		ThemeUiHelper.componentRounded(principalContactPhoneTxtFld);
+		ThemeUiHelper.componentRounded(principalContactTxtFld);
+		ThemeUiHelper.componentRounded(chiefInvestigatorTxtFld);
+		ThemeUiHelper.componentRounded(coInvestigatorTxtFld);
+		ThemeUiHelper.componentRounded(subjectIdPrefixTxtFld);
+		ThemeUiHelper.buttonRounded(saveButton);
+		ThemeUiHelper.buttonRounded(cancelButton);
+		ThemeUiHelper.buttonRounded(deleteButton);
+	}
+	
+	private void addComponents(){
+		add(studyIdTxtFld);
+		add(studyNameTxtFld);
+		add(studyDescriptionTxtArea);
+		add(estYearOfCompletionTxtFld);
+		add(studyStatusDpChoices);
+		add(dateOfApplicationDp);
+		add(principalContactPhoneTxtFld);
+		add(principalContactTxtFld);
+		add(chiefInvestigatorTxtFld);
+		add(coInvestigatorTxtFld);
+		add(autoGenSubIdRdChoice);
+		add(subjectIdPrefixTxtFld);
+		add(saveButton);
+		add(cancelButton.setDefaultFormProcessing(false));
+		add(deleteButton);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void initStudyStatusDropDown(Study study){
+		List<StudyStatus>  studyStatusList = studyService.getListOfStudyStatus();
+		ChoiceRenderer defaultChoiceRenderer = new ChoiceRenderer("name", "studyStatusKey");
+		PropertyModel propertyModel = new PropertyModel(study, "studyStatus");
+		studyStatusDpChoices = new DropDownChoice("studyChoice",propertyModel,studyStatusList,defaultChoiceRenderer);
+	}
+	
+	private void initRadioButtonChoice(Study study){
+		List<String> list = new ArrayList<String>();
+		list.add("Yes");
+		list.add("No");
+		PropertyModel propertyModel = new PropertyModel(study,"autoGenerateSubjectKey");
+		autoGenSubIdRdChoice = new RadioChoice<String>("autoGenSubId",propertyModel, list);
+	}
+	
+	public StudyForm(String id, Study study) {
+		
+		super(id, new CompoundPropertyModel<Study>(study));
+		
+		initFormFields(study);
 		
 		decorateComponents();
 		
