c50193f40b7aa8a42d5ea41e531a377ac7cee6f8
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-12 09:20:47 +0000

    Added  implementation for IChoiceRenderer for RadioChoice control to render and set the  Yes/No values using a Boolean Type from backend.
    git-svn-id: svn://localhost@1241 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
@@ -8,6 +8,7 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
@@ -33,14 +34,20 @@ public class StudyForm extends Form<Study>{
 	TextField<String> studyNameTxtFld = new TextField<String>(Constants.STUDY_NAME);
 	TextArea<String> studyDescriptionTxtArea = new TextArea<String>(Constants.STUDY_DESCRIPTION);
 	TextField<String> estYearOfCompletionTxtFld = new TextField<String>(Constants.STUDY_ESTIMATED_YEAR_OF_COMPLETION);
-	DropDownChoice<StudyStatus> studyStatusDpChoices;
-	DatePicker<Date> dateOfApplicationDp = new DatePicker<Date>(Constants.STUDY_DATE_OF_APPLICATION);
 	TextField<String> principalContactTxtFld = new TextField<String>(Constants.STUDY_CONTACT);
 	TextField<String> principalContactPhoneTxtFld = new TextField<String>(Constants.STUDY_CONTACT_PHONE);
 	TextField<String> chiefInvestigatorTxtFld = new TextField<String>(Constants.STUDY_CHIEF_INVESTIGATOR);
 	TextField<String> coInvestigatorTxtFld = new TextField<String>(Constants.STUDY_CO_INVESTIGATOR);
-	RadioChoice<String> autoGenSubIdRdChoice;
 	TextField<String> subjectIdPrefixTxtFld = new TextField<String>(Constants.SUBJECT_ID_PREFIX);
+	TextField<String> subjectIdStartAtTxtFld = new TextField<String>(Constants.SUBJECT_KEY_START);
+	TextField<String> bioSpecimenPrefixTxtFld = new TextField<String>(Constants.SUB_STUDY_BIOSPECIMENT_PREFIX);
+	DatePicker<Date> dateOfApplicationDp = new DatePicker<Date>(Constants.STUDY_DATE_OF_APPLICATION);
+	
+	DropDownChoice<StudyStatus> studyStatusDpChoices;
+	
+	RadioChoice<Boolean> autoGenSubIdRdChoice;
+	RadioChoice<Boolean> autoConsentRdChoice;
+	
 	Button saveButton;
 	Button cancelButton;
 	Button deleteButton;
@@ -84,8 +91,8 @@ public class StudyForm extends Form<Study>{
 		};
 		
 		initStudyStatusDropDown(study);
-		initRadioButtonChoice(study);
-		
+		autoGenSubIdRdChoice = initRadioButtonChoice(study,Constants.STUDY_AUTO_GENERATE_SUBJECT_KEY,"autoGenSubId");
+		autoConsentRdChoice = initRadioButtonChoice(study,Constants.STUDY_ATUO_CONSENT,"autoConsent");
 	}
 	
 	private void decorateComponents(){
@@ -100,6 +107,8 @@ public class StudyForm extends Form<Study>{
 		ThemeUiHelper.componentRounded(chiefInvestigatorTxtFld);
 		ThemeUiHelper.componentRounded(coInvestigatorTxtFld);
 		ThemeUiHelper.componentRounded(subjectIdPrefixTxtFld);
+		ThemeUiHelper.componentRounded(subjectIdStartAtTxtFld);
+		ThemeUiHelper.componentRounded(bioSpecimenPrefixTxtFld);
 		ThemeUiHelper.buttonRounded(saveButton);
 		ThemeUiHelper.buttonRounded(cancelButton);
 		ThemeUiHelper.buttonRounded(deleteButton);
@@ -116,8 +125,11 @@ public class StudyForm extends Form<Study>{
 		add(principalContactTxtFld);
 		add(chiefInvestigatorTxtFld);
 		add(coInvestigatorTxtFld);
-		add(autoGenSubIdRdChoice);
 		add(subjectIdPrefixTxtFld);
+		add(subjectIdStartAtTxtFld);
+		add(bioSpecimenPrefixTxtFld);
+		add(autoGenSubIdRdChoice);
+		add(autoConsentRdChoice);
 		add(saveButton);
 		add(cancelButton.setDefaultFormProcessing(false));
 		add(deleteButton);
@@ -126,18 +138,43 @@ public class StudyForm extends Form<Study>{
 	@SuppressWarnings("unchecked")
 	private void initStudyStatusDropDown(Study study){
 		List<StudyStatus>  studyStatusList = studyService.getListOfStudyStatus();
-		ChoiceRenderer defaultChoiceRenderer = new ChoiceRenderer("name", "studyStatusKey");
-		PropertyModel propertyModel = new PropertyModel(study, "studyStatus");
-		studyStatusDpChoices = new DropDownChoice("studyChoice",propertyModel,studyStatusList,defaultChoiceRenderer);
+		ChoiceRenderer defaultChoiceRenderer = new ChoiceRenderer(Constants.NAME, Constants.STUDY_STATUS_KEY);
+		PropertyModel propertyModel = new PropertyModel(study,Constants.STUDY_STATUS);
+		studyStatusDpChoices = new DropDownChoice(Constants.STUDY_DROP_DOWN_CHOICE,propertyModel,studyStatusList,defaultChoiceRenderer);
+	}
+	/**
+	 * A common method that can be used to render Yes/No using RadioChoice controls
+	 * @param study
+	 * @param propertyModelExpr
+	 * @param radioChoiceId
+	 * @return
+	 */
+	private RadioChoice<Boolean> initRadioButtonChoice(Study study, String propertyModelExpr,String radioChoiceId){
+	
+		List<Boolean> list = new ArrayList<Boolean>();
+		list.add(Boolean.TRUE);
+		list.add(Boolean.FALSE);
+		/* Implement the IChoiceRenderer*/
+		
+		IChoiceRenderer<Boolean> radioChoiceRender = new IChoiceRenderer<Boolean>() {
+			public Object getDisplayValue(final Boolean choice){
+				
+				String displayValue=Constants.NO;
+				if(choice !=null && choice.booleanValue()){
+					displayValue = Constants.NO;
+				}
+				return displayValue;
+			}
+			
+			public String getIdValue(final Boolean object,final int index){
+				return object.toString();
+			}
+		};
+		
+		PropertyModel<Boolean> propertyModel = new PropertyModel<Boolean>(study,propertyModelExpr);
+		return new RadioChoice<Boolean>(radioChoiceId,propertyModel,list,radioChoiceRender);
 	}
 	
-	private void initRadioButtonChoice(Study study){
-		List<String> list = new ArrayList<String>();
-		list.add("Yes");
-		list.add("No");
-		PropertyModel propertyModel = new PropertyModel(study,"autoGenerateSubjectKey");
-		autoGenSubIdRdChoice = new RadioChoice<String>("autoGenSubId",propertyModel, list);
-	}
 	
 	public StudyForm(String id, Study study) {
 		
