2d5b25ee731c26f1345743771f0a59d82a76fa4e
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-21 07:42:19 +0000

    Rearranged members.
    git-svn-id: svn://localhost@1271 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/vo/StudyVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/StudyVO.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 public class StudyVO extends BaseVO{
 
-	private static final long serialVersionUID = 1L;
-	
 	private String studyName;
+	private List<ModuleVO> modules;
 	private List<RoleVO> roles;//From a user' perspective the list of roles for a study in context. E.g. study_admin or ordinary_user
+
+	/**
+	 * Constructor
+	 */
+	public StudyVO(){
+		super();
+		this.roles = new ArrayList<RoleVO>();
+	}
 	
 	public List<RoleVO> getRoles() {
 		return roles;
@@ -17,8 +24,6 @@ public class StudyVO extends BaseVO{
 	public void setRoles(List<RoleVO> roles) {
 		this.roles = roles;
 	}
-
-	private List<ModuleVO> modules;
 	
 	public List<ModuleVO> getModules() {
 		return modules;
@@ -26,11 +31,6 @@ public class StudyVO extends BaseVO{
 
 	public void setModules(List<ModuleVO> modules) {
 		this.modules = modules;
-	}
-
-	public StudyVO(){
-		super();
-		this.roles = new ArrayList<RoleVO>();
 	}
 
 	public String getStudyName() {
