e1109b1b5b424dabb7d6040922a1416e538d837b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-22 10:13:44 +0000

    modified createStudy() to take in the selected applications into which the new study needs to be added to ldap.
    git-svn-id: svn://localhost@1289 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/service/StudyServiceImpl.java
+++ ark-study/src/main/java/au/org/theark/study/service/StudyServiceImpl.java
@@ -2,10 +2,19 @@ package au.org.theark.study.service;
 
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.exception.EntityExistsException;
+import au.org.theark.core.exception.UnAuthorizedOperation;
+import au.org.theark.core.security.RoleConstants;
+import au.org.theark.study.model.dao.ILdapUserDao;
 import au.org.theark.study.model.dao.IStudyDao;
 import au.org.theark.study.model.entity.Study;
 import au.org.theark.study.model.entity.StudyStatus;
@@ -16,7 +25,18 @@ import au.org.theark.study.web.Constants;
 public class StudyServiceImpl implements IStudyService{
 	
 	private IStudyDao studyDao;
+	private ILdapUserDao iLdapUserDao;
 
+	public ILdapUserDao getiLdapUserDao() {
+		return iLdapUserDao;
+	}
+	
+	@Autowired
+	public void setiLdapUserDao(ILdapUserDao iLdapUserDao) {
+		this.iLdapUserDao = iLdapUserDao;
+	}
+
+	/*To access Hibernate Study Dao */
 	@Autowired
 	public void setStudyDao(IStudyDao studyDao) {
 		this.studyDao = studyDao;
@@ -26,9 +46,6 @@ public class StudyServiceImpl implements IStudyService{
 		return studyDao;
 	}
 
-	public void createStudy(Study studyEntity) {
-		studyDao.create(studyEntity);
-	}
 	
 	public List<Study> getStudy(Study study){
 		return studyDao.getStudy(study);
@@ -36,5 +53,24 @@ public class StudyServiceImpl implements IStudyService{
 	
 	public List<StudyStatus> getListOfStudyStatus(){
 		return studyDao.getListOfStudyStatus();
+	}
+
+	public void createStudy(Study studyEntity, List<String> selectedApplications) throws ArkSystemException, EntityExistsException, UnAuthorizedOperation{
+		try{
+			//Create the study in the back-end database
+			//studyDao.create(studyEntity);
+			//Create the study group in the LDAP for the selected applications and also add the roles to each of the application.
+			SecurityManager securityManager =  ThreadContext.getSecurityManager();
+			Subject currentUser = SecurityUtils.getSubject();
+			
+			if(securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN)){
+				iLdapUserDao.createStudy(studyEntity.getName(), selectedApplications, au.org.theark.study.service.Constants.ARK_SYSTEM_USER);	
+			}else{
+			 throw new UnAuthorizedOperation("The logged in user does not have the permission to create a study.");//Throw an exception
+			}
+		}catch(ArkSystemException arkSystemException){
+			throw arkSystemException;
+		}
+		
 	}
 }
