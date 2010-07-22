ca73f3ecf127c19562bb127638d6c0abc50424de
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-07-22 10:12:45 +0000

    added createStudy to create a study  in backend and ldap
    git-svn-id: svn://localhost@1288 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/service/IStudyService.java
+++ ark-study/src/main/java/au/org/theark/study/service/IStudyService.java
@@ -2,12 +2,20 @@ package au.org.theark.study.service;
 
 import java.util.List;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.exception.EntityExistsException;
+import au.org.theark.core.exception.UnAuthorizedOperation;
 import au.org.theark.study.model.entity.Study;
 import au.org.theark.study.model.entity.StudyStatus;
 
 public interface IStudyService {
-	
-	public void createStudy(Study studyEntity);
+	/**
+	 * This interface must be accessible to only super administator role
+	 * @param studyEntity
+	 * @param selectedApplications
+	 * @throws ArkSystemException
+	 */
+	public void createStudy(Study studyEntity, List<String> selectedApplications) throws EntityExistsException,UnAuthorizedOperation, ArkSystemException;
 	
 	public List<Study> getStudy(Study study);
 	
