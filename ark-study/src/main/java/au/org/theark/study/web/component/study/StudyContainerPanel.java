708fc1e43661cffc1381b79b0da24fc85d79c7d7
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-02 08:25:36 +0000

    Removed unused form and construtors
    git-svn-id: svn://localhost@1322 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/StudyContainerPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/StudyContainerPanel.java
@@ -1,12 +1,8 @@
 package au.org.theark.study.web.component.study;
 
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import au.org.theark.study.model.entity.Study;
 
 public class StudyContainerPanel extends Panel{
 
@@ -14,6 +10,8 @@ public class StudyContainerPanel extends Panel{
 	private static final long serialVersionUID = 1L;
 	private transient Logger log = LoggerFactory.getLogger(StudyContainerPanel.class);
 	
+	
+	//Child Components
 	private Search searchStudyPanel;
 	
 	public StudyContainerPanel(String id) {
@@ -21,20 +19,6 @@ public class StudyContainerPanel extends Panel{
 		log.info("StudyContainerPanel Constructor invoked.");
 		searchStudyPanel = new Search("searchStudyPanel");
 		add(searchStudyPanel);
-	}
-	
-	public class SearchStudyForm extends Form<Study>{
-
-		private static final long serialVersionUID = 1L;
-
-		protected void onSave(Study study){}
-		
-		public SearchStudyForm(String id, Study study) {
-			super(id, new CompoundPropertyModel<Study>(study));
-			searchStudyPanel = new Search("searchStudyPanel");
-			add(searchStudyPanel);
-		}
-		
 	}
 
 }
