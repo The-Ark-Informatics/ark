cb20aaa52ead8c6363c8cce3f877a7e8e7895131
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-02 08:26:46 +0000

    Removed the inner class for form.
    git-svn-id: svn://localhost@1323 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/user/Search.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/user/Search.java
@@ -3,26 +3,15 @@ package au.org.theark.study.web.component.user;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.ThreadContext;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.validation.validator.EmailAddressValidator;
-import org.apache.wicket.validation.validator.StringValidator;
 
 import au.org.theark.core.exception.ArkSystemException;
-import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.vo.ArkUserVO;
 import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.form.SearchUserForm;
 /**
  * A custom domain(eta) component for searching for users in the system. The results of the search can 
  * be fed into another custom component that accepts a List<?> (need to define what a Custom DataView component will receive)
@@ -67,8 +56,8 @@ public class Search extends Panel{
 	 * @param id
 	 */
 	public Search(String id) {
+
 		super(id);
-		
 		//Create a new instance of the details panel with empty user object
 		detailsPanel = new Details("userDetailsPanel", new ArkUserVO(), this);
 		//Hide it since we have not looked up a user as yet
@@ -98,6 +87,17 @@ public class Search extends Panel{
 					//log.error("Exception occured when looking up a user in LDAP" + e.getMessage());
 				}
 			}
+			
+			protected void onNew(ArkUserVO etaUserVO){
+				//Set the Model Object for the existing Form object in DetailsPanel
+				detailsPanel.getUserForm().getGroupPasswordContainer().setVisible(true);
+				detailsPanel.getUserForm().setModelObject(etaUserVO);
+				detailsPanel.setVisible(true);
+				detailsPanel.getUserForm().getUserNameTxtField().setEnabled(true);
+				//Hide the Search and Search Results panels.
+				displayUserListPanel.setVisible(false);
+			}
+			
 		};
 
 		//Add the Form to the Panel. The Form object that will contain the child or UI components that will be part of the search or be affected by the search.
@@ -110,89 +110,5 @@ public class Search extends Panel{
 		userList.add(user);
 		displayUserListPanel = new SearchResultList("displayUserListPanel", userList,detailsPanel);
 		searchUserForm.add(displayUserListPanel);
-	}
-
-	/**
-	 * The Form object for the Search.
-	 */
-	public class SearchUserForm extends Form<ArkUserVO>{
-
-		TextField<String> userNameTxtField =new TextField<String>(Constants.USER_NAME);
-		TextField<String> firstNameTxtField = new TextField<String>(Constants.FIRST_NAME);
-		TextField<String> lastNameTxtField = new TextField<String>(Constants.LAST_NAME);
-		TextField<String> emailTxtField = new TextField<String>(Constants.EMAIL);
-		
-		private void initFormFields(){
-			emailTxtField.add(EmailAddressValidator.getInstance());
-			firstNameTxtField.add(StringValidator.lengthBetween(3, 50));
-			lastNameTxtField.add(StringValidator.lengthBetween(3, 50));
-			userNameTxtField.add(StringValidator.lengthBetween(3, 50));
-		}
-		private static final long serialVersionUID = 1L;
-
-		/* Form Constructor */
-		public SearchUserForm(String id, ArkUserVO userVO, String panelId) {
-		
-			super(id, new CompoundPropertyModel<ArkUserVO>(userVO));
-			initFormFields();
-			/* Add the look up fields */
-			add(userNameTxtField);
-			add(firstNameTxtField);
-			add(lastNameTxtField);
-			add(emailTxtField);
-			
-			add(new Button(Constants.SEARCH, new StringResourceModel("page.search", this, null))
-			{
-				public void onSubmit()
-				{
-					
-					onSearch((ArkUserVO) getForm().getModelObject());
-				}
-			});
-			
-			/**
-			 * Allow to create a New User
-			 */
-			add(new Button(Constants.NEW, new StringResourceModel("page.new", this, null))
-			{
-				public void onSubmit()
-				{
-					//Go to Search users page
-					//The mode will be new here
-					ArkUserVO etaUserVO = new ArkUserVO();
-					etaUserVO.setMode(Constants.MODE_NEW);
-					onNew(etaUserVO);
-				}
-				@Override
-				public boolean isVisible(){
-					
-					SecurityManager securityManager =  ThreadContext.getSecurityManager();
-					Subject currentUser = SecurityUtils.getSubject();		
-					boolean flag = false;
-					if(		securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN) ||
-							securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.STUDY_ADMIN)){
-						flag = true;
-					}
-					//if it is a Super or Study admin then make the new available
-					return flag;
-				}
-			
-			});
-		}
-
-		/* Processing logic for search */
-		protected  void onSearch(ArkUserVO userVO){}
-		
-		/* Processing logic for New User */
-		protected void onNew(ArkUserVO etaUserVO){
-			//Set the Model Object for the existing Form object in DetailsPanel
-			detailsPanel.getUserForm().getGroupPasswordContainer().setVisible(true);
-			detailsPanel.getUserForm().setModelObject(etaUserVO);
-			detailsPanel.setVisible(true);
-			detailsPanel.getUserForm().getUserNameTxtField().setEnabled(true);
-			//Hide the Search and Search Results panels.
-			displayUserListPanel.setVisible(false);
-		}
-		
 	}
 }
