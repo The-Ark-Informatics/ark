721f28ba1e8cf3a8d3acd74a29b9306a9719da07
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-04 03:44:47 +0000

    added implementation to onClick to load the selected item into the details panel.
    git-svn-id: svn://localhost@1341 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
@@ -1,28 +1,44 @@
 package au.org.theark.study.web.component.study;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.ListMultipleChoice;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.odlabs.wiquery.ui.themes.ThemeUiHelper;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.vo.ModuleVO;
 import au.org.theark.study.model.entity.Study;
+import au.org.theark.study.service.IStudyService;
+import au.org.theark.study.service.IUserService;
+import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.form.ListMultipleChoiceForm;
+import au.org.theark.study.web.form.StudyForm;
 
 
+@SuppressWarnings("serial")
 public class SearchResultList extends Panel{
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	@SpringBean( name = Constants.STUDY_SERVICE)
+	private IStudyService studyService;
+	
+	@SpringBean( name = "userService")
+	private IUserService userService;
+	
 	private List<Study> studyList;
 	private Details detailsPanel;
 	
@@ -31,7 +47,6 @@ public class SearchResultList extends Panel{
 		super(id);
 		this.studyList = studyList;
 		this.detailsPanel = (Details)component;
-		
 		PageableListView pageableListView = buildPageableListView(studyList, 10);//Rows per page to be in properties file
 		PagingNavigator pageNavigator = new PagingNavigator("navigator", pageableListView);
 		ThemeUiHelper.componentRounded(pageableListView);
@@ -41,7 +56,7 @@ public class SearchResultList extends Panel{
 	}
 	
 	@SuppressWarnings({ "unchecked", "serial" })
-	public PageableListView buildPageableListView(List<Study> list, int rowsPerPage){
+	private PageableListView buildPageableListView(List<Study> list, int rowsPerPage){
 		
 		PageableListView  pageableListView = new PageableListView("studyList", studyList, rowsPerPage){
 
@@ -56,7 +71,7 @@ public class SearchResultList extends Panel{
 					item.add(new Label("studyKey",""));
 				}
 				
-				Link studyNameLink = buildLink(item);
+				Link studyNameLink = buildLink(item, detailsPanel, study);
 				/* Build the caption for the Link*/
 				Label studyNameLinkLabel = new Label("studyNameLink", study.getName());
 				studyNameLink.add(studyNameLinkLabel);
@@ -78,10 +93,6 @@ public class SearchResultList extends Panel{
 					item.add(new Label("dateOfApplication",dateOfApplication));
 				}
 				
-				//item.add(new Label("vitalStatus", person.getVitalStatus()));
-				
-				//If we used DataView then can override newItem and return a Wicket Extension module back. EvenorOdd
-				//For a PageableListView we will implement the even odd logic
 				item.add(new AttributeModifier("class", true, new AbstractReadOnlyModel() {
 					@Override
 					public String getObject() {
@@ -96,15 +107,42 @@ public class SearchResultList extends Panel{
 	}
 	
 	@SuppressWarnings({ "unchecked", "serial" })
-	private Link buildLink(final ListItem item) {
+	private Link buildLink(final ListItem item, final Component detailsPanel, final Study study) {
 		
-		final Study study= (Study) item.getModelObject();
 
 		return new Link("studyName", item.getModel()) {
 			@Override
 			public void onClick() {
-				//TODO
 				
+				Details studyDetails = (Details)detailsPanel;
+				
+				StudyForm studyForm = studyDetails.getStudyForm();
+				studyForm.getStudyIdTxtFld().setEnabled(false);
+				studyForm.getStudyNameTxtFld().setEnabled(false);
+				
+				StudyModel studyModel = (StudyModel) studyForm.getModelObject();
+				Set<String> selectedApps = new HashSet<String>();
+				try {
+					
+					selectedApps = studyService.getModulesLinkedToStudy(study.getName(),true);
+					studyModel.setStudy(study);
+					studyModel.setLmcSelectedApps(selectedApps);
+					studyForm.setModelObject(studyModel);
+					
+					studyForm.remove("applicationSelector");
+					List<ModuleVO> modules = userService.getModules(true);
+					ApplicationSelector applicationSelector = new ApplicationSelector("applicationSelector", studyForm.getModelObject(), modules);
+					applicationSelector.setupSelector();
+					studyForm.add(applicationSelector);
+					studyDetails.setVisible(true);
+					
+				} catch (ArkSystemException e) {
+					System.out.println("An Error occured when loading the details panel from the onClick event of the list");
+					error(e.getMessage());
+				}
+				
+				//Place the selected study in session context for the user
+				SecurityUtils.getSubject().getSession().setAttribute("studyId", study.getStudyKey());
 			}
 		};
 		
