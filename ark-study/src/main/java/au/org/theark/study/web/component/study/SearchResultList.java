745fee65281b0d7b45ae91b102747e6190d56072
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-12 09:03:54 +0000

    Added local reference to the CPM Removed SpringBean references since the list is always sent in so we don't need to hold any bean references Uses IModel parameter to build the list added generic types to the list view.
    git-svn-id: svn://localhost@1367 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
@@ -1,88 +1,65 @@
 package au.org.theark.study.web.component.study;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.ListMultipleChoice;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PageableListView;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.odlabs.wiquery.ui.themes.ThemeUiHelper;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 
-import au.org.theark.core.exception.ArkSystemException;
-import au.org.theark.core.vo.ModuleVO;
 import au.org.theark.study.model.entity.Study;
-import au.org.theark.study.service.IStudyService;
-import au.org.theark.study.service.IUserService;
-import au.org.theark.study.web.Constants;
-import au.org.theark.study.web.form.ListMultipleChoiceForm;
-import au.org.theark.study.web.form.StudyForm;
 
 
 @SuppressWarnings("serial")
 public class SearchResultList extends Panel{
 	
-	@SpringBean( name = Constants.STUDY_SERVICE)
-	private IStudyService studyService;
-	
-	@SpringBean( name = "userService")
-	private IUserService userService;
-	
-	private List<Study> studyList;
 	private Details detailsPanel;
 	
-	public SearchResultList(String id, List<Study> studyList, Component component) {
-		
+	/* A reference of the Model from the Container in this case Search Panel */
+	private CompoundPropertyModel<StudyModel> cpm;
+	
+	public SearchResultList(String id, Details details){
 		super(id);
-		this.studyList = studyList;
-		this.detailsPanel = (Details)component;
-		PageableListView pageableListView = buildPageableListView(studyList, 10);//Rows per page to be in properties file
-		PagingNavigator pageNavigator = new PagingNavigator("navigator", pageableListView);
-		ThemeUiHelper.componentRounded(pageableListView);
-		ThemeUiHelper.componentRounded(pageNavigator);
-		add(pageNavigator);
-		add(pageableListView);
+		this.detailsPanel = details;
+	}
+	public CompoundPropertyModel<StudyModel> getCpm() {
+		return cpm;
+	}
+
+	public void setCpm(CompoundPropertyModel<StudyModel> cpm) {
+		this.cpm = cpm;
 	}
 	
-	@SuppressWarnings({ "unchecked", "serial" })
-	private PageableListView buildPageableListView(List<Study> list, int rowsPerPage){
+	
+	public PageableListView<Study> buildPageableListView(IModel iModel, final WebMarkupContainer searchContainer){
 		
-		PageableListView  pageableListView = new PageableListView("studyList", studyList, rowsPerPage){
-
+		PageableListView<Study> studyPageableListView = new PageableListView<Study>("studyList", iModel, 10) {
 			@Override
-			protected void populateItem(final ListItem item) {
+			protected void populateItem(final ListItem<Study> item) {
 				
-				Study study = (Study) item.getModelObject();
-
+				Study study = item.getModelObject();
+				
 				if(study.getStudyKey() != null){
 					item.add(new Label("studyKey", study.getStudyKey().toString()));	
 				}else{
 					item.add(new Label("studyKey",""));
 				}
 				
-				Link studyNameLink = buildLink(item, detailsPanel, study);
-				/* Build the caption for the Link*/
-				Label studyNameLinkLabel = new Label("studyNameLink", study.getName());
-				studyNameLink.add(studyNameLinkLabel);
-				item.add(studyNameLink);
+				item.add(buildLink(study,searchContainer));
 				
 				if(study.getContactPerson() != null){
 					item.add(new Label("contact", study.getContactPerson()));//the ID here must match the ones in mark-up	
 				}else{
 					item.add(new Label("contact", ""));//the ID here must match the ones in mark-up
 				}
-				
 				
 				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 				String dateOfApplication ="";
@@ -92,7 +69,7 @@ public class SearchResultList extends Panel{
 				}else{
 					item.add(new Label("dateOfApplication",dateOfApplication));
 				}
-				
+
 				item.add(new AttributeModifier("class", true, new AbstractReadOnlyModel() {
 					@Override
 					public String getObject() {
@@ -101,53 +78,38 @@ public class SearchResultList extends Panel{
 				}));
 				
 			}
-		
 		};
-		return pageableListView;
+		return studyPageableListView;
 	}
 	
+	
 	@SuppressWarnings({ "unchecked", "serial" })
-	private Link buildLink(final ListItem item, final Component detailsPanel, final Study study) {
+	private AjaxLink buildLink(final Study study, final WebMarkupContainer searchContainer) {
 		
+		AjaxLink link = new AjaxLink("studyName") {
 
-		return new Link("studyName", item.getModel()) {
 			@Override
-			public void onClick() {
-				
-				Details studyDetails = (Details)detailsPanel;
-				
-				StudyForm studyForm = studyDetails.getStudyForm();
-				studyForm.getStudyIdTxtFld().setEnabled(false);
-				studyForm.getStudyNameTxtFld().setEnabled(false);
-				
-				StudyModel studyModel = (StudyModel) studyForm.getModelObject();
-				Set<String> selectedApps = new HashSet<String>();
-				try {
-					
-					selectedApps = studyService.getModulesLinkedToStudy(study.getName(),true);
-					studyModel.setStudy(study);
-					studyModel.setLmcSelectedApps(selectedApps);
-					studyForm.setModelObject(studyModel);
-					
-					studyForm.remove("applicationSelector");
-					List<ModuleVO> modules = userService.getModules(true);
-					ApplicationSelector applicationSelector = new ApplicationSelector("applicationSelector", studyForm.getModelObject(), modules);
-					applicationSelector.setupSelector();
-					studyForm.add(applicationSelector);
-					studyDetails.setVisible(true);
-					
-				} catch (ArkSystemException e) {
-					System.out.println("An Error occured when loading the details panel from the onClick event of the list");
-					error(e.getMessage());
-				}
-				
+			public void onClick(AjaxRequestTarget target) {
 				//Place the selected study in session context for the user
 				SecurityUtils.getSubject().getSession().setAttribute("studyId", study.getStudyKey());
+				//Based on the user type navigate to another panel (Summary of study or the View panel)
+				
+				StudyModel studyModel  = cpm.getObject();
+				studyModel.setStudy(study);
+				detailsPanel.setCpm(cpm);
+				detailsPanel.setVisible(true);
+				//TODO make the ID and Name field disabled
+				searchContainer.setVisible(false);
+				target.addComponent(detailsPanel);
+				target.addComponent(searchContainer);
 			}
 		};
 		
+		//Add the label for the link
+		Label studyNameLinkLabel = new Label("studyNameLink", study.getName());
+		link.add(studyNameLinkLabel);
+		return link;
+
 	}
-	
-	
 
 }
