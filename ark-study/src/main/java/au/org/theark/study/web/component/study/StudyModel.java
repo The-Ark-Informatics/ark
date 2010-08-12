873ed8d704b1bdfa5afbf36c7382682d76095576
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-12 08:48:59 +0000

    The model now implements Serializable and not IModel. It is a plan java class.
    git-svn-id: svn://localhost@1362 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/StudyModel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/StudyModel.java
@@ -1,24 +1,29 @@
 package au.org.theark.study.web.component.study;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-
-import org.apache.wicket.model.IModel;
 
 import au.org.theark.study.model.entity.Study;
 
 @SuppressWarnings("serial")
-public class StudyModel implements IModel{
+public class StudyModel implements Serializable{
 
 	private Study study;
-	
-	private Set<String> lmcAvailableApps = new HashSet<String>();
-	private Set<String> lmcSelectedApps = new HashSet<String>();
+	private Set<String> lmcAvailableApps;
+	private Set<String> lmcSelectedApps;
+	private List<Study> studyList;
 	
 	
 	public StudyModel(){
 		study = new Study();
+		lmcAvailableApps = new HashSet<String>();
+		lmcSelectedApps = new HashSet<String>();
+		studyList = new ArrayList<Study>();
 	}
+	
 	public Study getStudy() {
 		return study;
 	}
@@ -39,19 +44,13 @@ public class StudyModel implements IModel{
 	public void setLmcSelectedApps(Set<String> lmcSelectedApps) {
 		this.lmcSelectedApps = lmcSelectedApps;
 	}
-	
 
-	public Object getObject() {
-		return this;
+	public List<Study> getStudyList() {
+		return studyList;
 	}
 
-	public void setObject(Object object) {
-		
+	public void setStudyList(List<Study> studyList) {
+		this.studyList = studyList;
 	}
-
-	public void detach() {
-	}
-	
-	
 	
 }
