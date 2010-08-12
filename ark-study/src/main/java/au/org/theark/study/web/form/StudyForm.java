25b662fff89394b73065fca51371302b91916846
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-12 09:08:28 +0000

    moved the code that was initialised at class level into a method added ajax buttons for save,cancel and delete appropriately initialise the model in context when cancel, save is done.
    git-svn-id: svn://localhost@1370 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/form/StudyForm.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
@@ -23,69 +25,134 @@ import org.apache.wicket.validation.validator.StringValidator;
 import org.odlabs.wiquery.ui.datepicker.DatePicker;
 import org.odlabs.wiquery.ui.themes.ThemeUiHelper;
 
-import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.vo.ModuleVO;
+import au.org.theark.study.model.entity.Study;
 import au.org.theark.study.model.entity.StudyStatus;
 import au.org.theark.study.service.IStudyService;
-import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.component.study.Details;
 import au.org.theark.study.web.component.study.StudyModel;
 
 @SuppressWarnings("serial")
 public class StudyForm extends Form<StudyModel>{
 	
+
 	@SpringBean( name = Constants.STUDY_SERVICE)
 	private IStudyService studyService;
+	
+	//Study Details Panel
+	Details details;
+	WebMarkupContainer  listContainer;
 	private int mode;
+	private TextField<String> studyIdTxtFld;
+	private TextField<String> studyNameTxtFld;
+	private TextArea<String> studyDescriptionTxtArea;
+	private TextField<String> estYearOfCompletionTxtFld;
+	private TextField<String> principalContactTxtFld;
+	private TextField<String> principalContactPhoneTxtFld;
+	private TextField<String> chiefInvestigatorTxtFld;
+	private TextField<String> coInvestigatorTxtFld;
+	private TextField<String> subjectKeyPrefixTxtFld;
+	private TextField<Integer> subjectKeyStartAtTxtFld;
+	private TextField<String> bioSpecimenPrefixTxtFld;
+	private DatePicker<Date> dateOfApplicationDp;
+	private DropDownChoice<StudyStatus> studyStatusDpChoices;
+	private RadioChoice<Boolean> autoGenSubIdRdChoice;
+	private RadioChoice<Boolean> autoConsentRdChoice;
 	
-	public int getMode() {
-		return mode;
-	}
-
-	public void setMode(int mode) {
-		this.mode = mode;
-	}
-
-	public StudyForm(String id, StudyModel studyModel) {
-		
-		super(id, new CompoundPropertyModel<StudyModel>(studyModel));
-		
-		try{
-
-			initFormFields(studyModel);
-			decorateComponents();
-			addComponents();
-		
-		}catch(ArkSystemException ase){
-			
-		}
-	}
-	
-	private StudyModel studyModel;
-	
-
-	TextField<String> studyIdTxtFld =new TextField<String>(Constants.STUDY_KEY);
-	TextField<String> studyNameTxtFld = new TextField<String>("study.name");
-	TextArea<String> studyDescriptionTxtArea = new TextArea<String>("study.description");
-	TextField<String> estYearOfCompletionTxtFld = new TextField<String>("study.estimatedYearOfCompletion");
-	TextField<String> principalContactTxtFld = new TextField<String>("study.contactPerson");
-	TextField<String> principalContactPhoneTxtFld = new TextField<String>("study.contactPersonPhone");
-	TextField<String> chiefInvestigatorTxtFld = new TextField<String>("study.chiefInvestigator");
-	TextField<String> coInvestigatorTxtFld = new TextField<String>("study.coInvestigator");
-	TextField<String> subjectKeyPrefixTxtFld = new TextField<String>("study.subjectIdPrefix");
-	TextField<Integer> subjectKeyStartAtTxtFld = new TextField<Integer>("study.subjectKeyStart", Integer.class);
-	TextField<String> bioSpecimenPrefixTxtFld = new TextField<String>("study.subStudyBiospecimenPrefix");
-	
-	DatePicker<Date> dateOfApplicationDp = new DatePicker<Date>("study.dateOfApplication");
-	DropDownChoice<StudyStatus> studyStatusDpChoices;
-	RadioChoice<Boolean> autoGenSubIdRdChoice;
-	RadioChoice<Boolean> autoConsentRdChoice;
-	
-	Button saveButton;
-	Button cancelButton;
-	Button deleteButton;
+	AjaxButton saveButton;
+	AjaxButton cancelButton;
+	AjaxButton deleteButton;
 	
 	List<ModuleVO> modules;
+	
+	protected void onSave(StudyModel studyModel, AjaxRequestTarget target){}
+	
+	protected  void onCancel(AjaxRequestTarget target){}
+	
+	protected void  onDelete(AjaxRequestTarget target){}
+	
+
+	
+	/**
+	 * Constructor: 
+	 * @param id
+	 * @param detailsPanel The panel that is linked to this Form instance
+	 * @param container The WebMarkupContainer that will wrap the SearchResults
+	 */
+	public StudyForm(String id, Details detailsPanel, WebMarkupContainer container){
+		
+		super(id);
+		
+		listContainer = container;
+		details = detailsPanel;
+		
+		cancelButton = new AjaxButton(Constants.CANCEL,  new StringResourceModel("cancelKey", this, null))
+		{
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				listContainer.setVisible(false);
+				details.setVisible(false);
+				target.addComponent(details);
+				target.addComponent(listContainer);
+				details.getCpm().getObject().setStudy(new Study());
+				onCancel(target);
+			}
+		};
+		
+		saveButton = new AjaxButton(Constants.SAVE, new StringResourceModel("saveKey", this, null))
+		{
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{	//Access the model associated using a reference to details panel
+				CompoundPropertyModel<StudyModel> detailsCpm = details.getCpm();
+				StudyModel model = detailsCpm.getObject();
+				target.addComponent(details);
+				onSave(model,target);
+			}
+		}; 
+		
+		deleteButton = new AjaxButton(Constants.DELETE, new StringResourceModel("deleteKey", this, null))
+		{
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
+				//Go to Search users page
+				target.addComponent(details);
+				onDelete(target);
+			}
+		};
+		
+		studyIdTxtFld =new TextField<String>(Constants.STUDY_KEY);
+		studyNameTxtFld = new TextField<String>("study.name");
+		studyDescriptionTxtArea = new TextArea<String>("study.description");
+		estYearOfCompletionTxtFld = new TextField<String>("study.estimatedYearOfCompletion");
+		principalContactTxtFld = new TextField<String>("study.contactPerson");
+		principalContactPhoneTxtFld = new TextField<String>("study.contactPersonPhone");
+		chiefInvestigatorTxtFld = new TextField<String>("study.chiefInvestigator");
+		coInvestigatorTxtFld = new TextField<String>("study.coInvestigator");
+		subjectKeyPrefixTxtFld = new TextField<String>("study.subjectIdPrefix");
+		subjectKeyStartAtTxtFld = new TextField<Integer>("study.subjectKeyStart", Integer.class);
+		bioSpecimenPrefixTxtFld = new TextField<String>("study.subStudyBiospecimenPrefix");
+		dateOfApplicationDp = new DatePicker<Date>("study.dateOfApplication");
+		
+		CompoundPropertyModel<StudyModel> detailsCpm = details.getCpm();
+		
+		initStudyStatusDropDown(detailsCpm.getObject());
+		autoGenSubIdRdChoice = initRadioButtonChoice(detailsCpm.getObject(),"study.autoGenerateSubjectKey","autoGenSubId");
+		autoConsentRdChoice = initRadioButtonChoice(detailsCpm.getObject(),"study.autoConsent","autoConsent");
+		attachValidation();
+		
+		if(mode == Constants.MODE_NEW){
+			studyIdTxtFld.setEnabled(false);
+		}else{
+			studyIdTxtFld.setEnabled(false);
+			studyNameTxtFld.setEnabled(false);
+		}
+		
+		decorateComponents();
+		addComponents();
+		
+	}
+	
 
 	public TextField<String> getStudyIdTxtFld() {
 		return studyIdTxtFld;
@@ -95,76 +162,29 @@ public class StudyForm extends Form<StudyModel>{
 		return studyNameTxtFld;
 	}
 	
-	
-	
-	protected  void onSave(StudyModel studyModel){}
-	protected  void onCancel(StudyModel studyModel){
-		studyModel = new StudyModel();
-		updateFormComponentModels();
-		
+	public int getMode() {
+		return mode;
 	}
-	protected void  onDelete(StudyModel studyModel){}
-	
-	private void initFormFields(StudyModel studyModel) throws ArkSystemException{
-		
-		saveButton = new Button(Constants.SAVE, new StringResourceModel("saveKey", this, null))
-		{
-			public void onSubmit()
-			{
-				StudyModel studyModel = (StudyModel) getForm().getModelObject();
-				onSave(studyModel);
-			}
-		}; 
-		
-		cancelButton = new Button(Constants.CANCEL,  new StringResourceModel("cancelKey", this, null))
-		{
-			public void onSubmit()
-			{
-				//Go to Search users page
-				StudyModel studyModel = (StudyModel) getForm().getModelObject();
-				studyModel = new StudyModel();
-				
-				
-				onCancel(studyModel);
-			}
-			
-		};
-		
-		deleteButton = new Button(Constants.DELETE, new StringResourceModel("deleteKey", this, null))
-		{
-			public void onSubmit()
-			{
-				//Go to Search users page
-				onDelete((StudyModel) getForm().getModelObject());
-			}
-			
-		};
-		
-		initStudyStatusDropDown(studyModel);
-		autoGenSubIdRdChoice = initRadioButtonChoice(studyModel,"study.autoGenerateSubjectKey","autoGenSubId");
-		autoConsentRdChoice = initRadioButtonChoice(studyModel,"study.autoConsent","autoConsent");
-		attachValidation();
-		
-		if(mode == Constants.MODE_NEW){
-			studyIdTxtFld.setEnabled(false);
-		}else{
-			studyIdTxtFld.setEnabled(false);
-			studyNameTxtFld.setEnabled(false);
-		}
+
+	public void setMode(int mode) {
+		this.mode = mode;
 	}
+
+	
 	
 	private void attachValidation(){
 	
-		studyNameTxtFld.setRequired(true).setLabel(new StringResourceModel("error.study.name.required", this, new Model("Name")));
-		studyDescriptionTxtArea.add(StringValidator.lengthBetween(1, 255));//TODO Have to stop the validator posting the content with the error message
-		studyStatusDpChoices.setRequired(true).setLabel(new StringResourceModel("error.study.status.required",this, new Model("Status")));
+		studyNameTxtFld.setRequired(true).setLabel(new StringResourceModel("error.study.name.required", this, new Model<String>("Name")));
+		//TODO Have to stop the validator posting the content with the error message
+		studyDescriptionTxtArea.add(StringValidator.lengthBetween(1, 255));
+		studyStatusDpChoices.setRequired(true).setLabel(new StringResourceModel("error.study.status.required",this, new Model<String>("Status")));
 		dateOfApplicationDp.add(DateValidator.maximum(new Date())).setLabel( new StringResourceModel("error.study.doa.max.range",this, null));
 		//Can be only today
 		//Estimate year of completion - should be a valid year. Must be less than dateOfApplication
-		chiefInvestigatorTxtFld.setRequired(true).setLabel(new StringResourceModel("error.study.chief",this,new Model("Chief Investigator")));
+		chiefInvestigatorTxtFld.setRequired(true).setLabel(new StringResourceModel("error.study.chief",this,new Model<String>("Chief Investigator")));
 		chiefInvestigatorTxtFld.add(StringValidator.lengthBetween(3, 50));
 		
-		coInvestigatorTxtFld.add(StringValidator.lengthBetween(3,50)).setLabel(new StringResourceModel("error.study.co.investigator",this, new Model("Co Investigator")));
+		coInvestigatorTxtFld.add(StringValidator.lengthBetween(3,50)).setLabel(new StringResourceModel("error.study.co.investigator",this, new Model<String>("Co Investigator")));
 		//selectedApplicationsLmc.setRequired(true).setLabel( new StringResourceModel("error.study.selected.app", this, null));
 		subjectKeyStartAtTxtFld.add( new RangeValidator<Integer>(1,Integer.MAX_VALUE)).setLabel( new StringResourceModel("error.study.subject.key.prefix", this, null));
 	}
@@ -251,15 +271,5 @@ public class StudyForm extends Form<StudyModel>{
 		return new RadioChoice<Boolean>(radioChoiceId,propertyModel,list,radioChoiceRender);
 	}
 
-	public StudyModel getStudyModel() {
-		return studyModel;
-	}
-
-	public void setStudyModel(StudyModel studyModel) {
-		this.studyModel = studyModel;
-	}
-	
-	
-	
 
 }
