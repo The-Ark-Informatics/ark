9251a849571b465dab0858b2acedaada5023be58
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-12 08:57:58 +0000

    Changes include the form that extends a form of type StudyModel The model is created once here and then passed down to the form instance. On new, search functions the model is updated approriately. So that it synchs with the operation. (earlier the data fetched was inconsistent) The new, search functions defined are Ajax based The Reset is a normal button that refreshes the search panel including the drop down. Added some process() for new,save,search events. The details panel is also refreshed using  ajax,.
    git-svn-id: svn://localhost@1364 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/Search.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/Search.java
@@ -1,13 +1,20 @@
 package au.org.theark.study.web.component.study;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.study.model.entity.Study;
+import au.org.theark.study.model.entity.StudyStatus;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.web.Constants;
 import au.org.theark.study.web.form.SearchStudyForm;
@@ -15,88 +22,122 @@ import au.org.theark.study.web.form.SearchStudyForm;
 @SuppressWarnings("serial")
 public class Search extends Panel {
 	
-
-	private SearchResultList searchResults;
-	private Details detailsPanel;
-	private FeedbackPanel feedBackPanel = new FeedbackPanel("feedbackMessage");
-	
-	public SearchResultList getSearchResults() {
-		return searchResults;
-	}
-
-	public void setSearchResults(SearchResultList searchResults) {
-		this.searchResults = searchResults;
-	}
-	
-	public void setDetailsPanelVisible(boolean isVisible){
-		detailsPanel.setVisible(isVisible);
-	}
-	
-	@SpringBean( name = Constants.STUDY_SERVICE)
-	private IStudyService studyService;
-	
-	
+	/**
+	 * Constructor
+	 * @param id
+	 */
 	public Search(String id) {
 		super(id);
 	}
 	
-	public void process(String id){
-		//Create a new instance of the details panel
-		StudyModel studyModel = new StudyModel();
-		studyModel.setStudy(new Study());
+	private FeedbackPanel feedBackPanel = new FeedbackPanel("feedbackMessage");
+	
+	private SearchResultList searchResults;
+	private PageableListView<Study> listView;
+	private Details detailsPanel;
+	private CompoundPropertyModel<StudyModel> cpm;
+	private WebMarkupContainer listContainer;
+	private IModel<Object> iModel;
+
+	@SpringBean( name = Constants.STUDY_SERVICE)
+	private IStudyService studyService;
+	
+	
+	private void initialiseSearchResults(){
 		
-		detailsPanel = new Details("detailsPanel", this,Constants.MODE_NEW);
+		searchResults = new SearchResultList("searchResults",detailsPanel);
+		
+		//Set the Model reference into the results panel
+		searchResults.setCpm(cpm);
+		
+		//Build a Pageable List
+		listView  = searchResults.buildPageableListView(iModel, listContainer);
+		listView.setReuseItems(true);
+		PagingNavigator pageNavigator = new PagingNavigator("navigator", listView);
+		searchResults.add(pageNavigator);
+		searchResults.add(listView);
+		listContainer.add(searchResults);
+		
+	}
+	
+	public void processSearch(AjaxRequestTarget target){
+		listContainer.setVisible(true);
+		target.addComponent(listContainer);
+	}
+	
+	public void processDetail(AjaxRequestTarget target){
+		detailsPanel.setVisible(true);
+		listContainer.setVisible(false);
+		target.addComponent(detailsPanel);
+		target.addComponent(listContainer);
+	}
+	
+	
+	public void initialise(String id){
+		
+		//The Model is defined here
+		cpm = new CompoundPropertyModel<StudyModel>(new StudyModel());
+		
+		//The wrapper for ResultsList panel that will contain a ListView
+		listContainer = new WebMarkupContainer("resultListContainer");
+		listContainer.setOutputMarkupPlaceholderTag(true);
+		listContainer.setVisible(true);
+		
+		//Initialise the Details Panel	
+		detailsPanel = new Details("detailsPanel", listContainer);
+		detailsPanel.setCpm(cpm);
+		detailsPanel.initialiseForm();
+		
+		detailsPanel.setVisible(false);
 		detailsPanel.setOutputMarkupPlaceholderTag(true);
-		detailsPanel.initialiseForm(studyModel);
 		
-		//Hide it since we have not looked up as yet
-		setDetailsPanelVisible(false);
+		iModel = new LoadableDetachableModel<Object>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected Object load() {
+				return cpm.getObject().getStudyList();
+			}
+		};
 		
-		// Uses an entirely new VO for the search so each time the search panel is loaded. The values provided will be refreshed.
-		SearchStudyForm searchForm = new SearchStudyForm(Constants.SEARCH_FORM, new Study(), id,studyService.getListOfStudyStatus(), detailsPanel){
+		//Create the Search Results Panel
+		initialiseSearchResults();
+		
+		List<StudyStatus> studyStatusList  = studyService.getListOfStudyStatus();
+		
+		//Instantiate the Form and pass in the reference to the model
+		SearchStudyForm searchForm = new SearchStudyForm(Constants.SEARCH_FORM, cpm, studyStatusList){
+			
 			/*When user has clicked on the Search Button*/
-			protected  void onSearch(Study study){
-				setDetailsPanelVisible(false);//Set the User Details panel to false/hide it
-				List<Study> resultList = studyService.getStudy(study);
+			protected  void onSearch(AjaxRequestTarget target){
+				
+				List<Study> resultList = studyService.getStudy(cpm.getObject().getStudy());
 				if(resultList != null && resultList.size() == 0){
 					this.info("Study with the specified criteria does not exist in the system.");	
 				}
 				
-				//Render the Search Results
-				//TODO NN Removing the panel is not efficient, I need to use another technique (Ajax)
-				remove(searchResults);//Since we already have the panel.We need to partially update the list the panel uses  rather than action it at the panel level
-				searchResults = new SearchResultList("searchResults", resultList,detailsPanel);
-				add(searchResults);
+				this.setModelObject(new StudyModel());
+				cpm = (CompoundPropertyModel<StudyModel>)this.getModel();////reset the original one
+				cpm.getObject().setStudyList(resultList);
+				listView.removeAll();
+				listContainer.setVisible(true);
+				target.addComponent(listContainer);
 			}
-		
-			protected void onNew(Study study){
-//				detailsPanel.getStudyForm().setDefaultModelObject(new StudyModel());
-//				detailsPanel.getStudyForm().getStudyIdTxtFld().setEnabled(false);
-//				detailsPanel.getStudyForm().getStudyNameTxtFld().setEnabled(true);
-				//detailsPanel.setVisible(true);
-				StudyModel studyModel = new StudyModel();
-				studyModel.setStudy(study);
-				detailsPanel.getStudyForm().setModelObject(studyModel);
-				detailsPanel.getStudyForm().getStudyIdTxtFld().setEnabled(false);
-				detailsPanel.getStudyForm().getStudyNameTxtFld().setEnabled(true);
 			
-				detailsPanel.setVisible(true);
-				searchResults.setVisible(false);
-				
+			protected void onNew(AjaxRequestTarget target){
+				this.setModelObject(new StudyModel());
+				cpm = (CompoundPropertyModel<StudyModel>)this.getModel();//reset the original one
+			 	processDetail(target);
 			}
-		
+			
 		};
 
 		//Add the Form to the Panel. The Form object that will contain the child or UI components that will be part of the search or be affected by the search.
+		searchForm.add(listContainer);
+		searchForm.add(detailsPanel);
 		add(searchForm);
 		add(feedBackPanel); //Add feedback panel
-		searchForm.add(detailsPanel);
-		//Just an empty list
-		List<Study> studyList = new ArrayList<Study>();
-		Study study = new Study();
-		studyList.add(study);
-		searchResults = new SearchResultList("searchResults",studyList,detailsPanel);
-		searchForm.add(searchResults);
+		add(searchForm);
 	}
 
 }
