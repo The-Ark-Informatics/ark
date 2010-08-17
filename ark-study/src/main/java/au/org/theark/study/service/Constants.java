b7c19846db453144b4f93b29d4eead1cd9324f6b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-08-17 10:21:46 +0000

    added field constants for study
    git-svn-id: svn://localhost@1383 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/service/Constants.java
+++ ark-study/src/main/java/au/org/theark/study/service/Constants.java
@@ -12,6 +12,14 @@ public class Constants {
 	public static final int UPDATE=3;
 	public static final String ARK_SYSTEM_USER="arksystemuser@the-ark.org.au";
 	
+	public static final String STUDY_KEY="studyKey";
+	public static final String STUDY_NAME="name";
+	public static final String DATE_OF_APPLICATION="dateOfApplication";
+	public static final String EST_YEAR_OF_COMPLETION="estimatedYearOfCompletion";
+	public static final String CHIEF_INVESTIGATOR ="chiefInvestigator";
+	public static final String CONTACT_PERSON ="contactPerson";
+	public static final String STUDY_STATUS="studyStatus";
+	
 	
 	
 
