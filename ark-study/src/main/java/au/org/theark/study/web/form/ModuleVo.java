354ef123b3d2809bd1823f9c622567b8eed14420
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-03 10:03:52 +0000

    Initial revision this is a light version of Module, used specifically for study. But I can move this into VO package and call it LightModuleVo
    git-svn-id: svn://localhost@1426 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-study/src/main/java/au/org/theark/study/web/form/ModuleVo.java
@@ -0,0 +1,17 @@
+package au.org.theark.study.web.form;
+
+import java.io.Serializable;
+
+public class ModuleVo implements Serializable{
+	
+	private String moduleName;
+
+	public String getModuleName() {
+		return moduleName;
+	}
+
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+
+}
