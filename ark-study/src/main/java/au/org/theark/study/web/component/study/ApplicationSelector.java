6b5957399a979548f892d6d2b36a6bcc438358f4
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-03 09:43:32 +0000

    git-svn-id: svn://localhost@1419 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/ApplicationSelector.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/ApplicationSelector.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.vo.ModuleVO;
@@ -15,19 +16,37 @@ public class ApplicationSelector extends Panel{
 	private List<ModuleVO> modules;
 	private StudyModel studyModel;
 	private ListMultipleChoiceForm form;
+	
+	
+	/* A reference of the Model from the Container in this case Search Panel */
+	private CompoundPropertyModel<StudyModel> cpm;
+	
+	public CompoundPropertyModel<StudyModel> getCpm() {
+		return cpm;
+	}
+
+	public void setCpm(CompoundPropertyModel<StudyModel> cpm) {
+		this.cpm = cpm;
+	}
+	
 	public ApplicationSelector(String id, StudyModel studyModel, List<ModuleVO> modules){
 		super(id);
 		this.modules = modules;
 		this.studyModel =studyModel;
 	}
+
 	
-//	public void setupSelector() throws ArkSystemException{
-//		
+	public ApplicationSelector(String id){
+		super(id);
+	}
+	
+	public void setupSelector() throws ArkSystemException{
+		
 //		form = new ListMultipleChoiceForm("appSelectorForm", studyModel, modules);
 //		WebMarkupContainer container = form.initLMCContainer();
 //		form.add(container);
 //		this.add(form);
-//	}
+	}
 	
 	public void getSelectedItems(){
 		
