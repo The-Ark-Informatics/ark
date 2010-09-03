1a7d62c1cc7ef69e7b0376d35708add39074c43f
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-03 09:49:12 +0000

    added support for delete and update.
    git-svn-id: svn://localhost@1423 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/Details.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/Details.java
@@ -1,19 +1,26 @@
 package au.org.theark.study.web.component.study;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.exception.EntityCannotBeRemoved;
+import au.org.theark.core.exception.EntityExistsException;
+import au.org.theark.core.exception.UnAuthorizedOperation;
 import au.org.theark.core.vo.ModuleVO;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.form.ModuleVo;
 import au.org.theark.study.web.form.StudyForm;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.markup.html.form.palette.Palette;
 
 @SuppressWarnings("serial")
 public class Details extends Panel{
@@ -39,34 +46,49 @@ public class Details extends Panel{
 		this.cpm = cpm;
 	}
 	
+	
+	public StudyForm getStudyForm() {
+		return studyForm;
+	}
+
+	private StudyForm studyForm;
+	
+	private FeedbackPanel feedBackPanel;
+	
 	/**
 	 * Sets the id and the container reference for the search results.
 	 * @param id
 	 * @param listContainer
 	 */
-	public Details(String id, final WebMarkupContainer listContainer) {
+	public Details(String id, final WebMarkupContainer listContainer, FeedbackPanel feedBackPanel) {
 		super(id);
 		this.listContainer = listContainer;
+		this.feedBackPanel = feedBackPanel;
 	}
 
 	
 	public void initialiseForm(){
 		
-		StudyForm studyForm = new StudyForm("studyForm", this, listContainer){
+		 studyForm = new StudyForm("studyForm", this, listContainer, feedBackPanel){
 			
 			protected void onSave(StudyModel studyModel, AjaxRequestTarget target){
 				
 				try{
 					if(studyModel.getStudy()!= null && studyModel.getStudy().getStudyKey() == null){
-						
 						service.createStudy(studyModel.getStudy(),studyModel.getLmcSelectedApps());
 						this.info("Study: " + studyModel.getStudy().getName().toUpperCase() + " has been saved.");
-						
 					}else{
 						//Update
-						service.updateStudy(studyModel.getStudy(), null);
+						service.updateStudy(studyModel.getStudy(),studyModel.getLmcSelectedApps() );
 						this.info("Update of Study: " + studyModel.getStudy().getName().toUpperCase() + " was Successful.");
 					}
+				}
+				catch(EntityExistsException eee){
+					this.error(eee.getMessage());
+				}
+				catch(EntityCannotBeRemoved ecbr){
+					
+					this.error(ecbr.getMessage());
 				}
 				catch(ArkSystemException arkSystemExeption){
 					this.error(arkSystemExeption.getMessage());
@@ -76,23 +98,25 @@ public class Details extends Panel{
 				}
 			}
 		
+		
 			protected void onCancel(AjaxRequestTarget target){
 				
 			}
 			
-			protected void onDelete(AjaxRequestTarget target){
+			protected void onDelete(StudyModel studyModel,AjaxRequestTarget target){
+				try{
 				
+					service.deleteStudy(studyModel.getStudy());	
+				
+				}catch(ArkSystemException arkException){
+					this.error(arkException.getMessage());
+				} catch (EntityCannotBeRemoved e) {
+					this.error(e.getMessage());
+				} catch (UnAuthorizedOperation e) {
+					this.error(e.getMessage());
+				}
 			}
 		};
-		//TODO Add the application selector multi select pallete
-		try {
-			List<ModuleVO> modules = userService.getModules(true);
-			ApplicationSelector applicationSelector = new ApplicationSelector("applicationSelector", studyForm.getModelObject(), modules);
-			//applicationSelector.setupSelector();
-			//studyForm.add(applicationSelector);
-		} catch (ArkSystemException e) {
-			e.printStackTrace();
-		}
 		
 		add(studyForm); //Add the form to the panel
 	}
