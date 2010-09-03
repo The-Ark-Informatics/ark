35188cc0c6d43100954eadf8a07545c683fdc1ca
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-03 10:03:06 +0000

    Added functionality to fetch the study related modules and available modules in order to populate the application selector palette on the OnClick even handler.
    git-svn-id: svn://localhost@1425 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/study/SearchResultList.java
@@ -1,6 +1,9 @@
 package au.org.theark.study.web.component.study;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.AttributeModifier;
@@ -14,14 +17,23 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.vo.ModuleVO;
 import au.org.theark.study.model.entity.Study;
+import au.org.theark.study.service.IUserService;
+import au.org.theark.study.web.form.ModuleVo;
 
 
 @SuppressWarnings("serial")
 public class SearchResultList extends Panel{
 	
 	private Details detailsPanel;
+	
+	@SpringBean( name = "userService")
+	private IUserService userService;
+	
 	
 	/* A reference of the Model from the Container in this case Search Panel */
 	private CompoundPropertyModel<StudyModel> cpm;
@@ -95,6 +107,26 @@ public class SearchResultList extends Panel{
 				//Based on the user type navigate to another panel (Summary of study or the View panel)
 				StudyModel studyModel  = cpm.getObject();
 				studyModel.setStudy(study);
+				
+				List<ModuleVO> modules;
+				List<ModuleVo> moduleVoList = new ArrayList<ModuleVo>();
+				Collection<ModuleVo> modulesLinkedToStudy = new  ArrayList<ModuleVo>();
+				try {
+					modules = userService.getModules(true);//source this from a static list or on application startup 
+					for (ModuleVO moduleVO : modules) {
+						ModuleVo moduleVo = new ModuleVo();
+						moduleVo.setModuleName(moduleVO.getModule());
+						moduleVoList.add(moduleVo);
+					}
+					
+					modulesLinkedToStudy  = userService.getModulesLinkedToStudy(study.getName(), true);
+					studyModel.setModulesSelected(modulesLinkedToStudy);
+					studyModel.setModulesAvailable(moduleVoList);
+				} catch (ArkSystemException e) {
+					//log the error message and notify sys admin to take appropriate action
+					this.error("A system error has occured. Please try after some time.");
+				}
+
 				detailsPanel.setVisible(true);
 				//TODO make the ID and Name field disabled
 				searchContainer.setVisible(false);
