bcd3d7c7bd6a2a4711636911b70da2d6e5fdc66a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-07 08:49:52 +0000

    archiveStudy, looks up a status of type archive and sets it to the StudyStatus instance of the Study and updates the study.
    git-svn-id: svn://localhost@1434 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/service/StudyServiceImpl.java
+++ ark-study/src/main/java/au/org/theark/study/service/StudyServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.exception.EntityCannotBeRemoved;
 import au.org.theark.core.exception.EntityExistsException;
+import au.org.theark.core.exception.StatusNotAvailableException;
 import au.org.theark.core.exception.UnAuthorizedOperation;
 import au.org.theark.core.security.RoleConstants;
 import au.org.theark.study.model.dao.ILdapUserDao;
@@ -75,7 +76,7 @@ public class StudyServiceImpl implements IStudyService{
 		
 	}
 	
-	public void updateStudy(Study studyEntity,Set<String> selectedApplications) throws EntityExistsException,EntityCannotBeRemoved, UnAuthorizedOperation, ArkSystemException{
+	public void updateStudy(Study studyEntity,Set<String> selectedApplications) throws EntityCannotBeRemoved, UnAuthorizedOperation, ArkSystemException, EntityExistsException{
 
 		SecurityManager securityManager =  ThreadContext.getSecurityManager();
 		Subject currentUser = SecurityUtils.getSubject();
@@ -89,62 +90,42 @@ public class StudyServiceImpl implements IStudyService{
 	
 	
 	private boolean validateDeleteStudy(Study studyEntity, StringBuffer messageBuffer){
-		boolean isDeletable = false;
+		int validationErrors =0;
+		boolean isDeletable = true;
 		/*Check if there are study components linked to the study */
 		if(studyEntity.getLinkStudyStudycomps().size() > 0){
-			isDeletable = true;
 			messageBuffer.append("\nThe study is linked to Study Components.");
+			validationErrors++;
 		}
 		
 		/* Check if the study has Sites linked to the study in LDAP or Backend*/
 		if(studyEntity.getLinkStudyStudysites().size() > 0 ){
-			isDeletable = true;
 			messageBuffer.append("\n The study is linked to Study Sites.");
+			validationErrors++;
 		}
 		
 		/*Check if the study has subjects linked via the backend database */
 		if(studyEntity.getLinkSubjectStudies().size() > 0){
-			isDeletable = true;
 			messageBuffer.append("\n There are Subjects linked to the Study.");
+			validationErrors++;
 		}
 		
 		/* Check if the study is linked with Subjects via Contacts */
 		if(studyEntity.getLinkSubjectContacts().size() > 0){
-			isDeletable = true;
 			messageBuffer.append("\n There are Contacts  linked to the Study via Subjects.");
+			validationErrors++;
 		}
 		
 		/* Check if the study has Sub-Study linked to the study*/
 		if(studyEntity.getLinkStudySubstudiesForStudyKey().size() > 0){
-			isDeletable = true;
 			messageBuffer.append("\n There are sub-studies refering to this Registry study.");
+			validationErrors++;
 		}
 		
+		if(validationErrors > 0){
+			isDeletable = false;
+		}
 		return isDeletable;
-	}
-	
-	/**
-	 * Removes a Study from the LDAP and the database if it passes the validations.
-	 */
-	public void deleteStudy(Study studyEntity) throws EntityCannotBeRemoved,UnAuthorizedOperation,ArkSystemException{
-		
-		StringBuffer messageBuffer = new StringBuffer();
-		SecurityManager securityManager =  ThreadContext.getSecurityManager();
-		Subject currentUser = SecurityUtils.getSubject();
-		if(securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN)){
-			
-			if(!validateDeleteStudy(studyEntity, messageBuffer)){
-				throw new EntityCannotBeRemoved(messageBuffer.toString());
-			}
-			else{
-				studyDao.delete(studyEntity);//Will remove the study object from the database
-				Set<String> modulesToDelinkFrom = iLdapUserDao.getModulesLinkedToStudy(studyEntity.getName(),true);
-				iLdapUserDao.removeStudy(studyEntity.getName(), modulesToDelinkFrom);
-			}
-			
-		}else{
-		 throw new UnAuthorizedOperation("The logged in user does not have the permission to create a study.");//Throw an exception
-		}
 	}
 	
 	public Study getStudy(Long id){
@@ -158,5 +139,15 @@ public class StudyServiceImpl implements IStudyService{
 	
 	public Set<String> getModulesLinkedToStudy(String studyNameCN, boolean isForDisplay) throws ArkSystemException{
 		return iLdapUserDao.getModulesLinkedToStudy(studyNameCN, isForDisplay);
+	}
+	/**
+	 * This will mark the study as archived.
+	 */
+	public void archiveStudy(Study studyEntity) throws UnAuthorizedOperation, StatusNotAvailableException, ArkSystemException
+	{
+		//For archive, set the status to Archived and then issue an update
+		StudyStatus status = studyDao.getStudyStatus("Archive");
+		studyEntity.setStudyStatus(status);
+		studyDao.updateStudy(studyEntity);
 	}
 }
