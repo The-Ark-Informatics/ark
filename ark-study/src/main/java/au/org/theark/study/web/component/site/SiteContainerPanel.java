07bab0f4a730f6b67749c8555986b6ca3fdb38f5
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-08 10:16:39 +0000

    added search panel to sites
    git-svn-id: svn://localhost@1449 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/site/SiteContainerPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/site/SiteContainerPanel.java
@@ -1,6 +1,8 @@
 package au.org.theark.study.web.component.site;
 
 import org.apache.wicket.markup.html.panel.Panel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SiteContainerPanel extends Panel{
 
@@ -8,10 +10,17 @@ public class SiteContainerPanel extends Panel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private transient Logger log = LoggerFactory.getLogger(SiteContainerPanel.class);
+	
+	//Search Site Panel
+	Search searchSitesPanel;
+	
 	public SiteContainerPanel(String id) {
 		super(id);
-		// TODO Auto-generated constructor stub
+		log.info("SiteContainerPanel initialised. Creating the Search Panel for Sites..");
+		searchSitesPanel = new Search("searchSitePanel");
+		searchSitesPanel.initialise();
+		add(searchSitesPanel);
 	}
 	
 
