758da4c5af780346d674468444cc71b757d410f2
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-13 03:59:50 +0000

    added site and siteDescription properties.
    git-svn-id: svn://localhost@1466 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/site/SiteModel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/site/SiteModel.java
@@ -9,7 +9,42 @@ public class SiteModel implements Serializable{
 	public SiteModel(){
 		
 	}
+	/**
+	 * Maps to ldap properties
+	 */
+	private String siteName;
+	private String siteDescription;
 	
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+
+	public String getSiteDescription() {
+		return siteDescription;
+	}
+
+	public void setSiteDescription(String siteDescription) {
+		this.siteDescription = siteDescription;
+	}
+
+	public StudySite getStudySite() {
+		return studySite;
+	}
+
+	public void setStudySite(StudySite studySite) {
+		this.studySite = studySite;
+	}
+	
+
+	/* Access this instance to get to the site details in database specifically for locating and storing address information. This
+	 * is done so we don't duplicate this in ldap at each study level. Having access to the study site name from ldap and a study in context we can 
+	 * get the study site and access the address. Yes we do duplicate the site name an description in the database*/
 	private StudySite studySite;
+	
+	
 
 }
