53f95a8b60eadfcf3f2625e9f26b87944deff274
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-13 09:13:42 +0000

    added functionality to lookup a site from ldap
    git-svn-id: svn://localhost@1479 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/site/Search.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/site/Search.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.study.model.entity.Person;
@@ -17,6 +20,7 @@ import au.org.theark.study.model.entity.Study;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.component.site.SearchResultList;
 import au.org.theark.study.web.form.SearchSiteForm;
 public class Search extends Panel{
 
@@ -26,6 +30,8 @@ public class Search extends Panel{
 	
 	private FeedbackPanel feedBackPanel;
 	private CompoundPropertyModel<SiteModel> cpm;
+	private PageableListView<SiteVo> listView;
+	private SearchResultList searchResults;
 	
 	//The container to wrap the Search Result List
 	private WebMarkupContainer listContainer;
@@ -64,28 +70,49 @@ public class Search extends Panel{
 		detailsPanel.initialisePanel();
 		detailsContainer.add(detailsPanel);
 		
-		//iModel = get a list of sites here
-		
+		iModel = new LoadableDetachableModel<Object>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected Object load() {
+				return cpm.getObject().getSiteVoList();
+			}
+		};
 		
 		initialiseSearchResults();
+		
 		//Get the study id from the session and get the study
 		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		
 		List<Person> availablePersons = new ArrayList<Person>();
-		if(sessionStudyId != null){
-			Study study = studyService.getStudy(sessionStudyId);
-			//For the given study get a list of applications.
-		}
+//		if(sessionStudyId != null){
+//			Study study = studyService.getStudy(sessionStudyId);
+//			//For the given study get a list of applications.
+//		}
 		
 		SearchSiteForm searchSiteForm = new SearchSiteForm(Constants.SEARCH_FORM, cpm,availablePersons){
 			
 			protected  void onSearch(AjaxRequestTarget target){
 				
+				List<SiteVo> resultList = studyService.getSite(cpm.getObject().getSiteVo());
+				if(resultList != null && resultList.size() == 0){
+					this.info("Site with the specified criteria does not exist in the system.");	
+				}
+				
+				this.setModelObject(new SiteModel());
+				cpm = (CompoundPropertyModel<SiteModel>)this.getModel();////reset the original one
+				cpm.getObject().setSiteVoList(resultList);//Place the results into the model
+				listView.removeAll();
+				listContainer.setVisible(true);//Make the WebMarkupContainer that houses the search results visible
+				target.addComponent(listContainer);//For ajax this is required so 
+				
 			}
 			
 			protected void onNew(AjaxRequestTarget target){
 				// Show the details panel name and description
-				this.setModelObject(new SiteModel());
+				SiteModel siteModel = new SiteModel();
+				siteModel.setMode(Constants.MODE_NEW);
+				this.setModelObject(siteModel);
 				cpm = (CompoundPropertyModel<SiteModel>)this.getModel();
 				//Any pre-population do it here
 				detailsPanel.setCpm(cpm);
@@ -112,5 +139,15 @@ public class Search extends Panel{
 		
 	private void initialiseSearchResults(){
 		
+		searchResults = new SearchResultList("searchResults",detailsContainer);
+		//Set the Model reference into the results panel
+		searchResults.setCpm(cpm);
+		//Build a Pageable List
+		listView  = searchResults.buildPageableListView(iModel, listContainer);
+		listView.setReuseItems(true);
+		PagingNavigator pageNavigator = new PagingNavigator("navigator", listView);
+		searchResults.add(pageNavigator);
+		searchResults.add(listView);
+		listContainer.add(searchResults);
 	}
 }
