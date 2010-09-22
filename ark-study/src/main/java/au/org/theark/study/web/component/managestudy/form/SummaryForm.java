04b8a5c50cdbf853642c899baf15ba5691a3c58e
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-09-22 06:53:01 +0000

    git-svn-id: svn://localhost@1522 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/managestudy/form/SummaryForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/managestudy/form/SummaryForm.java
@@ -1,13 +1,14 @@
 package au.org.theark.study.web.component.managestudy.form;
 
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.study.model.vo.StudyModel;
 
-public class SummaryForm extends BaseForm{
+public class SummaryForm extends Form{
 
 	public SummaryForm(String id, CompoundPropertyModel<StudyModel> model) {
-		super(id, model);
+		super(id);
 		// TODO Auto-generated constructor stub
 		
 	}
