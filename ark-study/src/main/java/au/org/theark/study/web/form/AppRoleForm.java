e1f52308d8af6562289aeb9899105702c6da8ae8
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-13 06:03:38 +0000

    organised imports
    git-svn-id: svn://localhost@1622 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/form/AppRoleForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/form/AppRoleForm.java
@@ -2,26 +2,19 @@ package au.org.theark.study.web.form;
 
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.odlabs.wiquery.ui.accordion.Accordion;
 
 import au.org.theark.core.vo.ArkUserVO;
 
 @SuppressWarnings("serial")
 public class AppRoleForm extends Form<ArkUserVO>{
 
-	protected Accordion accordion;
-	
-	public Accordion getAccordion() {
-		return accordion;
-	}
-
-	public void setAccordion(Accordion accordion) {
-		this.accordion = accordion;
-	}
-
 	public AppRoleForm(String id, ArkUserVO arkUserVO) {
 		super(id, new CompoundPropertyModel<ArkUserVO>(arkUserVO));
 	
+	}
+	
+	public AppRoleForm(String id) {
+		super(id);
 	}
 
 }
