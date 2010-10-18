8f91d0b398fe1039227913fc6f750b4cc82f1bee
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-18 06:20:19 +0000

    Added banner image reference
    git-svn-id: svn://localhost@1639 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/web/pages/Constants.java
+++ ark-container/src/main/java/au/org/theark/web/pages/Constants.java
@@ -4,4 +4,5 @@ public class Constants
 {
 	public static final String HOSTED_BY_IMAGE = "hosted_by_image.gif";
 	public static final String PRODUCT_IMAGE = "product_image.gif";
+	public static final String BANNER_IMAGE = "banner_image.gif";
 }
