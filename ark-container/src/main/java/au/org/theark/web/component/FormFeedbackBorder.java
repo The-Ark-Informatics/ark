d8b023f5539877838361d76b3dccaff375007427
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-22 01:08:45 +0000

    Formatted source code, added suppress warnings
    git-svn-id: svn://localhost@1666 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/web/component/FormFeedbackBorder.java
+++ ark-container/src/main/java/au/org/theark/web/component/FormFeedbackBorder.java
@@ -10,27 +10,29 @@ import org.apache.wicket.markup.html.border.Border;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
-
-public class FormFeedbackBorder extends Border implements IFeedback {
+@SuppressWarnings({"serial", "unchecked", "unused"})
+public class FormFeedbackBorder extends Border implements IFeedback
+{
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private boolean visible;
+	private static final long	serialVersionUID	= 1L;
+	private boolean				visible;
 
 	/**
-	 * Error indicator that will be shown whenever there is an error-level
-	 * message for the collecting component.
+	 * Error indicator that will be shown whenever there is an error-level message for the collecting component.
 	 */
-	private final class ErrorIndicator extends WebMarkupContainer {
-		
+	private final class ErrorIndicator extends WebMarkupContainer
+	{
+
 		/**
 		 * 
 		 */
-		private static final long serialVersionUID = 1L;
+		private static final long	serialVersionUID	= 1L;
 
-		public ErrorIndicator(String id) {
+		public ErrorIndicator(String id)
+		{
 			super(id);
 			add(new ErrorTextLabel("errorText", new Model("*")));
 			add(new ErrorStyleAttributeModifier("style", true, new Model("color:red;")));
@@ -38,47 +40,55 @@ public class FormFeedbackBorder extends Border implements IFeedback {
 
 		// An error style whose visiblity is determined by the presence
 		// of feedback error messages.
-		class ErrorStyleAttributeModifier extends AttributeModifier {
-			public ErrorStyleAttributeModifier(String attribute,boolean addAttributeIfNotPresent, IModel replaceModel) {
+
+		class ErrorStyleAttributeModifier extends AttributeModifier
+		{
+			public ErrorStyleAttributeModifier(String attribute, boolean addAttributeIfNotPresent, IModel replaceModel)
+			{
 				super(attribute, addAttributeIfNotPresent, replaceModel);
 			}
 
-			public boolean isVisible() {
+			public boolean isVisible()
+			{
 				return visible;
 			}
 		}
 
 		// An error text label whose visibility is determined by the presence
 		// of feedback error messages.
-		class ErrorTextLabel extends Label {
-			public ErrorTextLabel(String id, IModel model) {
+		class ErrorTextLabel extends Label
+		{
+			public ErrorTextLabel(String id, IModel model)
+			{
 				super(id, model);
 			}
 
-			public boolean isVisible() {
+			public boolean isVisible()
+			{
 				return visible;
 			}
 		}
 	}
 
-	public FormFeedbackBorder(final String id) {
+	public FormFeedbackBorder(final String id)
+	{
 		super(id);
 		add(new ErrorIndicator("errorIndicator"));
 	}
 
 	// The ContainerFeedbackMessageFilter is used to filter out
 	// feedback messages belonging to this component.
-	protected IFeedbackMessageFilter getMessagesFilter() {
+	protected IFeedbackMessageFilter getMessagesFilter()
+	{
 		return new ContainerFeedbackMessageFilter(this);
 	}
 
 	/*
-	 * This method will be called on the component during Ajax render so that it
-	 * gets a chance to determine the presence of error messages.
+	 * This method will be called on the component during Ajax render so that it gets a chance to determine the presence of error messages.
 	 */
-	public void updateFeedback() {
+	public void updateFeedback()
+	{
 		visible = true;
 	}
-	
-}
 
+}
