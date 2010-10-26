852a325910765ceb73a98df5716f5d9cc2da56c7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-26 06:31:51 +0000

    Added more complete CRUD methods for back-end tables
    git-svn-id: svn://localhost@1689 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/dao/IPhenotypicDao.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/dao/IPhenotypicDao.java
@@ -4,45 +4,64 @@ import java.util.List;
 
 import au.org.theark.phenotypic.model.entity.Collection;
 import au.org.theark.phenotypic.model.entity.CollectionImport;
-import au.org.theark.phenotypic.model.entity.FieldData;
-import au.org.theark.phenotypic.model.entity.Status;
 import au.org.theark.phenotypic.model.entity.Field;
+import au.org.theark.phenotypic.model.entity.FieldData;
 import au.org.theark.phenotypic.model.entity.FieldType;
+import au.org.theark.phenotypic.model.entity.Status;
+import au.org.theark.phenotypic.model.entity.Upload;
+import au.org.theark.phenotypic.model.entity.UploadCollection;
 
-
+/**
+ * Interface for all select/insert/update/delete operations on the backend database.
+ * 
+ */
 public interface IPhenotypicDao {
-
-	public void createCollection(Collection col);
-	
-	public List<Collection> getCollectionMatches(Collection colExample);
-	
-	/**
-	 * Interface to get a list of Study Status reference data from the backend.
-	 * These study status' are no associated with a study as such but can be used for
-	 * displaying a list of options for a particular study.
-	 * @return
-	 */
 
 	// Collection
 	public Collection getCollection(Long id);
-	public void createCollectionImport(CollectionImport colImport);
-	public void updateCollection(Collection colEntity);
+	public List<Collection> getCollectionMatches(Collection colExample);
+	public void createCollection(Collection col);
+	public void updateCollection(Collection col);
+	public void deleteCollection(Collection col);
+	
+	// Collection Import
+	public void createCollectionImport(CollectionImport collectionImport);
+	public void updateCollectionImport(CollectionImport collectionImport);
+	public void deleteCollectionImport(CollectionImport collectionImport);
 	
 	// Status
+	public Status getStatus(Long statusId);
 	public Status getStatusByName(String statusName);
+	public void createStatus(Status status);
+	public void updateStatus(Status status);
+	public void deleteStatus(Status status);
 	
 	// Field
 	public Field getField(Long fieldId);
 	public Field getFieldByName(String fieldName);
-	public void createField(Field f);
-	public void updateField(Field f);
+	public Field getFieldByName(Long studyId, String fieldName);
+	public void createField(Field field);
+	public void updateField(Field field);
+	public void deleteField(Field field);
 	
 	// Field Type
 	public FieldType getFieldTypeByName(String fieldTypeName);
 	public void createFieldType(FieldType fieldType);
 	public void updateFieldType(FieldType fieldType);
+	public void deleteFieldType(FieldType fieldType);
 
-	// FieldData
+	// Field Data
 	public void createFieldData(FieldData fieldData);
 	public void updateFieldData(FieldData fieldData);
+	public void deleteFieldData(FieldData fieldData);
+	
+	// Upload
+	public void createUpload(Upload upload);
+	public void updateUpload(Upload upload);
+	public void deleteUpload(Upload upload);
+	
+	// Upload
+	public void createUploadCollection(UploadCollection uploadCollection);
+	public void updateUploadCollection(UploadCollection uploadCollection);
+	public void deleteUploadCollection(UploadCollection uploadCollection);
 }
