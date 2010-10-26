9fa3d82e5a85bcef6c65c24295785ac48250b3e7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-26 02:13:25 +0000

    Added new Collection buttons/consants
    git-svn-id: svn://localhost@1676 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/Constants.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/Constants.java
@@ -1,9 +1,30 @@
 package au.org.theark.phenotypic.web;
 
 public class Constants {
+	// DAO
 	public static final String PHENOTYPIC_DAO = "phenotypicDao";
-	public static final String TEST = "Test";
+	
+	// Tabs/menus
+	public static final String PHENOTYPIC_MAIN_TAB = "Phenotypic";
+	
+	// Sub tabs/menus
 	public static final String PHENOTYPIC_SUBMENU = "phenotypicSubMenus";
+	public static final String COLLECTION_SUBMENU = "Collections";
+	public static final String COLLECTION_RESOURCEKEY = "tab.module.phenotypic.collection";
+	public static final String TEST_SUBMENU = "Test";
+	public static final String TEST_RESOURCEKEY = "tab.module.phenotypic.test";
+	
+	// Buttons
+	public static final String NEW_COLLECTION = "newCollection";
+	public static final String NEW_FIELD = "newField";
+	public static final String IMPORT_PHENOTYPIC_DATA_FILE = "importPhenotypicDataFile";
+	
+	public static final String NEW_BUTTON = "newButton";
+	public static final String SAVE_BUTTON = "saveButton";
+	public static final String EDIT_BUTTON = "editButton";
+	
+	// Test stubs
+	public static final String TEST = "Test";
 	public static final String FIRETEST = "fireInTheHole";
 	public static final String WATERTEST = "theGreatFlood";
 	public static final String NOAHTEST = "noahsRescue";
