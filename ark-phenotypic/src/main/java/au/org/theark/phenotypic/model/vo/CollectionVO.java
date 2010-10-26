9b520d66fd2385ba236d6e3092cae5ec9df0cfcc
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-26 03:27:41 +0000

    Updated class to contain more collection-related objects
    git-svn-id: svn://localhost@1685 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/vo/CollectionVO.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/vo/CollectionVO.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import au.org.theark.phenotypic.model.entity.Collection;
 import au.org.theark.phenotypic.model.entity.Field;
-import au.org.theark.phenotypic.model.entity.Subject;
+import au.org.theark.phenotypic.model.entity.FieldData;
+import au.org.theark.phenotypic.model.entity.FieldType;
+import au.org.theark.study.model.entity.Person;
 
 /**
  * @author cellis
@@ -16,14 +18,16 @@ public class CollectionVO implements Serializable
 {
 	protected Collection collection;
 	protected Field field;
-	
-	//TODO: Utilise au.org.theark.core.Subject;
-	protected Subject subject;
+	protected FieldType fieldType;
+	protected FieldData fieldData;
+	protected Person Person;
 	
 	/** A List of fields linked to this phenotypic collection*/
 	protected List<Field> fieldList;
 	
-	/** A List of subjects linked to this phenotypic collection*/
-	protected List<Subject> subjectList;
+	/** A List of persons linked to this phenotypic collection*/
+	protected List<Person> personList;
 	
+	/** A List of fieldData linked to this phenotypic collection*/
+	protected List<FieldData> fieldDataList;
 }
