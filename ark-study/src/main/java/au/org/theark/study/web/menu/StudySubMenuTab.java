7c1b015a6494d087999d068a765a8c0ee9f4e6ca
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-10-28 03:33:01 +0000

    Removed inner class MenuModule Changed hard-coded tab name resource keys to constants.
    git-svn-id: svn://localhost@1732 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
+++ ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
@@ -1,6 +1,5 @@
 package au.org.theark.study.web.menu;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +11,16 @@ import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
+import au.org.theark.core.Constants;
 import au.org.theark.core.security.ArkSecurityManager;
 import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.vo.ArkUserVO;
-import au.org.theark.study.web.Constants;
-import au.org.theark.study.web.component.consent.ConsentContainerPanel;
+import au.org.theark.core.vo.MenuModule;
 import au.org.theark.study.web.component.managestudy.StudyContainer;
 import au.org.theark.study.web.component.mydetails.MyDetailsContainer;
 import au.org.theark.study.web.component.site.SiteContainerPanel;
-import au.org.theark.study.web.component.study.StudyContainerPanel;
 import au.org.theark.study.web.component.studycomponent.StudyComponentContainerPanel;
+import au.org.theark.study.web.component.subject.SubjectContainer;
 import au.org.theark.study.web.component.user.UserContainer;
 
 
@@ -37,28 +36,6 @@ public class StudySubMenuTab extends Panel {
 		buildTabs();
 	}
 	
-
-	private class MenuModule implements Serializable{
-		
-		public MenuModule(){
-			super();
-		}
-		private String moduleName;
-		private String resourceKey;
-		public String getModuleName() {
-			return moduleName;
-		}
-		public void setModuleName(String moduleName) {
-			this.moduleName = moduleName;
-		}
-		public String getResourceKey() {
-			return resourceKey;
-		}
-		public void setResourceKey(String resourceKey) {
-			this.resourceKey = resourceKey;
-		}
-		
-	}
 	
 	public  void buildTabs(){
 		
@@ -67,48 +44,57 @@ public class StudySubMenuTab extends Panel {
 		
 		//THis way we can get the menus from the back-end. We should source this data from a table in the backend and wrap it up in a class like this
 		MenuModule menuModule = new MenuModule();
-		menuModule.setModuleName("Study Details");
-		menuModule.setResourceKey("tab.module.study.details");
+		menuModule.setModuleName(Constants.STUDY_DETAIL);
+		//menuModule.setResourceKey(au.org.theark.core.Constants.TAB_MODULE_STUDY_DETAIL);
+		menuModule.setResourceKey(Constants.TAB_MODULE_STUDY_DETAIL);
 		moduleTabs.add(menuModule);
 		
 		menuModule = new MenuModule();
-		menuModule.setModuleName("Sites");
-		menuModule.setResourceKey("tab.module.sites");
+		menuModule.setModuleName(Constants.SITE);
+		menuModule.setResourceKey(Constants.TAB_MODULE_SITE);
 		moduleTabs.add(menuModule);
 
 		menuModule = new MenuModule();
-		menuModule.setModuleName("Study Components");
-		menuModule.setResourceKey("tab.module.study.components");
+		menuModule.setModuleName(Constants.STUDY_COMPONENT);
+		menuModule.setResourceKey(Constants.TAB_MODULE_STUDY_COMPONENT);
 		moduleTabs.add(menuModule);
 		
 		menuModule = new MenuModule();
-		menuModule.setModuleName("Users");
-		menuModule.setResourceKey("tab.module.users");
+		menuModule.setModuleName(Constants.USER);
+		menuModule.setResourceKey(Constants.TAB_MODULE_USER);
 		moduleTabs.add(menuModule);
 
 		menuModule = new MenuModule();
-		menuModule.setModuleName("My Details");
-		menuModule.setResourceKey("tab.module.mydetails");
+		menuModule.setModuleName(Constants.MY_DETAIL);
+		menuModule.setResourceKey(Constants.TAB_MODULE_MY_DETAIL);
+		moduleTabs.add(menuModule);
+		
+		menuModule = new MenuModule();
+		menuModule.setModuleName(Constants.SUBJECT);
+		menuModule.setResourceKey(Constants.TAB_MODULE_SUBJECT);
 		moduleTabs.add(menuModule);
 		
 		for(final MenuModule moduleName : moduleTabs)
 		{
 			moduleSubTabsList.add( new AbstractTab(new Model<String>(getLocalizer().getString(moduleName.getResourceKey(), StudySubMenuTab.this, moduleName.getModuleName())) )
 			{
-				
-				
 				public boolean isVisible(){
 					
 					boolean flag = false;
-					if(moduleName.getModuleName().equalsIgnoreCase(Constants.USERS)){
+					if(moduleName.getModuleName().equalsIgnoreCase(Constants.USER) || moduleName.getModuleName().equalsIgnoreCase(Constants.SUBJECT)){
+						
 						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
 						Subject currentUser = SecurityUtils.getSubject();
-						if((arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN))){
-							 flag =  currentUser.isAuthenticated();	 
+						if(	(arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN) || 
+							(arkSecurityManager.subjectHasRole(RoleConstants.STUDY_ADMIN))	)){
+							
+							flag =  currentUser.isAuthenticated();	 
+						
 						}else{
 							 flag = false;
 						}
-					}else{
+					}
+					else{
 						flag=true;
 					}
 					return flag;
@@ -120,26 +106,30 @@ public class StudySubMenuTab extends Panel {
 					
 					Panel panelToReturn = null;//Set up a common tab that will be accessible for all users
 					
-					if(moduleName.getModuleName().equalsIgnoreCase(Constants.USERS)){
+					if(moduleName.getModuleName().equalsIgnoreCase(Constants.USER)){
 						
 						panelToReturn = new UserContainer(panelId, new ArkUserVO());//Note the constructor
 					
-					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_DETAILS)){
+					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_DETAIL)){
 					
 						panelToReturn = new StudyContainer(panelId);
 					
-					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.SITES)){
+					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.SITE)){
 						
 						panelToReturn = new SiteContainerPanel(panelId);
 					
-					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_COMPONENTS)){
+					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_COMPONENT)){
 					
 						panelToReturn = new StudyComponentContainerPanel(panelId);
 					
-					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.MY_DETAILS)){
+					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.MY_DETAIL)){
 						
 						Subject currentUser = SecurityUtils.getSubject();
 						panelToReturn = new MyDetailsContainer(panelId,new ArkUserVO(),currentUser );
+						
+					}else if(moduleName.getModuleName().equalsIgnoreCase(Constants.SUBJECT)){
+						
+						panelToReturn = new SubjectContainer(panelId);
 					}
 					
 					return panelToReturn;
