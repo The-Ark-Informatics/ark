099fd7b54527759adc663df4e991e31afc61ffb0
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-02 07:05:18 +0000

    Added common search/retrieve interfaces for study and subject. This is part of the refactoring process of study's persistence layer.
    git-svn-id: svn://localhost@1790 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
+++ ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
@@ -1,9 +1,18 @@
 package au.org.theark.core.service;
 
+import java.util.Collection;
 import java.util.List;
 
 import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.model.study.entity.GenderType;
+import au.org.theark.core.model.study.entity.PhoneType;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.model.study.entity.StudyStatus;
+import au.org.theark.core.model.study.entity.SubjectStatus;
+import au.org.theark.core.model.study.entity.TitleType;
+import au.org.theark.core.model.study.entity.VitalStatus;
 import au.org.theark.core.vo.ArkUserVO;
+import au.org.theark.core.vo.SubjectVO;
 
 public interface IArkCommonService {
 	
@@ -12,5 +21,24 @@ public interface IArkCommonService {
 	public ArkUserVO getUser(String name) throws ArkSystemException ;
 	
 	public List<String> getUserRole(String userName) throws ArkSystemException;
+	
+	
+	public List<Study> getStudy(Study study);
+	
+	public List<StudyStatus> getListOfStudyStatus();
+	
+	public Study getStudy(Long id);
+	
+	public Collection<SubjectVO> getSubject(SubjectVO subjectVO);
+	
+	public Collection<SubjectStatus> getSubjectStatus();
+	
+	public Collection<TitleType> getTitleType();
+	
+	public Collection<VitalStatus> getVitalStatus();
+	
+	public Collection<GenderType> getGenderType();
+	
+	public List<PhoneType> getListOfPhoneType();
 	
 }
