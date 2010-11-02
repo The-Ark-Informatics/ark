fcd780b28256cead718eac2e3cc4acf24bafe76d
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-02 07:10:56 +0000

    initial revision - implemented the DAO access for common study/subject related look up interfaces.
    git-svn-id: svn://localhost@1796 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/dao/IStudyDao.java
@@ -0,0 +1,105 @@
+package au.org.theark.core.dao;
+
+import java.util.Collection;
+import java.util.List;
+
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.exception.StatusNotAvailableException;
+import au.org.theark.core.model.study.entity.GenderType;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+import au.org.theark.core.model.study.entity.PhoneType;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.model.study.entity.StudyStatus;
+import au.org.theark.core.model.study.entity.SubjectStatus;
+import au.org.theark.core.model.study.entity.TitleType;
+import au.org.theark.core.model.study.entity.VitalStatus;
+import au.org.theark.core.vo.SubjectVO;
+
+/**
+ * @author nivedann
+ *
+ */
+public interface IStudyDao {
+	
+	/**
+	 * Looks up a Study based on the filter supplied as part of the Study. 
+	 * @param study
+	 * @return
+	 */
+	public List<Study> getStudy(Study study);
+	
+	/**
+	 * Interface to get a list of Study Status reference data from the backend.
+	 * These study status' are no associated with a study as such but can be used for
+	 * displaying a list of options for a particular study.
+	 * @return
+	 */
+	public List<StudyStatus> getListOfStudyStatus();
+	
+	/**
+	 * Fetch a Study entity by providing the primary key of the study.
+	 * @param id
+	 * @return
+	 */
+	public Study getStudy(Long id);
+	
+	/**
+	 * An interace to return a StudyStatus entity by providing the status name.
+	 * @param statusName
+	 * @return
+	 * @throws StatusNotAvailableException
+	 */
+	
+	public StudyStatus getStudyStatus(String statusName) throws StatusNotAvailableException;
+	
+	/**
+	 * A look up that returns a list of All Phone Types. Mobile, Land etc
+	 * In the event that there is a database/runtime error it is wrapped into a ArkSystemException and returned
+	 * @return List<PhoneType>
+	 */
+	public List<PhoneType> getListOfPhoneType();
+	
+	/**
+	 * An interface fetch a list of Title types. 
+	 * @return
+	 */
+	public Collection<TitleType> getTitleType();
+	
+	/**
+	 * An interace to fetch a list of Vital Status reference types.
+	 * @return
+	 */
+	public Collection<VitalStatus> getVitalStatus();
+	
+	/**
+	 * An interface to fetch a list of GenderType references.
+	 * @return
+	 */
+	public Collection<GenderType> getGenderType();
+	
+	/**
+	 * An interface to return a list of Subject Status types
+	 * @return
+	 */
+	public Collection<SubjectStatus> getSubjectStatus();
+	
+	/**
+	 * A look-up method that searches based on the Filter provided in the incoming 
+	 * SubjectVO value object.
+	 * @param subjectVO
+	 * @return
+	 */
+	public Collection<SubjectVO> getSubject(SubjectVO subjectVO);
+	
+	/**
+	 * Look up a list of subjects linked to the current study by passing in a primary key of the
+	 * LinkSubjectStudy. 
+	 * @param id
+	 * @return LinkSubjectStudy
+	 * @throws EntityNotFoundException
+	 */
+	public LinkSubjectStudy getLinkSubjectStudy(Long linkSubjectStudyId) throws EntityNotFoundException;
+	
+	
+
+}
---
+++ ark-common/src/main/java/au/org/theark/core/dao/StudyDao.java
@@ -0,0 +1,269 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.dao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import au.org.theark.core.Constants;
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.exception.StatusNotAvailableException;
+import au.org.theark.core.model.study.entity.GenderType;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+import au.org.theark.core.model.study.entity.Person;
+import au.org.theark.core.model.study.entity.PhoneType;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.model.study.entity.StudyStatus;
+import au.org.theark.core.model.study.entity.SubjectStatus;
+import au.org.theark.core.model.study.entity.TitleType;
+import au.org.theark.core.model.study.entity.VitalStatus;
+import au.org.theark.core.vo.SubjectVO;
+
+/**
+ * @author nivedann
+ *
+ */
+@Repository("commonStudyDao")
+public class StudyDao  extends HibernateSessionDao implements IStudyDao{
+
+	private static Logger log = LoggerFactory.getLogger(StudyDao.class);
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.dao.IStudyDao#getStudy(au.org.theark.core.model.study.entity.Study)
+	 */
+	public List<Study> getStudy(Study study)
+	{
+		
+		
+		Criteria studyCriteria =  getSession().createCriteria(Study.class);
+		
+		if(study.getStudyKey() != null){
+			studyCriteria.add(Restrictions.eq(Constants.STUDY_KEY,study.getStudyKey()));	
+		}
+		
+		if(study.getName() != null){
+			studyCriteria.add(Restrictions.ilike(Constants.STUDY_NAME, study.getName(), MatchMode.ANYWHERE));	
+		}
+		
+		if(study.getDateOfApplication() != null){
+			studyCriteria.add(Restrictions.eq(Constants.DATE_OF_APPLICATION, study.getDateOfApplication()));
+		}
+		
+		if(study.getEstimatedYearOfCompletion() != null){
+			studyCriteria.add(Restrictions.eq(Constants.EST_YEAR_OF_COMPLETION, study.getEstimatedYearOfCompletion()));
+		}
+
+		if(study.getChiefInvestigator() != null){
+			studyCriteria.add(Restrictions.ilike(Constants.CHIEF_INVESTIGATOR, study.getChiefInvestigator(),MatchMode.ANYWHERE));
+		}
+
+		if(study.getContactPerson() != null){
+			studyCriteria.add(Restrictions.ilike(Constants.CONTACT_PERSON, study.getContactPerson(), MatchMode.ANYWHERE));
+		}
+		
+		if(study.getStudyStatus() != null){
+			studyCriteria.add(Restrictions.eq(Constants.STUDY_STATUS, study.getStudyStatus()));
+			try{
+				StudyStatus status  = getStudyStatus("Archive");
+				studyCriteria.add(Restrictions.ne(Constants.STUDY_STATUS, status));
+			}catch(StatusNotAvailableException notAvailable){
+				log.error("Cannot look up and filter on archive status.Reference data could be missing");
+			}
+		}else{
+			try{
+				StudyStatus status  = getStudyStatus("Archive");
+				studyCriteria.add(Restrictions.ne(Constants.STUDY_STATUS, status));
+			}catch(StatusNotAvailableException notAvailable){
+				log.error("Cannot look up and filter on archive status.Reference data could be missing");
+			}
+
+		}
+		
+		
+
+		studyCriteria.addOrder(Order.asc(Constants.STUDY_NAME));
+		List<Study> studyList  = studyCriteria.list();
+		
+		return studyList;
+	}
+	
+	/**
+	 * Given a status name will return the StudyStatus object.
+	 */
+	public StudyStatus getStudyStatus(String statusName) throws StatusNotAvailableException{
+		StudyStatus studyStatus = new StudyStatus();
+		studyStatus.setName("Archive");
+		Example studyStatusExample = Example.create(studyStatus);
+		
+		Criteria studyStatusCriteria = getSession().createCriteria(StudyStatus.class).add(studyStatusExample);
+		if(studyStatusCriteria != null && studyStatusCriteria.list() != null && studyStatusCriteria.list().size() > 0){
+			return (StudyStatus)studyStatusCriteria.list().get(0);	
+		}else{
+			log.error("Study Status Table maybe out of synch. Please check if it has an entry for Archive status");
+			System.out.println("Cannot locate a study status with " + statusName + " in the database");
+			throw new StatusNotAvailableException();
+		}
+		
+	}
+	
+
+	public List<StudyStatus> getListOfStudyStatus() {
+
+		Example studyStatus = Example.create(new StudyStatus());
+		Criteria studyStatusCriteria = getSession().createCriteria(StudyStatus.class).add(studyStatus);
+		return   studyStatusCriteria.list();
+		
+	}
+	
+	public Study getStudy(Long id){
+		Study study =  (Study)getSession().get(Study.class, id);
+		return study;
+	}
+	
+	public Collection<TitleType> getTitleType(){
+		Example example = Example.create(new TitleType());
+		Criteria criteria = getSession().createCriteria(TitleType.class).add(example);
+		return criteria.list();
+	}
+	public Collection<VitalStatus> getVitalStatus(){
+		Example example = Example.create(new VitalStatus());
+		Criteria criteria = getSession().createCriteria(VitalStatus.class).add(example);
+		return criteria.list();
+	}
+	
+	public Collection<GenderType> getGenderType(){
+		Example example = Example.create(new GenderType());
+		Criteria criteria = getSession().createCriteria(GenderType.class).add(example);
+		return criteria.list();
+	}
+	
+	public List<PhoneType> getListOfPhoneType() {
+		Example phoneTypeExample = Example.create(new PhoneType());
+		Criteria criteria = getSession().createCriteria(PhoneType.class).add(phoneTypeExample);
+		return   criteria.list();
+	}
+
+
+	public Collection<SubjectStatus> getSubjectStatus(){
+		
+		Example example = Example.create(new SubjectStatus());
+		Criteria criteria = getSession().createCriteria(SubjectStatus.class).add(example);
+		return criteria.list();
+	
+	}
+	
+	/**
+	 * Look up the Link Subject Study for subjects linked to a study
+	 * @param subjectVO
+	 * @return
+	 */
+	public Collection<SubjectVO> getSubject(SubjectVO subjectVO){
+
+		
+		StringBuffer hqlString =	new StringBuffer();
+		hqlString.append(" select linkSubStudy.person,linkSubStudy.subjectStatus, linkSubStudy.linkSubjectStudyKey, linkSubStudy.study");
+		hqlString.append(" from LinkSubjectStudy as linkSubStudy ");
+		hqlString.append(" where linkSubStudy.study.studyKey = ");
+		hqlString.append( subjectVO.getStudy().getStudyKey());
+		
+		//TODO This should be the Subject UID or ID, this field will need to be in LinkSubjectStudy table
+		if(subjectVO.getPerson().getPersonKey() != null){
+			hqlString.append(" and linkSubStudy.person.personKey = ");
+			hqlString.append( subjectVO.getPerson().getPersonKey());
+		}
+		
+		if(subjectVO.getPerson().getFirstName() != null){
+			hqlString.append(" and linkSubStudy.person.firstName = ");
+			hqlString.append("\'");
+			hqlString.append(subjectVO.getPerson().getFirstName().trim());
+			hqlString.append("\'");
+		}
+		
+		if(subjectVO.getPerson().getMiddleName() != null){
+			hqlString.append(" and linkSubStudy.person.middleName = ");
+			hqlString.append("\'");
+			hqlString.append(subjectVO.getPerson().getMiddleName().trim());
+			hqlString.append("\'");
+		}
+							
+		if(subjectVO.getPerson().getLastName() != null){
+			hqlString.append(" and linkSubStudy.person.lastName = ");
+			hqlString.append("\'");
+			hqlString.append(subjectVO.getPerson().getLastName().trim());
+			hqlString.append("\'");
+		}
+		
+		if(subjectVO.getPerson().getGenderType() != null){
+			hqlString.append(" and linkSubStudy.person.genderType.id = ");
+			hqlString.append(subjectVO.getPerson().getGenderType().getId());
+		}
+		
+		if(subjectVO.getPerson().getVitalStatus() != null){
+			hqlString.append(" and linkSubStudy.person.vitalStatus.id = ");
+			hqlString.append(subjectVO.getPerson().getVitalStatus().getId());
+		}
+		
+		if(subjectVO.getSubjectStatus() != null){
+			hqlString.append(" and linkSubStudy.subjectStatus.subjectStatusKey = ");
+			hqlString.append(subjectVO.getSubjectStatus().getSubjectStatusKey());
+		}
+		
+		Query query = getSession().createQuery(hqlString.toString());
+		List<Object[]> list  = query.list();
+		
+		Collection<SubjectVO> subjectList = new ArrayList<SubjectVO>();
+		if(list.size() > 0){
+			log.info("Number of rows fetched " + list.size());
+			
+			for (Object[] objects : list) {
+				if(objects.length > 0 && objects.length == 4){
+					
+					SubjectVO subject = new SubjectVO();
+					Person person =(Person) objects[0];
+					subject.setPerson(person);
+					
+					SubjectStatus status = (SubjectStatus) objects[1];
+					subject.setSubjectStatus(status);
+					
+					subject.setLinkSubjectStudyId((Long)objects[2]);
+					subjectList.add(subject);
+					
+					subject.setStudy((Study)objects[3]);
+				}
+				
+			}
+			log.info("Size : " + subjectList.size());
+		}
+
+		return subjectList;
+	}
+	
+	public LinkSubjectStudy getLinkSubjectStudy(Long id) throws EntityNotFoundException{
+		
+		Criteria linkSubjectStudyCriteria =  getSession().createCriteria(LinkSubjectStudy.class);
+		linkSubjectStudyCriteria.add(Restrictions.eq("linkSubjectStudyKey",id));
+		List<LinkSubjectStudy> listOfSubjects = linkSubjectStudyCriteria.list();
+		if(listOfSubjects != null && listOfSubjects.size() > 0){
+			return listOfSubjects.get(0);
+		}else{
+			throw new EntityNotFoundException("The entity with id" + id.toString() +" cannot be found.");
+		}
+	}
+	
+
+
+}
