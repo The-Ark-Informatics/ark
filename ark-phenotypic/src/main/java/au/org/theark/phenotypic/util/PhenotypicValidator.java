9f871185624a009a67f7baf22c55cbf762a86bfb
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-03 09:17:44 +0000

    Amdended validation message logging Added new method validateFieldData that calls other validation methods by default
    git-svn-id: svn://localhost@1836 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/util/PhenotypicValidator.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/util/PhenotypicValidator.java
@@ -1,5 +1,12 @@
 package au.org.theark.phenotypic.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+import au.org.theark.core.Constants;
 import au.org.theark.phenotypic.model.dao.IPhenotypicDao;
 import au.org.theark.phenotypic.model.entity.Collection;
 import au.org.theark.phenotypic.model.entity.Field;
@@ -25,27 +32,161 @@ public class PhenotypicValidator
 	{
 	}
 	
-	public boolean isValidFieldData(FieldData fieldData)
+	/**
+	 * Returns true of the fiald data value is a valid format, either NUMBER, CHARACTER or DATE as specified in the data dictionary
+	 * @param fieldData
+	 * @return boolean
+	 */
+	public static boolean isValidFieldData(FieldData fieldData, java.util.Collection<String> errorMessages)
 	{
 		Field field = (Field)fieldData.getField();
-		log.info("Validating field data:" + field.getName().toString() + "\t" + fieldData.getValue().toString());
+		//errorMessages.add("Validating field data: " + field.getName().toString() + "\t" + fieldData.getValue().toString());
 		
-		if(field.getfieldType().getName().equalsIgnoreCase("NUMBER")){
-			log.info("Field is a NUMBER");
-			log.info("Field Min value: [" + field.getMinValue().toString() + "]");
-			log.info("Field Max value: [" + field.getMaxValue().toString() + "]");
-			
-			Float floatMinValue = Float.parseFloat(field.getMinValue());
-			Float floatMaxValue = Float.parseFloat(field.getMaxValue());
-			Float floatFieldValue = Float.parseFloat(fieldData.getValue());
-			
-			if(floatFieldValue > floatMaxValue){
-				log.error("Field data greater than defined max range:" + fieldData.getField().getMaxValue().toString() + "\t" + fieldData.getValue().toString());
+		// Number field type
+		if(field.getFieldType().getName().equalsIgnoreCase(Constants.FIELD_TYPE_NUMBER)){
+			try{
+				Float floatFieldValue = Float.parseFloat(fieldData.getValue());
+				return true;
 			}
-			if(floatFieldValue < floatMinValue){
-				log.error("Field data less than defined min range:" + fieldData.getField().getMinValue().toString() + "\t" + fieldData.getValue().toString());
+			catch(NumberFormatException nfe){
+				errorMessages.add(PhenotypicValidationMessage.fieldDataNotDefinedType(field, fieldData));
+				log.error("Field data number format exception " + nfe.getMessage());
+				return false;
+			}
+			catch(NullPointerException npe){
+				log.error("Field data null pointer exception " + npe.getMessage());
+				return false;
 			}
 		}
-		return true;
+		
+		// Character field type
+		if(field.getFieldType().getName().equalsIgnoreCase(Constants.FIELD_TYPE_CHARACTER)){
+			try{
+				String stringFieldValue = fieldData.getValue();
+				return true;
+			}
+			catch(NullPointerException npe){
+				log.error("Field data null pointer exception " + npe.getMessage());
+				return false;
+			}
+		}
+		
+		// Date field type
+		if(field.getFieldType().getName().equalsIgnoreCase(Constants.FIELD_TYPE_DATE)){
+			try{
+				Date dateFieldValue = new Date();
+				DateFormat dateFormat = new SimpleDateFormat(au.org.theark.core.Constants.DATE_FORMAT);
+				dateFieldValue = dateFormat.parse(fieldData.getValue());
+				return true;
+			}
+			catch (ParseException pe)
+			{
+				errorMessages.add(PhenotypicValidationMessage.fieldDataNotDefinedType(field, fieldData));
+				log.error("Field data date parse exception " + pe.getMessage());
+				return false;
+			}
+			catch(NullPointerException npe){
+				log.error("Field data null pointer exception " + npe.getMessage());
+				return false;
+			}
+			
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Returns true if field data value is within the defined range as specified in the data dictionary
+	 * @param fieldData
+	 * @return boolean
+	 */
+	public static boolean isInValidRange(FieldData fieldData, java.util.Collection<String> errorMessages){
+		Field field = (Field)fieldData.getField();
+		
+		if(field.getFieldType().getName().equalsIgnoreCase(Constants.FIELD_TYPE_NUMBER)){
+			try{
+				Float floatMinValue = Float.parseFloat(field.getMinValue());
+				Float floatMaxValue = Float.parseFloat(field.getMaxValue());
+				Float floatFieldValue = Float.parseFloat(fieldData.getValue());
+				
+				if((floatFieldValue > floatMaxValue) || (floatFieldValue < floatMinValue)){
+					if((floatFieldValue > floatMaxValue)){
+						errorMessages.add(PhenotypicValidationMessage.fieldDataGreaterThanMaxValue(field, fieldData));
+					}
+					if((floatFieldValue < floatMinValue)){
+						errorMessages.add(PhenotypicValidationMessage.fieldDataLessThanMaxValue(field, fieldData));
+					}
+					return false;
+				}
+				return true;
+			}
+			catch(NumberFormatException nfe){
+				log.error("Field data number format exception " + nfe.getMessage());
+				return false;
+			}
+			catch(NullPointerException npe){
+				log.error("Field data null pointer exception " + npe.getMessage());
+				return false;
+			}
+			
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns true if the field data value is within the discrete range as defined in the data dictionary
+	 * @param fieldData
+	 * @return boolean
+	 */
+	public static boolean isInDiscreteValues(FieldData fieldData, java.util.Collection<String> errorMessages){
+		boolean inDiscreteValues = false;
+		
+		Field field = (Field)fieldData.getField();
+		
+		// Validate if discrete values is defined
+		if(field.getDiscreteValues() != null){
+			try{
+				StringTokenizer stringTokenizer = new StringTokenizer(field.getDiscreteValues(), Constants.DISCRETE_RANGE_TOKEN); 
+				
+				// Iterate through all discrete defined values and compare to field data value
+				while(stringTokenizer.hasMoreTokens()) {
+					String stringToken = stringTokenizer.nextToken();
+					if(stringToken.equalsIgnoreCase(fieldData.getValue())){
+						inDiscreteValues = true;
+						break;
+					}
+					else{
+						inDiscreteValues = false;
+					}
+				} 
+				
+				if(!inDiscreteValues){
+					errorMessages.add(PhenotypicValidationMessage.fieldDataNotInDiscreteValues(field, fieldData));
+				}
+				
+			}
+			catch(NullPointerException npe){
+				log.error("Field data null format exception " + npe.getMessage());
+				inDiscreteValues = false;
+			}
+			
+		}
+		return inDiscreteValues;
+	}
+	
+	public static void validateFieldData(FieldData fieldData, java.util.Collection<String> errorMessages){
+		// Validate the field data
+		if(isValidFieldData(fieldData,errorMessages)){
+			//log.info("Field data valid");
+		}
+		
+		if(isInDiscreteValues(fieldData,errorMessages)){
+			//log.info("Field data in discrete values");
+		}
+		
+		// Validate the field data within discrete values
+		if(isInValidRange(fieldData,errorMessages)){
+			//log.info("Field data in valid range");
+		}
 	}
 }
