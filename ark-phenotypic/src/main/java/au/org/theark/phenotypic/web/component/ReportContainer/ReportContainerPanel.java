7ae6389b8a589922cd7d2d82c8cc57f9d764f6f7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-04 07:56:14 +0000

    Added new sub-menu basic skeletons: - Data Import - Reports
    git-svn-id: svn://localhost@1856 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/ReportContainer/ReportContainerPanel.java
@@ -0,0 +1,50 @@
+package au.org.theark.phenotypic.web.component.ReportContainer;
+
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.phenotypic.service.IPhenotypicService;
+
+@SuppressWarnings( { "unchecked", "serial", "unused" })
+public class ReportContainerPanel extends Panel
+{
+	@SpringBean(name = "phenotypicService")
+	private IPhenotypicService	serviceInterface;
+
+	private static final long	serialVersionUID	= 1L;
+	private transient Logger	log					= LoggerFactory.getLogger(ReportContainerPanel.class);
+
+	public ReportContainerPanel(String id)
+	{
+		super(id);
+		log.info("ReportContainerPanel Constructor invoked.");
+		Form reportForm = new Form("reportForm");
+		reportForm.add(new Button(au.org.theark.phenotypic.web.Constants.NEW_BUTTON, new StringResourceModel("page.reportButton1", this, null))
+		{
+			public void onSubmit()
+			{
+				log.info("Report button 1 test");
+			}
+		});
+		reportForm.add(new Button(au.org.theark.phenotypic.web.Constants.SAVE_BUTTON, new StringResourceModel("page.reportButton2", this, null))
+		{
+			public void onSubmit()
+			{
+				log.info("Report button 2 test");
+			}
+		});
+		reportForm.add(new Button(au.org.theark.phenotypic.web.Constants.EDIT_BUTTON, new StringResourceModel("page.reportButton3", this, null))
+		{
+			public void onSubmit()
+			{
+				log.info("Report button 3 test");
+			}
+		});
+		add(reportForm);
+	}
+}
---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenotypicImport/PhenotypicImportContainer.java
@@ -0,0 +1,57 @@
+package au.org.theark.phenotypic.web.component.phenotypicImport;
+
+import java.util.Date;
+
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.phenotypic.model.entity.Collection;
+import au.org.theark.phenotypic.model.entity.Field;
+import au.org.theark.phenotypic.model.entity.FieldType;
+import au.org.theark.phenotypic.service.IPhenotypicService;
+
+@SuppressWarnings( { "unchecked", "serial" ,"unused"})
+public class PhenotypicImportContainer extends Panel
+{
+
+	@SpringBean(name = "phenotypicService")
+	private IPhenotypicService	serviceInterface;
+
+	private static final long	serialVersionUID	= 1L;
+	private transient Logger	log					= LoggerFactory.getLogger(PhenotypicImportContainer.class);
+
+	public PhenotypicImportContainer(String id)
+	{
+		super(id);
+		log.info("PhenotypicImportContainer Constructor invoked.");
+		Form phenotypicImportForm = new Form("phenotypicImportForm");
+		
+		phenotypicImportForm.add(new Button(au.org.theark.phenotypic.web.Constants.VALIDATE_PHENOTYPIC_DATA_FILE, new StringResourceModel("page.validatePhenotypicDataFile", this, null))
+		{
+			public void onSubmit()
+			{
+				log.info("Validate Phenotypic Data File");
+				
+				java.util.Collection<String> validationMessages = null;
+				//TODO Add placeholder to store the validation messages 
+				validationMessages = serviceInterface.validatePhenotypicDataFile();
+			}
+		});
+		
+		phenotypicImportForm.add(new Button(au.org.theark.phenotypic.web.Constants.IMPORT_PHENOTYPIC_DATA_FILE, new StringResourceModel("page.importPhenotypicDataFile", this, null))
+		{
+			public void onSubmit()
+			{
+				log.info("Import Phenotypic Data File");
+				serviceInterface.importPhenotypicDataFile();
+			}
+		});
+
+		add(phenotypicImportForm);
+	}
+}
