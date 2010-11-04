477e24a8ed42e697d3025d857c6f95016196a7aa
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-04 07:57:04 +0000

    Updated Constants to include Field constants
    git-svn-id: svn://localhost@1858 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/Constants.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/Constants.java
@@ -10,25 +10,36 @@ public class Constants {
 	// Sub tabs/menus
 	public static final String PHENOTYPIC_SUBMENU = "phenotypicSubMenus";
 	public static final String COLLECTION_SUBMENU = "Collections";
+	public static final String FIELD_SUBMENU = "Fields";
+	public static final String PHENOTYPIC_IMPORT_SUBMENU = "Data Import";
+	public static final String REPORT_SUBMENU = "Reports";
+	
+	// Resource keys
 	public static final String COLLECTION_RESOURCEKEY = "tab.module.phenotypic.collection";
-	public static final String TEST_SUBMENU = "Test";
-	public static final String TEST_RESOURCEKEY = "tab.module.phenotypic.test";
+	public static final String FIELD_RESOURCEKEY = "tab.module.phenotypic.field";
+	public static final String PHENOTYPIC_IMPORT_RESOURCEKEY = "tab.module.phenotypic.phenotypicImport";
+	public static final String REPORT_RESOURCEKEY = "tab.module.phenotypic.report";
 	
-	// Buttons
-	public static final String NEW_COLLECTION = "newCollection";
-	public static final String NEW_FIELD = "newField";
-	public static final String VALIDATE_PHENOTYPIC_DATA_FILE = "validatePhenotypicDataFile";
-	public static final String IMPORT_PHENOTYPIC_DATA_FILE = "importPhenotypicDataFile";
-	
+	// Generic Buttons
 	public static final String NEW_BUTTON = "newButton";
 	public static final String SAVE_BUTTON = "saveButton";
 	public static final String EDIT_BUTTON = "editButton";
 	
-	// Test stubs
+	// Import buttons
+	public static final String VALIDATE_PHENOTYPIC_DATA_FILE = "validatePhenotypicDataFile";
+	public static final String IMPORT_PHENOTYPIC_DATA_FILE = "importPhenotypicDataFile";
+	
+	// Test
+	public static final String TEST_SUBMENU = "Test";
+	public static final String TEST_RESOURCEKEY = "tab.module.phenotypic.test";
 	public static final String TEST = "Test";
 	public static final String FIRETEST = "fireInTheHole";
 	public static final String WATERTEST = "theGreatFlood";
 	public static final String NOAHTEST = "noahsRescue";
 
 	public static final String	PHENOTYPIC_GRID_VIEW	= "phenotypicGridView";
+	
+	// Common Search fields
+	public static final String FIELD_ID = "field.id";
+	public static final String FIELD_NAME = "field.name";
 }
