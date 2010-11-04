b10b79cdd99943f0acd07d6c15e3e8ab062b6cf6
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-04 04:32:18 +0000

    initial revision
    git-svn-id: svn://localhost@1844 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/form/AbstractDetailForm.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.form;
+
+import org.apache.wicket.markup.html.form.Form;
+
+/**
+ * @author nivedann
+ * @param <T>
+ *
+ */
+public abstract class AbstractDetailForm<T> extends Form<T>{
+
+	/**
+	 * @param id
+	 */
+	public AbstractDetailForm(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
