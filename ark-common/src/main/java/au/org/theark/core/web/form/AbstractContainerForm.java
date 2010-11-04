00a07c47e42742260f98c30c980072ea8c07e15c
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-04 04:34:12 +0000

    initial revision
    git-svn-id: svn://localhost@1847 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/web/form/AbstractContainerForm.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.web.form;
+
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+/**
+ * @author nivedann
+ * @param <T>
+ *
+ */
+public abstract class AbstractContainerForm<T>  extends Form<T>{
+
+	/**
+	 * @param id
+	 */
+	public AbstractContainerForm(String id, CompoundPropertyModel<T> cpmModel) {
+		super(id,cpmModel);
+	}
+
+}
