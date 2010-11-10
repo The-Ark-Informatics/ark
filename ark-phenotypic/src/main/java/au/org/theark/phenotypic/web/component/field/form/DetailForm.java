eeef8b666b1c002cd670acc1bbca292b0654bdeb
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-10 01:30:21 +0000

    Implemented DetailForm with minimal field attributes (name, desc, field type, units and minValue)
    git-svn-id: svn://localhost@1883 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/form/DetailForm.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/form/DetailForm.java
@@ -16,6 +16,7 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -29,124 +30,156 @@ import au.org.theark.phenotypic.web.component.field.Detail;
 
 /**
  * @author nivedann
- *
+ * 
  */
-@SuppressWarnings({ "serial", "unchecked", "unused" })
-public class DetailForm extends Form{
+@SuppressWarnings( { "serial", "unchecked", "unused" })
+public class DetailForm extends Form<FieldVO>
+{
 
-	@SpringBean( name = Constants.PHENOTYPIC_SERVICE)
-	private IPhenotypicService phenotypicService;
-	
+	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
+	private IPhenotypicService				phenotypicService;
+
+	private WebMarkupContainer				resultListContainer;
+	private WebMarkupContainer				detailPanelContainer;
+	private WebMarkupContainer				detailFormContainer;
+
+	private ContainerForm					fieldContainerForm;
+
+	private int									mode;
+
+	private TextField<String>				fieldIdTxtFld;
+	private TextField<String>				fieldNameTxtFld;
+	private DropDownChoice<FieldType>	fieldTypeDdc;
+	private TextArea<String>				fieldDescriptionTxtAreaFld;
+	private TextField<String>				fieldUnitsTxtFld;
+	private TextField<String>				fieldMinValueTxtFld;
+	private TextField<String>				fieldMaxValueTxtFld;
+
+	private AjaxButton						deleteButton;
+	private AjaxButton						saveButton;
+	private AjaxButton						cancelButton;
+
+	/**
+	 * Default constructor
+	 * 
+	 * @param id
+	 */
 	public DetailForm(String id)
 	{
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
-	private WebMarkupContainer resultListContainer;
-	private WebMarkupContainer detailPanelContainer;
-	
-	private ContainerForm fieldContainerForm;
-	
-	private TextField<String> fieldIdTxtFld;
-	private TextField<String> fieldNameTxtFld;
-	private TextField<String> fieldDescriptionTxtFld;
-	private TextField<String> fieldUnitsTxtFld;
-	
-	private DatePicker<Date> dateOfBirth;
-	
-	//Reference Data 
-	private DropDownChoice<FieldType> fieldTypeDdc;
-	
-	private AjaxButton deleteButton;
-	private AjaxButton saveButton;
-	private AjaxButton cancelButton;
-	
+
 	/**
+	 * DetailForm constructor
+	 * 
 	 * @param id
 	 */
-	public DetailForm(	String id,
-						Detail detailPanel, 
-						WebMarkupContainer listContainer,
-						WebMarkupContainer detailsContainer,
-						ContainerForm containerForm) {
+	public DetailForm(String id, Detail detailPanel, WebMarkupContainer listContainer, WebMarkupContainer detailsContainer, ContainerForm containerForm)
+	{
 		super(id);
 		this.fieldContainerForm = containerForm;
 		this.resultListContainer = listContainer;
 		this.detailPanelContainer = detailsContainer;
 		this.fieldContainerForm = containerForm;
-		
-		
-		cancelButton = new AjaxButton(au.org.theark.core.Constants.CANCEL,  new StringResourceModel("cancelKey", this, null))
+
+		cancelButton = new AjaxButton(au.org.theark.core.Constants.CANCEL, new StringResourceModel("cancelKey", this, null))
 		{
 
 			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
 				onCancel(target);
 			}
 		};
-		
+
 		saveButton = new AjaxButton(au.org.theark.core.Constants.SAVE, new StringResourceModel("saveKey", this, null))
 		{
 
-			public void onSubmit(AjaxRequestTarget target, Form<?> form) {
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
 				onSave(fieldContainerForm.getModelObject(), target);
 				target.addComponent(detailPanelContainer);
 			}
-			
-			public void onError(AjaxRequestTarget target, Form<?> form){
+
+			public void onError(AjaxRequestTarget target, Form<?> form)
+			{
 				processErrors(target);
 			}
 		};
-		
+
 	}
-	
-	public void initialiseForm(){
+
+	public void initialiseForm()
+	{
+
+		fieldIdTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELDVO_FIELD_ID);
+		fieldNameTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELDVO_FIELD_NAME);
+		fieldDescriptionTxtAreaFld = new TextArea<String>(au.org.theark.phenotypic.web.Constants.FIELDVO_FIELD_DESCRIPTION);
+		fieldUnitsTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELDVO_FIELD_UNITS);
+		fieldMinValueTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELDVO_FIELD_MIN_VALUE);
 		
-		fieldNameTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELD_NAME);
-		fieldDescriptionTxtFld = new TextField<String>(au.org.theark.phenotypic.web.Constants.FIELD_DESCRIPTION);
 		
-		//Initialise Drop Down Choices 
-		//Title We can also have the reference data populated on Application start and refer to a static list instead of hitting the database
+		// Initialise Drop Down Choices
+		// Title We can also have the reference data populated on Application start and refer to a static list instead of hitting the database
 		Collection<FieldType> fieldTypeList = phenotypicService.getFieldTypes();
-		ChoiceRenderer<FieldType> defaultChoiceRenderer = new ChoiceRenderer<FieldType>(au.org.theark.phenotypic.web.Constants.FIELD_NAME,au.org.theark.phenotypic.web.Constants.FIELD_ID);
-		fieldTypeDdc = new DropDownChoice<FieldType>(au.org.theark.phenotypic.web.Constants.FIELD_TYPE,(List)fieldTypeList,defaultChoiceRenderer);
+		ChoiceRenderer<FieldType> defaultChoiceRenderer = new ChoiceRenderer<FieldType>(	au.org.theark.phenotypic.web.Constants.FIELD_NAME,
+																													au.org.theark.phenotypic.web.Constants.FIELD_ID);
+		fieldTypeDdc = new DropDownChoice<FieldType>(au.org.theark.phenotypic.web.Constants.FIELD_TYPE, (List) fieldTypeList, defaultChoiceRenderer);
 
 		attachValidators();
 		addComponents();
 	}
-	
-	private void attachValidators(){
-		
+
+	private void attachValidators()
+	{
 		fieldNameTxtFld.setRequired(true);
 	}
-	
-	private void addComponents(){
 
+	private void addComponents()
+	{
 		add(fieldIdTxtFld);
+		add(fieldNameTxtFld);
+		add(fieldDescriptionTxtAreaFld);
 		add(fieldTypeDdc);
+		add(fieldUnitsTxtFld);
+		add(fieldMinValueTxtFld);
 		add(saveButton);
 		add(cancelButton.setDefaultFormProcessing(false));
 	}
-	
-	protected void onSave(FieldVO fieldVo, AjaxRequestTarget target){
-		
-	}
-	
-	protected  void onCancel(AjaxRequestTarget target){
-		
-	}
-	
-	protected void processErrors(AjaxRequestTarget target){
-		
+
+	protected void onSave(FieldVO fieldVo, AjaxRequestTarget target)
+	{
+
 	}
 
-	public TextField<String> getFieldIdTxtFld() {
+	protected void onCancel(AjaxRequestTarget target)
+	{
+
+	}
+
+	protected void processErrors(AjaxRequestTarget target)
+	{
+
+	}
+
+	public TextField<String> getFieldIdTxtFld()
+	{
 		return fieldIdTxtFld;
 	}
 
-	public void setfieldIdTxtFld(TextField<String> fieldIdTxtFld) {
+	public void setfieldIdTxtFld(TextField<String> fieldIdTxtFld)
+	{
 		this.fieldIdTxtFld = fieldIdTxtFld;
+	}
+	
+	public TextField<String> getFieldNameTxtFld()
+	{
+		return fieldNameTxtFld;
+	}
+
+	public void setFieldNameTxtFld(TextField<String> fieldNameTxtFld)
+	{
+		this.fieldNameTxtFld = fieldNameTxtFld;
 	}
 
 }
