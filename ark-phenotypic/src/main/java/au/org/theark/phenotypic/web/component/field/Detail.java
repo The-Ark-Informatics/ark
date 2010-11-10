92f39c764d801bd28111c5ab22d2677416fa0b06
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-10 01:29:34 +0000

    Implemented Save of field correctly - Need to implement Exceptions
    git-svn-id: svn://localhost@1882 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Detail.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Detail.java
@@ -5,7 +5,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.exception.ArkSystemException;
@@ -18,32 +17,29 @@ import au.org.theark.phenotypic.service.IPhenotypicService;
 import au.org.theark.phenotypic.web.component.field.form.ContainerForm;
 import au.org.theark.phenotypic.web.component.field.form.DetailForm;
 
-public class Detail extends Panel{
-	
-	private CompoundPropertyModel<FieldVO> cpm;
-	
-	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
-	private IPhenotypicService phenotypicService;
-	
-	@SpringBean(name = au.org.theark.core.Constants.STUDY_SERVICE)
-	private IArkCommonService arkCommonService;
-	
-	private DetailForm detailForm;
-	
-	private FeedbackPanel feedBackPanel;
-	private WebMarkupContainer listContainer;
-	private WebMarkupContainer detailsContainer;
-	private WebMarkupContainer searchPanelContainer;
-	private ContainerForm containerForm;
-	private Study study;
+@SuppressWarnings("serial")
+public class Detail extends Panel
+{
+	//private CompoundPropertyModel<FieldVO>	cpm;
 
-	public Detail(	String id, 
-					final WebMarkupContainer listContainer, 
-					FeedbackPanel feedBackPanel,
-					WebMarkupContainer detailsContainer,
-					WebMarkupContainer searchPanelContainer,
-					ContainerForm containerForm){
-		
+	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
+	private IPhenotypicService					phenotypicService;
+
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService					iArkCommonService;
+
+	private DetailForm							detailForm;
+
+	private FeedbackPanel						feedBackPanel;
+	private WebMarkupContainer					listContainer;
+	private WebMarkupContainer					detailsContainer;
+	private WebMarkupContainer					searchPanelContainer;
+	private ContainerForm						containerForm;
+	private Study									study;
+
+	public Detail(String id, final WebMarkupContainer listContainer, FeedbackPanel feedBackPanel, WebMarkupContainer detailsContainer, WebMarkupContainer searchPanelContainer,
+			ContainerForm containerForm)
+	{
 		super(id);
 		this.feedBackPanel = feedBackPanel;
 		this.listContainer = listContainer;
@@ -51,73 +47,76 @@ public class Detail extends Panel{
 		this.containerForm = containerForm;
 		this.searchPanelContainer = searchPanelContainer;
 	}
-	
-	public void initialisePanel(){
-		
-		detailForm = new DetailForm("detailsForm", this, listContainer, detailsContainer, containerForm){
-			
-			protected void onSave(FieldVO fieldVo, AjaxRequestTarget target){
-				//Do the save Persist the Study component and the attached documents to the backend/upload the files and persist the file payload
-				//Enable Unhide a panel that will display a list of Files that "have been uploaded along with a download option"
+
+	public void initialisePanel()
+	{
+		detailForm = new DetailForm("detailForm", this, listContainer, detailsContainer, containerForm)
+		{
+			protected void onSave(FieldVO fieldVo, AjaxRequestTarget target)
+			{
+				// Save or update the Field
+				// try {
+				Long studyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+				study = iArkCommonService.getStudy(studyId);
 				
-				// TOD IMPLEMENT SAVE messages
-				/*
-				try {
-					Long studyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-					study = arkCommonService.getStudy(studyId);
-					
-					// TODO add study to VO
-					//fieldVo.get.setStudy(study);
-					
-					if(fieldVo.getStudyComponent().getStudyCompKey() == null){
-						
-						phenotypicService.create(fieldVo.getStudyComponent());
-						this.info("Study Component " + fieldVo.getStudyComponent().getName() + " was created successfully" );
-						processFeedback(target);
-					
-					}else{
-					
-						phenotypicService.update(fieldVo.getStudyComponent());
-						this.info("Study Component " + fieldVo.getStudyComponent().getName() + " was updated successfully" );
-						processFeedback(target);
-						
-					}
-					
-				} catch (UnAuthorizedOperation e) {
-					 this.error("You are not authorised to manage study components for the given study " + study.getName());
-					 processFeedback(target);
-				} catch (ArkSystemException e) {
-					this.error("A System error occured, we will have someone contact you.");
+				// Force uppercase field name
+				fieldVo.getField().setName(fieldVo.getField().getName().toUpperCase());
+				
+				// Set Study, field type
+				fieldVo.getField().setStudy(study);
+				fieldVo.getField().setFieldType(fieldVo.getFieldType());
+				
+				if (fieldVo.getField().getId() == null)
+				{
+					phenotypicService.createField(fieldVo.getField());
+					this.info("Field " + fieldVo.getField().getName() + " was created successfully");
 					processFeedback(target);
 				}
-				*/
-				
+				else
+				{
+					phenotypicService.updateField(fieldVo.getField());
+					this.info("Field " + fieldVo.getField().getName() + " was updated successfully");
+					processFeedback(target);
+
+				}
+
+				//TODO Implement Exceptions in PhentoypicService
+				//  } catch (UnAuthorizedOperation e) { this.error("You are not authorised to manage study components for the given study " +
+				//  study.getName()); processFeedback(target); } catch (ArkSystemException e) {
+				//  this.error("A System error occured, we will have someone contact you."); processFeedback(target); }
+				 
+
 			}
-			
-			protected void onCancel(AjaxRequestTarget target){
+
+			protected void onCancel(AjaxRequestTarget target)
+			{
 				FieldVO fieldVo = new FieldVO();
 				containerForm.setModelObject(fieldVo);
 				searchPanelContainer.setVisible(true);
+				detailsContainer.setVisible(false);
+				detailForm.setVisible(false);
 				target.addComponent(searchPanelContainer);
 				target.addComponent(feedBackPanel);
+				target.addComponent(detailsContainer);
 			}
 
-			protected void processFeedback(AjaxRequestTarget target){
+			protected void processFeedback(AjaxRequestTarget target)
+			{
 				target.addComponent(feedBackPanel);
 			}
-			
 		};
-		
+
 		detailForm.initialiseForm();
 		add(detailForm);
 	}
 
-	public DetailForm getDetailsForm() {
+	public DetailForm getDetailForm()
+	{
 		return detailForm;
 	}
 
-	public void setDetailsForm(DetailForm detailsForm) {
+	public void setDetailForm(DetailForm detailsForm)
+	{
 		this.detailForm = detailsForm;
 	}
-
 }
