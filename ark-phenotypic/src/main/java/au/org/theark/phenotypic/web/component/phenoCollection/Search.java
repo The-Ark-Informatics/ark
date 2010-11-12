d34a513525aa14e1ba5e4f61efa80b952f390b9f
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-12 08:58:49 +0000

    Added phenoCollection implementation
    git-svn-id: svn://localhost@1932 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenoCollection/Search.java
@@ -0,0 +1,150 @@
+/**
+ * 
+ */
+package au.org.theark.phenotypic.web.component.phenoCollection;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.phenotypic.model.entity.Collection;
+import au.org.theark.phenotypic.model.entity.Field;
+import au.org.theark.phenotypic.model.vo.CollectionVO;
+import au.org.theark.phenotypic.model.vo.FieldVO;
+import au.org.theark.phenotypic.service.Constants;
+import au.org.theark.phenotypic.service.IPhenotypicService;
+import au.org.theark.phenotypic.web.component.summary.form.ContainerForm;
+import au.org.theark.phenotypic.web.component.summary.form.SearchForm;
+
+/**
+ * @author cellis
+ * 
+ */
+@SuppressWarnings("serial")
+public class Search extends Panel
+{
+	private FeedbackPanel				feedBackPanel;
+	private WebMarkupContainer			searchMarkupContainer;
+	private WebMarkupContainer			listContainer;
+	private WebMarkupContainer			detailContainer;
+	private PageableListView<Collection>	listView;
+	private ContainerForm				containerForm;
+	private Detail							detail;
+	private WebMarkupContainer viewButtonContainer;
+	private WebMarkupContainer editButtonContainer;
+	private WebMarkupContainer detailPanelFormContainer;
+
+	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
+	private IPhenotypicService			phenotypicService;
+
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService			iArkCommonService;
+
+	/* Constructor */
+	public Search(	String id, 
+						FeedbackPanel feedBackPanel, 
+						WebMarkupContainer searchMarkupContainer, 
+						PageableListView<Collection> listView, 
+						WebMarkupContainer resultListContainer,
+						WebMarkupContainer detailPanelContainer, 
+						Detail detail, 
+						ContainerForm containerForm,
+						WebMarkupContainer viewButtonContainer,
+						WebMarkupContainer editButtonContainer,
+						WebMarkupContainer detailPanelFormContainer)
+	{
+		super(id);
+		this.searchMarkupContainer = searchMarkupContainer;
+		this.listView = listView;
+		this.feedBackPanel = feedBackPanel;
+		this.detailContainer = detailPanelContainer;
+		this.detail = detail;
+		this.containerForm = containerForm;
+		this.viewButtonContainer = viewButtonContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.detailPanelFormContainer = detailPanelFormContainer;
+		listContainer = resultListContainer;
+	}
+
+	public void processDetail(AjaxRequestTarget target)
+	{
+		searchMarkupContainer.setVisible(false);
+		detail.getDetailForm().getIdTxtFld().setEnabled(false);
+		detailContainer.setVisible(true);
+		listContainer.setVisible(false);
+		
+		// Button containers
+		// New Field, thus Edit container visible
+		viewButtonContainer.setVisible(false);
+		editButtonContainer.setVisible(true);
+		editButtonContainer.setEnabled(true);
+		
+		// Hide Delete button on New
+		detail.getDetailForm().getDeleteButton().setVisible(false);
+		
+		// Enable all fields to be edited
+		detailPanelFormContainer.setEnabled(true);
+		
+		target.addComponent(detailContainer);
+		target.addComponent(searchMarkupContainer);
+		target.addComponent(listContainer);
+		target.addComponent(viewButtonContainer);
+		target.addComponent(editButtonContainer);
+	}
+
+	public void initialisePanel()
+	{
+		// Get the study id from the session and get the study
+		final Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+
+		SearchForm searchForm = new SearchForm(au.org.theark.core.Constants.SEARCH_FORM, (CompoundPropertyModel<CollectionVO>) containerForm.getModel())
+		{
+			protected void onSearch(AjaxRequestTarget target)
+			{
+				// Refresh the FB panel if there was an old message from previous search result
+				target.addComponent(feedBackPanel);
+
+				// Get a list of all Fields for the Study in context
+				Study study = iArkCommonService.getStudy(sessionStudyId);
+				//Field searchField = containerForm.getModelObject().getField();
+				//searchField.setStudy(study);
+				Collection phenoCollection = containerForm.getModelObject().getCollection();
+
+				java.util.Collection<Collection> phenoCollectionCollection = phenotypicService.searchPhenotypicCollection(phenoCollection);
+
+				if (phenoCollectionCollection != null && phenoCollectionCollection.size() == 0)
+				{
+					this.info("Phenotypic Collections with the specified criteria does not exist in the system.");
+					target.addComponent(feedBackPanel);
+				}
+				containerForm.getModelObject().setPhenoCollectionCollection(phenoCollectionCollection);
+				listView.removeAll();
+				listContainer.setVisible(true);// Make the WebMarkupContainer that houses the search results visible
+				target.addComponent(listContainer);// For ajax this is required so
+			}
+
+			protected void onNew(AjaxRequestTarget target)
+			{
+				// Show the details panel name and description
+				CollectionVO phenoCollectionVo = new CollectionVO();
+				phenoCollectionVo.setMode(au.org.theark.core.Constants.MODE_NEW);
+				
+				// Set study for the new field
+				Long studyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+				Study	study = iArkCommonService.getStudy(studyId);
+				phenoCollectionVo.getField().setStudy(study);
+				
+				containerForm.setModelObject(phenoCollectionVo);
+				processDetail(target);
+			}
+		};
+		add(searchForm);
+	}
+}
---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenoCollection/SearchResultList.java
@@ -0,0 +1,175 @@
+package au.org.theark.phenotypic.web.component.phenoCollection;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+
+import au.org.theark.phenotypic.model.entity.Collection;
+import au.org.theark.phenotypic.model.entity.Field;
+import au.org.theark.phenotypic.model.vo.CollectionVO;
+import au.org.theark.phenotypic.model.vo.FieldVO;
+import au.org.theark.phenotypic.web.component.summary.form.ContainerForm;
+
+@SuppressWarnings( { "serial", "unchecked" })
+public class SearchResultList extends Panel
+{
+
+	private WebMarkupContainer	detailsPanelContainer;
+	private WebMarkupContainer	searchPanelContainer;
+	private WebMarkupContainer	searchResultContainer;
+	private ContainerForm		containerForm;
+	private Detail					detailPanel;
+	private WebMarkupContainer detailPanelFormContainer;
+	private WebMarkupContainer viewButtonContainer;
+	private WebMarkupContainer editButtonContainer;
+
+	public SearchResultList(String id, WebMarkupContainer detailPanelContainer, WebMarkupContainer searchPanelContainer, ContainerForm studyCompContainerForm, WebMarkupContainer searchResultContainer,
+			Detail detail,
+			WebMarkupContainer viewButtonContainer,
+			WebMarkupContainer editButtonContainer,
+			WebMarkupContainer detailPanelFormContainer)
+	{
+		super(id);
+		this.detailsPanelContainer = detailPanelContainer;
+		this.containerForm = studyCompContainerForm;
+		this.searchPanelContainer = searchPanelContainer;
+		this.searchResultContainer = searchResultContainer;
+		this.viewButtonContainer = viewButtonContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.detailPanelFormContainer = detailPanelFormContainer; 
+		this.setDetailPanel(detail);
+	}
+
+	/**
+	 * 
+	 * @param iModel
+	 * @return the pageableListView of PhenoCollection
+	 */
+	public PageableListView<Collection> buildPageableListView(IModel iModel)
+	{
+
+		PageableListView<Collection> sitePageableListView = new PageableListView<Collection>("collectionList", iModel, 10)
+		{
+			@Override
+			protected void populateItem(final ListItem<Collection> item)
+			{
+				Collection phenoCollection = item.getModelObject();
+
+				/* The phenoCollection ID */
+				if (phenoCollection.getId() != null)
+				{
+					// Add the id component here
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_ID, phenoCollection.getId().toString()));
+				}
+				else
+				{
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_ID, ""));
+				}
+
+				// Component Name Link
+				item.add(buildLink(phenoCollection));
+
+				// TODO when displaying text escape any special characters
+				// PhenoCollection status
+				if (phenoCollection.getStatus() != null)
+				{
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_STATUS, phenoCollection.getStatus().getName()));
+				}
+				else
+				{
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_STATUS, ""));// the ID here must match the ones in mark-up
+				}
+
+				// TODO when displaying text escape any special characters
+				// Description
+				if (phenoCollection.getDescription() != null)
+				{
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_DESCRIPTION, phenoCollection.getDescription()));// the ID here must match
+					// the ones in mark-up
+				}
+				else
+				{
+					item.add(new Label(au.org.theark.phenotypic.web.Constants.COLLECTIONVO_COLLECTION_DESCRIPTION, ""));// the ID here must match the ones in mark-up
+				}
+
+				/* For the alternative stripes */
+				item.add(new AttributeModifier("class", true, new AbstractReadOnlyModel()
+				{
+					@Override
+					public String getObject()
+					{
+						return (item.getIndex() % 2 == 1) ? "even" : "odd";
+					}
+				}));
+
+			}
+		};
+		return sitePageableListView;
+	}
+
+	private AjaxLink buildLink(final Collection phenoCollection)
+	{
+		AjaxLink link = new AjaxLink("field.name")
+		{
+			@Override
+			public void onClick(AjaxRequestTarget target)
+			{
+				// Sets the selected object into the model
+				CollectionVO collectionVo = containerForm.getModelObject();
+				collectionVo.setCollection(phenoCollection);
+				
+				detailsPanelContainer.setVisible(true);
+				detailPanelFormContainer.setEnabled(false);
+				searchResultContainer.setVisible(false);
+				searchPanelContainer.setVisible(false);
+
+				detailPanel.getDetailForm().getIdTxtFld().setEnabled(false);
+				
+				// Button containers
+				// View Field, thus view container visible
+				viewButtonContainer.setVisible(true);
+				viewButtonContainer.setEnabled(true);
+				editButtonContainer.setVisible(false);
+				
+				// Have to Edit, before allowing delete
+				detailPanel.getDetailForm().getDeleteButton().setEnabled(false);
+
+				target.addComponent(searchResultContainer);
+				target.addComponent(detailsPanelContainer);
+				target.addComponent(searchPanelContainer);
+				target.addComponent(viewButtonContainer);
+				target.addComponent(editButtonContainer);
+			}
+		};
+
+		// Add the label for the link
+		// TODO when displaying text escape any special characters
+		Label nameLinkLabel = new Label("nameLbl", phenoCollection.getName());
+		link.add(nameLinkLabel);
+		return link;
+	}
+
+	/**
+	 * @param detailPanel
+	 *           the detailPanel to set
+	 */
+	public void setDetailPanel(Detail detailPanel)
+	{
+		this.detailPanel = detailPanel;
+	}
+
+	/**
+	 * @return the detailPanel
+	 */
+	public Detail getDetailPanel()
+	{
+		return detailPanel;
+	}
+}
