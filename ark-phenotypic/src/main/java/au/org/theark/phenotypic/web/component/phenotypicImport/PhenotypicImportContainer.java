1f81f184f3da69d83d64b96411b67dd49a12985f
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-16 04:19:03 +0000

    Refactored to use PhenoCollectionVO / PhenoCollection
    git-svn-id: svn://localhost@1971 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenotypicImport/PhenotypicImportContainer.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenotypicImport/PhenotypicImportContainer.java
@@ -10,7 +10,7 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.org.theark.phenotypic.model.entity.Collection;
+import au.org.theark.phenotypic.model.entity.PhenoCollection;
 import au.org.theark.phenotypic.model.entity.Field;
 import au.org.theark.phenotypic.model.entity.FieldType;
 import au.org.theark.phenotypic.service.IPhenotypicService;
