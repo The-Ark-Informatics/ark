8f9f000c90c3acdc972726c985c95aaf7522643a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-16 03:26:47 +0000

    Uses the SearchForm that extends AbstractSearchForm from core. onNew and onSearch functionality is now pushed into the subclass of AbstractSearchForm and as such the code which was on the panel class is now inside SearchForm.
    git-svn-id: svn://localhost@1953 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/studycomponent/Search.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/studycomponent/Search.java
@@ -1,19 +1,13 @@
 package au.org.theark.study.web.component.studycomponent;
 
-import java.util.List;
-
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.model.study.entity.StudyComp;
 import au.org.theark.study.model.vo.StudyCompVo;
-import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.web.Constants;
 import au.org.theark.study.web.component.studycomponent.form.ContainerForm;
 import au.org.theark.study.web.component.studycomponent.form.SearchForm;
@@ -24,14 +18,10 @@ public class Search extends Panel{
 	private WebMarkupContainer searchMarkupContainer;
 	private WebMarkupContainer listContainer;
 	private WebMarkupContainer detailsContainer;
-	private Details detailPanel;
-	private ContainerForm containerForm;
+	private WebMarkupContainer viewButtonContainer;
+	private WebMarkupContainer editButtonContainer;
+	private WebMarkupContainer detailFormContainer;
 	private PageableListView<StudyComp> listView;
-	
-	@SpringBean( name = Constants.STUDY_SERVICE)
-	private IStudyService studyService;
-	
-
 	
 	
 	/*Constructor*/
@@ -41,6 +31,9 @@ public class Search extends Panel{
 					PageableListView<StudyComp> listView,  
 					WebMarkupContainer resultListContainer, 
 					WebMarkupContainer detailPanelContainer,
+					WebMarkupContainer detailFormContainer,
+					WebMarkupContainer viewButtonContainer,
+					WebMarkupContainer editButtonContainer,	
 					Details detailPanel,
 					ContainerForm studyCompContainerForm) {
 		
@@ -48,10 +41,11 @@ public class Search extends Panel{
 		this.searchMarkupContainer =  searchMarkupContainer;
 		this.listView = listView;
 		this.feedBackPanel = feedBackPanel;
-		listContainer = resultListContainer;
-		detailsContainer = detailPanelContainer;
-		this.detailPanel = detailPanel;
-		containerForm = studyCompContainerForm;
+		this.listContainer = resultListContainer;
+		this.detailsContainer = detailPanelContainer;
+		this.viewButtonContainer = viewButtonContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.detailFormContainer = detailFormContainer;
 	}
 	
 	
@@ -59,55 +53,18 @@ public class Search extends Panel{
 	public void initialisePanel(CompoundPropertyModel<StudyCompVo> studyCompCpm){
 		
 		
-		SearchForm searchStudyCompForm = new SearchForm(Constants.SEARCH_FORM, studyCompCpm){
-			
-			protected  void onSearch(AjaxRequestTarget target){
-				
-				//Refresh the FB panel if there was an old message from previous search result
-				target.addComponent(feedBackPanel);
-				
-				//Get a list of StudyComponents with the given criteria
-				try{
-					
-					List<StudyComp> resultList = studyService.searchStudyComp(containerForm.getModelObject().getStudyComponent());
-					
-					if(resultList != null && resultList.size() == 0){
-						this.info("Study Component with the specified criteria does not exist in the system.");
-						target.addComponent(feedBackPanel);
-					}
-					containerForm.getModelObject().setStudyCompList(resultList);
-					listView.removeAll();
-					listContainer.setVisible(true);//Make the WebMarkupContainer that houses the search results visible
-					target.addComponent(listContainer);//For ajax this is required so 
-				}catch(ArkSystemException arkEx){
-					this.error("A system error has occured. Please try after sometime.");
-				}
-				
-			}
-			
-			protected void onNew(AjaxRequestTarget target){
-				// Show the details panel name and description
-				StudyCompVo studyCompVo = new StudyCompVo();
-				studyCompVo.setMode(Constants.MODE_NEW);
-				containerForm.setModelObject(studyCompVo);
-				processDetail(target);
-			}
-		};
+		SearchForm searchStudyCompForm = new SearchForm(Constants.SEARCH_FORM, 
+														studyCompCpm, 
+														listView,
+														feedBackPanel,
+														listContainer,
+														searchMarkupContainer,
+														detailsContainer,
+														detailFormContainer,
+														viewButtonContainer,
+														editButtonContainer);
 		add(searchStudyCompForm);
-	}
-	
-	public void processDetail(AjaxRequestTarget target){
-		//Hide the Search Panel before the Details Panel is made visible via the detailsContainer
-		searchMarkupContainer.setVisible(false);
-		//Hide the Search Result List 
-		listContainer.setVisible(false);
-		//Un-hide the Details Panel visible 
-		detailsContainer.setVisible(true);
-		detailPanel.getDetailsForm().getComponentIdTxtFld().setEnabled(false);
-		//Attach the containers that need to be re-painted
-		target.addComponent(searchMarkupContainer);
-		target.addComponent(detailsContainer);
-		target.addComponent(listContainer);
+		
 	}
 
 }
