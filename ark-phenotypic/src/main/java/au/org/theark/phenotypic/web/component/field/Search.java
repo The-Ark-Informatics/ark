79bdc86b33bf0a0203318a64ea8b51e438e91a1d
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-16 08:40:10 +0000

    Moved the onNew and onSearch implementation into the SearchForm. Removed redundant code.
    git-svn-id: svn://localhost@1993 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Search.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Search.java
@@ -3,21 +3,14 @@
  */
 package au.org.theark.phenotypic.web.component.field;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import au.org.theark.core.model.study.entity.Study;
-import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.phenotypic.model.entity.Field;
 import au.org.theark.phenotypic.model.vo.FieldVO;
-import au.org.theark.phenotypic.service.Constants;
-import au.org.theark.phenotypic.service.IPhenotypicService;
 import au.org.theark.phenotypic.web.component.field.form.ContainerForm;
 import au.org.theark.phenotypic.web.component.field.form.SearchForm;
 
@@ -39,24 +32,18 @@ public class Search extends Panel
 	private WebMarkupContainer editButtonContainer;
 	private WebMarkupContainer detailPanelFormContainer;
 
-	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
-	private IPhenotypicService			phenotypicService;
-
-	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService			iArkCommonService;
-
 	/* Constructor */
 	public Search(	String id, 
-						FeedbackPanel feedBackPanel, 
-						WebMarkupContainer searchMarkupContainer, 
-						PageableListView<Field> listView, 
-						WebMarkupContainer resultListContainer,
-						WebMarkupContainer detailPanelContainer, 
-						Detail detail, 
-						ContainerForm containerForm,
-						WebMarkupContainer viewButtonContainer,
-						WebMarkupContainer editButtonContainer,
-						WebMarkupContainer detailPanelFormContainer)
+					FeedbackPanel feedBackPanel, 
+					WebMarkupContainer searchMarkupContainer, 
+					PageableListView<Field> listView, 
+					WebMarkupContainer resultListContainer,
+					WebMarkupContainer detailPanelContainer, 
+					Detail detail, 
+					ContainerForm containerForm,
+					WebMarkupContainer viewButtonContainer,
+					WebMarkupContainer editButtonContainer,
+					WebMarkupContainer detailPanelFormContainer)
 	{
 		super(id);
 		this.searchMarkupContainer = searchMarkupContainer;
@@ -71,77 +58,22 @@ public class Search extends Panel
 		listContainer = resultListContainer;
 	}
 
-	public void processDetail(AjaxRequestTarget target)
-	{
-		searchMarkupContainer.setVisible(false);
-		detail.getDetailForm().getFieldIdTxtFld().setEnabled(false);
-		detailContainer.setVisible(true);
-		listContainer.setVisible(false);
-		
-		// Button containers
-		// New Field, thus Edit container visible
-		viewButtonContainer.setVisible(false);
-		editButtonContainer.setVisible(true);
-		editButtonContainer.setEnabled(true);
-		
-		// Hide Delete button on New
-		detail.getDetailForm().getDeleteButton().setVisible(false);
-		
-		// Enable all fields to be edited
-		detailPanelFormContainer.setEnabled(true);
-		
-		target.addComponent(detailContainer);
-		target.addComponent(searchMarkupContainer);
-		target.addComponent(listContainer);
-		target.addComponent(viewButtonContainer);
-		target.addComponent(editButtonContainer);
-	}
 
 	public void initialisePanel()
 	{
-		// Get the study id from the session and get the study
-		final Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 
-		SearchForm searchForm = new SearchForm(au.org.theark.core.Constants.SEARCH_FORM, (CompoundPropertyModel<FieldVO>) containerForm.getModel())
-		{
-			protected void onSearch(AjaxRequestTarget target)
-			{
-				// Refresh the FB panel if there was an old message from previous search result
-				target.addComponent(feedBackPanel);
-
-				// Get a list of all Fields for the Study in context
-				Study study = iArkCommonService.getStudy(sessionStudyId);
-				Field searchField = containerForm.getModelObject().getField();
-				searchField.setStudy(study);
-
-				java.util.Collection<Field> fieldCollection = phenotypicService.searchField(searchField);
-
-				if (fieldCollection != null && fieldCollection.size() == 0)
-				{
-					this.info("Fields with the specified criteria does not exist in the system.");
-					target.addComponent(feedBackPanel);
-				}
-				containerForm.getModelObject().setFieldCollection(fieldCollection);
-				listView.removeAll();
-				listContainer.setVisible(true);// Make the WebMarkupContainer that houses the search results visible
-				target.addComponent(listContainer);// For ajax this is required so
-			}
-
-			protected void onNew(AjaxRequestTarget target)
-			{
-				// Show the details panel name and description
-				FieldVO fieldVo = new FieldVO();
-				fieldVo.setMode(au.org.theark.core.Constants.MODE_NEW);
-				
-				// Set study for the new field
-				Long studyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-				Study	study = iArkCommonService.getStudy(studyId);
-				fieldVo.getField().setStudy(study);
-				
-				containerForm.setModelObject(fieldVo);
-				processDetail(target);
-			}
-		};
+		SearchForm searchForm = new SearchForm(	au.org.theark.core.Constants.SEARCH_FORM, 
+												(CompoundPropertyModel<FieldVO>) containerForm.getModel(),
+												listView,
+												feedBackPanel,
+												listContainer,
+												searchMarkupContainer,
+												detailContainer,
+												detailPanelFormContainer,
+												viewButtonContainer,
+												editButtonContainer
+												);
+		
 		add(searchForm);
 	}
 }
