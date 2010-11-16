aef450298a442a28666a4300c8ee0d975bf27e52
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-16 03:23:07 +0000

    Implemented the onSearch and onNew()'s added isSecure() implementation. The isSecure is not invoked at the moment but will need to do that to secure the buttons.
    git-svn-id: svn://localhost@1950 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/studycomponent/form/SearchForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/studycomponent/form/SearchForm.java
@@ -6,13 +6,28 @@
  */
 package au.org.theark.study.web.component.studycomponent.form;
 
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.model.study.entity.StudyComp;
+import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.web.form.*;
 import au.org.theark.study.model.vo.StudyCompVo;
+import au.org.theark.study.service.IStudyService;
+import au.org.theark.study.web.Constants;
 
 /**
  * @author nivedann
@@ -24,7 +39,11 @@ public class SearchForm extends AbstractSearchForm<StudyCompVo>{
 	private TextField<String> compNameTxtFld;
 	private TextArea<String> descriptionTxtArea;
 	private TextArea<String> keywordTxtArea;
+	private PageableListView<StudyComp> listView;
+	
 
+	@SpringBean( name = Constants.STUDY_SERVICE)
+	private IStudyService studyService;
 	protected void addSearchComponentsToForm(){
 		add(studyCompIdTxtFld);
 		add(compNameTxtFld);
@@ -42,8 +61,29 @@ public class SearchForm extends AbstractSearchForm<StudyCompVo>{
 	 * @param id
 	 * @param cpmModel
 	 */
-	public SearchForm(String id, CompoundPropertyModel<StudyCompVo> cpmModel) {
-		super(id, cpmModel);
+	public SearchForm(	String id, 
+						CompoundPropertyModel<StudyCompVo> cpmModel,
+						PageableListView<StudyComp> listView, 
+						FeedbackPanel feedBackPanel,
+						WebMarkupContainer listContainer,
+						WebMarkupContainer searchMarkupContainer,
+						WebMarkupContainer detailsContainer,
+						WebMarkupContainer detailPanelFormContainer,
+						WebMarkupContainer viewButtonContainer,
+						WebMarkupContainer editButtonContainer	) {
+		
+		//super(id, cpmModel);
+		super	(id,
+				cpmModel,
+				detailsContainer,
+				detailPanelFormContainer,
+				viewButtonContainer,
+				editButtonContainer,
+				searchMarkupContainer,
+				listContainer,
+				feedBackPanel);
+		
+		this.listView = listView;
 		initialiseSearchForm();
 		addSearchComponentsToForm();
 	}
@@ -54,14 +94,53 @@ public class SearchForm extends AbstractSearchForm<StudyCompVo>{
 	@Override
 	protected void onNew(AjaxRequestTarget target) {
 		
+		StudyCompVo studyCompVo = new StudyCompVo();
+		studyCompVo.setMode(Constants.MODE_NEW);
+		setModelObject(studyCompVo);
+		//processDetail(target);
+		preProcessDetailPanel(target);
+		
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see au.org.theark.core.form.AbstractSearchForm#onSearch(org.apache.wicket.ajax.AjaxRequestTarget)
 	 */
 	@Override
 	protected void onSearch(AjaxRequestTarget target) {
 		
+		target.addComponent(feedbackPanel);
+		try{
+			
+			List<StudyComp> resultList = studyService.searchStudyComp(getModelObject().getStudyComponent());
+			
+			if(resultList != null && resultList.size() == 0){
+				this.info("Study Component with the specified criteria does not exist in the system.");
+				target.addComponent(feedbackPanel);
+			}
+			getModelObject().setStudyCompList(resultList);
+			listView.removeAll();
+			listContainer.setVisible(true);//Make the WebMarkupContainer that houses the search results visible
+			target.addComponent(listContainer);//For ajax this is required so 
+		}catch(ArkSystemException arkEx){
+			this.error("A system error has occured. Please try after sometime.");
+		}
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractSearchForm#isSecure()
+	 */
+	//@Override
+	protected boolean isSecure() {
+		SecurityManager securityManager =  ThreadContext.getSecurityManager();
+		Subject currentUser = SecurityUtils.getSubject();		
+		boolean flag = false;
+		if(		securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN) ||
+				securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.STUDY_ADMIN)){
+			flag = true;
+		}
+		//if it is a Super or Study admin then make the new available
+		return flag;
 	}
 
 }
