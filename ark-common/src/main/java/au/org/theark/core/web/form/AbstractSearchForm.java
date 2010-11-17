a96b2631dbc233ffd34730f730ccb78f326710a9
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-17 05:33:33 +0000

    Added common functionality sub-classes can inherit of this Form.
    git-svn-id: svn://localhost@1996 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/form/AbstractSearchForm.java
+++ ark-common/src/main/java/au/org/theark/core/web/form/AbstractSearchForm.java
@@ -1,16 +1,12 @@
-/**
- * 
- * This is a new file
- *
- *
- */
+
 package au.org.theark.core.web.form;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.core.Constants;
@@ -21,6 +17,7 @@ import au.org.theark.core.Constants;
  * An Abstract Form class for Search. This class contains common behaviour that the  sub-classes can inherit. 
  * The sub-classes themselves can override the behaviour of the abstract but can also add more specific implementation if needed. 
  * As part of this class we  have defined the New,Search and Reset button and their behaviour which will be common for all search functions.
+ * </p>
  * @author nivedann
  * @param <T>
  *
@@ -31,6 +28,13 @@ public abstract class AbstractSearchForm<T>  extends Form<T>{
 	protected AjaxButton searchButton;
 	protected AjaxButton newButton;
 	protected Button resetButton;
+	protected WebMarkupContainer viewButtonContainer;
+	protected WebMarkupContainer editButtonContainer;
+	protected WebMarkupContainer detailPanelContainer;
+	protected WebMarkupContainer searchMarkupContainer;
+	protected WebMarkupContainer listContainer;
+	protected WebMarkupContainer detailFormCompContainer;
+	protected FeedbackPanel feedbackPanel;
 	
 	/**
 	 * @param id
@@ -44,10 +48,36 @@ public abstract class AbstractSearchForm<T>  extends Form<T>{
 		
 	}
 	
+	public AbstractSearchForm(	String id, 
+								CompoundPropertyModel<T> cpmModel,
+								WebMarkupContainer detailPanelContainer,
+								WebMarkupContainer detailFormCompContainer,
+								WebMarkupContainer viewButtonContainer,
+								WebMarkupContainer editButtonContainer,
+								WebMarkupContainer searchMarkupContainer,
+								WebMarkupContainer listContainer,
+								FeedbackPanel feedBackPanel
+								) {
+		
+		super(id, cpmModel);
+		this.viewButtonContainer = viewButtonContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.detailPanelContainer = detailPanelContainer;
+		this.searchMarkupContainer = searchMarkupContainer;
+		this.listContainer = listContainer;
+		this.detailFormCompContainer = detailFormCompContainer;
+		this.feedbackPanel = feedBackPanel;
+		initialiseForm();
+		
+	}
+	
 	
 	abstract protected void onSearch(AjaxRequestTarget target);
 	
 	abstract protected void onNew(AjaxRequestTarget target);
+
+	/*This method should be implemented by sub-classes to secure a control(New button etc..) */
+	//abstract protected boolean isSecure();
 	
 	protected void onReset(){
 		clearInput();
@@ -84,7 +114,8 @@ public abstract class AbstractSearchForm<T>  extends Form<T>{
 			}
 		};
 		
-		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+	/* Commented this section of code.Sub-classes should enforce this check until we have a mechanism to disable tabs.
+	 * Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		if(sessionStudyId == null){
 			searchButton.setEnabled(false);
 			newButton.setEnabled(false);
@@ -96,6 +127,7 @@ public abstract class AbstractSearchForm<T>  extends Form<T>{
 			searchButton.setEnabled(true);
 			resetButton.setEnabled(true);
 		}
+	*/
 		
 		addComponentsToForm();
 	}
@@ -104,6 +136,23 @@ public abstract class AbstractSearchForm<T>  extends Form<T>{
 		add(searchButton);
 		add(resetButton.setDefaultFormProcessing(false));
 		add(newButton);
+	}
+	
+	protected void preProcessDetailPanel(AjaxRequestTarget target){
+		
+		detailPanelContainer.setVisible(true);
+		listContainer.setVisible(false);
+		editButtonContainer.setVisible(true);
+		viewButtonContainer.setVisible(false);
+		searchMarkupContainer.setVisible(false);
+		detailFormCompContainer.setEnabled(true);
+		
+		target.addComponent(detailPanelContainer);
+		target.addComponent(listContainer);
+		target.addComponent(searchMarkupContainer);
+		target.addComponent(viewButtonContainer);
+		target.addComponent(editButtonContainer);
+		target.addComponent(detailFormCompContainer);
 	}
 	
 
