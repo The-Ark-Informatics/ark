70be2b1fae76a1cc253737bf590fa44bb5012600
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-17 06:06:57 +0000

    Form related event handling code moved into DetailForm Removed code related to ModalWindow Organised imports.
    git-svn-id: svn://localhost@1999 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Detail.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/field/Detail.java
@@ -1,40 +1,37 @@
 package au.org.theark.phenotypic.web.component.field;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import au.org.theark.phenotypic.model.vo.FieldVO;
-import au.org.theark.phenotypic.service.Constants;
-import au.org.theark.phenotypic.service.IPhenotypicService;
 import au.org.theark.phenotypic.web.component.field.form.ContainerForm;
 import au.org.theark.phenotypic.web.component.field.form.DetailForm;
 
 @SuppressWarnings("serial")
 public class Detail extends Panel
 {
-	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
-	private IPhenotypicService					phenotypicService;
-
 	private DetailForm							detailForm;
 	private FeedbackPanel						feedBackPanel;
 	private WebMarkupContainer					listContainer;
 	private WebMarkupContainer					detailsContainer;
 	private WebMarkupContainer					searchPanelContainer;
-	private ContainerForm						containerForm;
 	private WebMarkupContainer 				detailPanelFormContainer;
 	private WebMarkupContainer 				viewButtonContainer;
 	private WebMarkupContainer 				editButtonContainer;
-	private ModalWindow 					selectModalWindow;
+	
+	private ContainerForm						containerForm;
+	
+	
 
-	public Detail(String id, final WebMarkupContainer listContainer, FeedbackPanel feedBackPanel, WebMarkupContainer detailsContainer, WebMarkupContainer searchPanelContainer,
-			ContainerForm containerForm,
-			WebMarkupContainer viewButtonContainer,
-			WebMarkupContainer editButtonContainer,
-			WebMarkupContainer detailPanelFormContainer)
+	public Detail(	String id, 
+					final WebMarkupContainer listContainer, 
+					FeedbackPanel feedBackPanel,
+					WebMarkupContainer detailsContainer, 
+					WebMarkupContainer searchPanelContainer,
+					ContainerForm containerForm,
+					WebMarkupContainer viewButtonContainer,
+					WebMarkupContainer editButtonContainer,
+					WebMarkupContainer detailPanelFormContainer)
 	{
 		super(id);
 		this.feedBackPanel = feedBackPanel;
@@ -49,147 +46,21 @@ public class Detail extends Panel
 
 	public void initialisePanel()
 	{
-		detailForm = new DetailForm("detailForm", this, listContainer, detailsContainer, containerForm, viewButtonContainer, editButtonContainer, detailPanelFormContainer)
-		{
-			protected void onSave(FieldVO fieldVo, AjaxRequestTarget target)
-			{
-				//TODO Implement try catch for exception handling
-				// try {
-				if (fieldVo.getField().getId() == null)
-				{
-					// Save the Field
-					phenotypicService.createField(fieldVo.getField());
-					this.info("Field " + fieldVo.getField().getName() + " was created successfully");
-					processFeedback(target);
-				}
-				else
-				{
-					// Update the Field
-					phenotypicService.updateField(fieldVo.getField());
-					this.info("Field " + fieldVo.getField().getName() + " was updated successfully");
-					processFeedback(target);
-				}
-				
-				postSaveUpdate(target);
-
-				//TODO Implement Exceptions in PhentoypicService
-				//  } catch (UnAuthorizedOperation e) { this.error("You are not authorised to manage study components for the given study " +
-				//  study.getName()); processFeedback(target); } catch (ArkSystemException e) {
-				//  this.error("A System error occured, we will have someone contact you."); processFeedback(target); }
-			}
-
-			protected void onCancel(AjaxRequestTarget target)
-			{
-				FieldVO fieldVo = new FieldVO();
-				containerForm.setModelObject(fieldVo);
-				searchPanelContainer.setVisible(true);
-				detailsContainer.setVisible(false);
-				target.addComponent(searchPanelContainer);
-				target.addComponent(feedBackPanel);
-				target.addComponent(detailsContainer);
-			}
-			
-			protected void onDelete(FieldVO fieldVo, AjaxRequestTarget target)
-			{
-				selectModalWindow.show(target);
-				target.addComponent(selectModalWindow);
-			}
-			
-			// On click of Edit button, allow form to be editable
-			protected void onEdit(FieldVO fieldVo, AjaxRequestTarget target)
-			{
-				detailPanelFormContainer.setEnabled(true);
-				editButtonContainer.setVisible(true);
-				viewButtonContainer.setVisible(false);
-				detailForm.getDeleteButton().setEnabled(true);
-				detailForm.getDeleteButton().setVisible(true);
-				
-				target.addComponent(feedBackPanel);
-				target.addComponent(detailPanelFormContainer);
-				target.addComponent(viewButtonContainer);
-				target.addComponent(editButtonContainer);
-			}
-
-			protected void processFeedback(AjaxRequestTarget target)
-			{
-				target.addComponent(feedBackPanel);
-			}
-			
-			protected void processErrors(AjaxRequestTarget target)
-			{
-				target.addComponent(feedBackPanel);
-			}
-		};
-
-		detailForm.initialiseForm();
-		detailPanelFormContainer.add(initialiseModalWindow());
+		detailForm = new DetailForm(	"detailForm", 
+										feedBackPanel,
+										this, 
+										listContainer, 
+										detailsContainer, 
+										containerForm, 
+										viewButtonContainer, 
+										editButtonContainer, 
+										detailPanelFormContainer,
+										searchPanelContainer);
+		
+		detailForm.initialiseDetailForm();
 		add(detailForm);
 	}
-	
-	private void postSaveUpdate(AjaxRequestTarget target){
-		// Button containers
-		// View Field, thus view container visible
-		viewButtonContainer.setVisible(true);
-		editButtonContainer.setVisible(false);
-		detailPanelFormContainer.setEnabled(false);
-		
-		target.addComponent(feedBackPanel);
-		target.addComponent(viewButtonContainer);
-		target.addComponent(editButtonContainer);
-	}
-	
-	private ModalWindow initialiseModalWindow(){
-	   // The ModalWindow, showing some choices for the user to select.
-		selectModalWindow = new au.org.theark.core.web.component.SelectModalWindow("modalwindow"){
 
-	      protected void onSelect(AjaxRequestTarget target, String selection) {
-	      	//TODO Implement try catch for exception handling
-				// try {
-	      	
-	      	// Handle Delete action
-	   		phenotypicService.deleteField(containerForm.getModelObject().getField());
-	   		this.info("Field " + containerForm.getModelObject().getField().getName() + " was deleted successfully");
-	   		
-	   		// Display delete confirmation message
-	   		target.addComponent(feedBackPanel);
-	   		
-	   		//TODO Implement Exceptions in PhentoypicService
-				//  } catch (UnAuthorizedOperation e) { this.error("You are not authorised to manage study components for the given study " +
-				//  study.getName()); processFeedback(target); } catch (ArkSystemException e) {
-				//  this.error("A System error occured, we will have someone contact you."); processFeedback(target); }
-	         
-	   		// Close the confirm modal window
-	   		close(target);
-	         
-				// Move focus back to Search form
-	   		FieldVO fieldVo = new FieldVO();
-				containerForm.setModelObject(fieldVo);
-				searchPanelContainer.setVisible(true);
-				detailsContainer.setVisible(false);
-				target.addComponent(searchPanelContainer);
-				target.addComponent(detailsContainer);
-	      }
-
-	      protected void onCancel(AjaxRequestTarget target) {
-	         // Handle Cancel action
-	      	// Close the confirm modal window
-	         close(target);
-	         
-	         // Go back into Edit mode (and remove feedback, if straight after "New")
-	         detailPanelFormContainer.setEnabled(true);
-				editButtonContainer.setVisible(true);
-				viewButtonContainer.setVisible(false);
-				detailForm.getDeleteButton().setEnabled(true);
-				detailForm.getDeleteButton().setVisible(true);
-				
-				target.addComponent(detailPanelFormContainer);
-				target.addComponent(viewButtonContainer);
-				target.addComponent(editButtonContainer);
-	      }
-	  };
-	  return selectModalWindow;
-	}
-	
 	public DetailForm getDetailForm()
 	{
 		return detailForm;
