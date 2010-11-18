f346eb1a9ce254be0db366e2d15267c9a4221037
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-18 06:07:16 +0000

    Organised imports
    git-svn-id: svn://localhost@2024 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/entity/FieldDataLog.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/model/entity/FieldDataLog.java
@@ -2,7 +2,6 @@ package au.org.theark.phenotypic.model.entity;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
