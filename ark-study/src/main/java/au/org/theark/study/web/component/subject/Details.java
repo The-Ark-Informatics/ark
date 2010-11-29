23eeabc2db51afe07998070dae6a18d99c9c75e5
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-29 05:51:50 +0000

    Moved onSave into DeailsForm
    git-svn-id: svn://localhost@2084 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subject/Details.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subject/Details.java
@@ -6,15 +6,18 @@
  */
 package au.org.theark.study.web.component.subject;
 
+import java.util.Collection;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.model.study.entity.Phone;
 import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.SubjectVO;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.web.component.subject.form.ContainerForm;
@@ -26,21 +29,24 @@ import au.org.theark.study.web.component.subject.form.DetailsForm;
  */
 public class Details extends Panel {
 
-	private CompoundPropertyModel<SubjectVO> cpmModel;
 	
 	@SpringBean(name ="studyService")
 	private IStudyService studyService;
 	
+
+	@SpringBean( name =  au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService iArkCommonService;
 	private DetailsForm detailsForm;
-	
-	
 	private FeedbackPanel feedBackPanel;
 	private WebMarkupContainer listContainer;
 	private WebMarkupContainer detailsContainer;
 	private WebMarkupContainer searchPanelContainer;
 	private ContainerForm containerForm;
 	private Study study;
+
+	
 	/**
+	 * Constructor
 	 * @param id
 	 */
 	public Details(	String id, 
@@ -56,46 +62,21 @@ public class Details extends Panel {
 		this.detailsContainer = detailsContainer;
 		this.searchPanelContainer = searchPanelContainer;
 		this.containerForm = containerForm;
-		
 	}
 	
 	public void initialisePanel(){
 		
-		detailsForm = new DetailsForm("detailsForm",this,listContainer,detailsContainer,containerForm){
-			
-			protected void onSave(SubjectVO subjectVO, AjaxRequestTarget target){
-				
-				Long studyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-				if(studyId == null){
-					//No study in context
-					this.error("There is no study in Context. Please select a study to manage a subject.");
-				}
-				else{
-					study = studyService.getStudy(studyId);
-					
-					if(subjectVO.getPerson().getPersonKey() == null || subjectVO.getPerson().getPersonKey() == 0){
-						subjectVO.setStudy(study);
-						studyService.createSubject(subjectVO);
-						this.info("Subject has been saved successfully and linked to the study in context " + study.getName());
-					}else{
-						studyService.updateSubject(subjectVO);
-						this.info("Subject has been updated successfully and linked to the study in context " + study.getName());
-					}
-				}
-				target.addComponent(feedBackPanel);
-				
-				//Link the Subject with the study
-			}
+		detailsForm = new DetailsForm("detailsForm",this,listContainer,detailsContainer,searchPanelContainer,containerForm, feedBackPanel){
 			
 			protected void onCancel(AjaxRequestTarget target){
-				SubjectVO subjectVO = new SubjectVO();
-				containerForm.setModelObject(subjectVO);
-				searchPanelContainer.setVisible(true);
-				detailsContainer.setVisible(false);
-				listContainer.setVisible(false);
-				target.addComponent(searchPanelContainer);
-				target.addComponent(detailsContainer);
-				target.addComponent(feedBackPanel);
+//				SubjectVO subjectVO = new SubjectVO();
+//				containerForm.setModelObject(subjectVO);
+//				searchPanelContainer.setVisible(true);
+//				detailsContainer.setVisible(false);
+//				listContainer.setVisible(false);
+//				target.addComponent(searchPanelContainer);
+//				target.addComponent(detailsContainer);
+//				target.addComponent(feedBackPanel);
 			}
 			
 			protected void processErrors(AjaxRequestTarget target){
@@ -114,6 +95,5 @@ public class Details extends Panel {
 	public void setDetailsForm(DetailsForm detailsForm) {
 		this.detailsForm = detailsForm;
 	}
-	
 
 }
