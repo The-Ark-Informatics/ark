9d19e80e741ef1ef7d94cedb4f18404ff9026622
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-11-29 05:48:17 +0000

    extends AbstractContainerForm
    git-svn-id: svn://localhost@2080 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/managestudy/form/Container.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/managestudy/form/Container.java
@@ -3,10 +3,11 @@ package au.org.theark.study.web.component.managestudy.form;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.core.vo.StudyModelVO;
+import au.org.theark.core.web.form.AbstractContainerForm;
 
 
 
-public class Container extends BaseForm{
+public class Container extends AbstractContainerForm<StudyModelVO>{
 
 	public Container(String id, CompoundPropertyModel<StudyModelVO> model) {
 		super(id, model);
