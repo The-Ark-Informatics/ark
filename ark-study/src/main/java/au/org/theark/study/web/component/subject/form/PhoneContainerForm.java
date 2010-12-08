f27dce45952c7931b5276d22241241504f10c076
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-08 06:05:10 +0000

    removed the old constructor that takes in a Model
    git-svn-id: svn://localhost@2116 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subject/form/PhoneContainerForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subject/form/PhoneContainerForm.java
@@ -8,8 +8,6 @@ package au.org.theark.study.web.component.subject.form;
 
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 
 import au.org.theark.core.vo.SubjectVO;
 
@@ -21,14 +19,9 @@ public class PhoneContainerForm extends Form<SubjectVO>{
 
 	
 	AjaxButton addPhoneButton;
-	/**
-	 * @param id
-	 * @param model
-	 */
-	public PhoneContainerForm(String id, IModel<SubjectVO> model) {
-		super(id, model);
 	
-	}
-	
+	public PhoneContainerForm(String id) {
+		super(id);
+	}	
 
 }
