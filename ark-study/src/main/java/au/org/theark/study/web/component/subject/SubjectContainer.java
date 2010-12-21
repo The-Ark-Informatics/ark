0e343bf760671410e3bfc1d1ad8eedf7e3d61e5d
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-21 05:34:26 +0000

    Refactored to use Abstract Functionality.
    git-svn-id: svn://localhost@2199 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subject/SubjectContainer.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subject/SubjectContainer.java
@@ -1,9 +1,4 @@
-/**
- * 
- * This is a new file
- *
- *
- */
+
 package au.org.theark.study.web.component.subject;
 
 import java.util.ArrayList;
@@ -13,73 +8,54 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.SubjectVO;
-import au.org.theark.study.service.IStudyService;
-import au.org.theark.study.web.Constants;
+import au.org.theark.core.web.component.AbstractContainerPanel;
 import au.org.theark.study.web.component.subject.form.ContainerForm;
 
 /**
  * @author nivedann
  *
  */
-public class SubjectContainer extends Panel{
+public class SubjectContainer extends AbstractContainerPanel<SubjectVO>{
 
-	private ContainerForm containerForm;
-	private WebMarkupContainer detailsContainer;
-	private WebMarkupContainer searchWebMarkupContainer;
-	private WebMarkupContainer resultListContainer;//Search Results Container
-	//private WebMarkupContainer markupContainerPhoneList;//Phone List Markup Container
-	//private WebMarkupContainer phoneDetailPanelContainer;
-	
-	private IModel<Object> iModel;
-	private PageableListView<SubjectVO> pageableListView;
-	private FeedbackPanel feedBackPanel;
-	private Details detailsPanel;
-	
-	private SearchResults searchResultsPanel;
-	
+
 	private Search searchPanel;
-	@SpringBean( name = Constants.STUDY_SERVICE)
-	private IStudyService studyService;
+	private SearchResults searchResultsPanel;
+	private Details detailsPanel;
+	private PageableListView<SubjectVO> pageableListView;
+	private ContainerForm containerForm;
 	
 	@SpringBean( name =  au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
-	
-	
-	private void initialiseMarkupContainers(){
-		/* The markup container for search panel */
-		searchWebMarkupContainer = new WebMarkupContainer("searchContainer");
-		searchWebMarkupContainer.setOutputMarkupPlaceholderTag(true);
-		
-		detailsContainer = new WebMarkupContainer("detailsContainer");
-		detailsContainer.setOutputMarkupPlaceholderTag(true);
-		detailsContainer.setVisible(false);
-		
-		resultListContainer = new WebMarkupContainer("resultListContainer");
-		resultListContainer.setOutputMarkupPlaceholderTag(true);
-		resultListContainer.setVisible(true);
-		
-		/* Contains the List of Phone Numbers. This is required on the SearchResult Panel of Subject to refresh the PhoneList to avoid LazyLoadException */
-		//		markupContainerPhoneList = new WebMarkupContainer("phoneListMarkupContainer");
-		//		markupContainerPhoneList.setOutputMarkupPlaceholderTag(true);
-		//		markupContainerPhoneList.setVisible(true);
 
-		//phoneDetailPanelContainer = new WebMarkupContainer("phoneDetailMarkupContainer");
-		//phoneDetailPanelContainer.setOutputMarkupPlaceholderTag(true);
-		//phoneDetailPanelContainer.setVisible(false);
+	/**
+	 * @param id
+	 */
+	public SubjectContainer(String id) {
+		
+		super(id);
+		/*Initialise the CPM */
+		cpModel = new CompoundPropertyModel<SubjectVO>(new SubjectVO());
+		containerForm = new ContainerForm("containerForm", cpModel);
+		
+		containerForm.add(initialiseFeedBackPanel());
+		containerForm.add(initialiseDetailPanel());
+		containerForm.add(initialiseSearchResults());
+		containerForm.add(initialiseSearchPanel());
+		
+		add(containerForm);
+		
 	}
 	
-	private WebMarkupContainer initialiseSearchPanel(){
+	protected WebMarkupContainer initialiseSearchPanel(){
 		
 		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		
 		Collection<SubjectVO> subjectVOCollection = new ArrayList<SubjectVO>();
 		
 		if(sessionStudyId != null && sessionStudyId > 0){
@@ -87,41 +63,38 @@ public class SubjectContainer extends Panel{
 			containerForm.getModelObject().setStudy(iArkCommonService.getStudy(sessionStudyId));
 			subjectVOCollection = iArkCommonService.getSubject(containerForm.getModelObject());	
 		}
-		containerForm.getModelObject().setSubjectList(subjectVOCollection);
 		
-		searchPanel = new Search(	"searchPanel", 
-									feedBackPanel, 
-									searchWebMarkupContainer, 
-									pageableListView, 
-									resultListContainer, 
-									detailsContainer, 
+		cpModel.getObject().setSubjectList(subjectVOCollection);
+		
+		searchPanel = new Search("searchComponentPanel",
+									feedBackPanel,
+									searchPanelContainer,
+									pageableListView,
+									searchResultPanelContainer,
+									detailPanelContainer,
+									detailPanelFormContainer,
+									viewButtonContainer,
+									editButtonContainer,
 									detailsPanel,
 									containerForm);
+
+		searchPanel.initialisePanel(cpModel);
+		searchPanelContainer.add(searchPanel);
+		return searchPanelContainer;
+	}
+	
+	
+	protected WebMarkupContainer initialiseDetailPanel(){
 		
-		searchPanel.initialisePanel();
-		searchWebMarkupContainer.add(searchPanel);
-		return searchWebMarkupContainer;
-	}
-	
-	private FeedbackPanel initialiseFeedBackPanel(){
-		/* Feedback Panel */
-		feedBackPanel= new FeedbackPanel("feedbackMessage");
-		feedBackPanel.setOutputMarkupId(true);
-		return feedBackPanel;
-	}
-	
-	private WebMarkupContainer initialiseDetailPanel(){
-		detailsPanel = new Details("detailsPanel", resultListContainer, feedBackPanel, detailsContainer, searchWebMarkupContainer, containerForm);
+		detailsPanel = new Details("detailsPanel",feedBackPanel,searchResultPanelContainer,detailPanelContainer,detailPanelFormContainer,searchPanelContainer,viewButtonContainer,editButtonContainer,containerForm);
 		detailsPanel.initialisePanel();
-		detailsContainer.add(detailsPanel);
-		return detailsContainer;
-		
+		detailPanelContainer.add(detailsPanel);
+		return detailPanelContainer;
 	}
 	
-	
-	private WebMarkupContainer initialiseSearchResults(){
+	protected WebMarkupContainer initialiseSearchResults(){
 		
-		searchResultsPanel = new SearchResults("resultsPanel", detailsContainer, searchWebMarkupContainer, containerForm, resultListContainer, detailsPanel);
+		searchResultsPanel = new SearchResults("searchResults",detailPanelContainer,detailPanelFormContainer,searchPanelContainer,searchResultPanelContainer,viewButtonContainer,editButtonContainer,containerForm);
 		
 		iModel = new LoadableDetachableModel<Object>() {
 			private static final long serialVersionUID = 1L;
@@ -137,27 +110,8 @@ public class SubjectContainer extends Panel{
 		PagingNavigator pageNavigator = new PagingNavigator("navigator", pageableListView);
 		searchResultsPanel.add(pageNavigator);
 		searchResultsPanel.add(pageableListView);
-		resultListContainer.add(searchResultsPanel);
-		return resultListContainer;
-	}
-	
-	/**
-	 * @param id
-	 */
-	public SubjectContainer(String id) {
-		
-		super(id);
-
-		initialiseMarkupContainers();
-		containerForm = new ContainerForm("containerForm", new CompoundPropertyModel<SubjectVO>(new SubjectVO()));
-		
-		containerForm.add(initialiseFeedBackPanel());
-		containerForm.add(initialiseDetailPanel());
-		containerForm.add(initialiseSearchResults());
-		containerForm.add(initialiseSearchPanel());
-		
-		add(containerForm);
-		
+		searchResultPanelContainer.add(searchResultsPanel);
+		return searchResultPanelContainer;
 	}
 
 }
