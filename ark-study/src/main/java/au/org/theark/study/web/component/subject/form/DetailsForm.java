9f0eab484b6629c7395b6d31743693f9d1f7aca4
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-21 05:30:56 +0000

    Changes made to use the Abstract functionality.Removed redundant code.
    git-svn-id: svn://localhost@2194 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subject/form/DetailsForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subject/form/DetailsForm.java
@@ -12,14 +12,13 @@ import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.validator.StringValidator;
 import org.odlabs.wiquery.ui.datepicker.DatePicker;
@@ -32,36 +31,24 @@ import au.org.theark.core.model.study.entity.TitleType;
 import au.org.theark.core.model.study.entity.VitalStatus;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.SubjectVO;
+import au.org.theark.core.web.form.AbstractDetailForm;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.web.Constants;
-import au.org.theark.study.web.component.subject.Details;
 
 /**
  * @author nivedann
  *
  */
-public class DetailsForm extends Form<SubjectVO>{
+public class DetailsForm extends AbstractDetailForm<SubjectVO>{
 
 	@SpringBean( name = Constants.STUDY_SERVICE)
 	private IStudyService studyService;
 	
 	@SpringBean( name =  au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
+
 	
-	private WebMarkupContainer  resultListContainer;
-	private WebMarkupContainer detailPanelContainer;
-	private WebMarkupContainer searchSubjectPanelContainer;
-	private WebMarkupContainer phoneListMarkupContainer;
-	private WebMarkupContainer phoneDetailPanelContainer;
-	
-	private ContainerForm subjectContainerForm;
-	
-//	private PhoneListContainer phoneListContainer;
-//	private PhoneList phoneListPanel;
-//	private IModel<Object> iModel;
-//	private PageableListView<Phone> phonePageableListView;
-	
-	private TextField<String> subjectIdTxtFld;
+
 	private TextField<String> firstNameTxtFld;
 	private TextField<String> middleNameTxtFld;
 	private TextField<String> lastNameTxtFld;
@@ -77,135 +64,27 @@ public class DetailsForm extends Form<SubjectVO>{
 	private DropDownChoice<SubjectStatus> subjectStatusDdc;
 	private DropDownChoice<MaritalStatus> maritalStatusDdc;
 	
-	//TODO There will be mobile, email and address that will added via a component
-	
-	private AjaxButton deleteButton;
-	private AjaxButton saveButton;
-	private AjaxButton cancelButton;
-	private FeedbackPanel detailFeedbackPanel;
-	
-	private AjaxButton addPhoneButton;
 	private Study study;
-	/**
-	 * @param id
-	 */
+	
+	
 	public DetailsForm(	String id,
-						Details detailsPanel, 
-						WebMarkupContainer listContainer,
-						WebMarkupContainer detailsContainer,
+						FeedbackPanel feedBackPanel,
+						WebMarkupContainer resultListContainer,
+						WebMarkupContainer detailPanelContainer,
+						WebMarkupContainer detailPanelFormContainer,
 						WebMarkupContainer searchPanelContainer,
-						//WebMarkupContainer phoneListWebMarkupContainer,
-						ContainerForm containerForm, 
-						FeedbackPanel feedbackPanel	) {
-		super(id);
-		this.subjectContainerForm = containerForm;
-		this.resultListContainer = listContainer;
-		this.detailPanelContainer = detailsContainer;
-		this.detailFeedbackPanel = feedbackPanel;
-		this.searchSubjectPanelContainer = searchPanelContainer;
-		//this.phoneListMarkupContainer = phoneListWebMarkupContainer;
+						WebMarkupContainer viewButtonContainer,
+						WebMarkupContainer editButtonContainer,
+						ContainerForm containerForm				) {
 		
 	
-		
-		cancelButton = new AjaxButton(Constants.CANCEL,  new StringResourceModel("cancelKey", this, null))
-		{
-
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				
-				SubjectVO subjectVO = new SubjectVO();
-				
-				subjectContainerForm.setModelObject(subjectVO);
-				searchSubjectPanelContainer.setVisible(true);
-				detailPanelContainer.setVisible(false);
-				resultListContainer.setVisible(false);
-				
-				//phoneDetailPanelContainer.setVisible(false);
-				//phoneListMarkupContainer.setVisible(true);
-				
-				target.addComponent(searchSubjectPanelContainer);
-				target.addComponent(detailPanelContainer);
-				target.addComponent(detailFeedbackPanel);
-				//target.addComponent(phoneDetailPanelContainer);
-				//target.addComponent(phoneListMarkupContainer);
-				onCancel(target);
-			}
-		};
-		
-		saveButton = new AjaxButton(Constants.SAVE, new StringResourceModel("saveKey", this, null))
-		{
-
-			public void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				
-				Long studyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-				if(studyId == null){
-					//No study in context
-					this.error("There is no study in Context. Please select a study to manage a subject.");
-				}
-				else{
-					study = iArkCommonService.getStudy(studyId);
-					
-					if(subjectContainerForm.getModelObject().getPerson().getId() == null || subjectContainerForm.getModelObject().getPerson().getId() == 0){
-						subjectContainerForm.getModelObject().setStudy(study);
-						studyService.createSubject(subjectContainerForm.getModelObject());
-						this.info("Subject has been saved successfully and linked to the study in context " + study.getName());
-					}else{
-						studyService.updateSubject(subjectContainerForm.getModelObject());
-						this.info("Subject has been updated successfully and linked to the study in context " + study.getName());
-					}
-					
-					//Collection<SubjectVO> collectionOfSubject = iArkCommonService.getSubject(subjectContainerForm.getModelObject());
-					//for (SubjectVO subjectVO2 : collectionOfSubject) {
-					//subjectContainerForm.setModelObject(subjectVO2);
-					//break;
-					//}
-						
-					//gain access to the ListView in the markup container.
-					// PhoneList phoneListPanel  = (PhoneList) phoneListMarkupContainer.get("phoneListPanel");
-					// phonePageableListView = (PageableListView<Phone>)phoneListPanel.get("phoneNumberList");
-					// phonePageableListView.removeAll();
-					// phoneListMarkupContainer.setVisible(true);
-					// target.addComponent(phoneListMarkupContainer);
-					detailPanelContainer.setVisible(true);
-
-					target.addComponent(detailPanelContainer);
-				}
-				target.addComponent(detailFeedbackPanel);
-			}
-			
-			public void onError(AjaxRequestTarget target, Form<?> form){
-				processErrors(target);
-			}
-		};
-		
-		addPhoneButton = new AjaxButton(Constants.ADD_PHONE, new StringResourceModel("addPhoneKey", this, null))
-		{
-
-			public void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				onAddPhone(subjectContainerForm.getModelObject(), target);
-				//target.addComponent(detailPanelContainer);
-			}
-			
-			public void onError(AjaxRequestTarget target, Form<?> form){
-				processErrors(target);
-			}
-		};
+			super(id,feedBackPanel,resultListContainer,detailPanelContainer,detailPanelFormContainer,searchPanelContainer,viewButtonContainer,editButtonContainer,containerForm);
 		
 	}
-	
-	public void initialiseForm(){
+
 		
-		/* Contains the List of Phone Numbers */
-		//		markupContainerPhoneList = new WebMarkupContainer("phoneListMarkupContainer");
-		//		markupContainerPhoneList.setOutputMarkupPlaceholderTag(true);
-		//		markupContainerPhoneList.setVisible(true);
-		
-		phoneDetailPanelContainer = new WebMarkupContainer("phoneDetailMarkupContainer");
-		phoneDetailPanelContainer.setOutputMarkupPlaceholderTag(true);
-		phoneDetailPanelContainer.setVisible(false);
-		
-		
-		//subjectIdTxtFld = new TextField<String>(Constants.PERSON_PERSON_ID);
+	 public void initialiseDetailForm(){
+
 		firstNameTxtFld = new TextField<String>(Constants.PERSON_FIRST_NAME);
 		middleNameTxtFld = new TextField<String>(Constants.PERSON_MIDDLE_NAME);
 		lastNameTxtFld = new TextField<String>(Constants.PERSON_LAST_NAME);
@@ -237,18 +116,47 @@ public class DetailsForm extends Form<SubjectVO>{
 		Collection<MaritalStatus> maritalStatusList = iArkCommonService.getMaritalStatus(); 
 		ChoiceRenderer<MaritalStatus> maritalStatusRender = new ChoiceRenderer<MaritalStatus>(Constants.NAME,Constants.ID);
 		maritalStatusDdc = new DropDownChoice<MaritalStatus>(Constants.PERSON_MARITAL_STATUS,(List) maritalStatusList, maritalStatusRender);
-		
-		//Add the PhoneListContainer
-		//phoneListContainer = new PhoneListContainer("phoneListContainer", subjectContainerForm, detailFeedbackPanel,phoneListMarkupContainer,phoneDetailPanelContainer);
-		
-		
 		attachValidators();
-		addComponents();
+		addDetailFormComponents();
 	}
 	
+	public void addDetailFormComponents(){
+
+		detailPanelFormContainer.add(subjectUIDTxtFld);
+		detailPanelFormContainer.add(titleTypeDdc);
+		detailPanelFormContainer.add(firstNameTxtFld);
+		detailPanelFormContainer.add(middleNameTxtFld);
+		detailPanelFormContainer.add(lastNameTxtFld);
+		detailPanelFormContainer.add(preferredNameTxtFld);
+		detailPanelFormContainer.add(dateOfBirth);
+		detailPanelFormContainer.add(vitalStatusDdc);
+		detailPanelFormContainer.add(genderTypeDdc);
+		detailPanelFormContainer.add(subjectStatusDdc);
+		detailPanelFormContainer.add(maritalStatusDdc);
+	}
+
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#processErrors(org.apache.wicket.ajax.AjaxRequestTarget)
+	 */
+	@Override
+	protected void processErrors(AjaxRequestTarget target) {
+		target.addComponent(feedBackPanel);
+	}
 	
-	private void attachValidators(){
+	protected  void onCancel(AjaxRequestTarget target){
+		SubjectVO subjectVO = new SubjectVO();
+		containerForm.setModelObject(subjectVO);
+		onCancelPostProcess(target);
 		
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#attachValidators()
+	 */
+	@Override
+	protected void attachValidators() {
 		firstNameTxtFld.setRequired(true);
 		firstNameTxtFld.add(StringValidator.lengthBetween(3, 50));
 		middleNameTxtFld.add(StringValidator.lengthBetween(3, 50));
@@ -261,54 +169,53 @@ public class DetailsForm extends Form<SubjectVO>{
 		subjectUIDTxtFld.setRequired(true);
 		
 	}
-	
-	private void addComponents(){
 
-		//add(subjectIdTxtFld);
-		add(titleTypeDdc);
-		add(firstNameTxtFld);
-		add(middleNameTxtFld);
-		add(lastNameTxtFld);
-		add(preferredNameTxtFld);
-		add(dateOfBirth);
-		add(vitalStatusDdc);
-		add(genderTypeDdc);
-		add(subjectStatusDdc);
-		add(maritalStatusDdc);
-		add(subjectUIDTxtFld);
-		add(saveButton);
-		add(cancelButton.setDefaultFormProcessing(false));
-		//Add the Phone Management Container Panel into this form
-		//add(phoneListContainer);
-	}
-	
-
-	
-	protected void onSave(SubjectVO subjectVo, AjaxRequestTarget target){
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#onDeleteConfirmed(org.apache.wicket.ajax.AjaxRequestTarget, java.lang.String, org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow)
+	 */
+	@Override
+	protected void onDeleteConfirmed(AjaxRequestTarget target,	String selection, ModalWindow selectModalWindow) {
+		
+		selectModalWindow.close(target);
+		containerForm.setModelObject(new SubjectVO());
+		onCancel(target);
 		
 	}
-	
-	protected  void onCancel(AjaxRequestTarget target){
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#onSave(org.apache.wicket.markup.html.form.Form, org.apache.wicket.ajax.AjaxRequestTarget)
+	 */
+	@Override
+	protected void onSave(Form<SubjectVO> containerForm,	AjaxRequestTarget target) {
 		
-	}
-	
-	protected  void onAddPhone(SubjectVO subjectVo, AjaxRequestTarget target){
-		phoneDetailPanelContainer.setVisible(true);
-		target.addComponent(phoneDetailPanelContainer);
-	}
-
-	protected void processErrors(AjaxRequestTarget target){
-		target.addComponent(detailFeedbackPanel);
-	}
-	
-	
-
-	public TextField<String> getSubjectIdTxtFld() {
-		return subjectIdTxtFld;
-	}
-
-	public void setSubjectIdTxtFld(TextField<String> subjectIdTxtFld) {
-		this.subjectIdTxtFld = subjectIdTxtFld;
+		target.addComponent(detailPanelContainer);
+		
+		Long studyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		if(studyId == null){
+			//No study in context
+			this.error("There is no study in Context. Please select a study to manage a subject.");
+			processErrors(target);
+		}
+		else{
+			study = iArkCommonService.getStudy(studyId);
+			
+			if(containerForm.getModelObject().getPerson().getId() == null || containerForm.getModelObject().getPerson().getId() == 0){
+				
+				containerForm.getModelObject().setStudy(study);
+				studyService.createSubject(containerForm.getModelObject());
+				this.info("Subject has been saved successfully and linked to the study in context " + study.getName());
+				processErrors(target);
+			
+			}else{
+			
+				studyService.updateSubject(containerForm.getModelObject());
+				this.info("Subject has been updated successfully and linked to the study in context " + study.getName());
+				processErrors(target);
+			
+			}
+			detailPanelContainer.setVisible(true);
+		}
+		onSavePostProcess(target);
 	}
 
 }
