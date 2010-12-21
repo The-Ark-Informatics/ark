71de46b04488ca1dbb1556992a929a51eed33632
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-21 00:24:16 +0000

    Added new constant TABLE_SCHEMA
    git-svn-id: svn://localhost@2180 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/service/Constants.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/service/Constants.java
@@ -1,6 +1,8 @@
 package au.org.theark.phenotypic.service;
 
 public class Constants {
+	public static final String TABLE_SCHEMA = "PHENO";
+	
 	public static final String STATUS_CREATED = "Created"; 
 	public static final String STATUS_ACTIVE = "Active"; 
 	public static final String STATUS_EXPIRED = "Expired"; 
