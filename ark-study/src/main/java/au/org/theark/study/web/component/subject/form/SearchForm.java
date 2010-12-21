092056d2d9eb60f72b2515bc5b00884d1962fde3
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-21 05:31:03 +0000

    Changes made to use the Abstract functionality.Removed redundant code.
    git-svn-id: svn://localhost@2195 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subject/form/SearchForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subject/form/SearchForm.java
@@ -15,22 +15,28 @@ import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ThreadContext;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.model.study.entity.GenderType;
 import au.org.theark.core.model.study.entity.Person;
+import au.org.theark.core.model.study.entity.StudyComp;
 import au.org.theark.core.model.study.entity.SubjectStatus;
 import au.org.theark.core.model.study.entity.VitalStatus;
 import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.SubjectVO;
+import au.org.theark.core.web.form.AbstractSearchForm;
+import au.org.theark.study.model.vo.StudyCompVo;
 import au.org.theark.study.service.IStudyService;
 import au.org.theark.study.web.Constants;
 
@@ -38,7 +44,7 @@ import au.org.theark.study.web.Constants;
  * @author nivedann
  *
  */
-public class SearchForm extends Form<SubjectVO>{
+public class SearchForm extends AbstractSearchForm<SubjectVO>{
 
 
 	@SpringBean( name = Constants.STUDY_SERVICE)
@@ -47,83 +53,131 @@ public class SearchForm extends Form<SubjectVO>{
 	@SpringBean( name =  au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
 
-	
 	private TextField<String> subjectUIDTxtFld;
 	private TextField<String> firstNameTxtFld;
 	private TextField<String> middleNameTxtFld;
 	private TextField<String> lastNameTxtFld;
-	
 	private DropDownChoice<VitalStatus> vitalStatusDdc;
 	private DropDownChoice<GenderType> genderTypeDdc;
 	private DropDownChoice<SubjectStatus> subjectStatusDdc;
-	
-	
-	
-	private AjaxButton searchButton;
-	private AjaxButton newButton;
-	private Button resetButton;
-	
+	private PageableListView<SubjectVO> listView;
 	private CompoundPropertyModel<SubjectVO> cpmModel;
+	
+	
+	/**
+	 * @param id
+	 * @param cpmModel
+	 */
+	public SearchForm(	String id, 
+						CompoundPropertyModel<SubjectVO> cpmModel,
+						PageableListView<SubjectVO> listView, 
+						FeedbackPanel feedBackPanel,
+						WebMarkupContainer listContainer,
+						WebMarkupContainer searchMarkupContainer,
+						WebMarkupContainer detailsContainer,
+						WebMarkupContainer detailPanelFormContainer,
+						WebMarkupContainer viewButtonContainer,
+						WebMarkupContainer editButtonContainer	) {
+		
+		//super(id, cpmModel);
+		super	(id,
+				cpmModel,
+				detailsContainer,
+				detailPanelFormContainer,
+				viewButtonContainer,
+				editButtonContainer,
+				searchMarkupContainer,
+				listContainer,
+				feedBackPanel);
+		
+		this.cpmModel = cpmModel;
+		this.listView = listView;
+		initialiseSearchForm();
+		addSearchComponentsToForm();
+		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		disableSearchButtons(sessionStudyId, "There is no study in context. Please select a study");
+	}
+	
+	protected void addSearchComponentsToForm(){
+		add(subjectUIDTxtFld);
+		add(firstNameTxtFld);
+		add(middleNameTxtFld);
+		add(lastNameTxtFld);
+		add(vitalStatusDdc);
+		add(subjectStatusDdc);
+		add(genderTypeDdc);
+	}
+	
+	protected void initialiseSearchForm(){
+		subjectUIDTxtFld = new TextField<String>(Constants.SUBJECT_UID);
+		firstNameTxtFld = new TextField<String>(Constants.PERSON_FIRST_NAME);
+		middleNameTxtFld = new TextField<String>(Constants.PERSON_MIDDLE_NAME);
+		lastNameTxtFld = new TextField<String>(Constants.PERSON_LAST_NAME);
+		initVitalStatusDdc();
+		initSubjectStatusDdc();
+		initGenderTypeDdc();
+		//addComponents();
+	}
 	
 	/**
 	 * @param id
 	 */
-	public SearchForm(String id, CompoundPropertyModel<SubjectVO> model) {
-		super(id);
-		this.cpmModel = model; 
-		// TODO Auto-generated constructor stub
-		
-		newButton = new AjaxButton(Constants.NEW){
-			
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				//Make the details panel visible
-				onNew(target);
-			}
-			
-			@Override
-			public boolean isVisible(){
-				
-				SecurityManager securityManager =  ThreadContext.getSecurityManager();
-				Subject currentUser = SecurityUtils.getSubject();		
-				boolean flag = false;
-				if(		securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN) ||
-						securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.STUDY_ADMIN)){
-					flag = true;
-				}
-				//if it is a Super or Study admin then make the new available
-				return flag;
-			}
-			
-		};
-		
-		searchButton = new AjaxButton(Constants.SEARCH){
-			
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				onSearch(target);
-			}
-			
-		};
-		
-		resetButton = new Button(Constants.RESET){
-			public void onSubmit(){
-				onReset();
-			}
-		};
-		
-		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-		if(sessionStudyId == null){
-			newButton.setEnabled(false);
-			searchButton.setEnabled(false);
-			this.error("There is no study in context. You can only search or manage subjects based on a study.");
-		}
-		
-		initVitalStatusDdc();
-		initSubjectStatusDdc();
-		initGenderTypeDdc();
-		
-	}
+//	public SearchForm(String id, CompoundPropertyModel<SubjectVO> model) {
+//		super(id);
+//		this.cpmModel = model; 
+//		// TODO Auto-generated constructor stub
+//		
+//		newButton = new AjaxButton(Constants.NEW){
+//			
+//			@Override
+//			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+//				//Make the details panel visible
+//				onNew(target);
+//			}
+//			
+//			@Override
+//			public boolean isVisible(){
+//				
+//				SecurityManager securityManager =  ThreadContext.getSecurityManager();
+//				Subject currentUser = SecurityUtils.getSubject();		
+//				boolean flag = false;
+//				if(		securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN) ||
+//						securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.STUDY_ADMIN)){
+//					flag = true;
+//				}
+//				//if it is a Super or Study admin then make the new available
+//				return flag;
+//			}
+//			
+//		};
+//		
+//		searchButton = new AjaxButton(Constants.SEARCH){
+//			
+//			@Override
+//			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+//				onSearch(target);
+//			}
+//			
+//		};
+//		
+//		resetButton = new Button(Constants.RESET){
+//			public void onSubmit(){
+//				onReset();
+//			}
+//		};
+//		
+//		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+//		if(sessionStudyId == null){
+//			newButton.setEnabled(false);
+//			searchButton.setEnabled(false);
+//			this.error("There is no study in context. You can only search or manage subjects based on a study.");
+//		}
+//		
+//		initVitalStatusDdc();
+//		initSubjectStatusDdc();
+//		initGenderTypeDdc();
+//		
+//	}
 	
 	private void initVitalStatusDdc(){
 		CompoundPropertyModel<SubjectVO> subjectCpm = cpmModel;
@@ -154,37 +208,39 @@ public class SearchForm extends Form<SubjectVO>{
 		genderTypeDdc = new DropDownChoice<GenderType>(Constants.GENDER_TYPE,genderTypePm, (List)genderTypeList,genderTypeRenderer);
 	}
 	
-	protected void onNew(AjaxRequestTarget target){}
+	protected void onNew(AjaxRequestTarget target){
+		setModelObject(new SubjectVO());
+		preProcessDetailPanel(target);
+	}
 
-	protected void onSearch(AjaxRequestTarget target){}
-	
-	protected void onReset(){
-		clearInput();
-		updateFormComponentModels();
+	protected void onSearch(AjaxRequestTarget target){
+		
+		target.addComponent(feedbackPanel);
+		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		getModelObject().setStudy(iArkCommonService.getStudy(sessionStudyId));
+
+		Collection<SubjectVO> subjects = iArkCommonService.getSubject(getModelObject());
+		
+		if(subjects != null && subjects.size() == 0){
+			this.info("There are no subjects with the specified criteria.");
+			target.addComponent(feedbackPanel);
+		}
+		getModelObject().setSubjectList(subjects);
+		listView.removeAll();
+		listContainer.setVisible(true);//Make the WebMarkupContainer that houses the search results visible
+		target.addComponent(listContainer);//For ajax this is required so 
 		
 	}
 	
-	private void addComponents(){
-		
-		add(subjectUIDTxtFld);
-		add(firstNameTxtFld);
-		add(middleNameTxtFld);
-		add(lastNameTxtFld);
-		add(vitalStatusDdc);
-		add(subjectStatusDdc);
-		add(genderTypeDdc);
-		add(newButton);
-		add(searchButton);
-		add(resetButton.setDefaultFormProcessing(false));
-		
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.form.AbstractSearchForm#isSecure()
+	 */
+	@Override
+	protected boolean isSecure() {
+		// TODO Auto-generated method stub
+		return true;
 	}
-	public void initialiseForm(){
-		subjectUIDTxtFld = new TextField<String>(Constants.SUBJECT_UID);
-		firstNameTxtFld = new TextField<String>(Constants.PERSON_FIRST_NAME);
-		middleNameTxtFld = new TextField<String>(Constants.PERSON_MIDDLE_NAME);
-		lastNameTxtFld = new TextField<String>(Constants.PERSON_LAST_NAME);
-		
-		addComponents();
-	}
+	
 
 }
