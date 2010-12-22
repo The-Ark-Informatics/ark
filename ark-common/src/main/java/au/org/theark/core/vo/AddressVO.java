ca634694c7e75e1fbacf547612a863d3c1ddc8d5
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2010-12-22 09:16:45 +0000

    git-svn-id: svn://localhost@2239 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/vo/AddressVO.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.vo;
+
+import java.io.Serializable;
+
+/**
+ * @author nivedann
+ *
+ */
+public class AddressVO implements Serializable{
+
+}
