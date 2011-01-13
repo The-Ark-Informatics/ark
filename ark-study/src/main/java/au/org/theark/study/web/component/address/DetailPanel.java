da64990ddd2f0edd3c4f5dbd9426f025bcc6c51a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-01-13 08:48:02 +0000

    Implemented detailPanel related functions instantiate the Form and call its initiialise()
    git-svn-id: svn://localhost@2309 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/address/DetailPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/address/DetailPanel.java
@@ -1,6 +1,11 @@
 package au.org.theark.study.web.component.address;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
+
+import au.org.theark.study.web.component.address.form.ContainerForm;
+import au.org.theark.study.web.component.address.form.DetailForm;
 
 /**
  * @author nivedann
@@ -8,12 +13,62 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public class DetailPanel extends Panel{
 
+	private FeedbackPanel feedBackPanel;
+	private WebMarkupContainer searchResultPanelContainer;
+	private WebMarkupContainer detailPanelContainer;
+	private WebMarkupContainer detailPanelFormContainer; 
+	private WebMarkupContainer searchPanelContainer;
+	private WebMarkupContainer viewButtonContainer;
+	private WebMarkupContainer editButtonContainer;
+	private ContainerForm containerForm;
+	private DetailForm detailForm;
+	
 	/**
 	 * @param id
 	 */
-	public DetailPanel(String id) {
+	public DetailPanel(String id,
+						FeedbackPanel feedBackPanel,
+						WebMarkupContainer searchResultPanelContainer,
+						WebMarkupContainer detailPanelContainer,
+						WebMarkupContainer detailPanelFormContainer, 
+						WebMarkupContainer searchPanelContainer,
+						WebMarkupContainer viewButtonContainer,
+						WebMarkupContainer editButtonContainer,
+						ContainerForm containerForm	) {
 		super(id);
-		// TODO Auto-generated constructor stub
+		this.feedBackPanel = feedBackPanel;
+		this.searchResultPanelContainer = searchResultPanelContainer;
+		this.detailPanelContainer = detailPanelContainer;
+		this.detailPanelFormContainer = detailPanelFormContainer;
+		this.searchPanelContainer = searchPanelContainer;
+		this.viewButtonContainer = viewButtonContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.containerForm = containerForm;
+	}
+	
+	public void initialisePanel(){
+		
+		detailForm = new DetailForm("detailsForm",
+									feedBackPanel, 
+									searchResultPanelContainer, 
+									detailPanelContainer, 
+									detailPanelFormContainer, 
+									searchPanelContainer, 
+									viewButtonContainer, 
+									editButtonContainer,
+									containerForm);
+		
+		detailForm.initialiseDetailForm();
+		add(detailForm);
+		
+	}
+
+	public DetailForm getDetailForm() {
+		return detailForm;
+	}
+
+	public void setDetailForm(DetailForm detailForm) {
+		this.detailForm = detailForm;
 	}
 
 }
