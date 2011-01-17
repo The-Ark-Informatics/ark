b5741a556d688818d162375009b143a8be7d1852
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-01-17 08:26:39 +0000

    initial revision
    git-svn-id: svn://localhost@2343 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/ConsentStatus.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.model.study.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+/**
+ * @author nivedann
+ *
+ */
+@Entity
+@Table(name = "CONSENT_STATUS", schema = Constants.STUDY_SCHEMA)
+public class ConsentStatus implements Serializable{
+	
+	
+	private Long id;
+	private String name;
+	private String description;
+	
+	/**
+	 * Constructor no-arg
+	 */
+	public ConsentStatus(){
+		
+	}
+	/**
+	 * Constructor
+	 * @param id
+	 */
+	public ConsentStatus(Long id){
+		this.id = id;
+	}
+	
+	
+	
+	@Id
+	@SequenceGenerator(name="consent_status_generator", sequenceName="CONSENT_STATUS_SEQUENCE")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator = "consent_status_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "NAME", length = 255)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "DESCRIPTION")
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
