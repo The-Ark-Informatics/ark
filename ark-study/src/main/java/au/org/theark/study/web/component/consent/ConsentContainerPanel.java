99bdc7d0ad815890c95bef27b8b6473f87371952
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-01-19 01:27:27 +0000

    Extends the AbstractContainerPanel added initialiseSearchPanel
    git-svn-id: svn://localhost@2358 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/consent/ConsentContainerPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/consent/ConsentContainerPanel.java
@@ -1,17 +1,177 @@
 package au.org.theark.study.web.component.consent;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
-public class ConsentContainerPanel extends Panel {
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.model.study.entity.Consent;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.ConsentVO;
+import au.org.theark.core.web.component.AbstractContainerPanel;
+import au.org.theark.study.service.IStudyService;
+import au.org.theark.study.web.Constants;
+import au.org.theark.study.web.component.consent.form.ContainerForm;
 
-	/**
-	 * 
-	 */
+
+public class ConsentContainerPanel extends AbstractContainerPanel<ConsentVO> {
+
 	private static final long serialVersionUID = 1L;
 
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService iArkCommonService;
+
+	@SpringBean( name = Constants.STUDY_SERVICE)
+	private IStudyService studyService;
+	
+	//Container Form
+	private ContainerForm containerForm;
+	//Panels
+	//private SearchResultListPanel searchResultListPanel;
+	private SearchPanel searchPanel;
+	private DetailPanel detailPanel;
+	private PageableListView<Consent> pageableListView;
+	
+	
+	/**
+	 * Constructor
+	 * @param id
+	 */
 	public ConsentContainerPanel(String id) {
+		
 		super(id);
-		// TODO Auto-generated constructor stub
+		cpModel = new CompoundPropertyModel<ConsentVO>(new ConsentVO());
+		containerForm = new ContainerForm("containerForm",cpModel);
+		containerForm.add(initialiseFeedBackPanel());
+		//containerForm.add(initialiseDetailPanel());
+		//containerForm.add(initialiseSearchResults());
+		containerForm.add(initialiseSearchPanel());
+		add(containerForm);
+	}
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.component.AbstractContainerPanel#initialiseDetailPanel()
+	 */
+	@Override
+	protected WebMarkupContainer initialiseDetailPanel() {
+
+//		detailPanel = new DetailPanel("detailsPanel",
+//										feedBackPanel,
+//										searchResultPanelContainer, 
+//										detailPanelContainer,
+//										detailPanelFormContainer,
+//										searchPanelContainer,
+//										viewButtonContainer,
+//										editButtonContainer,
+//										containerForm);
+//		detailPanel.initialisePanel();
+//		detailPanelContainer.add(detailPanel);
+//		return detailPanelContainer;
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.component.AbstractContainerPanel#initialiseSearchPanel()
+	 */
+	@Override
+	protected WebMarkupContainer initialiseSearchPanel() {
+		
+		//Get the Person in Context and determine the Person Type
+		Long sessionPersonId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.PERSON_CONTEXT_ID);
+		String sessionPersonType = (String)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.PERSON_TYPE);//Subject or Contact: Denotes if it was a subject or contact placed in session
+		
+		try{
+			//Initialise the phoneList;
+			Collection<Consent> consentList = new ArrayList<Consent>();
+			
+			if(sessionPersonId != null){
+				containerForm.getModelObject().getConsent().setSubject(studyService.getPerson(sessionPersonId));//Can be a Subject or Contact
+				//getSubjectConsentList(subjectId,)TODO 
+				//personPhoneList = studyService.getPersonPhoneList(sessionPersonId, containerForm.getModelObject().getPhone());
+			}
+				
+			//All the phone items related to the person if one found in session or an empty list
+			cpModel.getObject().setConsentList(consentList);
+			searchPanel = new SearchPanel("searchComponentPanel", 
+											feedBackPanel,
+											searchPanelContainer,
+											pageableListView,
+											searchResultPanelContainer,
+											detailPanelContainer,
+											detailPanel,
+											containerForm,
+											viewButtonContainer,
+											editButtonContainer,
+											detailPanelFormContainer);
+			searchPanel.initialisePanel(cpModel);
+			searchPanelContainer.add(searchPanel);
+			
+		}catch(EntityNotFoundException entityNotFoundException){
+			//Report this to the user
+			
+		}catch(ArkSystemException arkSystemException){
+			//Logged by the back end. Report this to the user
+		}
+		
+		return searchPanelContainer;
+	}
+
+	/* (non-Javadoc)
+	 * @see au.org.theark.core.web.component.AbstractContainerPanel#initialiseSearchResults()
+	 */
+	@Override
+	protected WebMarkupContainer initialiseSearchResults() {
+		
+//		SearchResultListPanel searchResultPanel = new SearchResultListPanel("searchResults",
+//																			detailPanelContainer,
+//																			detailPanelFormContainer,
+//																			searchPanelContainer,
+//																			searchResultPanelContainer,
+//																			viewButtonContainer,
+//																			editButtonContainer,
+//																			containerForm																				);
+//		iModel = new LoadableDetachableModel<Object>() {
+//
+//			private static final long serialVersionUID = 1L;
+//			@Override
+//			protected Object load() {
+//				//Get the PersonId from session and get the phoneList from backend
+//				Collection<Phone> personPhoneList = new ArrayList<Phone>();
+//				Long sessionPersonId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.PERSON_CONTEXT_ID);
+//				try {
+//					if(sessionPersonId != null){
+//						personPhoneList = studyService.getPersonPhoneList(sessionPersonId, containerForm.getModelObject().getPhone());	
+//					}
+//				} catch (EntityNotFoundException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (ArkSystemException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//				return personPhoneList;
+//			}
+//		};
+//	
+//		pageableListView  = searchResultPanel.buildPageableListView(iModel);
+//		pageableListView.setReuseItems(true);
+//		PagingNavigator pageNavigator = new PagingNavigator("phoneNavigator", pageableListView);
+//		searchResultPanel.add(pageNavigator);
+//		searchResultPanel.add(pageableListView);
+//		searchResultPanelContainer.add(searchResultPanel);
+		return null;
+		
+		
 	}
 	
 
