232d82b1f942fa6fbd90a7cba01382e0353a4c33
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-01-28 06:05:38 +0000

    git-svn-id: svn://localhost@2484 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/Constants.java
+++ ark-common/src/main/java/au/org/theark/core/Constants.java
@@ -77,6 +77,9 @@ public class Constants {
 	public static final String SUBJECT="Subject";
 	public static final String TAB_MODULE_SUBJECT="tab.module.subject";
 	public static final String MENU_STUDY_SUBMENU ="studySubMenus";
+	public static final String CUSTOM_FIELD="Custom Field";
+	public static final String TAB_CUSTOM_FIELD="tab.custom.field";
+	
 	
 	public static final String STUDY_KEY="id";
 	
