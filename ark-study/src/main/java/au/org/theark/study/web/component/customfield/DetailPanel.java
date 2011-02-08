b5aa748a85cc0e15cd3032b7e2ec376aa9e04af6
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-02-08 08:55:41 +0000

    Modified DetailPanel
    git-svn-id: svn://localhost@2526 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/customfield/DetailPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/customfield/DetailPanel.java
@@ -6,10 +6,35 @@
  */
 package au.org.theark.study.web.component.customfield;
 
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import au.org.theark.core.vo.ArkCrudContainerVO;
+import au.org.theark.study.web.component.customfield.form.ContainerForm;
+
 /**
  * @author nivedann
  *
  */
-public class DetailPanel {
+public class DetailPanel extends Panel {
+
+	private FeedbackPanel feedbackPanel;
+	private ArkCrudContainerVO arkCrudContainerVO;
+	/**
+	 * @param id
+	 */
+	public DetailPanel(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public DetailPanel(String id, FeedbackPanel feedBackPanel,	ArkCrudContainerVO arkCrudContainerVO, ContainerForm containerForm){
+	
+		super(id);
+		this.arkCrudContainerVO = arkCrudContainerVO;
+		this.feedbackPanel = feedBackPanel;
+		
+	}
+	
 
 }
