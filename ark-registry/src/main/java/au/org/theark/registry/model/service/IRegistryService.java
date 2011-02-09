d502c539fb6a7665d846013d404630ae313724d4
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-02-09 05:29:06 +0000

    Initial set up for ark-registry module.
    git-svn-id: svn://localhost@2530 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-registry/src/main/java/au/org/theark/registry/model/service/IRegistryService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.registry.model.service;
+
+/**
+ * @author nivedann
+ *
+ */
+public interface IRegistryService {
+
+}
---
+++ ark-registry/src/main/java/au/org/theark/registry/web/Constants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.registry.web;
+
+/**
+ * @author nivedann
+ *
+ */
+public class Constants {
+
+}
