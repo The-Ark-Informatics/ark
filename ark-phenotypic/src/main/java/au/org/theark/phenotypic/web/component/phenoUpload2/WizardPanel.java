84406334c0a715447a2303ad70f00f88b7d5a3ab
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-02-17 07:33:19 +0000

    Modified Wizard layout accordingly
    git-svn-id: svn://localhost@2556 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenoUpload2/WizardPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenoUpload2/WizardPanel.java
@@ -1,13 +1,9 @@
 package au.org.theark.phenotypic.web.component.phenoUpload2;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.wizard.WizardStep;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 
-import au.org.theark.core.web.component.wizard.ArkCommonWizard;
-import au.org.theark.phenotypic.model.vo.UploadVO;
 import au.org.theark.phenotypic.web.component.phenoUpload2.form.ContainerForm;
 import au.org.theark.phenotypic.web.component.phenoUpload2.form.WizardForm;
 
@@ -25,13 +21,15 @@ public class WizardPanel extends Panel
 	private WebMarkupContainer		editButtonContainer;
 	private ContainerForm			containerForm;
 
-	private WebMarkupContainer		phenoUploadWizardContainer;
-	private ArkCommonWizard			phenoUploadWizardPanel;
-	private AjaxRequestTarget		target;
-	private java.util.Collection<String>	validationMessages;
+	
 
-	public WizardPanel(String id, final WebMarkupContainer listContainer, FeedbackPanel feedBackPanel, WebMarkupContainer wizardPanelContainer, WebMarkupContainer searchPanelContainer,
-			WebMarkupContainer wizardPanelFormContainer, ContainerForm containerForm)
+	public WizardPanel(String id, 
+							final WebMarkupContainer listContainer, 
+							FeedbackPanel feedBackPanel, 
+							WebMarkupContainer wizardPanelContainer, 
+							WebMarkupContainer searchPanelContainer,
+							WebMarkupContainer wizardPanelFormContainer, 
+							ContainerForm containerForm)
 	{
 		super(id);
 		this.feedBackPanel = feedBackPanel;
@@ -41,20 +39,33 @@ public class WizardPanel extends Panel
 		this.searchPanelContainer = searchPanelContainer;
 		this.wizardPanelFormContainer = wizardPanelFormContainer;
 		this.containerForm = containerForm;
-
-		wizardForm = new WizardForm("wizardForm", feedBackPanel, this, listContainer, wizardPanelContainer, containerForm, viewButtonContainer, editButtonContainer, wizardPanelFormContainer,
-				searchPanelContainer);
-		wizardForm.initialiseDetailForm();
-		wizardPanelFormContainer.add(phenoUploadWizardPanel);
-		wizardForm.add(wizardPanelFormContainer);
-		phenoUploadWizardContainer.add(wizardForm);
-
-		add(phenoUploadWizardContainer);
-		setOutputMarkupPlaceholderTag(true);
 	}
 
 	public void initialisePanel()
 	{
-		// TODO: Add initilisation here
+		wizardForm = new WizardForm("wizardForm", 
+											feedBackPanel, 
+											this, 
+											listContainer, 
+											wizardPanelContainer, 
+											containerForm, 
+											viewButtonContainer, 
+											editButtonContainer, 
+											wizardPanelFormContainer,
+											searchPanelContainer);
+		wizardForm.initialiseDetailForm();
+		add(wizardForm);
+
+		setOutputMarkupPlaceholderTag(true);
+	}
+
+	public WizardForm getWizardForm()
+	{
+		return wizardForm;
+	}
+
+	public void setWizardForm(WizardForm wizardForm)
+	{
+		this.wizardForm = wizardForm;
 	}
 }
