f7740e09f7db72e34efd878d070e67987ae72ccc
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-02-27 13:19:28 +0000

    Added ENCODED_VALUES_TOKEN and ENCODED_VALUES_SEPARATOR
    git-svn-id: svn://localhost@2702 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/Constants.java
+++ ark-common/src/main/java/au/org/theark/core/Constants.java
@@ -104,6 +104,8 @@ public class Constants {
 	public static final String FIELD_TYPE_CHARACTER ="CHARACTER";
 	public static final String FIELD_TYPE_DATE ="DATE";
 	public static final String DISCRETE_RANGE_TOKEN =",";
+	public static final String ENCODED_VALUES_TOKEN = ";";
+	public static final String ENCODED_VALUES_SEPARATOR = "=";
 	
 	/* Button names/labels */
 	public static final String SEARCH="search";
