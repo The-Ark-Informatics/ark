d3a6bdb30a5bc7c8c46955e307b7c6b8cd80140e
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-03 03:11:24 +0000

    ADDED  constant for ACTION TYPE created
    git-svn-id: svn://localhost@2791 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/Constants.java
+++ ark-common/src/main/java/au/org/theark/core/Constants.java
@@ -155,4 +155,9 @@ public class Constants {
 	public static final String DEFAULT_COUNTRY="Australia";
 	public static final String NO = "No";
 	public static final String YES = "Yes";
+	
+	
+	/***/
+	public static final String  ACTION_TYPE_CREATED= "CREATED";
+	
 }
