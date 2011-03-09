c41560a65dfa80bcb7b52495a7121e7e55d036d0
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-09 08:25:30 +0000

    initial revision - to be used for any subclass of detail form that will not use delete function.
    git-svn-id: svn://localhost@2911 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/web/form/AbstractArchiveDetailForm.java
@@ -0,0 +1,227 @@
+/**
+ * 
+ * This is a new file
+ *
+ *
+ */
+package au.org.theark.core.web.form;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.StringResourceModel;
+
+import au.org.theark.core.Constants;
+
+/**
+ * @author nivedann
+ *
+ */
+public abstract class  AbstractArchiveDetailForm <T> extends Form<T>{
+	
+	
+	/**
+	 * @param id
+	 */
+	public AbstractArchiveDetailForm(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long		serialVersionUID	= 1L;
+
+	protected WebMarkupContainer	resultListContainer;
+	protected WebMarkupContainer	detailPanelContainer;
+	protected WebMarkupContainer	searchPanelContainer;
+	protected WebMarkupContainer	viewButtonContainer;
+	protected WebMarkupContainer	editButtonContainer;
+	protected WebMarkupContainer	detailPanelFormContainer;
+	protected FeedbackPanel			feedBackPanel;
+	protected Form<T>					containerForm;
+
+	protected AjaxButton				saveButton;
+	protected AjaxButton				cancelButton;
+	protected AjaxButton				editButton;
+	protected AjaxButton				editCancelButton;
+
+	/**
+	 * Implement this to add all the form components/objects
+	 */
+	protected void addFormComponents()
+	{
+		add(saveButton);
+		add(cancelButton.setDefaultFormProcessing(false));
+	}
+
+	abstract protected void attachValidators();
+
+
+	abstract protected void onCancel(AjaxRequestTarget target);
+
+	abstract protected void onSave(Form<T> containerForm, AjaxRequestTarget target);
+
+	abstract protected void processErrors(AjaxRequestTarget target);
+
+	protected void onCancelPostProcess(AjaxRequestTarget target)
+	{
+
+		resultListContainer.setVisible(true);
+		detailPanelContainer.setVisible(false);
+		searchPanelContainer.setVisible(true);
+
+		target.addComponent(feedBackPanel);
+		target.addComponent(searchPanelContainer);
+		target.addComponent(detailPanelContainer);
+		target.addComponent(resultListContainer);
+	}
+
+	/**
+	 * Constructor for AbstractArchiveDetailForm class
+	 * 
+	 * @param id
+	 * @param feedBackPanel
+	 * @param resultListContainer
+	 * @param detailPanelContainer
+	 * @param detailPanelFormContainer
+	 * @param searchPanelContainer
+	 * @param viewButtonContainer
+	 * @param editButtonContainer
+	 * @param containerForm
+	 */
+	public AbstractArchiveDetailForm(	String id,
+										FeedbackPanel feedBackPanel, 
+										WebMarkupContainer resultListContainer, 
+										WebMarkupContainer detailPanelContainer, 
+										WebMarkupContainer detailPanelFormContainer,
+										WebMarkupContainer searchPanelContainer, 
+										WebMarkupContainer viewButtonContainer, 
+										WebMarkupContainer editButtonContainer, 
+										Form<T> containerForm){
+		
+		super(id);
+		
+		this.resultListContainer = resultListContainer;
+		this.detailPanelContainer = detailPanelContainer;
+		this.feedBackPanel = feedBackPanel;
+		this.searchPanelContainer = searchPanelContainer;
+		this.editButtonContainer = editButtonContainer;
+		this.viewButtonContainer = viewButtonContainer;
+		this.detailPanelFormContainer = detailPanelFormContainer;
+		this.containerForm = containerForm;
+
+		initialiseForm();
+	}
+
+	@SuppressWarnings("serial")
+	protected void initialiseForm()
+	{
+
+		cancelButton = new AjaxButton(Constants.CANCEL, new StringResourceModel("cancelKey", this, null))
+		{
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
+
+				resultListContainer.setVisible(false); // Hide the Search Result List Panel via the WebMarkupContainer
+				detailPanelContainer.setVisible(false); // Hide the Detail Panle via the WebMarkupContainer
+				target.addComponent(detailPanelContainer);// Attach the Detail WebMarkupContainer to be re-rendered using Ajax
+				target.addComponent(resultListContainer);// Attach the resultListContainer WebMarkupContainer to be re-rendered using Ajax
+				onCancel(target);// Invoke a onCancel() that the sub-class can use to build anything more specific
+			}
+		};
+
+		saveButton = new AjaxButton(Constants.SAVE, new StringResourceModel("saveKey", this, null))
+		{
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
+				onSave(containerForm, target);
+				target.addComponent(detailPanelContainer);
+			}
+
+			public void onError(AjaxRequestTarget target, Form<?> form)
+			{
+				processErrors(target);
+			}
+		};
+
+		editButton = new AjaxButton("edit", new StringResourceModel("editKey", this, null))
+		{
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
+				viewButtonContainer.setVisible(false);
+				editButtonContainer.setVisible(true);
+				detailPanelFormContainer.setEnabled(true);
+				target.addComponent(viewButtonContainer);
+				target.addComponent(editButtonContainer);
+				target.addComponent(detailPanelFormContainer);
+			}
+
+			public void onError(AjaxRequestTarget target, Form<?> form)
+			{
+				processErrors(target);
+			}
+		};
+
+		editCancelButton = new AjaxButton("editCancel", new StringResourceModel("editCancelKey", this, null))
+		{
+			public void onSubmit(AjaxRequestTarget target, Form<?> form)
+			{
+				onCancel(target);
+			}
+
+			public void onError(AjaxRequestTarget target, Form<?> form)
+			{
+				processErrors(target);
+			}
+		};
+
+		addComponentsToForm();
+	}
+
+	protected void addComponentsToForm()
+	{
+
+		add(detailPanelFormContainer);
+
+		editButtonContainer.add(saveButton);
+		editButtonContainer.add(cancelButton.setDefaultFormProcessing(false));
+		viewButtonContainer.add(editButton);
+		viewButtonContainer.add(editCancelButton.setDefaultFormProcessing(false));
+
+		add(editButtonContainer);
+		add(viewButtonContainer);
+
+	}
+
+	/**
+	 * A helper method that will allow the toggle of panels and buttons. This method can be invoked by sub-classes as part of the onSave()
+	 * implementation.Once the user has pressed Save either to create a new entity or update, invoking this method will place the new/edited record
+	 * panel in View/Read only mode.
+	 * 
+	 * @param target
+	 */
+	protected void onSavePostProcess(AjaxRequestTarget target)
+	{
+
+		detailPanelContainer.setVisible(true);
+		viewButtonContainer.setVisible(true);
+		viewButtonContainer.setEnabled(true);
+		detailPanelFormContainer.setEnabled(false);
+		resultListContainer.setVisible(false);
+		searchPanelContainer.setVisible(false);
+		editButtonContainer.setVisible(false);
+
+		target.addComponent(resultListContainer);
+		target.addComponent(detailPanelContainer);
+		target.addComponent(detailPanelFormContainer);
+		target.addComponent(searchPanelContainer);
+		target.addComponent(viewButtonContainer);
+		target.addComponent(editButtonContainer);
+
+	}
+
+
+}
