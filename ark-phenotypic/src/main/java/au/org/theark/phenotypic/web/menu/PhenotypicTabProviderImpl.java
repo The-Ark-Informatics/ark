708fc1b93e22c1228e62ab8ca2ce85b67772ee06
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-15 06:58:31 +0000

    Removed redundant code
    git-svn-id: svn://localhost@2992 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
@@ -3,15 +3,11 @@ package au.org.theark.phenotypic.web.menu;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import au.org.theark.core.service.IMainTabProvider;
 import au.org.theark.phenotypic.web.Constants;
@@ -21,7 +17,6 @@ public class PhenotypicTabProviderImpl extends Panel implements IMainTabProvider
 {
 
 	private static final long	serialVersionUID	= 1L;
-	private transient Logger	log					= LoggerFactory.getLogger(PhenotypicTabProviderImpl.class);
 	
 	WebMarkupContainer arkContextPanelMarkup;
 
@@ -38,28 +33,7 @@ public class PhenotypicTabProviderImpl extends Panel implements IMainTabProvider
 		// Main tab
 		ITab iTab = createTab(Constants.PHENOTYPIC_MAIN_TAB);
 		moduleTabsList.add(iTab);
-		
-		// Hide tab if study not selected
-		try
-		{
-			Subject currentUser = SecurityUtils.getSubject();
-			Long studyId = (Long) currentUser.getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 
-			// TODO: Disable main tab if studyId not selected (in context)
-			if (studyId != null)
-			{
-				log.info("Study selected, tab should be visible");
-			}
-			else
-			{
-				log.info("Study NOT selected, tab should NOT be visible");
-			}
-		}
-		catch (Exception ex)
-		{
-			log.error("PhenotypicSubMenuTab: General exception when determing tab visibility. " + ex.getMessage());
-		}
-		
 		return moduleTabsList;
 	}
 	
@@ -70,27 +44,6 @@ public class PhenotypicTabProviderImpl extends Panel implements IMainTabProvider
 		// Main tab
 		ITab iTab = createTab(Constants.PHENOTYPIC_MAIN_TAB);
 		moduleTabsList.add(iTab);
-		
-		// Hide tab if study not selected
-		try
-		{
-			Subject currentUser = SecurityUtils.getSubject();
-			Long studyId = (Long) currentUser.getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-
-			// TODO: Disable main tab if studyId not selected (in context)
-			if (studyId != null)
-			{
-				log.info("Study selected, tab should be visible");
-			}
-			else
-			{
-				log.info("Study NOT selected, tab should NOT be visible");
-			}
-		}
-		catch (Exception ex)
-		{
-			log.error("PhenotypicSubMenuTab: General exception when determing tab visibility. " + ex.getMessage());
-		}
 		
 		return moduleTabsList;
 	}
