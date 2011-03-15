96ed8ad264673369a80bf69fe6820940a87a6b73
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-15 07:00:07 +0000

    Made parameters private, formatted source
    git-svn-id: svn://localhost@2993 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
@@ -13,48 +13,40 @@ import au.org.theark.core.service.IMainTabProvider;
 import au.org.theark.phenotypic.web.Constants;
 
 @SuppressWarnings("serial")
-public class PhenotypicTabProviderImpl extends Panel implements IMainTabProvider
-{
+public class PhenotypicTabProviderImpl extends Panel implements
+		IMainTabProvider {
 
-	private static final long	serialVersionUID	= 1L;
-	
-	WebMarkupContainer arkContextPanelMarkup;
+	private static final long serialVersionUID = 1L;
+	private WebMarkupContainer arkContextPanelMarkup;
+	private List<ITab> moduleTabsList;
 
-	List<ITab>						moduleTabsList;
-
-	public PhenotypicTabProviderImpl(String panelId)
-	{
+	public PhenotypicTabProviderImpl(String panelId) {
 		super(panelId);
 		moduleTabsList = new ArrayList<ITab>();
 	}
 
-	public List<ITab> buildTabs()
-	{
+	public List<ITab> buildTabs() {
 		// Main tab
 		ITab iTab = createTab(Constants.PHENOTYPIC_MAIN_TAB);
 		moduleTabsList.add(iTab);
 
 		return moduleTabsList;
 	}
-	
-	public List<ITab> buildTabs(WebMarkupContainer arkContextPanelMarkup)
-	{
+
+	public List<ITab> buildTabs(WebMarkupContainer arkContextPanelMarkup) {
 		this.arkContextPanelMarkup = arkContextPanelMarkup;
-		
+
 		// Main tab
 		ITab iTab = createTab(Constants.PHENOTYPIC_MAIN_TAB);
 		moduleTabsList.add(iTab);
-		
+
 		return moduleTabsList;
 	}
-	
-	public ITab createTab(String tabName)
-	{
-		return new AbstractTab(new Model<String>(tabName))
-		{
+
+	public ITab createTab(String tabName) {
+		return new AbstractTab(new Model<String>(tabName)) {
 			@Override
-			public Panel getPanel(String pid)
-			{
+			public Panel getPanel(String pid) {
 				// The sub menu(s) for Phenotypic
 				return new PhenotypicSubMenuTab(pid, arkContextPanelMarkup);
 			}
