0d9bc65c80ff1f1e69c2b80ffedb2ba4831d7ffa
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-22 10:11:01 +0000

    Slightly changed to specify the [study] database (i.e. script patches [study].[subject_file] table).
    git-svn-id: svn://localhost@3084 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110322/mysql/20110322_study_subject_file_schemaPatch.sql
+++ ark-database/db-scripts/patch/20110322/mysql/20110322_study_subject_file_schemaPatch.sql
@@ -1,2 +1,3 @@
--- Add a new "comments" column to the [subject_file] table
+-- Add a new "comments" column to the [study].[subject_file] table
+USE study;
 ALTER TABLE `subject_file` ADD COLUMN `COMMENTS`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL AFTER `USER_ID`;
