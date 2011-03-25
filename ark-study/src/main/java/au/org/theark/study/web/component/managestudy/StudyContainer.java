82fbb3ad203c1845a412ae6e3e6fb34f87309aca
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-25 03:27:12 +0000

    added prerenderContextCheck() it is not used as yet. This allows to place the study into context  detail view. However from teh container we do not have access to the AjaxRequestTarget instance. Hence till we find a solution to this it will not support loading the study detail view page even though it is in context.
    git-svn-id: svn://localhost@3129 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/managestudy/StudyContainer.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/managestudy/StudyContainer.java
@@ -1,17 +1,28 @@
 package au.org.theark.study.web.component.managestudy;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.ModuleVO;
 import au.org.theark.core.vo.StudyModelVO;
 import au.org.theark.core.web.component.AbstractContainerPanel;
+import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.component.managestudy.form.Container;
+import au.org.theark.study.web.component.managestudy.form.DetailForm;
 
 public class StudyContainer extends AbstractContainerPanel<StudyModelVO>{
 
@@ -24,6 +35,9 @@ public class StudyContainer extends AbstractContainerPanel<StudyModelVO>{
 	
 	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
+	
+	@SpringBean( name = "userService")
+	private IUserService userService;
 	
 	private IModel<Object> iModel;
 	private StudyCrudContainerVO studyCrudContainerVO;
@@ -58,11 +72,69 @@ public class StudyContainer extends AbstractContainerPanel<StudyModelVO>{
 		containerForm.add(initialiseDetailPanel());		
 		containerForm.add(initialiseSearchResults());
 		containerForm.add(initialiseSearchPanel());
-		
+		//prerenderContextCheck();
 		add(containerForm);
 		
 	}
 	
+	/**
+	 * TODO: To be hooked when a solution is found for AjaxRequestTarget. We need this to render the subjectUidContainer fields.
+	 * We do not have a reference to the Target instance here.
+	 */
+	protected void prerenderContextCheck() {	
+		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		if(sessionStudyId != null){
+			Study study = iArkCommonService.getStudy(sessionStudyId);
+			String subjectUidExample = iArkCommonService.getSubjectUidExample(study);
+			containerForm.getModelObject().setStudy(study);
+			containerForm.getModelObject().setSubjectUidExample(subjectUidExample);
+			
+			DetailForm detailForm = (DetailForm) detailsPanel.get("detailForm");
+			
+			// All SubjectUID generator fields grouped within a container(s)
+			WebMarkupContainer autoSubjectUidcontainer = detailForm.getAutoSubjectUidContainer();
+			WebMarkupContainer subjectUidcontainer = detailForm.getSubjectUidContainer();
+			
+			// Disable all SubjectUID generation fields is subjects exist
+			if(iArkCommonService.getSubjectCount(study) > 0)
+			{
+				autoSubjectUidcontainer.setEnabled(false);
+				subjectUidcontainer.setEnabled(false);
+				//target.addComponent(subjectUidcontainer);
+			}
+		
+			// Example auto-generated SubjectUID
+			Label subjectUidExampleLbl = detailForm.getSubjectUidExampleLbl();
+			subjectUidExampleLbl.setDefaultModelObject(containerForm.getModelObject().getSubjectUidExample());
+			//target.addComponent(subjectUidExampleLbl);
+			
+			List<ModuleVO> modules = new ArrayList<ModuleVO>();
+			Collection<ModuleVO> modulesLinkedToStudy = new  ArrayList<ModuleVO>();
+			try {
+				modules = userService.getModules(true);//source this from a static list or on application startup 
+				modulesLinkedToStudy  = userService.getModulesLinkedToStudy(study.getName(), true);
+				containerForm.getModelObject().setModulesSelected(modulesLinkedToStudy);
+				containerForm.getModelObject().setModulesAvailable(modules);
+				
+			} catch (ArkSystemException e) {
+				//log the error message and notify sys admin to take appropriate action
+				this.error("A system error has occured. Please try after some time.");
+			}
+			
+			
+			studyCrudContainerVO.getSearchResultPanelContainer().setVisible(false);
+			studyCrudContainerVO.getSearchPanelContainer().setVisible(false);
+			studyCrudContainerVO.getDetailPanelContainer().setVisible(true);
+			studyCrudContainerVO.getDetailPanelFormContainer().setEnabled(false);
+			
+			studyCrudContainerVO.getViewButtonContainer().setVisible(true);//saveBtn
+			studyCrudContainerVO.getViewButtonContainer().setEnabled(true);//saveBtn
+			
+			studyCrudContainerVO.getEditButtonContainer().setVisible(false);
+			studyCrudContainerVO.getSummaryContainer().setVisible(true);
+
+		}
+	}
 
 	protected WebMarkupContainer initialiseSearchPanel(){
 		
