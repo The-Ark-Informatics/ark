d07990712fd0a625b6a66318449ff3c0f795d66f
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-03-31 06:02:09 +0000

    Tidied up class. Add todo's.
    git-svn-id: svn://localhost@3202 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/form/ArkFormVisitor.java
+++ ark-common/src/main/java/au/org/theark/core/web/form/ArkFormVisitor.java
@@ -6,12 +6,11 @@ import java.util.Set;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.Component.IVisitor;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 
 import au.org.theark.core.web.component.ArkErrorHighlightBehaviour;
 import au.org.theark.core.web.component.ArkRequiredBorder;
-import au.org.theark.core.web.component.ArkValidationMsgBehaviour;
 
+@SuppressWarnings("rawtypes")
 public class ArkFormVisitor implements IVisitor, Serializable
 {
 	/**
@@ -20,15 +19,18 @@ public class ArkFormVisitor implements IVisitor, Serializable
 	private static final long	serialVersionUID	= -1613309540699904032L;
 	Set	visited	= new HashSet();
 
+	@SuppressWarnings({ "unchecked", "deprecation" })
 	public Object component(Component c)
 	{
 		if (!visited.contains(c))
 		{
 			visited.add(c);
+			//TODO: Implement using non deprecated method
 			c.setComponentBorder(new ArkRequiredBorder());
-			//TODO: Inplement msg validation neater
+			//TODO: Implement message validation neater
 			//c.add(new ArkValidationMsgBehaviour());
-			c.add(new ArkErrorHighlightBehaviour());
+			//TODO: Implement error highlighting
+			//c.add(new ArkErrorHighlightBehaviour());
 		}
 		return IVisitor.CONTINUE_TRAVERSAL;
 	}
