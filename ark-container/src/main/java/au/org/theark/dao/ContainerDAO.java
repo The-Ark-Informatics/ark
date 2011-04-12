b7b4cea1bed9a389c3700474ef5993b4ebb8d35b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-04-12 09:19:29 +0000

    git-svn-id: svn://localhost@3264 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/dao/ContainerDAO.java
+++
@@ -1,9 +0,0 @@
-package au.org.theark.dao;
-
-import au.org.theark.core.exception.ArkSystemException;
-
-public interface ContainerDAO {
-	
-	public void getStudy(String name) throws ArkSystemException;
-
-}
