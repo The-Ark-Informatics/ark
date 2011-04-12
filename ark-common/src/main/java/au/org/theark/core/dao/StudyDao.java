38c05093204935301f2de7d379aeb1b03049ea91
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-04-12 04:57:21 +0000

    Add implementation for 1.	public boolean isSuperAdministrator(String userName); 2. public boolean isAdministator(String userName); 3.	public List<ArkRole> getArkRoles(); 4. protected ArkUser getArkUser(String ldapUserName);//uses stateless session, cannot be used for front end. 5. protected ArkRole getArkRoleByName(String roleName);
    git-svn-id: svn://localhost@3259 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/dao/StudyDao.java
+++ ark-common/src/main/java/au/org/theark/core/dao/StudyDao.java
@@ -1,9 +1,3 @@
-/**
- * 
- * This is a new file
- *
- *
- */
 package au.org.theark.core.dao;
 
 import java.util.ArrayList;
@@ -16,7 +10,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.hibernate.Criteria;
-import org.hibernate.Query;
+import org.hibernate.StatelessSession;
 import org.hibernate.criterion.Example;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
@@ -30,6 +24,9 @@ import au.org.theark.core.exception.EntityNotFoundException;
 import au.org.theark.core.exception.StatusNotAvailableException;
 import au.org.theark.core.model.study.entity.AddressStatus;
 import au.org.theark.core.model.study.entity.AddressType;
+import au.org.theark.core.model.study.entity.ArkRole;
+import au.org.theark.core.model.study.entity.ArkUser;
+import au.org.theark.core.model.study.entity.ArkUserRole;
 import au.org.theark.core.model.study.entity.AuditHistory;
 import au.org.theark.core.model.study.entity.Consent;
 import au.org.theark.core.model.study.entity.ConsentAnswer;
@@ -604,5 +601,111 @@ public class StudyDao<T>  extends HibernateSessionDao implements IStudyDao{
 		Example subjectUidToken = Example.create(new SubjectUidToken());
 		Criteria studyStatusCriteria = getSession().createCriteria(SubjectUidToken.class).add(subjectUidToken);
 		return   studyStatusCriteria.list();
+	}
+
+	
+	/**
+	 * Looks up a ArkUser based on a String that represents the user name in LDAP.
+	 * If the user name provided is null or is invalid/does not exist in the database, the method will return NULL for a ArkUser
+	 * instance.
+	 * @param ldapUserName
+	 * @return ArkUser
+	 */
+	protected ArkUser getArkUser(String ldapUserName){
+		StatelessSession session = getStatelessSession();
+		Criteria criteria = session.createCriteria(ArkUser.class);
+		criteria.add(Restrictions.eq("ldapUserName", ldapUserName));
+		session.close();
+		return (ArkUser)criteria.uniqueResult();
+	}
+	
+	/**
+	 * Returns a list of ArkRole objects from the backend. This does not use the Stateless session.
+	 * Can be used by front-end client's.
+	 * @return  List<ArkRole>
+	 */
+	public List<ArkRole> getArkRoles(){
+		
+		Criteria criteria = getSession().createCriteria(ArkRole.class);
+		List<ArkRole> arkRoleList = criteria.list();
+		return arkRoleList;
+	}
+	
+	/**<p>
+	 * Given a Ark Role name like Super Administrator or Administrator the method will return the actual instance of
+	 * ArkRole object.</p>
+	 * @param roleName
+	 * @return ArkRole
+	 */
+	protected ArkRole getArkRoleByName(String roleName){
+		StatelessSession session = getStatelessSession();
+		Criteria criteria = session.createCriteria(ArkRole.class);
+		criteria.add(Restrictions.eq("name", roleName));
+		ArkRole arkRole = (ArkRole)criteria.uniqueResult();
+		session.close();
+		return arkRole;
+	}
+	
+	/**
+	 * <p>
+	 * The method takes in a LdapUserName and queries the Database using a Stateless session.
+	 * The query is again Ark_User_Role table/ArkUserRole entity. The getArkRoleByName is invoked
+	 * to get a object that represents Administrator. The method uses this to then check if the given user
+	 * is/has a role of the type in ArkUserRole table/instance. If yes a boolean true is returned and otherwise
+	 * false is returned.</p><br>
+	 * @return boolean 
+	 */
+	public boolean isAdministator(String ldapUserName) {
+		
+		boolean isAdministrator = false;
+		
+		StatelessSession session = getStatelessSession();
+		//Check or get user ark_user object based on ldapUserName
+		ArkUser arkUser  = getArkUser(ldapUserName);
+		if(arkUser != null){
+			Criteria criteria = session.createCriteria(ArkUserRole.class);
+			ArkRole arkRole  = getArkRoleByName(Constants.ARK_ROLE_ADMINISTATOR);
+			criteria.add(Restrictions.eq("arkRole", arkRole));
+			ArkUserRole arkUserRole = (ArkUserRole) criteria.uniqueResult();
+			if(arkUserRole != null){
+				isAdministrator = true;
+			}
+		}
+		
+		session.close();//Close the session here since it is stateless.  
+		return isAdministrator;
+	}
+
+	/**
+	 * <p>
+	 * The method takes in a LdapUserName and queries the Database using a Stateless session.
+	 * The query is again Ark_User_Role table/ArkUserRole entity. The getArkRoleByName is invoked
+	 * to get a object that represents Administrator. The method uses this to then check if the given user
+	 * is/has a role of the type in ArkUserRole table/instance. If yes a boolean true is returned and otherwise
+	 * false is returned.</p><br>
+	 * 
+	 * @return boolean 
+	 * @see au.org.theark.core.dao.IStudyDao#isAdministator(java.lang.String)
+	 */
+	public boolean isSuperAdministrator(String ldapUserName) {
+		
+		boolean isSuperAdministrator = false;
+		
+		StatelessSession session = getStatelessSession();
+		//Check or get user ark_user object based on ldapUserName
+		ArkUser arkUser  = getArkUser(ldapUserName);
+		if(arkUser != null){
+			Criteria criteria = session.createCriteria(ArkUserRole.class);
+			ArkRole arkRole  = getArkRoleByName(Constants.ARK_ROLE_SUPER_ADMINISTATOR);
+			criteria.add(Restrictions.eq("arkRole", arkRole));
+			ArkUserRole arkUserRole = (ArkUserRole) criteria.uniqueResult();
+			if(arkUserRole != null){
+				isSuperAdministrator = true;
+			}
+		}
+		
+		session.close();//Close the session here since it is stateless.  
+		return isSuperAdministrator;
+	
 	}
 }
