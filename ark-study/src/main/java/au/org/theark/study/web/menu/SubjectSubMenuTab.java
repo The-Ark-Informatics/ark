23bde1e7f3845d895d332670a98b1df895a964f8
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-04-20 08:18:51 +0000

    Loads the module and usecase id into the session. Some code is commented i.e the reference to ArkLdapRealm in Common.
    git-svn-id: svn://localhost@3359 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/menu/SubjectSubMenuTab.java
+++ ark-study/src/main/java/au/org/theark/study/web/menu/SubjectSubMenuTab.java
@@ -9,14 +9,23 @@ package au.org.theark.study.web.menu;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.Constants;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.model.study.entity.ArkUsecase;
+import au.org.theark.core.security.ArkLdapRealm;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.MenuModule;
 import au.org.theark.study.web.component.address.AddressContainerPanel;
 import au.org.theark.study.web.component.consent.ConsentContainerPanel;
@@ -31,8 +40,16 @@ import au.org.theark.study.web.component.subjectUpload.SubjectUploadContainerPan
  */
 public class SubjectSubMenuTab extends Panel{
 	
+	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService iArkCommonService;
+	
+//	@SpringBean( name="arkLdapRealm")
+//	private ArkLdapRealm realm;
+	
 	private WebMarkupContainer	arkContextMarkup;
 	List<ITab> tabList;
+	ArkUsecase  arkUseCase;
+	ArkModule arkModule;
 	/**
 	 * @param id
 	 */
@@ -97,32 +114,44 @@ public class SubjectSubMenuTab extends Panel{
 					
 					if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_SUBJECT_DETAIL)){
 						
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_SUBJECT); //Place a default use case into session
 						panelToReturn = new SubjectContainer(panelId, arkContextMarkup);//Note the constructor
 					
 					}
 					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_PERSON_PHONE)){
-					
+
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_PHONE); //Place a default use case into session
 						panelToReturn = new PhoneContainerPanel(panelId);
 					
 					}
 					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_PERSON_ADDRESS)){
-						
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_ADDRESS); //Place a default use case into session
 						panelToReturn = new AddressContainerPanel(panelId);
-					
 					}
 					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_SUBJECT_CONSENT)){
-						
+
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_CONSENT); //Place a default use case into session
 						panelToReturn = new ConsentContainerPanel(panelId);
 					
 					}
 					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_SUBJECT_SUBJECT_FILE)){
-						
+
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_SUBJECT_FILE); //Place a default use case into session
 						panelToReturn = new SubjectFileContainerPanel(panelId);
 					}
 					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_SUBJECT_SUBJECT_UPLOAD)){
 						
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_SUBJECT_UPLOAD); //Place a default use case into session
 						panelToReturn = new SubjectUploadContainerPanel(panelId);
 					}
+					
+					arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_SUBJECT); //Place a default module into session
+					SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_USECASE_KEY, arkUseCase.getId());
+					SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
+					SecurityManager securityManager =  ThreadContext.getSecurityManager();
+					Subject currentUser = SecurityUtils.getSubject();	
+					//realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
+					//securityManager.hasRole(currentUser.getPrincipals(), "Administrator");//Enforce authorization check here so it loads the roles
 					return panelToReturn;
 				};
 			});
