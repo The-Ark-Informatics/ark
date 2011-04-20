0d419e39f612b254785fae6498ae7f62e837ca7b
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-04-20 08:17:32 +0000

    Loaded the usecase and module id based on the particular module/usecase tab that was selected. Some of the code is commented as it is not going to  used with the existing ArkLdapRealm.
    git-svn-id: svn://localhost@3358 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
+++ ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.Constants;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.model.study.entity.ArkUsecase;
+import au.org.theark.core.security.ArkLdapRealm;
 import au.org.theark.core.security.ArkSecurityManager;
 import au.org.theark.core.security.RoleConstants;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.ArkUserVO;
 import au.org.theark.core.vo.MenuModule;
 import au.org.theark.study.web.component.customfield.CustomFieldContainer;
@@ -27,11 +34,21 @@ import au.org.theark.study.web.component.user.UserContainer;
 @SuppressWarnings("serial")
 public class StudySubMenuTab extends Panel
 {
-
+	
+	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService iArkCommonService;
+	
+	//NN Commented not used as yet
+	//	@SpringBean( name="arkLdapRealm")
+	//	private ArkLdapRealm realm;
+	
 	List<ITab> tabList;
 	private WebMarkupContainer	studyNameMarkup;
 	private WebMarkupContainer	studyLogoMarkup;
 	private WebMarkupContainer	arkContextMarkup;
+	
+	private ArkUsecase  arkUseCase;
+	private ArkModule arkModule;
 
 	public StudySubMenuTab(String id)
 	{
@@ -106,9 +123,12 @@ public class StudySubMenuTab extends Panel
 					boolean flag = false;
 					if (moduleName.getModuleName().equalsIgnoreCase(Constants.USER) || moduleName.getModuleName().equalsIgnoreCase(Constants.SUBJECT))
 					{
-
-						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
+						
 						Subject currentUser = SecurityUtils.getSubject();
+						//Clear Cache here to load the roles & permissions for the user in session.The very next auth check will delegate the call to doAuthorizationInfo()
+						//realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
+						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
+					
 						if (  (arkSecurityManager.subjectHasRole(RoleConstants.SUPER_ADMIN))|| (arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN) || (arkSecurityManager.subjectHasRole(RoleConstants.STUDY_ADMIN))))
 						{
 
@@ -135,13 +155,13 @@ public class StudySubMenuTab extends Panel
 
 					if (moduleName.getModuleName().equalsIgnoreCase(Constants.USER))
 					{
-
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_USER); //Place a default use case into session
 						panelToReturn = new UserContainer(panelId, new ArkUserVO());// Note the constructor
 
 					}
 					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_DETAIL))
 					{
-
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_STUDY); //Place a default use case into session
 						panelToReturn = new StudyContainer(panelId, studyNameMarkup, studyLogoMarkup, arkContextMarkup);
 
 					}
@@ -153,7 +173,7 @@ public class StudySubMenuTab extends Panel
 //					}
 					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_COMPONENT))
 					{
-
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_STUDY_COMPONENT); //Place a default use case into session
 						panelToReturn = new StudyComponentContainerPanel(panelId);
 
 					}
@@ -164,12 +184,21 @@ public class StudySubMenuTab extends Panel
 //					}
 					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.MY_DETAIL))
 					{
-
+						arkUseCase = iArkCommonService.getArkUsecaseByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_MY_DETAIL); //Place a default use case into session
 						Subject currentUser = SecurityUtils.getSubject();
 						panelToReturn = new MyDetailsContainer(panelId, new ArkUserVO(), currentUser);
 
 					}
 					
+					arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_STUDY); //Place a default module into session
+					SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_USECASE_KEY, arkUseCase.getId());
+					SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
+				
+					//refresh cache (NN Commented not used as yet)
+					//SecurityManager securityManager =  ThreadContext.getSecurityManager();
+					//Subject currentUser = SecurityUtils.getSubject();	
+					//realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
+					//securityManager.hasRole(currentUser.getPrincipals(), "Administrator");//Enforce authorization check here so it loads the roles
 					return panelToReturn;
 				};
 			});
