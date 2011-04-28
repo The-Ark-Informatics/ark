3c6ff28ec1a4632bd613bd5e617b754232f3f93a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-04-28 04:55:15 +0000

    An entity that maps to LINK_STUDY_ARKMODULE table.
    git-svn-id: svn://localhost@3397 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/LinkStudyArkModule.java
@@ -0,0 +1,60 @@
+package au.org.theark.core.model.study.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "link_study_arkmodule", schema = Constants.STUDY_SCHEMA)
+public class LinkStudyArkModule implements Serializable{
+	
+	private Long id;
+	private Study study;
+	private ArkModule arkModule;
+	
+	/**
+	 * Constructor
+	 */
+	public LinkStudyArkModule(){
+		
+	}
+
+	@Id
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "STUDY_ID")
+	public Study getStudy() {
+		return study;
+	}
+
+	public void setStudy(Study study) {
+		this.study = study;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ARK_MODULE_ID")
+	public ArkModule getArkModule() {
+		return arkModule;
+	}
+
+	public void setArkModule(ArkModule arkModule) {
+		this.arkModule = arkModule;
+	}
+
+}
