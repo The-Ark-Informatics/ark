94d3598e39022adb5a6104c8c5c3a1d1b1b8d246
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-12 09:26:04 +0000

    patch for ark function
    git-svn-id: svn://localhost@3506 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/20110322/mysql/20110512_study_ark_function_schemaPatch.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `ark_function`;
+CREATE TABLE `ark_function` (
+  `ID` int(11) NOT NULL auto_increment,
+  `NAME` varchar(255) default NULL,
+  `DESCRIPTION` varchar(1000) default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB;
