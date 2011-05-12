abe15053308821af09a96e39111196a8c43037e9
adrianb <adrianb@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-12 06:59:17 +0000

    Added CorrespondenceAttachment data members and member functions.
    git-svn-id: svn://localhost@3500 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/vo/CorrespondenceVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/CorrespondenceVO.java
@@ -4,16 +4,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import au.org.theark.core.model.study.entity.CorrespondenceAttachment;
 import au.org.theark.core.model.study.entity.Correspondences;
 
 public class CorrespondenceVO implements Serializable {
 
 	private Correspondences correspondence;
 	private Collection<Correspondences> correspondenceList;
-	
+	// TODO: why is this single item needed if we have a collection?
+	private CorrespondenceAttachment correspondenceAttachment;
+	private Collection<CorrespondenceAttachment> correspondenceAttachmentList;
+
 	public CorrespondenceVO() {
 		correspondence = new Correspondences();
 		correspondenceList = new ArrayList<Correspondences>();
+		correspondenceAttachment = new CorrespondenceAttachment();
+		correspondenceAttachmentList = new ArrayList<CorrespondenceAttachment>();
 	}
 	
 	public Correspondences getCorrespondence() {
@@ -30,6 +36,24 @@ public class CorrespondenceVO implements Serializable {
 	
 	public void setCorrespondenceList(Collection<Correspondences> correspondenceList) {
 		this.correspondenceList = correspondenceList;
+	}
+
+	public CorrespondenceAttachment getCorrespondenceAttachment() {
+		return correspondenceAttachment;
+	}
+
+	public void setCorrespondenceAttachment(
+			CorrespondenceAttachment correspondenceAttachment) {
+		this.correspondenceAttachment = correspondenceAttachment;
+	}
+
+	public Collection<CorrespondenceAttachment> getCorrespondenceAttachmentList() {
+		return correspondenceAttachmentList;
+	}
+
+	public void setCorrespondenceAttachmentList(
+			Collection<CorrespondenceAttachment> correspondenceAttachmentList) {
+		this.correspondenceAttachmentList = correspondenceAttachmentList;
 	}
 	
 }
