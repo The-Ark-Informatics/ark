5611affcf78f766281527998ba1fa1b8fc3a193c
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-24 04:33:21 +0000

    Revised the ArkAjaxButton, which is an IndicatingAjaxButton that will in addition to automatically (button disabled as per when the Indicator is shown).
    git-svn-id: svn://localhost@3547 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/component/ArkAjaxButton.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/ArkAjaxButton.java
@@ -1,8 +1,11 @@
 package au.org.theark.core.web.component;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.IAjaxCallDecorator;
 import org.apache.wicket.ajax.calldecorator.AjaxPostprocessingCallDecorator;
 import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
 
 public abstract class ArkAjaxButton extends IndicatingAjaxButton{
 
@@ -10,21 +13,36 @@ public abstract class ArkAjaxButton extends IndicatingAjaxButton{
 	 * 
 	 */
 	private static final long serialVersionUID = 5799668393803636626L;
-
+	
 	public ArkAjaxButton(String id) {
 		super(id);
 		setOutputMarkupPlaceholderTag(true);
-		setOutputMarkupId(true);
 	}
 	
-	@Override
+	public ArkAjaxButton(String id, IModel<String> model)
+	{
+		super(id, model, null);
+		setOutputMarkupPlaceholderTag(true);
+	}
+	
+    @Override
     protected IAjaxCallDecorator getAjaxCallDecorator() {
         return new AjaxPostprocessingCallDecorator(super.getAjaxCallDecorator()) {
             private static final long serialVersionUID = 1L;
  
             @Override
             public CharSequence postDecorateScript(CharSequence script) {
-                return script + "this.disabled = true;";
+                return script + "document.getElementById('" + getMarkupId() + "').disabled = true;";
+            }
+            
+            @Override
+            public CharSequence postDecorateOnFailureScript(CharSequence script) {
+                return script + "document.getElementById('" + getMarkupId() + "').disabled = false;";
+            }
+            
+            @Override
+            public CharSequence postDecorateOnSuccessScript(CharSequence script) {
+                return script + "document.getElementById('" + getMarkupId() + "').disabled = false;";
             }
         };
     }
