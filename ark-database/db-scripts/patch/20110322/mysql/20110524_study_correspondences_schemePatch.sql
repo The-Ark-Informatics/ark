bc9651b3f8dccadf5c1fa111b2cc978118729b72
adrianb <adrianb@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-27 06:44:11 +0000

    Fixing a typo in the patch filename.
    git-svn-id: svn://localhost@3590 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110322/mysql/20110524_study_correspondences_schemePatch.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `study`.`correspondences` ADD COLUMN `PERSON_ID` INT(11) NOT NULL  AFTER `ID` ,
-  ADD CONSTRAINT `correspondences_person_id`
-  FOREIGN KEY (`PERSON_ID` )
-  REFERENCES `study`.`person` (`ID` )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION
-, ADD INDEX `correspondences_person_id` (`PERSON_ID` ASC) ;
-
