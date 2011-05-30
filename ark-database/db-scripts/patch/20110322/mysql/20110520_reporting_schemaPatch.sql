d6fd09bb87f9393f74eaf98ea64b3e1435813bd2
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-30 09:47:43 +0000

    Schema update patch for ark-reporting module.
    Not complete yet - still waiting for security update to allow for foreign key references to the ark_module and ark_function tables in report_template.
    
    git-svn-id: svn://localhost@3629 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/20110322/mysql/20110520_reporting_schemaPatch.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE `reporting` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `reporting`;
+
+CREATE TABLE `report_output_format` (
+  `ID` int(11) NOT NULL auto_increment,
+  `NAME` varchar(45) NOT NULL,
+  `DESCRIPTION` varchar(250) default NULL,
+  PRIMARY KEY  (`ID`),
+  UNIQUE KEY `NAME_UNIQUE` (`NAME`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `report_template` (
+  `ID` int(11) NOT NULL auto_increment,
+  `NAME` varchar(100) NOT NULL,
+  `DESCRIPTION` varchar(1024) default NULL,
+  `TEMPLATE_PATH` varchar(255) NOT NULL,
+  `MODULE_ID` int(11) default NULL,
+  `FUNCTION_ID` int(11) default NULL,
+  PRIMARY KEY  (`ID`),
+  UNIQUE KEY `NAME_UNIQUE` (`NAME`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- reference data
+
+INSERT INTO `reporting`.`report_output_format`
+(`ID`, `NAME`, `DESCRIPTION`)
+VALUES
+(1, 'PDF', 'Portable Document Format (compatible with Adobe Reader)');
+
+INSERT INTO `reporting`.`report_output_format`
+(`ID`, `NAME`, `DESCRIPTION`)
+VALUES
+(2, 'CSV', 'Comma Separated Value (compatible with Excel)');
+
+INSERT INTO `reporting`.`report_template`
+(`ID`, `NAME`, `TEMPLATE_PATH`)
+VALUES
+(1, 'Study Summary Report', 'StudySummaryReport.jrxml');
+
+INSERT INTO `reporting`.`report_template`
+(`ID`, `NAME`, `TEMPLATE_PATH`)
+VALUES 
+(2, 'Study-level Consent Details Reportt', 'ConsentDetailsReport.jrxml');
