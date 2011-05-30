67354fa9c0a50978bb4001dcd455ccaf8b542cae
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-05-30 08:57:58 +0000

    Accidentally checked in this ArkUserVO.java code that wouldn't compile previously... Committing this fixed version.
    git-svn-id: svn://localhost@3612 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/vo/ArkUserVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/ArkUserVO.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ArkUserVO extends BaseVO implements Comparable {
+public class ArkUserVO extends BaseVO {
 
 	private static final long serialVersionUID = 1L;
 	
