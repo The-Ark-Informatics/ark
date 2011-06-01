9385d768facf5fe888525cf6128fbb086a71799e
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-01 08:41:13 +0000

    Updated/ran
    git-svn-id: svn://localhost@3675 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110322/mysql/20110527_address_schemaPatch.sql
+++ ark-database/db-scripts/patch/20110322/mysql/20110527_address_schemaPatch.sql
@@ -1,4 +1,5 @@
 USE study;
+
 ALTER TABLE `study`.`address` DROP FOREIGN KEY `fk_address_preferred_mailing_address_id` ;
 ALTER TABLE `study`.`address` CHANGE COLUMN `PREFERRED_MAILING_ADDRESS_ID` `PREFERRED_MAILING_ADDRESS` INT(11) NOT NULL  
 , DROP INDEX `fk_address_preferred_mailing_address_id` 
