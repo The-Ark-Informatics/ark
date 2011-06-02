d74b39ffad896374b60dfa95e25ac5b77ea43d78
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-02 07:23:11 +0000

    Pass in size of file for correct reporting on fileSize
    git-svn-id: svn://localhost@3690 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/subjectUpload/SubjectUploadStep4.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subjectUpload/SubjectUploadStep4.java
@@ -79,7 +79,7 @@ public class SubjectUploadStep4 extends AbstractWizardStepPanel
 		try 
 		{
 			InputStream inputStream = containerForm.getModelObject().getFileUpload().getInputStream();
-			uploadReport = iStudyService.uploadAndReportMatrixSubjectFile(inputStream, fileFormat, delimiterChar);
+			uploadReport = iStudyService.uploadAndReportMatrixSubjectFile(inputStream, containerForm.getModelObject().getFileUpload().getSize(), fileFormat, delimiterChar);
 		} 
 		catch (IOException e) 
 		{
