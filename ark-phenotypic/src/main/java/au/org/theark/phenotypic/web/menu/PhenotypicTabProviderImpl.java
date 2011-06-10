74d670fe43bb4be411f4b809becc6b33309ff166
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-10 06:26:59 +0000

    Implemented ArkMainTab
    git-svn-id: svn://localhost@3819 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicTabProviderImpl.java
@@ -3,13 +3,14 @@ package au.org.theark.phenotypic.web.menu;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
 import au.org.theark.core.service.IMainTabProvider;
+import au.org.theark.core.web.component.ArkMainTab;
 import au.org.theark.phenotypic.web.Constants;
 
 @SuppressWarnings("serial")
@@ -44,11 +45,23 @@ public class PhenotypicTabProviderImpl extends Panel implements
 	}
 
 	public ITab createTab(String tabName) {
-		return new AbstractTab(new Model<String>(tabName)) {
+		return new ArkMainTab(new Model<String>(tabName)) {
 			@Override
 			public Panel getPanel(String pid) {
 				// The sub menu(s) for Phenotypic
 				return new PhenotypicSubMenuTab(pid, arkContextPanelMarkup);
+			}
+
+			public boolean isAccessible()
+			{
+				Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+				if(sessionStudyId == null)
+				{
+					this.getPanel(Constants.PHENOTYPIC_MAIN_TAB).error(au.org.theark.core.Constants.STUDY_IN_CONTEXT_MESSAGE);
+					return false;
+				}
+				else
+					return true;
 			}
 		};
 	}
