5ac9dd05be8e78e1785abcf0bb59dd5562b15c48
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-13 12:54:40 +0000

    Implemented equals()
    git-svn-id: svn://localhost@3840 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/ArkModule.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/ArkModule.java
@@ -1,12 +1,17 @@
 package au.org.theark.core.model.study.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -23,9 +28,9 @@ public class ArkModule implements Serializable{
 	private Long id;
 	private String name;
 	private String description;
+
 	
 	public ArkModule(){
-		
 	}
 	
 	@Id
@@ -55,6 +60,31 @@ public class ArkModule implements Serializable{
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArkModule other = (ArkModule) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
