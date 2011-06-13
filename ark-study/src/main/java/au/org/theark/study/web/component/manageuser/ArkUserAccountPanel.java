bfcf8ed910157e63924ed90e4961eb95272842fd
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-13 18:59:15 +0000

    modified populateItem() to pull the roles for a given module as the list is rendered which will serve as the data source for the drop down list and the modules.
    git-svn-id: svn://localhost@3849 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/manageuser/ArkUserAccountPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/manageuser/ArkUserAccountPanel.java
@@ -1,5 +1,6 @@
 package au.org.theark.study.web.component.manageuser;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,11 +15,12 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.model.study.entity.ArkModule;
 import au.org.theark.core.model.study.entity.ArkRole;
+import au.org.theark.core.model.study.entity.ArkUserRole;
 import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.ArkModuleVO;
-import au.org.theark.core.vo.RoleVO;
 import au.org.theark.study.web.Constants;
 import au.org.theark.study.web.component.manageuser.form.ContainerForm;
 /**
@@ -30,11 +32,12 @@ import au.org.theark.study.web.component.manageuser.form.ContainerForm;
 public class ArkUserAccountPanel extends Panel{
 
 	
+	@SuppressWarnings("rawtypes")
 	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
 	
 	private ContainerForm containerForm;
-	
+	//ListView  listView;
 	/**
 	 * Constructor
 	 * @param id
@@ -45,16 +48,17 @@ public class ArkUserAccountPanel extends Panel{
 		super(id);
 		
 		this.containerForm = containerForm;
-		
+		//this.listView = listView;
 		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		Study study = iArkCommonService.getStudy(sessionStudyId);
 		//For the Given study get the linked Modules and the associated Roles for each Module as a List of ArkModuleVO 
 		//Set this list into the ArkUserVO
 		final Collection<ArkModuleVO> listArkModuleVO = iArkCommonService.getArkModulesLinkedToStudy(study);
-		containerForm.getModelObject().setArkModuleVOList((List)listArkModuleVO);
 		
 		
-		ListView  listView = new ListView("moduleRoleList",containerForm.getModelObject().getArkModuleVOList()) {
+		final List<ArkUserRole> arkUserRoleList = containerForm.getModelObject().getArkUserRoleList();
+		
+	ListView listView = new ListView("arkUserRoleList",(List)arkUserRoleList) {
 
 			private static final long serialVersionUID = 1L;
 			
@@ -63,17 +67,25 @@ public class ArkUserAccountPanel extends Panel{
 				
 				
 				//Each item will be ArkModuleVO use that to build the Module name and the drop down
-				ArkModuleVO arkModuleVO = (ArkModuleVO)item.getModelObject();
-				item.add(new Label("moduleName", arkModuleVO.getArkModule().getName()));
-				Model model = new Model(arkModuleVO);
-				PropertyModel<List<ArkRole>> rolespm = new PropertyModel<List<ArkRole>>(model,"arkModuleRoles");
+				ArkUserRole arkUserRole = (ArkUserRole)item.getModelObject();
+				ArkModule arkModule = arkUserRole.getArkModule();
+				ArrayList<ArkRole> arkRoleList = iArkCommonService.getArkRoleLinkedToModule(arkModule);
+				
+				PropertyModel arkUserRolePm = new PropertyModel(arkUserRole,"arkRole");
 				ChoiceRenderer<ArkRole> defaultChoiceRenderer = new ChoiceRenderer<ArkRole>(Constants.NAME, "id");
-				DropDownChoice<ArkRole> ddc = new DropDownChoice<ArkRole>("arkRole",new Model(),rolespm,defaultChoiceRenderer);
+				
+				DropDownChoice<ArkRole> ddc = new DropDownChoice<ArkRole>("arkRole",arkUserRolePm,arkRoleList,defaultChoiceRenderer);
+				
+				item.add(new Label("moduleName", arkModule.getName()));//arkModule within ArkUserRole
 				item.add(ddc);
+				
 			}
 		};
-		
+
+
 		this.add(listView);
 	}
+	
+
 
 }
