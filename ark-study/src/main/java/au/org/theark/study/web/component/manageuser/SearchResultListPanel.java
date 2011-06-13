f8a367fb956acdfa560c19f3b5f43b6ba4fe2e8a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-13 19:02:27 +0000

    Implemented (WIP) onClick - load the user from backend(ldap+ backend details)
    git-svn-id: svn://localhost@3853 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/manageuser/SearchResultListPanel.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/manageuser/SearchResultListPanel.java
@@ -1,7 +1,9 @@
 package au.org.theark.study.web.component.manageuser;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -14,11 +16,16 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import au.org.theark.core.exception.ArkSystemException;
+import au.org.theark.core.exception.EntityNotFoundException;
 import au.org.theark.core.model.study.entity.ArkUsecase;
+import au.org.theark.core.model.study.entity.ArkUserRole;
+import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.model.study.entity.StudyStatus;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.ArkCrudContainerVO;
 import au.org.theark.core.vo.ArkUserVO;
+import au.org.theark.study.service.IUserService;
 import au.org.theark.study.web.Constants;
 import au.org.theark.study.web.component.manageuser.form.ContainerForm;
 
@@ -29,6 +36,9 @@ public class SearchResultListPanel extends Panel{
 	
 	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
+	
+	@SpringBean( name = "userService")
+	private IUserService userService;
 	
 	/**
 	 * Constructor
@@ -69,7 +79,43 @@ public class SearchResultListPanel extends Panel{
 		AjaxLink link = new AjaxLink("userName") {
 				@Override
 				public void onClick(AjaxRequestTarget target) {
-					//TODO
+					//TODO Get the ArkUser LDAP details again and the backend Module/Roles details and updat the model
+					
+					try {
+						
+						//Fetch the user and related details from backend
+						Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+						Study study = iArkCommonService.getStudy(sessionStudyId);
+						ArkUserVO arkUserVOFromBackend = userService.lookupArkUser(arkUserVo.getUserName(),study);
+						containerForm.getModelObject().setArkUserRoleList(arkUserVOFromBackend.getArkUserRoleList());
+						containerForm.setModelObject(arkUserVOFromBackend);
+						
+						//Render the UI
+						arkCrudContainerVO.getSearchResultPanelContainer().setVisible(false);
+						arkCrudContainerVO.getSearchPanelContainer().setVisible(false);
+						arkCrudContainerVO.getDetailPanelContainer().setVisible(true);
+						arkCrudContainerVO.getDetailPanelFormContainer().setEnabled(false);
+						arkCrudContainerVO.getViewButtonContainer().setVisible(true);//saveBtn
+						arkCrudContainerVO.getViewButtonContainer().setEnabled(true);//saveBtn
+						arkCrudContainerVO.getEditButtonContainer().setVisible(false);
+						
+						
+						target.addComponent(arkCrudContainerVO.getSearchPanelContainer());
+						target.addComponent(arkCrudContainerVO.getDetailPanelContainer());
+						target.addComponent(arkCrudContainerVO.getSearchResultPanelContainer());
+						target.addComponent(arkCrudContainerVO.getViewButtonContainer());
+						target.addComponent(arkCrudContainerVO.getEditButtonContainer());
+						
+						target.addComponent(arkCrudContainerVO.getDetailPanelFormContainer());
+						target.addComponent(containerForm);
+					} catch (ArkSystemException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (EntityNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 				}
 		};
 		//Add the label for the link
