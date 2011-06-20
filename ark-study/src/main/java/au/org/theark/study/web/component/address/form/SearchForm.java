07dd212b7a8df99188dd508b7bd97b042e5cb6f1
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-20 10:24:40 +0000

    Removed references to isSecure
    git-svn-id: svn://localhost@3966 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/component/address/form/SearchForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/address/form/SearchForm.java
@@ -243,13 +243,4 @@ public class SearchForm extends AbstractSearchForm<AddressVO>
 		}
 
 	}
-
-	/* (non-Javadoc)
-	 * @see au.org.theark.core.web.form.AbstractSearchForm#isSecure(java.lang.String)
-	 */
-	@Override
-	protected boolean isSecure(String actionType) {
-		// TODO Auto-generated method stub
-		return true;
-	}
 }
