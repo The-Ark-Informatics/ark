fc9f267facb1001fbb4cc445a32dacdbc7ab58c7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-20 06:55:06 +0000

    Formatted code, implemented isSecure (to hide New button)
    git-svn-id: svn://localhost@3945 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/subject/form/SearchForm.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/subject/form/SearchForm.java
@@ -33,6 +33,7 @@ import au.org.theark.core.model.study.entity.Person;
 import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.model.study.entity.SubjectStatus;
 import au.org.theark.core.model.study.entity.VitalStatus;
+import au.org.theark.core.security.PermissionConstants;
 import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.SubjectVO;
@@ -42,66 +43,50 @@ import au.org.theark.lims.web.Constants;
 
 /**
  * @author nivedann
- *
+ * 
  */
-public class SearchForm extends AbstractSearchForm<SubjectVO>{
-
+public class SearchForm extends AbstractSearchForm<SubjectVO>
+{
 
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 5853988156214275754L;
+	private static final long						serialVersionUID	= 5853988156214275754L;
 
-	@SpringBean( name =  au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService<Void> iArkCommonService;
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>				iArkCommonService;
 
-	private TextField<String> subjectUIDTxtFld;
-	private TextField<String> firstNameTxtFld;
-	private TextField<String> middleNameTxtFld;
-	private TextField<String> lastNameTxtFld;
-	private DropDownChoice<VitalStatus> vitalStatusDdc;
-	private DropDownChoice<GenderType> genderTypeDdc;
-	private DropDownChoice<SubjectStatus> subjectStatusDdc;
-	private DateTextField dateOfBirthTxtFld;
-	private CompoundPropertyModel<SubjectVO> cpmModel;
-	
+	private TextField<String>						subjectUIDTxtFld;
+	private TextField<String>						firstNameTxtFld;
+	private TextField<String>						middleNameTxtFld;
+	private TextField<String>						lastNameTxtFld;
+	private DropDownChoice<VitalStatus>			vitalStatusDdc;
+	private DropDownChoice<GenderType>			genderTypeDdc;
+	private DropDownChoice<SubjectStatus>		subjectStatusDdc;
+	private DateTextField							dateOfBirthTxtFld;
+	private CompoundPropertyModel<SubjectVO>	cpmModel;
+
 	/**
 	 * @param id
 	 * @param cpmModel
 	 */
-	public SearchForm(	String id, 
-						CompoundPropertyModel<SubjectVO> cpmModel,
-						PageableListView<SubjectVO> listView, 
-						FeedbackPanel feedBackPanel,
-						WebMarkupContainer listContainer,
-						WebMarkupContainer searchMarkupContainer,
-						WebMarkupContainer detailsContainer,
-						WebMarkupContainer detailPanelFormContainer,
-						WebMarkupContainer viewButtonContainer,
-						WebMarkupContainer editButtonContainer	) {
-		
-		//super(id, cpmModel);
-		super	(id,
-				cpmModel,
-				detailsContainer,
-				detailPanelFormContainer,
-				viewButtonContainer,
-				editButtonContainer,
-				searchMarkupContainer,
-				listContainer,
-				feedBackPanel);
-		
+	public SearchForm(String id, CompoundPropertyModel<SubjectVO> cpmModel, PageableListView<SubjectVO> listView, FeedbackPanel feedBackPanel, WebMarkupContainer listContainer,
+			WebMarkupContainer searchMarkupContainer, WebMarkupContainer detailsContainer, WebMarkupContainer detailPanelFormContainer, WebMarkupContainer viewButtonContainer,
+			WebMarkupContainer editButtonContainer)
+	{
+
+		// super(id, cpmModel);
+		super(id, cpmModel, detailsContainer, detailPanelFormContainer, viewButtonContainer, editButtonContainer, searchMarkupContainer, listContainer, feedBackPanel);
+
 		this.cpmModel = cpmModel;
 		initialiseSearchForm();
 		addSearchComponentsToForm();
-		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		disableSearchForm(sessionStudyId, "There is no study in context. Please select a study");
-		
-		// Disable New button in LIMS
-		newButton.setVisible(false);
 	}
-	
-	protected void addSearchComponentsToForm(){
+
+	protected void addSearchComponentsToForm()
+	{
 		add(subjectUIDTxtFld);
 		add(firstNameTxtFld);
 		add(middleNameTxtFld);
@@ -111,8 +96,9 @@ public class SearchForm extends AbstractSearchForm<SubjectVO>{
 		add(genderTypeDdc);
 		add(dateOfBirthTxtFld);
 	}
-	
-	protected void initialiseSearchForm(){
+
+	protected void initialiseSearchForm()
+	{
 		subjectUIDTxtFld = new TextField<String>(Constants.SUBJECT_UID);
 		firstNameTxtFld = new TextField<String>(Constants.PERSON_FIRST_NAME);
 		middleNameTxtFld = new TextField<String>(Constants.PERSON_MIDDLE_NAME);
@@ -120,58 +106,63 @@ public class SearchForm extends AbstractSearchForm<SubjectVO>{
 		initVitalStatusDdc();
 		initSubjectStatusDdc();
 		initGenderTypeDdc();
-		
-		dateOfBirthTxtFld = new DateTextField(Constants.PERSON_DOB,au.org.theark.core.Constants.DD_MM_YYYY);
+
+		dateOfBirthTxtFld = new DateTextField(Constants.PERSON_DOB, au.org.theark.core.Constants.DD_MM_YYYY);
 		ArkDatePicker dobDatePicker = new ArkDatePicker();
 		dobDatePicker.bind(dateOfBirthTxtFld);
 		dateOfBirthTxtFld.add(dobDatePicker);
 	}
 
-	private void initVitalStatusDdc(){
+	private void initVitalStatusDdc()
+	{
 		CompoundPropertyModel<SubjectVO> subjectCpm = cpmModel;
-		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm,"linkSubjectStudy");
-		PropertyModel<Person> personPm = new PropertyModel<Person>(linkSubjectStudyPm,"person");
-		PropertyModel<VitalStatus> vitalStatusPm = new PropertyModel<VitalStatus>(personPm,Constants.VITAL_STATUS);
+		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm, "linkSubjectStudy");
+		PropertyModel<Person> personPm = new PropertyModel<Person>(linkSubjectStudyPm, "person");
+		PropertyModel<VitalStatus> vitalStatusPm = new PropertyModel<VitalStatus>(personPm, Constants.VITAL_STATUS);
 		Collection<VitalStatus> vitalStatusList = iArkCommonService.getVitalStatus();
 		ChoiceRenderer vitalStatusRenderer = new ChoiceRenderer(Constants.NAME, Constants.ID);
-		vitalStatusDdc = new DropDownChoice<VitalStatus>(Constants.VITAL_STATUS,vitalStatusPm,(List)vitalStatusList,vitalStatusRenderer);
+		vitalStatusDdc = new DropDownChoice<VitalStatus>(Constants.VITAL_STATUS, vitalStatusPm, (List) vitalStatusList, vitalStatusRenderer);
 	}
-	
-	private void initSubjectStatusDdc(){
+
+	private void initSubjectStatusDdc()
+	{
 		CompoundPropertyModel<SubjectVO> subjectCpm = cpmModel;
-		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm,"linkSubjectStudy");
-		PropertyModel<SubjectStatus> subjectStatusPm = new PropertyModel<SubjectStatus>(linkSubjectStudyPm,"subjectStatus");
+		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm, "linkSubjectStudy");
+		PropertyModel<SubjectStatus> subjectStatusPm = new PropertyModel<SubjectStatus>(linkSubjectStudyPm, "subjectStatus");
 		List<SubjectStatus> subjectStatusList = iArkCommonService.getSubjectStatus();
-		ChoiceRenderer subjectStatusRenderer = new ChoiceRenderer(Constants.NAME,Constants.SUBJECT_STATUS_ID);
-		subjectStatusDdc = new DropDownChoice<SubjectStatus>(Constants.SUBJECT_STATUS,subjectStatusPm,subjectStatusList,subjectStatusRenderer);
+		ChoiceRenderer subjectStatusRenderer = new ChoiceRenderer(Constants.NAME, Constants.SUBJECT_STATUS_ID);
+		subjectStatusDdc = new DropDownChoice<SubjectStatus>(Constants.SUBJECT_STATUS, subjectStatusPm, subjectStatusList, subjectStatusRenderer);
 	}
-	
-	private void initGenderTypeDdc(){
+
+	private void initGenderTypeDdc()
+	{
 		CompoundPropertyModel<SubjectVO> subjectCpm = cpmModel;
-		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm,"linkSubjectStudy");
-		PropertyModel<Person> personPm = new PropertyModel<Person>(linkSubjectStudyPm,Constants.PERSON);
-		PropertyModel<GenderType> genderTypePm = new PropertyModel<GenderType>(personPm,Constants.GENDER_TYPE);
-		Collection<GenderType> genderTypeList = iArkCommonService.getGenderType(); 
-		ChoiceRenderer genderTypeRenderer = new ChoiceRenderer(Constants.NAME,Constants.ID);
-		genderTypeDdc = new DropDownChoice<GenderType>(Constants.GENDER_TYPE,genderTypePm, (List)genderTypeList,genderTypeRenderer);
+		PropertyModel<LinkSubjectStudy> linkSubjectStudyPm = new PropertyModel<LinkSubjectStudy>(subjectCpm, "linkSubjectStudy");
+		PropertyModel<Person> personPm = new PropertyModel<Person>(linkSubjectStudyPm, Constants.PERSON);
+		PropertyModel<GenderType> genderTypePm = new PropertyModel<GenderType>(personPm, Constants.GENDER_TYPE);
+		Collection<GenderType> genderTypeList = iArkCommonService.getGenderType();
+		ChoiceRenderer genderTypeRenderer = new ChoiceRenderer(Constants.NAME, Constants.ID);
+		genderTypeDdc = new DropDownChoice<GenderType>(Constants.GENDER_TYPE, genderTypePm, (List) genderTypeList, genderTypeRenderer);
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	protected void onNew(AjaxRequestTarget target) {
+	protected void onNew(AjaxRequestTarget target)
+	{
 		// ARK-108:: no longer do full reset to VO
 		// Set a default Country on new when the Country field is empty
-		if (getModelObject().getLinkSubjectStudy().getCountry() == null) {
+		if (getModelObject().getLinkSubjectStudy().getCountry() == null)
+		{
 			final List<Country> countryList = iArkCommonService.getCountries();
-			//getModelObject().getSubjectStudy().setCountry(countryList.get(0));
-			
+			// getModelObject().getSubjectStudy().setCountry(countryList.get(0));
+
 			getModelObject().getLinkSubjectStudy().setCountry(iArkCommonService.getCountry(au.org.theark.core.Constants.DEFAULT_COUNTRY_CODE));
 		}
 		updateDetailFormPrerender(getModelObject().getLinkSubjectStudy());
-		
+
 		WebMarkupContainer wmc = (WebMarkupContainer) detailFormCompContainer;
-		
+
 		// Disable SubjectUID if auto-generation set
-		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		Study studyInContext = iArkCommonService.getStudy(sessionStudyId);
 		if ((studyInContext != null) && (studyInContext.getAutoGenerateSubjectUid()))
 		{
@@ -190,61 +181,83 @@ public class SearchForm extends AbstractSearchForm<SubjectVO>{
 		preProcessDetailPanel(target);
 	}
 
-	public void updateDetailFormPrerender(LinkSubjectStudy linkSubjectStudy){
+	public void updateDetailFormPrerender(LinkSubjectStudy linkSubjectStudy)
+	{
 		List<CountryState> countryStateList = iArkCommonService.getStates(linkSubjectStudy.getCountry());
 		WebMarkupContainer wmcStateSelector = (WebMarkupContainer) detailFormCompContainer.get(Constants.COUNTRY_STATE_SELECTOR_WMC);
-		DropDownChoice<CountryState> stateChoice = (DropDownChoice<CountryState> )wmcStateSelector.get(Constants.SUBJECT_STATE);
-		TextField<String> otherState = (TextField<String>)wmcStateSelector.get(Constants.SUBJECT_OTHER_STATE);
-		
-		if(countryStateList != null && countryStateList.size() > 0){
+		DropDownChoice<CountryState> stateChoice = (DropDownChoice<CountryState>) wmcStateSelector.get(Constants.SUBJECT_STATE);
+		TextField<String> otherState = (TextField<String>) wmcStateSelector.get(Constants.SUBJECT_OTHER_STATE);
+
+		if (countryStateList != null && countryStateList.size() > 0)
+		{
 			stateChoice.getChoices().clear();
 			stateChoice.setChoices(countryStateList);
 			stateChoice.setVisible(true);
 			otherState.setVisible(false);
-		}else{
-			//hide it
+		}
+		else
+		{
+			// hide it
 			stateChoice.setVisible(false);
 			otherState.setVisible(true);
 		}
-	
+
 	}
 
-	protected void onSearch(AjaxRequestTarget target){
-		
+	protected void onSearch(AjaxRequestTarget target)
+	{
+
 		target.addComponent(feedbackPanel);
-		Long sessionStudyId = (Long)SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 		getModelObject().getLinkSubjectStudy().setStudy(iArkCommonService.getStudy(sessionStudyId));
-		
+
 		int count = iArkCommonService.getStudySubjectCount(cpmModel.getObject());
-		if(count == 0){
+		if (count == 0)
+		{
 			this.info("There are no subjects with the specified criteria.");
 			target.addComponent(feedbackPanel);
 		}
-		
-		listContainer.setVisible(true);//Make the WebMarkupContainer that houses the search results visible
-		target.addComponent(listContainer);//For ajax this is required so
+
+		listContainer.setVisible(true);// Make the WebMarkupContainer that houses the search results visible
+		target.addComponent(listContainer);// For ajax this is required so
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see au.org.theark.core.web.form.AbstractSearchForm#isSecure(java.lang.String)
 	 */
 	@Override
-	protected boolean isSecure(String actionType) {
-		SecurityManager securityManager =  ThreadContext.getSecurityManager();
-		Subject currentUser = SecurityUtils.getSubject();		
+	protected boolean isSecure(String actionType)
+	{
 		boolean flag = false;
-		
-		if(actionType.equalsIgnoreCase(Constants.NEW)){
-			if(		
-					securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.SUPER_ADMIN) ||
-					securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.ARK_SUPER_ADMIN) ||
-					securityManager.hasRole(currentUser.getPrincipals(), RoleConstants.STUDY_ADMIN)){
+		SecurityManager securityManager = ThreadContext.getSecurityManager();
+		Subject currentUser = SecurityUtils.getSubject();
+
+		Long useCaseId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY);
+		Long module = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY);
+
+		if (actionType.equalsIgnoreCase(Constants.NEW))
+		{
+			// Disable New button in LIMS
+			flag = false;
+		}
+		else if (actionType.equalsIgnoreCase(Constants.SEARCH))
+		{
+
+			if (securityManager.isPermitted(currentUser.getPrincipals(), PermissionConstants.READ))
+			{
 				flag = true;
 			}
-			
-		}else if (actionType.equalsIgnoreCase(Constants.SEARCH)){
+			else
+			{
+				flag = false;
+			}
+
 			flag = true;
-		}else{
+		}
+		else
+		{
 			flag = true;
 		}
 		return flag;
