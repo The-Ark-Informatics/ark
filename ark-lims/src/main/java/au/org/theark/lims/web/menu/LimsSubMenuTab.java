3abf6dfc0565be811ce41dfea65d09c57632d0b7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-21 06:51:27 +0000

    Organised code to be similar to PhenoSubMenuTab, implemented authorisation
    git-svn-id: svn://localhost@4008 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/menu/LimsSubMenuTab.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/menu/LimsSubMenuTab.java
@@ -10,9 +10,12 @@ import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import au.org.theark.core.security.ArkSecurityManager;
-import au.org.theark.core.security.RoleConstants;
+import au.org.theark.core.model.study.entity.ArkFunction;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.security.ArkLdapRealm;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.MenuModule;
 import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
 import au.org.theark.lims.web.Constants;
@@ -22,16 +25,16 @@ import au.org.theark.lims.web.component.subject.SubjectContainerPanel;
 @SuppressWarnings("serial")
 public class LimsSubMenuTab extends Panel
 {
+	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void> iArkCommonService;
 	
-	//@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	//private IArkCommonService iArkCommonService;
-	
-	//NN Commented not used as yet
-	//@SpringBean( name="arkLdapRealm")
-	//private ArkLdapRealm realm;
+	@SpringBean( name="arkLdapRealm")
+	private ArkLdapRealm realm;
 	
 	List<ITab> tabList;
 	private WebMarkupContainer	arkContextMarkup;
+	private ArkFunction  arkFunction;
+	private ArkModule arkModule;
 	
 	public LimsSubMenuTab(String id)
 	{
@@ -50,7 +53,6 @@ public class LimsSubMenuTab extends Panel
 
 	public void buildTabs()
 	{
-
 		List<ITab> moduleSubTabsList = new ArrayList<ITab>();
 		List<MenuModule> moduleTabs = new ArrayList<MenuModule>();
 
@@ -68,57 +70,39 @@ public class LimsSubMenuTab extends Panel
 		{
 			moduleSubTabsList.add(new AbstractTab(new Model<String>(getLocalizer().getString(moduleName.getResourceKey(), LimsSubMenuTab.this, moduleName.getModuleName())))
 			{
-				
-				
-				public boolean isVisible()
-				{
-
-					boolean flag = false;
-					if (moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.USER) || moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.SUBJECT))
-					{
-						
-						Subject currentUser = SecurityUtils.getSubject();
-						//Clear Cache here to load the roles & permissions for the user in session.The very next auth check will delegate the call to doAuthorizationInfo()
-						//realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
-						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
-					
-						if (  (arkSecurityManager.subjectHasRole(RoleConstants.SUPER_ADMIN))|| (arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN) || (arkSecurityManager.subjectHasRole(RoleConstants.STUDY_ADMIN))))
-						{
-
-							flag = currentUser.isAuthenticated();
-
-						}
-						else
-						{
-							flag = false;
-						}
-					}
-					else
-					{
-						flag = true;
-					}
-					return flag;
-				}
-
 				@Override
 				public Panel getPanel(String panelId)
 				{
-					
-					Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
-					if(moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.TAB_SUBJECT_DETAIL)){
-						//arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_SUBJECT); //Place a default use case into session
-						panelToReturn = new SubjectContainerPanel(panelId, arkContextMarkup);//Note the constructor
-					}
-					else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_BIO_COLLECTION)){
-						//arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_SUBJECT); //Place a default use case into session
-						panelToReturn = new BioCollectionContainerPanel(panelId, arkContextMarkup);//Note the constructor
-					}	 
-					return panelToReturn;
+					return buildPanels(moduleName, panelId);
 				};
 			});
 		}
 
 		ArkAjaxTabbedPanel moduleTabbedPanel = new ArkAjaxTabbedPanel(Constants.MENU_LIMS_SUBMENU, moduleSubTabsList);
 		add(moduleTabbedPanel);
+	}
+	
+	protected Panel buildPanels(final MenuModule moduleName, String panelId)
+	{
+		Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
+		if(moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.TAB_SUBJECT_DETAIL)){
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_LIMS_SUBJECT); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
+			panelToReturn = new SubjectContainerPanel(panelId, arkContextMarkup);//Note the constructor
+		}
+		else if(moduleName.getModuleName().equalsIgnoreCase(Constants.TAB_BIO_COLLECTION)){
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_LIMS_COLLECTION); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
+			panelToReturn = new BioCollectionContainerPanel(panelId, arkContextMarkup);//Note the constructor
+		}	 
+		return panelToReturn;
+	}
+	
+	private void processAuthorizationCache(ArkFunction arkFunction){
+		arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_LIMS); //Place a default module into session
+		SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY, arkFunction.getId());
+		SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
+		Subject currentUser = SecurityUtils.getSubject();	
+		realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
 	}
 }
