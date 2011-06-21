8944ed0bc78f12305d5d6e31ddc815144a3539f7
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-21 06:02:14 +0000

    Removed ModuleVO that was earlier used to build the sub-functions for the Study Module. It now gets it from back end and renders the tab names.
    git-svn-id: svn://localhost@4002 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
+++ ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.ThreadContext;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.Constants;
@@ -20,13 +18,11 @@ import au.org.theark.core.model.study.entity.ArkModule;
 import au.org.theark.core.security.ArkLdapRealm;
 import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.ArkUserVO;
-import au.org.theark.core.vo.MenuModule;
 import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
 import au.org.theark.study.web.component.managestudy.StudyContainer;
 import au.org.theark.study.web.component.manageuser.UserContainerPanel;
 import au.org.theark.study.web.component.mydetails.MyDetailsContainer;
 import au.org.theark.study.web.component.studycomponent.StudyComponentContainerPanel;
-import au.org.theark.study.web.component.user.UserContainer;
 
 @SuppressWarnings("serial")
 public class StudySubMenuTab extends Panel
@@ -35,7 +31,6 @@ public class StudySubMenuTab extends Panel
 	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
 	private IArkCommonService iArkCommonService;
 	
-	//NN Commented not used as yet
 	@SpringBean( name="arkLdapRealm")
 	private ArkLdapRealm realm;
 	
@@ -43,7 +38,6 @@ public class StudySubMenuTab extends Panel
 	private WebMarkupContainer	studyNameMarkup;
 	private WebMarkupContainer	studyLogoMarkup;
 	private WebMarkupContainer	arkContextMarkup;
-	private ArkFunction  arkFunction;
 	private ArkModule arkModule;
 	
 	public StudySubMenuTab(String id)
@@ -75,114 +69,45 @@ public class StudySubMenuTab extends Panel
 	{
 
 		List<ITab> moduleSubTabsList = new ArrayList<ITab>();
-		List<MenuModule> moduleTabs = new ArrayList<MenuModule>();
-
-		// THis way we can get the menus from the back-end. We should source this data from a table in the backend and wrap it up in a class like this
-		MenuModule menuModule = new MenuModule();
-		menuModule.setModuleName(Constants.STUDY_DETAIL);
-		// menuModule.setResourceKey(au.org.theark.core.Constants.TAB_MODULE_STUDY_DETAIL);
-		menuModule.setResourceKey(Constants.TAB_MODULE_STUDY_DETAIL);
-		moduleTabs.add(menuModule);
-
-		menuModule = new MenuModule();
-		menuModule.setModuleName(Constants.USER);
-		menuModule.setResourceKey(Constants.TAB_MODULE_USER);
-		moduleTabs.add(menuModule);
+		ArkModule arkModule = iArkCommonService.getArkModuleByName(Constants.ARK_MODULE_STUDY);
+		List<ArkFunction>   arkFunctionList = iArkCommonService.getModuleFunction(arkModule);//Gets a list of ArkFunctions for the given Module
 		
-		menuModule = new MenuModule();
-		menuModule.setModuleName("User Management");
-		menuModule.setResourceKey(Constants.TAB_MODULE_USER_MANAGEMENT);
-		moduleTabs.add(menuModule);
-	
-		menuModule = new MenuModule();
-		menuModule.setModuleName(Constants.MY_DETAIL);
-		menuModule.setResourceKey(Constants.TAB_MODULE_MY_DETAIL);
-		moduleTabs.add(menuModule);
-		
-		menuModule = new MenuModule();
-		menuModule.setModuleName(Constants.STUDY_COMPONENT);
-		menuModule.setResourceKey(Constants.TAB_MODULE_STUDY_COMPONENT);
-		moduleTabs.add(menuModule);
-
-		for (final MenuModule moduleName : moduleTabs)
-		{
-			System.out.println("\n Module Name ---> " + moduleName);
-			moduleSubTabsList.add(new AbstractTab(new Model<String>(getLocalizer().getString(moduleName.getResourceKey(), StudySubMenuTab.this, moduleName.getModuleName())))
+		//Iterate each ArkFunction render the Tabs.When something is clicked it uses the arkFunction and calls processAuthorixationCache to clear principals of the user
+		//and loads the new set of principals.(permissions)
+		for (final ArkFunction menuArkFunction : arkFunctionList) {
+			
+			moduleSubTabsList.add(new AbstractTab(new StringResourceModel(menuArkFunction.getResourceKey(),this, null))
 			{
 				
-				
-//				public boolean isVisible()
-//				{
-//
-//					boolean flag = false;
-//					if (moduleName.getModuleName().equalsIgnoreCase(Constants.USER) || moduleName.getModuleName().equalsIgnoreCase(Constants.SUBJECT))
-//					{
-//						
-//						Subject currentUser = SecurityUtils.getSubject();
-//						//Clear Cache here to load the roles & permissions for the user in session.The very next auth check will delegate the call to doAuthorizationInfo()
-//						//realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
-//						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
-//					
-//						if (  (arkSecurityManager.subjectHasRole(RoleConstants.SUPER_ADMIN))|| (arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN) || (arkSecurityManager.subjectHasRole(RoleConstants.STUDY_ADMIN))))
-//						{
-//
-//							flag = currentUser.isAuthenticated();
-//
-//						}
-//						else
-//						{
-//							flag = false;
-//						}
-//					}
-//					else
-//					{
-//						flag = true;
-//					}
-//					return flag;
-//				}
-
 				@Override
 				public Panel getPanel(String panelId)
 				{
-					
 					Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
-//					if (moduleName.getModuleName().equalsIgnoreCase(Constants.USER))
-//					{
-//						arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.USECASE_KEY_VALUE_USER); //Place a default use case into session
-//						arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_STUDY); //Place a default module into session
-//						SecurityUtils.getSubject().getSession().removeAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY);
-//						SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY, arkFunction.getId());
-//						SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
-//						panelToReturn = new UserContainer(panelId, new ArkUserVO());// Note the constructor						
-//					}
-					if(moduleName.getModuleName().equalsIgnoreCase("User Management")){
-						
-						arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_USER); //Place a default use case into session
-						processAuthorizationCache(arkFunction);
+					
+					if(menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_USER)){
+						processAuthorizationCache(menuArkFunction);
 						panelToReturn = new UserContainerPanel(panelId);
 					}
-					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_DETAIL))
+					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY))
 					{
-						arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY); //Place a default use case into session
-						processAuthorizationCache(arkFunction);
+						processAuthorizationCache(menuArkFunction);
 						panelToReturn = new StudyContainer(panelId, studyNameMarkup, studyLogoMarkup, arkContextMarkup);						
 					}
-					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.STUDY_COMPONENT))
+					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY_COMPONENT))
 					{
-						arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY_COMPONENT); //Place a default use case into session
-						processAuthorizationCache(arkFunction);
+						processAuthorizationCache(menuArkFunction);
 						panelToReturn = new StudyComponentContainerPanel(panelId);
 					}
-					else if (moduleName.getModuleName().equalsIgnoreCase(Constants.MY_DETAIL))
+					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MY_DETAIL))
 					{
-						arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MY_DETAIL); //Place a default use case into session
-						processAuthorizationCache(arkFunction);
+						processAuthorizationCache(menuArkFunction);
 						Subject currentUser = SecurityUtils.getSubject();	
 						panelToReturn = new MyDetailsContainer(panelId, new ArkUserVO(), currentUser);
 					}
 					return panelToReturn;
-				};
+				}
 			});
+			
 		}
 
 		ArkAjaxTabbedPanel moduleTabbedPanel = new ArkAjaxTabbedPanel(Constants.MENU_STUDY_SUBMENU, moduleSubTabsList);
