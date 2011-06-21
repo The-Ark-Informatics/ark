1e8cfb36b71263d4e825c1203da5242aad7fe6bd
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-21 04:50:35 +0000

    Added processAuthorizationCache method and corresponding module/function permission checks
    git-svn-id: svn://localhost@3992 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicSubMenuTab.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicSubMenuTab.java
@@ -3,15 +3,21 @@ package au.org.theark.phenotypic.web.menu;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import au.org.theark.core.model.study.entity.ArkFunction;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.security.ArkLdapRealm;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.vo.MenuModule;
 import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
 import au.org.theark.phenotypic.web.Constants;
@@ -25,6 +31,12 @@ import au.org.theark.phenotypic.web.component.summaryModule.SummaryContainerPane
 @SuppressWarnings( { "serial", "unused" })
 public class PhenotypicSubMenuTab extends Panel
 {
+	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void> iArkCommonService;
+	
+	@SpringBean( name="arkLdapRealm")
+	private ArkLdapRealm realm;
+	
 	private transient Logger	log					= LoggerFactory.getLogger(PhenotypicSubMenuTab.class);
 	private transient Subject	currentUser;
 	private transient Long		studyId;
@@ -32,6 +44,8 @@ public class PhenotypicSubMenuTab extends Panel
 	List<ITab>						moduleSubTabsList	= new ArrayList<ITab>();
 	List<MenuModule>				moduleTabs			= new ArrayList<MenuModule>();
 	MenuModule						menuModule			= new MenuModule();
+	private ArkFunction  arkFunction;
+	private ArkModule arkModule;
 
 	public PhenotypicSubMenuTab(String id)
 	{
@@ -75,7 +89,7 @@ public class PhenotypicSubMenuTab extends Panel
 		moduleTabs.add(menuModule);
 
 		menuModule = new MenuModule();
-		menuModule.setModuleName(Constants.PHENOTYPIC_DATA_UPLOAD_SUBMENU);
+		menuModule.setModuleName(Constants.FIELD_DATA_UPLOAD_SUBMENU);
 		menuModule.setResourceKey(Constants.PHENOTYPIC_DATA_UPLOAD_RESOURCEKEY);
 		moduleTabs.add(menuModule);
 	}
@@ -130,26 +144,44 @@ public class PhenotypicSubMenuTab extends Panel
 		{
 			panelToReturn = new SummaryContainerPanel(panelId); // Note the constructor
 		}
-		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.PHENO_COLLECTION_SUBMENU))
-		{
-			panelToReturn = new PhenoCollectionContainerPanel(panelId, arkContextMarkup); // Note the constructor
-		}
 		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.FIELD_SUBMENU))
 		{
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
 			panelToReturn = new FieldContainerPanel(panelId); // Note the constructor
 		}
 		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.FIELD_UPLOAD_SUBMENU))
 		{
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY_UPLOAD); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
 			panelToReturn = new FieldUploadContainerPanel(panelId); // Note the constructor
+		}
+		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.PHENO_COLLECTION_SUBMENU))
+		{
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_PHENO_COLLECTION); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
+			panelToReturn = new PhenoCollectionContainerPanel(panelId, arkContextMarkup); // Note the constructor
 		}
 		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.FIELD_DATA_SUBMENU))
 		{
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_FIELD_DATA); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
 			panelToReturn = new FieldDataContainerPanel(panelId); // Note the constructor
 		}
-		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.PHENOTYPIC_DATA_UPLOAD_SUBMENU))
+		else if (moduleName.getModuleName().equalsIgnoreCase(Constants.FIELD_DATA_UPLOAD_SUBMENU))
 		{
+			arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_FIELD_DATA_UPLOAD); //Place a default use case into session
+			processAuthorizationCache(arkFunction);
 			panelToReturn = new PhenoUploadContainerPanel(panelId); // Note the constructor
 		}
 		return panelToReturn;
+	}
+	
+	private void processAuthorizationCache(ArkFunction arkFunction){
+		arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_PHENOTYPIC); //Place a default module into session
+		SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY, arkFunction.getId());
+		SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
+		Subject currentUser = SecurityUtils.getSubject();	
+		realm.clearCachedAuthorizationInfo(currentUser.getPrincipals());
 	}
 }
