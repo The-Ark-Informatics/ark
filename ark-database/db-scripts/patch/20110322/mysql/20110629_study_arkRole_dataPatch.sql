3a6c4c9694db6ba66aab56bc8be6444e4f9fc287
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-06-29 03:05:23 +0000

    ammended
    git-svn-id: svn://localhost@4121 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110322/mysql/20110629_study_arkRole_dataPatch.sql
+++ ark-database/db-scripts/patch/20110322/mysql/20110629_study_arkRole_dataPatch.sql
@@ -6,3 +6,6 @@ INSERT INTO `study`.`ark_module_function` (`ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_
 -- Adde permissions
 INSERT INTO `study`.`ark_role_policy_template` (`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES ('89', '8', '3', '22', '2');
 INSERT INTO `study`.`ark_role_policy_template` (`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES ('90', '7', '3', '22', '2');
+
+-- Add LIMS_DATA_MANAGER delete permissions
+INSERT INTO `study`.`ark_role_policy_template` (`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES ('91','10','5','17','4');
