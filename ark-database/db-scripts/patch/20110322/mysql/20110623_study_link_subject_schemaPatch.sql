1d88c9d75c9e25f01148a92e141956dfeba938fa
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-01 08:19:12 +0000

    Applied on arktestdb.
    git-svn-id: svn://localhost@4186 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110322/mysql/20110623_study_link_subject_schemaPatch.sql
+++ ark-database/db-scripts/patch/20110322/mysql/20110623_study_link_subject_schemaPatch.sql
@@ -1,3 +1,3 @@
 use study;
-ALTER TABLE `study`.`link_subject_study` CHANGE COLUMN `AMDRIF_ID` `AMDRF_ID` INT(11) NULL DEFAULT NULL  ;
 
+ALTER TABLE `study`.`link_subject_study` CHANGE COLUMN `AMDRIF_ID` `AMDRF_ID` INT(11) NULL DEFAULT NULL  ;
