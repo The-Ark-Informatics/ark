39ec5ebc131811fee1ad74ddeb9c88b822c92a02
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-05 09:21:55 +0000

    added equals()
    git-svn-id: svn://localhost@4246 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/lims/entity/Biospecimen.java
+++ ark-common/src/main/java/au/org/theark/core/model/lims/entity/Biospecimen.java
@@ -604,4 +604,36 @@ public class Biospecimen implements java.io.Serializable
 	{
 		this.bioTransactions = bioTransactions;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bioCollection == null) ? 0 : bioCollection.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Biospecimen other = (Biospecimen) obj;
+		if (bioCollection == null) {
+			if (other.bioCollection != null)
+				return false;
+		} else if (!bioCollection.equals(other.bioCollection))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
