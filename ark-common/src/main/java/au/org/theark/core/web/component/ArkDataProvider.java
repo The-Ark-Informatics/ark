ce8a0f2fbb1240e50ffeabd5eae84a2d1b09c350
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-07 03:42:44 +0000

    Organised imports
    git-svn-id: svn://localhost@4282 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/component/ArkDataProvider.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/ArkDataProvider.java
@@ -1,7 +1,6 @@
 package au.org.theark.core.web.component;
 
 import org.apache.wicket.markup.repeater.data.IDataProvider;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 
@@ -30,6 +29,11 @@ public abstract class ArkDataProvider<T,U> implements IDataProvider<T> {
 
 	public IModel<T> model (final T object) {
 		return new LoadableDetachableModel<T>() {
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= -4738032546393837333L;
+
 			@Override
 			protected T load() {
 				return (T)object;
