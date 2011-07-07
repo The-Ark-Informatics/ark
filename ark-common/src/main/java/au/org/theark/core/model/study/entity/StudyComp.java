aefe448430b78d8bb04e22755f66e18f030b761a
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-07 04:43:29 +0000

    git-svn-id: svn://localhost@4293 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/StudyComp.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/StudyComp.java
@@ -136,7 +136,11 @@ public class StudyComp implements java.io.Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((keyword == null) ? 0 : keyword.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -149,12 +153,29 @@ public class StudyComp implements java.io.Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		StudyComp other = (StudyComp) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (keyword == null) {
+			if (other.keyword != null)
+				return false;
+		} else if (!keyword.equals(other.keyword))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		return true;
 	}
+
+	
 
 }
