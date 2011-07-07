ad26eb688a5c4971bad6b5eb9e1a055ae77824e1
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-07 02:03:06 +0000

    Now using au.org.theark.core.Constants.ARK_MODULE_REGISTRY
    git-svn-id: svn://localhost@4271 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-registry/src/main/java/au/org/theark/registry/web/menu/RegistryTabProviderImpl.java
+++ ark-registry/src/main/java/au/org/theark/registry/web/menu/RegistryTabProviderImpl.java
@@ -16,71 +16,73 @@ import org.apache.wicket.model.Model;
 
 import au.org.theark.core.service.IMainTabProvider;
 import au.org.theark.core.web.component.ArkMainTab;
-import au.org.theark.registry.web.Constants;
 
 /**
  * @author nivedann
- *
+ * 
  */
-public class RegistryTabProviderImpl extends Panel implements  IMainTabProvider{
-
-	
+public class RegistryTabProviderImpl extends Panel implements IMainTabProvider
+{
 	/**
 	 * 
 	 */
 	private static final long	serialVersionUID	= -5905065151035360540L;
-	List<ITab> moduleTabsList;
+	List<ITab>						moduleTabsList;
+
 	/**
 	 * @param id
 	 */
-	public RegistryTabProviderImpl(String id) {
+	public RegistryTabProviderImpl(String id)
+	{
 		super(id);
 		moduleTabsList = new ArrayList<ITab>();
 	}
-	
-	public  List<ITab> buildTabs(){
-		ITab tab1 = createTab("Registry");
+
+	public List<ITab> buildTabs()
+	{
+		ITab tab1 = createTab(au.org.theark.core.Constants.ARK_MODULE_REGISTRY);
 		moduleTabsList.add(tab1);
 		return moduleTabsList;
 	}
 
-	public ITab createTab(final String tabName) {
-		
-		return  new ArkMainTab(new Model<String>(tabName)) {
+	public ITab createTab(final String tabName)
+	{
 
+		return new ArkMainTab(new Model<String>(tabName))
+		{
 			/**
 			 * 
 			 */
 			private static final long	serialVersionUID	= -4616700064526881402L;
 
-			public boolean isVisible(){
-				//If the logged in user is a member of this module then allow him to view this tab
-				return true;
-			}
-			
 			@Override
-			public Panel getPanel(String panelId) {
-				
-				Panel panelToReturn = null;//S
+			public Panel getPanel(String panelId)
+			{
+				Panel panelToReturn = null;
 				// TODO Auto-generated method stub
-				if(tabName.equals(Constants.REGISTRY_MAIN_TAB)){
+				if (tabName.equals(au.org.theark.core.Constants.ARK_MODULE_REGISTRY))
+				{
 					return new RegistrySubMenuTab(panelId);
 				}
 				return panelToReturn;
 			}
-			
+
 			public boolean isAccessible()
 			{
 				Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-				if(sessionStudyId == null)
+				if (sessionStudyId == null)
 				{
-					this.getPanel(Constants.REGISTRY_MAIN_TAB).error(au.org.theark.core.Constants.STUDY_IN_CONTEXT_MESSAGE);
+					this.getPanel(au.org.theark.core.Constants.ARK_MODULE_REGISTRY).error(au.org.theark.core.Constants.NO_STUDY_IN_CONTEXT_MESSAGE);
 					return false;
 				}
 				else
 					return true;
 			}
 			
+			public boolean isVisible()
+			{
+				return true;
+			}
 		};
 	}
 
