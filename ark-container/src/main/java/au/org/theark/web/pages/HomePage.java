8e01eb50f7f8260ba38376bc378ce0b3e81e2be0
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-08 06:06:08 +0000

    Formatted, added logger
    git-svn-id: svn://localhost@4318 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/web/pages/HomePage.java
+++ ark-container/src/main/java/au/org/theark/web/pages/HomePage.java
@@ -10,6 +10,8 @@ import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
 import au.org.theark.lims.web.menu.LimsTabProviderImpl;
@@ -22,10 +24,9 @@ import au.org.theark.study.web.menu.MainTabProviderImpl;
  */
 public class HomePage extends BasePage
 {
-
-	private static final long	serialVersionUID	= 1L;
-	private WebMarkupContainer arkContextPanelMarkup;
-	private TabbedPanel moduleTabbedPanel;
+	private transient static Logger	log	= LoggerFactory.getLogger(HomePage.class);
+	private WebMarkupContainer			arkContextPanelMarkup;
+	private TabbedPanel					moduleTabbedPanel;
 
 	/**
 	 * Constructor that is invoked when page is invoked without a session.
@@ -37,80 +38,89 @@ public class HomePage extends BasePage
 	{
 		Subject currentUser = SecurityUtils.getSubject();
 
-		if(currentUser.getPrincipal() != null){
+		if (currentUser.getPrincipal() != null)
+		{
 			buildContextPanel();
 			buildModuleTabs();
-		}else{
+		}
+		else
+		{
 			setResponsePage(LoginPage.class);
 		}
-	
 	}
-	
+
 	protected void buildContextPanel()
 	{
 		this.arkContextPanelMarkup = new WebMarkupContainer("contextMarkupContainer");
-		
+
 		// Require a Label for each context item (currently hard coded in mark up)
-		//TODO: Use dynamic component to remove hard-coded Labels
-		Label studyLabel = new Label("studyLabel","");
+		// TODO: Use dynamic component to remove hard-coded Labels
+		Label studyLabel = new Label("studyLabel", "");
 		arkContextPanelMarkup.add(studyLabel);
-		Label subjectLabel = new Label("subjectLabel","");
+		Label subjectLabel = new Label("subjectLabel", "");
 		arkContextPanelMarkup.add(subjectLabel);
-		Label phenoLabel = new Label("phenoLabel","");
+		Label phenoLabel = new Label("phenoLabel", "");
 		arkContextPanelMarkup.add(phenoLabel);
-		Label genoLabel = new Label("genoLabel","");
+		Label genoLabel = new Label("genoLabel", "");
 		arkContextPanelMarkup.add(genoLabel);
 		add(arkContextPanelMarkup);
-		arkContextPanelMarkup.setOutputMarkupPlaceholderTag(true);		
+		arkContextPanelMarkup.setOutputMarkupPlaceholderTag(true);
 	}
 
 	@Override
 	protected void buildModuleTabs()
 	{
+		log.info("Called buildModuleTabs()");
+		
 		List<ITab> moduleTabsList = new ArrayList<ITab>();
-	
+
 		// Study
 		MainTabProviderImpl studyMainTabProvider = new MainTabProviderImpl("study");
 		// Pass in the Study logo mark up, to allow dynamic logo reference
 		moduleTabsList = studyMainTabProvider.buildTabs(this.studyNameMarkup, this.studyLogoMarkup, this.arkContextPanelMarkup);
 
 		// Pheno
-		PhenotypicTabProviderImpl phenotypicTabs = new PhenotypicTabProviderImpl("phenotypic");
-		List<ITab> phenotypicTabsList = phenotypicTabs.buildTabs(this.arkContextPanelMarkup);
+		PhenotypicTabProviderImpl phenotypicTabProvidor = new PhenotypicTabProviderImpl("phenotypic");
+		List<ITab> phenotypicTabsList = phenotypicTabProvidor.buildTabs(this.arkContextPanelMarkup);
+		log.info("Called phenotypicTabProvidor.buildTabs");
 		for (ITab itab : phenotypicTabsList)
 		{
 			moduleTabsList.add(itab);
 		}
 
 		// Geno & Registry not to be deployed as yet into Test
-//		GenoTabProviderImpl genoTabs = new GenoTabProviderImpl("geno");
-//		List<ITab> genoTabsList = genoTabs.buildTabs();
-//		for (ITab itab : genoTabsList)
-//		{
-//			moduleTabsList.add(itab);
-//		}
-		
+		// GenoTabProviderImpl genoTabs = new GenoTabProviderImpl("geno");
+		// List<ITab> genoTabsList = genoTabs.buildTabs();
+		// for (ITab itab : genoTabsList)
+		// {
+		// moduleTabsList.add(itab);
+		// }
+
 		// Registry
-//		RegistryTabProviderImpl registryTabProvider = new RegistryTabProviderImpl("registry");
-//		List<ITab> registryTabList =	registryTabProvider.buildTabs();
-//		for(ITab tab : registryTabList){
-//			moduleTabsList.add(tab);
-//		}
-//		
+		// RegistryTabProviderImpl registryTabProvider = new RegistryTabProviderImpl("registry");
+		// List<ITab> registryTabList = registryTabProvider.buildTabs();
+		// for(ITab tab : registryTabList){
+		// moduleTabsList.add(tab);
+		// }
+		//		
 		// LIMS
 		LimsTabProviderImpl limsTabProvider = new LimsTabProviderImpl("lims");
-		List<ITab> limsTabList =	limsTabProvider.buildTabs(this.arkContextPanelMarkup);
-		for(ITab tab : limsTabList){
+		List<ITab> limsTabList = limsTabProvider.buildTabs(this.arkContextPanelMarkup);
+		log.info("Called limsTabProvider.buildTabs");
+		for (ITab tab : limsTabList)
+		{
 			moduleTabsList.add(tab);
 		}
-		
+
 		// Report
 		ReportTabProviderImpl reportTabProvider = new ReportTabProviderImpl("report");
-		List<ITab> reportTabList =	reportTabProvider.buildTabs();
-		for(ITab tab : reportTabList){
+		List<ITab> reportTabList = reportTabProvider.buildTabs();
+		log.info("Called reportTabProvider.buildTabs");
+		for (ITab tab : reportTabList)
+		{
 			moduleTabsList.add(tab);
 		}
-		
+
 		moduleTabbedPanel = new ArkAjaxTabbedPanel("moduleTabsList", moduleTabsList);
 		moduleTabbedPanel.setOutputMarkupPlaceholderTag(true);
 		studyMainTabProvider.setModuleTabbedPanel(moduleTabbedPanel);
@@ -126,5 +136,22 @@ public class HomePage extends BasePage
 	String getTitle()
 	{
 		return null;
+	}
+
+	/**
+	 * @param log
+	 *           the log to set
+	 */
+	public static void setLog(Logger log)
+	{
+		HomePage.log = log;
+	}
+
+	/**
+	 * @return the log
+	 */
+	public static Logger getLog()
+	{
+		return log;
 	}
 }
