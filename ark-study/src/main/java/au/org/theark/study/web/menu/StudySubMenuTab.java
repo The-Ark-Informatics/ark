f33082727dc1425d73b28608c7039dc56d1d2cb1
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-08 06:08:30 +0000

    JavaDoc'd, removed redundant log calls, only implemented the one processAuthorizationCache call
    git-svn-id: svn://localhost@4321 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
+++ ark-study/src/main/java/au/org/theark/study/web/menu/StudySubMenuTab.java
@@ -7,11 +7,12 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
-import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import au.org.theark.core.Constants;
 import au.org.theark.core.model.study.entity.ArkFunction;
@@ -25,33 +26,66 @@ import au.org.theark.study.web.component.manageuser.UserContainerPanel;
 import au.org.theark.study.web.component.mydetails.MyDetailsContainer;
 import au.org.theark.study.web.component.studycomponent.StudyComponentContainerPanel;
 
-@SuppressWarnings("serial")
+/**
+ * <p>The <code>StudySubMenuTab</code> class that extends the {@link au.org.theark.core.web.component.menu.AbstractArkTabPanel AbstractArkTabPanel} class.
+ * It provides the implementation of the Study tab panel (sub-menu).</p>
+ * @author nivedann
+ * @author cellis
+ */
 public class StudySubMenuTab extends AbstractArkTabPanel
 {
-	
-	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService<Void> iArkCommonService;
-	
-	private List<ITab> tabList;
-	private WebMarkupContainer	studyNameMarkup;
-	private WebMarkupContainer	studyLogoMarkup;
-	private WebMarkupContainer	arkContextMarkup;
-	private MainTabProviderImpl mainTabProvider;
-	
+	/**
+	 * 
+	 */
+	private static final long			serialVersionUID	= -2725142200726870636L;
+	private transient static Logger	log					= LoggerFactory.getLogger(StudySubMenuTab.class);
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>	iArkCommonService;
+
+	private WebMarkupContainer			studyNameMarkup;
+	private WebMarkupContainer			studyLogoMarkup;
+	private WebMarkupContainer			arkContextMarkup;
+	private MainTabProviderImpl		mainTabProvider;
+	private List<ITab>					moduleSubTabsList	= new ArrayList<ITab>();
+
+	/**
+	 * StudySubMenuTab Constructor
+	 * 
+	 * @param id
+	 *           this component identifier
+	 * @param studyNameMarkup
+	 *           the WebMarkupContainer that references the study name in context
+	 * @param studyLogoMarkup
+	 *           the WebMarkupContainer that references the study logo in context
+	 * @param arkContextMarkup
+	 *           the WebMarkupContainer that references the context items
+	 */
 	public StudySubMenuTab(String id, WebMarkupContainer studyNameMarkup, WebMarkupContainer studyLogoMarkup, WebMarkupContainer arkContextMarkup)
 	{
 		super(id);
-		setTabList(new ArrayList<ITab>());
 		this.studyNameMarkup = studyNameMarkup;
 		this.studyLogoMarkup = studyLogoMarkup;
 		this.arkContextMarkup = arkContextMarkup;
 		buildTabs();
 	}
-	
+
+	/**
+	 * StudySubMenuTab Constructor
+	 * 
+	 * @param id
+	 *           this component identifier
+	 * @param studyNameMarkup
+	 *           the WebMarkupContainer that references the study name in context
+	 * @param studyLogoMarkup
+	 *           the WebMarkupContainer that references the study logo in context
+	 * @param arkContextMarkup
+	 *           the WebMarkupContainer that references the context items
+	 * @param mainTabProvider
+	 *           the reference to the main tabs (to allow repaint on Study selection)
+	 */
 	public StudySubMenuTab(String id, WebMarkupContainer studyNameMarkup, WebMarkupContainer studyLogoMarkup, WebMarkupContainer arkContextMarkup, MainTabProviderImpl mainTabProvider)
 	{
 		super(id);
-		setTabList(new ArrayList<ITab>());
 		this.studyNameMarkup = studyNameMarkup;
 		this.studyLogoMarkup = studyLogoMarkup;
 		this.arkContextMarkup = arkContextMarkup;
@@ -59,50 +93,56 @@ public class StudySubMenuTab extends AbstractArkTabPanel
 		buildTabs();
 	}
 
+	/**
+	 * Build the list of tabs that represent the sub-menus
+	 */
 	public void buildTabs()
 	{
-		List<ITab> moduleSubTabsList = new ArrayList<ITab>();
+		log.info("Called buildTabs()");
+
 		ArkModule arkModule = iArkCommonService.getArkModuleByName(Constants.ARK_MODULE_STUDY);
-		List<ArkFunction>   arkFunctionList = iArkCommonService.getModuleFunction(arkModule);//Gets a list of ArkFunctions for the given Module
-		
-		//Iterate each ArkFunction render the Tabs.When something is clicked it uses the arkFunction and calls processAuthorixationCache to clear principals of the user
-		//and loads the new set of principals.(permissions)
-		for (final ArkFunction menuArkFunction : arkFunctionList) {
-			
-			moduleSubTabsList.add(new AbstractTab(new StringResourceModel(menuArkFunction.getResourceKey(),this, null))
+		List<ArkFunction> arkFunctionList = iArkCommonService.getModuleFunction(arkModule);// Gets a list of ArkFunctions for the given Module
+
+		/*
+		 * Iterate each ArkFunction render the Tabs.When something is clicked it uses the arkFunction and calls processAuthorizationCache to clear
+		 * principals of the user and loads the new set of principals.(permissions)
+		 */
+		for (final ArkFunction menuArkFunction : arkFunctionList)
+		{
+			moduleSubTabsList.add(new AbstractTab(new StringResourceModel(menuArkFunction.getResourceKey(), this, null))
 			{
-				
+				/**
+				 * 
+				 */
+				private static final long	serialVersionUID	= -8421399480756599074L;
+
 				@Override
 				public Panel getPanel(String panelId)
 				{
 					Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
 
 					// Clear authorisation cache
-					processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY,menuArkFunction);
-					if(menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_USER)){
-						//processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY,menuArkFunction);
+					processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY, menuArkFunction);
+					if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_USER))
+					{
 						panelToReturn = new UserContainerPanel(panelId);
 					}
 					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY))
 					{
-						//processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY,menuArkFunction);
-						panelToReturn = new StudyContainer(panelId, studyNameMarkup, studyLogoMarkup, arkContextMarkup, mainTabProvider.getModuleTabbedPanel());				
+						panelToReturn = new StudyContainer(panelId, studyNameMarkup, studyLogoMarkup, arkContextMarkup, mainTabProvider.getModuleTabbedPanel());
 					}
 					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY_COMPONENT))
 					{
-						//processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY,menuArkFunction);
 						panelToReturn = new StudyComponentContainerPanel(panelId);
 					}
 					else if (menuArkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MY_DETAIL))
 					{
-						//processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_STUDY,menuArkFunction);
-						Subject currentUser = SecurityUtils.getSubject();	
+						Subject currentUser = SecurityUtils.getSubject();
 						panelToReturn = new MyDetailsContainer(panelId, new ArkUserVO(), currentUser);
 					}
 					return panelToReturn;
 				}
 			});
-			
 		}
 
 		ArkAjaxTabbedPanel moduleTabbedPanel = new ArkAjaxTabbedPanel(Constants.MENU_STUDY_SUBMENU, moduleSubTabsList);
@@ -110,18 +150,19 @@ public class StudySubMenuTab extends AbstractArkTabPanel
 	}
 
 	/**
-	 * @param tabList the tabList to set
+	 * @param log
+	 *           the log to set
 	 */
-	public void setTabList(List<ITab> tabList)
+	public static void setLog(Logger log)
 	{
-		this.tabList = tabList;
+		StudySubMenuTab.log = log;
 	}
 
 	/**
-	 * @return the tabList
+	 * @return the log
 	 */
-	public List<ITab> getTabList()
+	public static Logger getLog()
 	{
-		return tabList;
+		return log;
 	}
 }
