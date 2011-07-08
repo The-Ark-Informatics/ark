91f6670d25da2be1d81fe38250ce1d744db89094
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-08 04:34:06 +0000

    Formatted, removed redundant comment-out code
    git-svn-id: svn://localhost@4316 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/web/pages/BasePage.java
+++ ark-container/src/main/java/au/org/theark/web/pages/BasePage.java
@@ -11,42 +11,39 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.protocol.http.WebResponse;
+
 /**
- * This page will be inherited by all wicket pages. Contains basic functionality that all 
- * pages will require.e.g add the menu in here so all pages can inherit it by default.
- * Access to each menu item will be determined by the subclass via annotations. e.g if one of the
- * menu item was Admin the class linked or invoked will place the constraints via annotations.
- * Using IStrategyAuthorization the application will determine if the link is accessible to the
- * current logged in user. 
+ * This page will be inherited by all wicket pages. Contains basic functionality that all pages will require.e.g add the menu in here so all pages can
+ * inherit it by default. Access to each menu item will be determined by the subclass via annotations. e.g if one of the menu item was Admin the class
+ * linked or invoked will place the constraints via annotations. Using IStrategyAuthorization the application will determine if the link is accessible
+ * to the current logged in user.
+ * 
  * @author nivedann
- *
+ * 
  */
-@SuppressWarnings("unchecked")
-public abstract class BasePage extends WebPage{
-	
-	//private Tabs tabs;
-	//private Tabs etaTabs;
-	private String principal;
-	private Label userNameLbl;
-	private Label studyNameLbl;
-	
-	protected WebMarkupContainer studyNameMarkup;
-	protected WebMarkupContainer studyLogoMarkup;
-	
-	public BasePage(){
-		
-		ContextImage hostedByImage = new ContextImage("hostedByImage",new Model<String>("images/"+Constants.HOSTED_BY_IMAGE));
-		ContextImage studyLogoImage = new ContextImage("studyLogoImage", new Model<String>("images/"+Constants.NO_STUDY_LOGO_IMAGE));
-		ContextImage productImage = new ContextImage("productImage", new Model<String>("images/"+Constants.PRODUCT_IMAGE));
-		
+public abstract class BasePage extends WebPage
+{
+	private String						principal;
+	private Label						userNameLbl;
+	private Label						studyNameLbl;
+
+	protected WebMarkupContainer	studyNameMarkup;
+	protected WebMarkupContainer	studyLogoMarkup;
+
+	public BasePage()
+	{
+		ContextImage hostedByImage = new ContextImage("hostedByImage", new Model<String>("images/" + Constants.HOSTED_BY_IMAGE));
+		ContextImage studyLogoImage = new ContextImage("studyLogoImage", new Model<String>("images/" + Constants.NO_STUDY_LOGO_IMAGE));
+		ContextImage productImage = new ContextImage("productImage", new Model<String>("images/" + Constants.PRODUCT_IMAGE));
+
 		Subject currentUser = SecurityUtils.getSubject();
 
-		if(currentUser.getPrincipal() != null)
+		if (currentUser.getPrincipal() != null)
 		{
 			principal = (String) currentUser.getPrincipal();
-			userNameLbl = new Label("loggedInUser", new Model(principal));
-			studyNameLbl = new Label("studyNameLabel", new Model(" "));
-			
+			userNameLbl = new Label("loggedInUser", new Model<String>(principal));
+			studyNameLbl = new Label("studyNameLabel", new Model<String>(" "));
+
 			// Markup for Study name
 			studyNameMarkup = new WebMarkupContainer("studyNameMarkupContainer");
 			studyNameMarkup.add(studyNameLbl);
@@ -56,21 +53,29 @@ public abstract class BasePage extends WebPage{
 			studyLogoMarkup = new WebMarkupContainer("studyLogoMarkupContainer");
 			studyLogoMarkup.add(studyLogoImage);
 			studyLogoMarkup.setOutputMarkupPlaceholderTag(true);
-			
+
 			// Add images
 			add(hostedByImage);
 			add(studyNameMarkup);
 			add(studyLogoMarkup);
 			add(productImage);
-			//add(bannerImage);
-			
+			// add(bannerImage);
+
 			add(userNameLbl);
-		
-			AjaxLink link = new AjaxLink("ajaxLogoutLink") {
+
+			@SuppressWarnings("unchecked")
+			AjaxLink link = new AjaxLink("ajaxLogoutLink")
+			{
+				/**
+				 * 
+				 */
+				private static final long	serialVersionUID	= 422053857225833627L;
+
 				@Override
-				public void onClick(AjaxRequestTarget target) {
+				public void onClick(AjaxRequestTarget target)
+				{
 					Subject subject = SecurityUtils.getSubject();
-					//Place the selected study in session context for the user
+					// Place the selected study in session context for the user
 					SecurityUtils.getSubject().getSession().removeAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
 					SecurityUtils.getSubject().getSession().removeAttribute(au.org.theark.core.Constants.PERSON_CONTEXT_ID);
 					SecurityUtils.getSubject().getSession().removeAttribute(au.org.theark.core.Constants.PERSON_TYPE);
@@ -85,26 +90,27 @@ public abstract class BasePage extends WebPage{
 		{
 			setResponsePage(LoginPage.class);
 		}
-		
 	}
 
 	@Override
-	protected void configureResponse() { 
+	protected void configureResponse()
+	{
 		super.configureResponse();
 		WebResponse response = getWebRequestCycle().getWebResponse();
 		response.setHeader("Cache-Control", "no-cache, max-age=0,must-revalidate, no-store");
 		response.setHeader("Expires", "-1");
 		response.setHeader("Pragma", "no-cache");
 	}
-	
+
 	/**
 	 * Build module tabs
 	 */
 	protected abstract void buildModuleTabs();
+
 	/**
 	 * Implement method to add tab menus based on the module
 	 */
 	protected abstract void buildTabMenu();
-	
+
 	abstract String getTitle();
 }
