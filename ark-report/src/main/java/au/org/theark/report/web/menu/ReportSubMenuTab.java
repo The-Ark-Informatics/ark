5e98e09216e3e8ba2a79438b72d71bb007a2a731
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-13 03:05:46 +0000

    Tidied up code, removed redundant code
    git-svn-id: svn://localhost@4369 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-report/src/main/java/au/org/theark/report/web/menu/ReportSubMenuTab.java
+++ ark-report/src/main/java/au/org/theark/report/web/menu/ReportSubMenuTab.java
@@ -3,17 +3,11 @@ package au.org.theark.report.web.menu;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
-import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
-import au.org.theark.core.security.ArkSecurityManager;
-import au.org.theark.core.security.RoleConstants;
 import au.org.theark.core.vo.MenuModule;
 import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
 import au.org.theark.report.web.Constants;
@@ -21,23 +15,24 @@ import au.org.theark.report.web.component.viewReport.ReportContainerPanel;
 
 public class ReportSubMenuTab extends Panel
 {
-	
-	List<ITab> tabList;
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -3695404298701886701L;
+	private List<ITab> moduleSubTabsList;
 
 	public ReportSubMenuTab(String id)
 	{
 		super(id);
-		tabList = new ArrayList<ITab>();
+		moduleSubTabsList = new ArrayList<ITab>();
 		buildTabs();
 	}
 
 	public void buildTabs()
 	{
-
-		List<ITab> moduleSubTabsList = new ArrayList<ITab>();
 		List<MenuModule> moduleTabs = new ArrayList<MenuModule>();
 
-		// THis way we can get the menus from the back-end. We should source this data from a table in the backend and wrap it up in a class like this
+		// This way we can get the menus from the back-end. We should source this data from a table in the backend and wrap it up in a class like this
 		MenuModule menuModule = new MenuModule();
 		menuModule.setModuleName(Constants.REPORT_DETAIL);
 		menuModule.setResourceKey(Constants.TAB_MODULE_REPORT_DETAIL);
@@ -47,35 +42,20 @@ public class ReportSubMenuTab extends Panel
 		{
 			moduleSubTabsList.add(new AbstractTab(new Model<String>(getLocalizer().getString(moduleName.getResourceKey(), ReportSubMenuTab.this, moduleName.getModuleName())))
 			{
+				/**
+				 * 
+				 */
+				private static final long	serialVersionUID	= -7414890128705025350L;
+
 				public boolean isVisible()
 				{
-
-					boolean flag = false;
-					if (moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.USER) || moduleName.getModuleName().equalsIgnoreCase(au.org.theark.core.Constants.SUBJECT))
-					{
-
-						ArkSecurityManager arkSecurityManager = ArkSecurityManager.getInstance();
-						Subject currentUser = SecurityUtils.getSubject();
-						if (  (arkSecurityManager.subjectHasRole(RoleConstants.SUPER_ADMIN))|| (arkSecurityManager.subjectHasRole(RoleConstants.ARK_SUPER_ADMIN) || (arkSecurityManager.subjectHasRole(RoleConstants.STUDY_ADMIN))))
-						{
-							flag = currentUser.isAuthenticated();
-						}
-						else
-						{
-							flag = false;
-						}
-					}
-					else
-					{
-						flag = true;
-					}
-					return flag;
+					// Reporting tab always visible
+					return true;
 				}
 
 				@Override
 				public Panel getPanel(String panelId)
 				{
-
 					Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
 
 					if (moduleName.getModuleName().equalsIgnoreCase(Constants.REPORT_DETAIL))
