af197e48a6087a1475b08d11b0313cfe6fb589c1
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-13 03:22:39 +0000

    Formatted, add TODO comment (needs checking cleaning up)
    git-svn-id: svn://localhost@4371 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/security/RoleConstants.java
+++ ark-common/src/main/java/au/org/theark/core/security/RoleConstants.java
@@ -1,25 +1,24 @@
 package au.org.theark.core.security;
 
-public class RoleConstants {
-	
-	public static final String SUPER_ADMIN="super_administrator";
-	public static final String STUDY_ADMIN="study_administrator";
-	public static final String GENERAL_USER="ordinary_person";
-	/* Module based roles, module name prefix and role name suffix*/
-	public static final String ARK_SUPER_ADMIN="ARK_super_administrator";
-	public static final String ARK_ORDINARY_USER="ARK_ordinary_user";
-	public static final String LIMS_SUPER_ADMIN=	"wager_lab_super_administrator";
-	public static final String LIMS_ORDINARY_USER=	"wager_lab_ordinary_user";
-	public static final String GWAS_SUPER_ADMIN	="gwas_super_administrator";
-	public static final String STUDY_MANAGER_SUPER_ADMIN	="study_manager_super_administrator";
-	public static final String STUDY_MANAGER_ORDINARY_USER = "study_manager_ordinary_user";
-	
-	/**
-	 * These Role Constants and Permissions are for the new user management. 
-	 * The role names here should match the string one Role_Name table
-	 */
-	public static final String ARK_ROLE_SUPER_ADMINISTATOR = "Super Administrator";
-	public static final String ARK_ROLE_ADMINISTATOR="Administrator";
-	public static final String ARK_ROLE_ORDINARY_USER="Ordinary User";
+public class RoleConstants
+{
+	// TODO: Remove constants not used
+	public static final String	SUPER_ADMIN							= "super_administrator";
+	public static final String	STUDY_ADMIN							= "study_administrator";
+	public static final String	GENERAL_USER						= "ordinary_person";
+	/* Module based roles, module name prefix and role name suffix */
+	public static final String	ARK_SUPER_ADMIN					= "ARK_super_administrator";
+	public static final String	ARK_ORDINARY_USER					= "ARK_ordinary_user";
+	public static final String	LIMS_SUPER_ADMIN					= "wager_lab_super_administrator";
+	public static final String	LIMS_ORDINARY_USER				= "wager_lab_ordinary_user";
+	public static final String	GWAS_SUPER_ADMIN					= "gwas_super_administrator";
+	public static final String	STUDY_MANAGER_SUPER_ADMIN		= "study_manager_super_administrator";
+	public static final String	STUDY_MANAGER_ORDINARY_USER	= "study_manager_ordinary_user";
 
+	/**
+	 * These Role Constants and Permissions are for the new user management. The role names here should match the string one Role_Name table
+	 */
+	public static final String	ARK_ROLE_SUPER_ADMINISTATOR	= "Super Administrator";
+	public static final String	ARK_ROLE_ADMINISTATOR			= "Administrator";
+	public static final String	ARK_ROLE_ORDINARY_USER			= "Ordinary User";
 }
