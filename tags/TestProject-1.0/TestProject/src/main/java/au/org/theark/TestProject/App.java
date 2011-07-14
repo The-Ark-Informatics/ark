1dd6b51b7ebc9596b666697019caba02ecf3dd2e
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-14 02:25:39 +0000

    git-svn-id: svn://localhost@4413 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ tags/TestProject-1.0/TestProject/src/main/java/au/org/theark/TestProject/App.java
@@ -0,0 +1,13 @@
+package au.org.theark.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
