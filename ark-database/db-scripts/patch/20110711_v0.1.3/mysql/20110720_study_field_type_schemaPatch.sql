d8ce498a6d38d46245f79080086b0650f00b0646
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-20 05:10:06 +0000

    Added insert statements
    git-svn-id: svn://localhost@4486 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110720_study_field_type_schemaPatch.sql
+++ ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110720_study_field_type_schemaPatch.sql
@@ -3,5 +3,10 @@ CREATE TABLE `field_type` (
   `ID` int(11) NOT NULL auto_increment,
   `NAME` varchar(50) NOT NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=InnoDB
+) ENGINE=InnoDB;
+
+
+INSERT INTO `study`.`field_type` (`ID`, `NAME`) VALUES (1, 'CHARACTER');
+INSERT INTO `study`.`field_type` (`ID`, `NAME`) VALUES (2, 'NUMBER');
+INSERT INTO `study`.`field_type` (`ID`, `NAME`) VALUES (3, 'DATE');
 
