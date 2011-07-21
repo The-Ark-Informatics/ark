55286bd3a1998f4a709bda858b1d15d911e04f96
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-21 03:52:28 +0000

    added column mapping for name
    git-svn-id: svn://localhost@4511 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/FieldType.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/FieldType.java
@@ -37,6 +37,8 @@ public class FieldType implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	@Column(name = "NAME", nullable = false, length = 255, unique=true)
 	public String getName() {
 		return name;
 	}
