17c5a5203cdffcfa17da314760e3eaf12b4d99f7
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-25 05:43:57 +0000

    Refactored SearchForm to SummaryForm (doesn't extend AbstractSearchForm)
    git-svn-id: svn://localhost@4548 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/summaryModule/SearchPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/summaryModule/SearchPanel.java
@@ -7,12 +7,10 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.core.model.pheno.entity.PhenoCollection;
-import au.org.theark.phenotypic.model.vo.PhenoCollectionVO;
 import au.org.theark.phenotypic.web.component.summaryModule.form.ContainerForm;
-import au.org.theark.phenotypic.web.component.summaryModule.form.SearchForm;
+import au.org.theark.phenotypic.web.component.summaryModule.form.SummaryForm;
 
 /**
  * @author cellis
@@ -22,14 +20,7 @@ import au.org.theark.phenotypic.web.component.summaryModule.form.SearchForm;
 public class SearchPanel extends Panel {
 	private FeedbackPanel							feedBackPanel;
 	private WebMarkupContainer						searchMarkupContainer;
-	private WebMarkupContainer						listContainer;
-	private WebMarkupContainer						detailContainer;
-	private PageableListView<PhenoCollection>	listView;
-	private ContainerForm							containerForm;
-	private DetailPanel								detailPanel;
-	private WebMarkupContainer						viewButtonContainer;
-	private WebMarkupContainer						editButtonContainer;
-	private WebMarkupContainer						detailPanelFormContainer;
+	private WebMarkupContainer						resultListContainer;
 
 	/* Constructor */
 	public SearchPanel(String id, FeedbackPanel feedBackPanel, WebMarkupContainer searchMarkupContainer, PageableListView<PhenoCollection> listView, WebMarkupContainer resultListContainer,
@@ -37,20 +28,11 @@ public class SearchPanel extends Panel {
 			WebMarkupContainer detailPanelFormContainer) {
 		super(id);
 		this.searchMarkupContainer = searchMarkupContainer;
-		this.listView = listView;
-		this.feedBackPanel = feedBackPanel;
-		this.detailContainer = detailPanelContainer;
-		this.detailPanel = detail;
-		this.containerForm = containerForm;
-		this.viewButtonContainer = viewButtonContainer;
-		this.editButtonContainer = editButtonContainer;
-		this.detailPanelFormContainer = detailPanelFormContainer;
-		listContainer = resultListContainer;
+		this.resultListContainer = resultListContainer;
 	}
 
 	public void initialisePanel() {
-		SearchForm searchForm = new SearchForm(au.org.theark.core.Constants.SEARCH_FORM, (CompoundPropertyModel<PhenoCollectionVO>) containerForm.getModel(), listView, feedBackPanel, detailPanel,
-				listContainer, searchMarkupContainer, detailContainer, detailPanelFormContainer, viewButtonContainer, editButtonContainer);
+		SummaryForm searchForm = new SummaryForm("summaryForm", feedBackPanel, searchMarkupContainer, resultListContainer);
 		add(searchForm);
 	}
 }
