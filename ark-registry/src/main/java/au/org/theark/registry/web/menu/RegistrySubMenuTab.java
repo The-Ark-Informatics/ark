6df3e6687421e94d800fb7cb42147cd696a0c67a
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-26 05:24:04 +0000

    ArkAjaxTabbedPanel was moved into it's own package (in ark-common)
    git-svn-id: svn://localhost@4571 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-registry/src/main/java/au/org/theark/registry/web/menu/RegistrySubMenuTab.java
+++ ark-registry/src/main/java/au/org/theark/registry/web/menu/RegistrySubMenuTab.java
@@ -15,7 +15,7 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
 import au.org.theark.core.vo.MenuModule;
-import au.org.theark.core.web.component.ArkAjaxTabbedPanel;
+import au.org.theark.core.web.component.tabbedPanel.ArkAjaxTabbedPanel;
 import au.org.theark.registry.web.Constants;
 import au.org.theark.registry.web.component.invoice.InvoiceContainerPanel;
 
