e5500edd094196ae6d0bed8e9a10e390ece779df
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-07-29 09:27:32 +0000

    vo for SubjectCustomFieldVO
    git-svn-id: svn://localhost@4630 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/vo/SubjectCustomFieldVO.java
@@ -0,0 +1,18 @@
+package au.org.theark.core.vo;
+
+import java.io.Serializable;
+
+/**
+ * @author nivedann
+ *
+ */
+public class SubjectCustomFieldVO implements Serializable{
+	
+	/**
+	 * 
+	 */
+	public SubjectCustomFieldVO(){
+		
+	}
+
+}
