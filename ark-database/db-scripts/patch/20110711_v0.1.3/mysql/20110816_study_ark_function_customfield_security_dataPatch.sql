7bbe3af557bd192178f6c9ff85d9cd081d2ff81b
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-08-17 09:27:40 +0000

    Additional permission changes to reflect the following
    git-svn-id: svn://localhost@4726 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110816_study_ark_function_customfield_security_dataPatch.sql
+++ ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110816_study_ark_function_customfield_security_dataPatch.sql
@@ -14,3 +14,93 @@ UPDATE `study`.`ark_module_function` SET `FUNCTION_SEQUENCE`=5 WHERE `ID`='8';
 UPDATE `study`.`ark_module_function` SET `FUNCTION_SEQUENCE`=4 WHERE `ID`='7';
 UPDATE `study`.`ark_module_function` SET `FUNCTION_SEQUENCE`=3 WHERE `ID`='6';
 UPDATE `study`.`ark_module_function` SET `FUNCTION_SEQUENCE`=7 WHERE `ID`='12';
+
+-- Add the permission for the new SUBJECT_CUSTOM_DATA function to the ArkRolePolicyTemplate
+-- *Subject Admin role* 
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(184, 
+4, -- Subject Admin role
+2, -- Subject module
+34, -- SUBJECT_CUSTOM_DATA entry function
+1 -- Create permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(185, 
+4, -- Subject Admin role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+2 -- Read permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(186, 
+4, -- Subject Admin role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+3 -- Update permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(187, 
+4, -- Subject Admin role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+4 -- Delete permission (this won't actually be used, but is there to reflect that it is possible)
+);
+
+-- *Subject Data Manager* role
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(188, 
+5, -- Subject Data Manager role
+2, -- Subject module
+34, -- SUBJECT_CUSTOM_DATA entry function
+1 -- Create permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(189, 
+5, -- Subject Data Manager role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+2 -- Read permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(190, 
+5, -- Subject Data Manager role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+3 -- Update permission
+);
+INSERT INTO `study`.`ark_role_policy_template`
+(`ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`)
+VALUES
+(191, 
+5, -- Subject Data Manager role
+2, -- Subject module
+34, -- Subject Custom Data entry function
+4 -- Delete permission (this won't actually be used, but is there to reflect that it is possible)
+);
+
+-- Remap an existing permission for *Subject Read-Only* role to the newly created function
+UPDATE `study`.`ark_role_policy_template` SET `ARK_FUNCTION_ID`=34 WHERE `ID`='69';
+
+-- Verify the updates have been correctly implemented in the database
+SELECT * 
+  FROM `study`.`ark_role_policy_template` arpt
+ INNER JOIN `study`.`ark_function` af
+    ON af.id = arpt.ark_function_id
+ INNER JOIN `study`.`ark_role` ar
+    ON ar.id = arpt.ark_role_id
+ WHERE arpt.ark_function_id = 11
+    OR arpt.ark_function_id = 34;
