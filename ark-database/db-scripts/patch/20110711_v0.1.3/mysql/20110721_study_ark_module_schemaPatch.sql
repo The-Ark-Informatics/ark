137192f141d283c9a5342d19a4854fa5b564dab7
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-08-22 10:04:42 +0000

    Needed to change the INDEX name to be unique (NAME_UNIQUE is seemingly already used in ArkTestDB).
    git-svn-id: svn://localhost@4751 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110721_study_ark_module_schemaPatch.sql
+++ ark-database/db-scripts/patch/20110711_v0.1.3/mysql/20110721_study_ark_module_schemaPatch.sql
@@ -2,4 +2,4 @@ USE study;
 
 -- Ensures the module names are unique
 ALTER TABLE `study`.`ark_module` 
-ADD UNIQUE INDEX `NAME_UNIQUE` (`NAME` ASC) ;
+ADD UNIQUE INDEX `ARK_MODULE_NAME_UNIQUE` (`NAME` ASC) ;
