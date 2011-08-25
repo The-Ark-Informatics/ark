d8592d5f28c4436122b3bd31d832accc0fed3b1c
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-08-25 06:54:31 +0000

    Reworked to simplify classes (much quicker response than before). Commented methods, formatted etc Amended cell icons
    git-svn-id: svn://localhost@4807 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/box/display/GridBoxPanel.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/box/display/GridBoxPanel.java
@@ -18,16 +18,18 @@
  ******************************************************************************/
 package au.org.theark.lims.web.component.inventory.panel.box.display;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import jxl.Cell;
-import jxl.Sheet;
 import jxl.Workbook;
-import jxl.format.Colour;
-import jxl.write.WritableCellFormat;
-import jxl.write.WritableFont;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 
@@ -38,7 +40,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.html.JavascriptPackageResource;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -50,143 +51,93 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.file.File;
 import org.apache.wicket.util.io.ByteArrayOutputStream;
+import org.apache.wicket.util.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import au.org.theark.core.model.lims.entity.Biospecimen;
 import au.org.theark.core.model.lims.entity.InvBox;
 import au.org.theark.core.model.lims.entity.InvCell;
-import au.org.theark.core.util.ArkSheetMetaData;
 import au.org.theark.lims.service.IInventoryService;
-import au.org.theark.lims.service.ILimsService;
 import au.org.theark.lims.web.Constants;
 
+/**
+ * 
+ * @author cellis
+ *
+ */
 public class GridBoxPanel extends Panel {
 	/**
 	 * 
 	 */
 	private static final long					serialVersionUID		= -4769477913855966069L;
-	private transient Sheet						sheet;
-	private transient ArkSheetMetaData		sheetMetaData;
 	private byte[]									workBookAsBytes;
 	private WebMarkupContainer					gridBoxKeyContainer	= new WebMarkupContainer("gridBoxKeyContainer");
 	public String									exportXlsFileName;
 
 	private static final ResourceReference	EMPTY_CELL_ICON		= new ResourceReference(GridBoxPanel.class, "emptyCell.gif");
 	private static final ResourceReference	USED_CELL_ICON			= new ResourceReference(GridBoxPanel.class, "usedCell.gif");
-	private static final ResourceReference	BARCODE_CELL_ICON = new ResourceReference(GridBoxPanel.class, "barcodeCell.gif");
+	private static final ResourceReference	BARCODE_CELL_ICON		= new ResourceReference(GridBoxPanel.class, "barcodeCell.gif");
+
+	private static final Logger				log						= LoggerFactory.getLogger(GridBoxPanel.class);
 
 	@SpringBean(name = Constants.LIMS_INVENTORY_SERVICE)
 	private IInventoryService					iInventoryService;
 
-	@SpringBean(name = Constants.LIMS_SERVICE)
-	private ILimsService							iLimsService;
-
 	public GridBoxPanel(String id, InvBox invBox, String exportXlsfileName) {
 		super(id);
-		this.sheetMetaData = new ArkSheetMetaData();
 		this.exportXlsFileName = exportXlsfileName;
-		if (this.exportXlsFileName.isEmpty() || this.exportXlsFileName == null) {
+		if (this.exportXlsFileName == null) {
 			this.exportXlsFileName = "GridBoxData";
 		}
 		initialiseGrid(invBox);
-		initialiseGridKey();
-
-		add(JavascriptPackageResource.getHeaderContribution("js/addEvent.js"));
-		add(JavascriptPackageResource.getHeaderContribution("js/sweetTitles.js"));
-	}
-
-	private void initialiseGrid(InvBox invBox) {
-		workBookAsBytes = initialiseWorkBook(invBox);
-
-		System.out.println("Adding headings");
-		add(createHeadings(invBox));
-		add(createMainGrid());
-		System.out.println("Added panel");
 	}
 
 	/**
-	 * Initialise a WorkBook object representing the Box from the database and store as byte array
-	 * 
+	 * Initialises the grid to display for the InvBox in question
 	 * @param invBox
-	 * @return WorkBook as a byte array
 	 */
-	private byte[] initialiseWorkBook(InvBox invBox) {
+	private void initialiseGrid(InvBox invBox) {
 		invBox = iInventoryService.getInvBox(invBox.getId());
-		List<InvCell> invCellList = invBox.getInvCells();
-
-		byte[] bytes = null;
-		try {
-			ByteArrayOutputStream output = new ByteArrayOutputStream();
-			WritableWorkbook writableWorkBook = Workbook.createWorkbook(output);
-			WritableSheet writableSheet = writableWorkBook.createSheet("Sheet", 0);
-			WritableFont normalFont = new WritableFont(WritableFont.ARIAL, 10, WritableFont.NO_BOLD);
-			WritableCellFormat usedCellFormat = new WritableCellFormat(normalFont);
-			usedCellFormat.setBackground(Colour.BLUE);
-
-			int col = 1;
-			int row = 1;
-			int cellCount = 0;
-
-			for (Iterator<InvCell> iterator = invCellList.iterator(); iterator.hasNext();) {
-				InvCell invCell = (InvCell) iterator.next();
-				row = invCell.getRowno().intValue();
-				col = invCell.getColno().intValue();
-
-				Biospecimen biospecimen = new Biospecimen();
-				biospecimen = invCell.getBiospecimen();
-				String cellData = null;
-				jxl.write.Label label = null;
-
-				if (biospecimen != null) {
-					cellData = biospecimen.getBiospecimenUid();
-					label = new jxl.write.Label(col, row, cellData);
-					label.setCellFormat(usedCellFormat);
-				}
-				else {
-					label = new jxl.write.Label(col, row, "");
-				}
-				writableSheet.addCell(label);
-				cellCount = cellCount + 1;
-			}
-
-			System.out.println("Cell count: " + cellCount);
-			// get First Work Sheet to display in webpage
-			sheet = writableWorkBook.getSheet(0);
-
-			bytes = output.toByteArray();
-			writableWorkBook.write();
-			writableWorkBook.close();
-
-			output.close();
+		List<InvCell> invCellList = new ArrayList<InvCell>(0);
+		invCellList = iInventoryService.getCellAndBiospecimenListByBox(invBox);
+		
+		// Handle for no cells in InvCell table!
+		int cells = invBox.getNoofcol() * invBox.getNoofrow();
+		if (invCellList.size() != cells) {
+			log.error("InvCell table is missing data for invBox.id " + invBox.getId());
+			this.error("InvCell table is missing data for invBox.id " + invBox.getId());
+			this.setVisible(false);
+			add(createHeadings(new InvBox()));
+			add(createMainGrid(new InvBox(), invCellList));
 		}
-		catch (Exception e) {
-			e.printStackTrace();
+		else {
+			add(createHeadings(invBox));
+			add(createMainGrid(invBox, invCellList));
+			initialiseGridKey(invCellList);
 		}
-
-		// Sets Sheet meta data. The HTML table creation needs this object to know about the rows and columns
-		sheetMetaData.setRows(sheet.getRows() - 1);
-		sheetMetaData.setCols(sheet.getColumns() - 1);
-
-		return bytes;
 	}
 
+	/**
+	 * Creates the header of the table for the represented InvBox in question
+	 * @param invBox
+	 * @return
+	 */
 	@SuppressWarnings( { "unchecked" })
 	private Loop createHeadings(final InvBox invBox) {
-		return new Loop("heading", new PropertyModel(sheetMetaData, "cols")) {
+		// Outer Loop instance, using a PropertyModel to bind the Loop iteration to invBox "noofcol" value
+		return new Loop("heading", new PropertyModel(invBox, "noofcol")) {
 			/**
 			 * 
 			 */
 			private static final long	serialVersionUID	= -7027878243061138904L;
 
 			public void populateItem(LoopItem item) {
-				// Note, added 1 to correct 0th element start
-				final int col = item.getIteration() + 1;
+				final int col = item.getIteration();
 
-				/*
-				 * this model used for Label component gets data from cell instance Because we are interacting directly with the sheet instance which gets
-				 * updated each time we upload a new Excel File, the value for each cell is automatically updated
-				 */
-				IModel<Object> model = new Model() {
+				IModel<String> colModel = new Model() {
 					/**
 					 * 
 					 */
@@ -201,31 +152,44 @@ public class GridBoxPanel extends Panel {
 							label = new Character(character).toString();
 						}
 						else {
-							label = new Integer(col).toString();
+							label = new Integer(col+1).toString();
 						}
 						return label;
 					}
 				};
-				Label cellData = new Label("cellHead", model);
-				item.add(cellData);
+				
+				// Create the col number/label
+				Label colLabel = new Label("cellHead", colModel);
+				item.add(colLabel);
 			}
 		};
 	}
-
-	/*
-	 * generating rows using the Loop class and the PropertyModel with SheetMetaData instance works magicWe bound the numbers of rows stored in
-	 * SheetMetaData instance to the Loop using PropertyModel. No table will be displayed before an upload.
+	
+	/**
+	 * Creates the table data that represents the cells of the InvBox in question
+	 * @param invBox
+	 * @param invCellList
+	 * @return
 	 */
 	@SuppressWarnings( { "serial", "unchecked" })
-	private Loop createMainGrid() {
-		// We create a Loop instance and uses PropertyModel to bind the Loop iteration to ExcelMetaData "rows" value
-		return new Loop("rows", new PropertyModel(sheetMetaData, "rows")) {
+	private Loop createMainGrid(final InvBox invBox, final List<InvCell> invCellList) {
+		// Outer Loop instance, using a PropertyModel to bind the Loop iteration to invBox "noofrow" value
+		Loop loop = new Loop("rows", new PropertyModel(invBox, "noofrow")) {
 			public void populateItem(LoopItem item) {
-				// Note, added 1 to correct 0th element start
-				final int row = item.getIteration() +1;
-
+				final int row = item.getIteration();
+				
 				// Create the row number/label
-				Label rowLabel = new Label("rowNo", new Model(String.valueOf(row)));
+				String label = new String();
+				String rowNoType = invBox.getRownotype();
+				if (rowNoType.equalsIgnoreCase("ALPHABET")) {
+					char character = (char) (row + 65);
+					label = new Character(character).toString();
+				}
+				else {
+					label = new Integer(row+1).toString();
+				}
+
+				Label rowLabel = new Label("rowNo", new Model(label));
 				rowLabel.add(new AbstractBehavior() {
 					public void onComponentTag(Component component, ComponentTag tag) {
 						super.onComponentTag(component, tag);
@@ -233,75 +197,70 @@ public class GridBoxPanel extends Panel {
 					};
 				});
 				item.add(rowLabel);
-				
-					// We create an inner Loop instance and uses PropertyModel to bind the Loop iteration to ExcelMetaData "cols" value
-					item.add(new Loop("cols", new PropertyModel(sheetMetaData, "cols")) {
-						public void populateItem(LoopItem item) {
-							final int col = item.getIteration() +1;
-							//if(col>0) {
-								/*
-								 * this model used for Label component gets data from cell instance Because we are interacting directly with the sheet instance
-								 * which gets updated each time we upload a new Excel File, the value for each cell is automatically updated
-								 */						
-								IModel<Biospecimen> biospecimenModel = new Model() {
-									@Override
-									public Serializable getObject() {
-										Cell cell = sheet.getCell(col, row);
-										Biospecimen biospecimen = iLimsService.getBiospecimenByUid(cell.getContents());
-										return biospecimen;
-									}
-								};
-		
-								//Label cellData = new Label("cellData", model);
-								//item.add(cellData);
-		
-								// Cell used/empty icon
-								Component cellIconComponent = cellIconComponent("cellIcon", biospecimenModel);
-								
-								// tooltip of cell
-								StringBuffer stringBuffer = new StringBuffer();
-								stringBuffer.append("Column: ");
-								stringBuffer.append(col);
-								stringBuffer.append("\t");
-								stringBuffer.append("Row: ");
-								stringBuffer.append(row);
-								stringBuffer.append("\t");
-								stringBuffer.append("Status: ");
-								
-								if(biospecimenModel.getObject() != null) {
-									stringBuffer.append("Used");
-									stringBuffer.append("\t");
-									stringBuffer.append("BiospecimenUID: ");
-									stringBuffer.append(biospecimenModel.getObject().getBiospecimenUid());
-									stringBuffer.append("\t");
-									stringBuffer.append("Sample Type: ");
-									stringBuffer.append(biospecimenModel.getObject().getSampleType().getName());
-									stringBuffer.append("\t");
-									stringBuffer.append("Quantity: ");
-									stringBuffer.append(biospecimenModel.getObject().getQuantity());
-								}
-								else {
-									stringBuffer.append("Empty");
-								}
-								
-								String toolTip = stringBuffer.toString();
-								cellIconComponent.add(new AttributeModifier("showtooltip", true, new Model("true")));
-								cellIconComponent.add(new AttributeModifier("title", true, new Model(toolTip)));
-								cellIconComponent.add(new AbstractBehavior() {
-									public void onComponentTag(Component component, ComponentTag tag) {
-										super.onComponentTag(component, tag);
-										tag.put("style", "padding: 1px;");
-									};
-								});
-								item.add(cellIconComponent);		
-							}
-						//}
-					});
-				//}
+
+				// We create an inner Loop instance and uses PropertyModel to bind the Loop iteration to invBox "noofcol" value
+				item.add(new Loop("cols", new PropertyModel(invBox, "noofcol")) {
+					public void populateItem(LoopItem item) {
+						final int col = item.getIteration();
+						
+						final int index = (row * invBox.getNoofcol()) + col;
+						
+						InvCell invCell = invCellList.get(index);
+						IModel<Biospecimen> biospecimenModel = new PropertyModel<Biospecimen>(invCell, "biospecimen");
+						
+						// Cell used/empty icon
+						Component cellIconComponent = cellIconComponent("cellIcon", biospecimenModel);
+
+						// tooltip of cell
+						StringBuffer stringBuffer = new StringBuffer();
+						stringBuffer.append("Column: ");
+						stringBuffer.append(col);
+						stringBuffer.append("\t");
+						stringBuffer.append("Row: ");
+						stringBuffer.append(row);
+						stringBuffer.append("\t");
+						stringBuffer.append("Status: ");
+
+						if (biospecimenModel.getObject() != null) {
+							stringBuffer.append("Used");
+							stringBuffer.append("\t");
+							stringBuffer.append("BiospecimenUID: ");
+							stringBuffer.append(biospecimenModel.getObject().getBiospecimenUid());
+							stringBuffer.append("\t");
+							stringBuffer.append("Sample Type: ");
+							stringBuffer.append(biospecimenModel.getObject().getSampleType().getName());
+							stringBuffer.append("\t");
+							stringBuffer.append("Quantity: ");
+							stringBuffer.append(biospecimenModel.getObject().getQuantity());
+						}
+						else {
+							stringBuffer.append("Empty");
+						}
+
+						String toolTip = stringBuffer.toString();
+						cellIconComponent.add(new AttributeModifier("showtooltip", true, new Model("true")));
+						cellIconComponent.add(new AttributeModifier("title", true, new Model(toolTip)));
+						cellIconComponent.add(new AbstractBehavior() {
+							public void onComponentTag(Component component, ComponentTag tag) {
+								super.onComponentTag(component, tag);
+								tag.put("style", "padding: 1px;");
+							};
+						});
+						item.add(cellIconComponent);
+					}
+					// }
+				});
 			}
 		};
+		return loop;
 	}
 
+	/**
+	 * Creates the icon dislplayed at the cell in question
+	 * @param componentId
+	 * @param biospecimenModel
+	 * @return
+	 */
 	protected Component cellIconComponent(String componentId, final IModel<Biospecimen> biospecimenModel) {
 		return new Image(componentId) {
 			private static final long	serialVersionUID	= 1L;
@@ -332,7 +291,7 @@ public class GridBoxPanel extends Panel {
 		return resourceReference;
 	}
 
-	private void initialiseGridKey() {
+	private void initialiseGridKey(List<InvCell> invCellList) {
 		gridBoxKeyContainer.setOutputMarkupId(true);
 
 		gridBoxKeyContainer.add(new Image("emptyCellIcon", EMPTY_CELL_ICON));
@@ -340,11 +299,59 @@ public class GridBoxPanel extends Panel {
 		gridBoxKeyContainer.add(new Image("barcodeCellIcon", BARCODE_CELL_ICON));
 
 		// Download file link button
-		gridBoxKeyContainer.add(buildDownloadButton());
+		gridBoxKeyContainer.add(buildDownloadButton(invCellList));
 		add(gridBoxKeyContainer);
 	}
+	
+	/**
+	 * Initialise a WorkBook object representing the Box from the database and store as byte array
+	 * 
+	 * @param invCellList
+	 * @return WorkBook as a byte array
+	 */
+	private byte[] createWorkBookAsByteArray(List<InvCell> invCellList) {
+		byte[] bytes = null;
+		try {
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			WritableWorkbook writableWorkBook = Workbook.createWorkbook(output);
+			WritableSheet writableSheet = writableWorkBook.createSheet("Sheet", 0);
 
-	private AjaxButton buildDownloadButton() {
+			int col = 0;
+			int row = 0;
+			int cellCount = 0;
+
+			for (Iterator<InvCell> iterator = invCellList.iterator(); iterator.hasNext();) {
+				InvCell invCell = (InvCell) iterator.next();
+				row = invCell.getRowno().intValue();
+				col = invCell.getColno().intValue();
+
+				Biospecimen biospecimen = new Biospecimen();
+				biospecimen = invCell.getBiospecimen();
+				jxl.write.Label label = null;
+
+				if (biospecimen != null) {
+					label = new jxl.write.Label(col, row, biospecimen.getBiospecimenUid());
+				}
+				else {
+					label = new jxl.write.Label(col, row, "");
+				}
+				writableSheet.addCell(label);
+				cellCount = cellCount + 1;
+			}
+
+			writableWorkBook.write();
+			writableWorkBook.close();
+			bytes = output.toByteArray();
+			output.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return bytes;
+	}
+
+	private AjaxButton buildDownloadButton(final List<InvCell> invCellList) {
 		AjaxButton ajaxButton = new AjaxButton("downloadGridBoxData", new StringResourceModel("downloadGridBoxData", this, null)) {
 			/**
 			 * 
@@ -353,11 +360,29 @@ public class GridBoxPanel extends Panel {
 
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				System.out.println("Downloading grid as XLS");
-				byte[] data = workBookAsBytes;
+				log.info("Downloading grid as XLS");
+				byte[] data = createWorkBookAsByteArray(invCellList);
 				String filename = exportXlsFileName + ".xls";
 				if (data != null) {
-					getRequestCycle().setRequestTarget(new au.org.theark.core.util.ByteDataRequestTarget("application/vnd.ms-excel", data, filename));
+					log.info("Writing out XLS to client");
+					au.org.theark.core.util.ByteDataRequestTarget requestTarget = new au.org.theark.core.util.ByteDataRequestTarget("application/vnd.ms-excel", data, filename);
+					getRequestCycle().setRequestTarget(requestTarget);
+				}
+
+				//TODO: Remove when working
+				InputStream inputStream = new ByteArrayInputStream(data);
+				OutputStream outputStream;
+				try {
+					java.io.File file = File.createTempFile("tmpBoxData_", ".xls");
+					log.info("Writing out temp file to: " + file.getCanonicalPath());
+					outputStream = new FileOutputStream(file);
+					IOUtils.copy(inputStream, outputStream);
+				}
+				catch (FileNotFoundException e) {
+					log.error(e.getMessage());
+				}
+				catch (IOException e) {
+					log.error(e.getMessage());
 				}
 			};
 		};
