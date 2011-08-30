6884358a4f54277836057c0bf8ec69ae2f18cb66
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-08-30 03:42:20 +0000

    Amended isVisibile() and isAccessible() to call isModuleVisible()
    git-svn-id: svn://localhost@4878 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-geno/src/main/java/au/org/theark/geno/web/menu/GenoTabProviderImpl.java
+++ ark-geno/src/main/java/au/org/theark/geno/web/menu/GenoTabProviderImpl.java
@@ -25,77 +25,62 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.org.theark.core.model.study.entity.ArkModule;
-import au.org.theark.core.security.ArkPermissionHelper;
-import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.core.service.IMainTabProvider;
 import au.org.theark.core.web.component.ArkMainTab;
-import au.org.theark.geno.web.Constants;
 
-public class GenoTabProviderImpl extends Panel implements IMainTabProvider
-{
+public class GenoTabProviderImpl extends Panel implements IMainTabProvider {
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 5330252275905661708L;
-	private transient static Logger log = LoggerFactory.getLogger(GenoTabProviderImpl.class);
-	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService<Void>	iArkCommonService;
+	private static final long			serialVersionUID	= 5330252275905661708L;
+	private transient static Logger	log					= LoggerFactory.getLogger(GenoTabProviderImpl.class);
 	List<ITab>								moduleTabsList;
 
-	public GenoTabProviderImpl(String panelId)
-	{
+	public GenoTabProviderImpl(String panelId) {
 		super(panelId);
 		moduleTabsList = new ArrayList<ITab>();
 	}
 
-	public List<ITab> buildTabs()
-	{
+	public List<ITab> buildTabs() {
 		log.info("Creating main tab: " + au.org.theark.core.Constants.ARK_MODULE_GENOTYPIC);
 		ITab tab1 = createTab(au.org.theark.core.Constants.ARK_MODULE_GENOTYPIC);// Forms the Main Top level Tab
 		moduleTabsList.add(tab1);
 		return moduleTabsList;
 	}
 
-	public ITab createTab(final String tabName)
-	{
-		return new ArkMainTab(new Model<String>(tabName))
-		{
+	public ITab createTab(final String tabName) {
+		return new ArkMainTab(new Model<String>(tabName)) {
 			/**
 			 * 
 			 */
 			private static final long	serialVersionUID	= 197065531396767547L;
 
 			@Override
-			public Panel getPanel(String pid)
-			{
+			public Panel getPanel(String pid) {
 				GenoSubMenuTab genoSubMenu = new GenoSubMenuTab(pid);// The sub menus Genotypic
 				genoSubMenu.buildTabs();
 				return genoSubMenu;
 			}
 
-			public boolean isAccessible()
-			{
-				Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
-				if (sessionStudyId == null)
-				{
-					this.getPanel(Constants.GENOTYPIC_MAIN_TAB).error(au.org.theark.core.Constants.NO_STUDY_IN_CONTEXT_MESSAGE);
-					return false;
-				}
-				else
-					return true;
+			public boolean isAccessible() {
+				return isModuleVisible();
 			}
-			
-			public boolean isVisible()
-			{
-				//ArkModule arkModule = iArkCommonService.getArkModuleByName(tabName);
-				//return ArkPermissionHelper.isModuleAccessPermitted(arkModule);
-				return true;
+
+			public boolean isVisible() {
+				return isModuleVisible();
 			}
 		};
+	}
+
+	public boolean isModuleVisible() {
+		Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		if (sessionStudyId == null) {
+			return false;
+		}
+		else
+			return true;
 	}
 }
