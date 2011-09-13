bb24935ce6e8cc5b78e1361739f50db828f59476
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-13 16:55:08 +0000

    Removed this patch because we're blowing away the old table in the 20110912_lims_4_bio_transaction_schemaPatch.sql
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5108 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/0.1.4/20110909_lims_bio_transaction_schemaPatch.sql
+++
@@ -1,9 +0,0 @@
-USE lims;
-
--- Biotransaction are against a biospecimen, and the biospecimen already knows which study it is attached to.
--- So no need have the study on this table
-ALTER TABLE `lims`.`bio_transaction` DROP COLUMN `SOURCESTUDY_ID` , DROP COLUMN `STUDY_ID` ;
-ALTER TABLE `lims`.`bio_transaction` DROP COLUMN `STUDY` ;
-
--- Support soft-deletes (now mapped to a Boolean on the entity)
-ALTER TABLE `lims`.`bio_transaction` CHANGE COLUMN `DELETED` `DELETED` TINYINT(4) NULL DEFAULT NULL  ;
