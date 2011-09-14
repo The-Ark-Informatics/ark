3dcc60dca883f337eb922668796299b0afc79477
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-14 06:01:19 +0000

    Added unit_id schema update
    git-svn-id: svn://localhost/ARK@5136 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.4/20110913_lims_biospecimen_schemaPatch.sql
+++ ark-database/db-scripts/patch/0.1.4/20110913_lims_biospecimen_schemaPatch.sql
@@ -14,3 +14,11 @@ ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `TREATMENT` `TREATMENT_TYPE_ID` I
 
 -- quantity is sum of all transactions
 ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `QUANTITY` `QUANTITY` DOUBLE NULL DEFAULT NULL  ;
+
+-- Change units to unit_id (reference table)
+ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `UNITS` `UNIT_ID` INT(11) NULL DEFAULT NULL  , 
+  ADD CONSTRAINT `fk_biospecimen_unit`
+  FOREIGN KEY (`UNIT_ID` )
+  REFERENCES `lims`.`unit` (`ID` )
+  ON UPDATE CASCADE
+, ADD INDEX `fk_biospecimen_unit` (`UNIT_ID` ASC) ;
