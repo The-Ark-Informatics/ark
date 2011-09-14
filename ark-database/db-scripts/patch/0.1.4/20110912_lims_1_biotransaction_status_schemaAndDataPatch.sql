5fd15e1072752053327f478b9cf36054ee78eb72
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-14 01:35:50 +0000

    Corrected bad (wrong table name) insert statements.
    git-svn-id: svn://localhost/ARK@5124 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.4/20110912_lims_1_biotransaction_status_schemaAndDataPatch.sql
+++ ark-database/db-scripts/patch/0.1.4/20110912_lims_1_biotransaction_status_schemaAndDataPatch.sql
@@ -6,3 +6,7 @@ CREATE  TABLE `lims`.`bio_transaction_status` (
   PRIMARY KEY (`ID`) ,
   UNIQUE INDEX `NAME_UNIQUE` (`NAME` ASC) )
 ENGINE = InnoDB;
+
+INSERT INTO `lims`.`bio_transaction_status` (`NAME`) VALUES ('Initial Qty');
+INSERT INTO `lims`.`bio_transaction_status` (`NAME`) VALUES ('Pending');
+INSERT INTO `lims`.`bio_transaction_status` (`NAME`) VALUES ('Completed');
