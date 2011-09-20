ff3fd2033422cd4f36f1709a4893d0aea9b0b83d
nivedann <nivedann@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-20 03:00:13 +0000

    New version of Pheno Collection. Once this is implemented, the older PhenoCollection entity must be removed.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5209 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/model/pheno/entity/PhenotypicCollection.java
@@ -0,0 +1,139 @@
+package au.org.theark.core.model.pheno.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import au.org.theark.core.model.Constants;
+import au.org.theark.core.model.study.entity.ArkUser;
+import au.org.theark.core.model.study.entity.CustomFieldGroup;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+
+/**
+ * @author nivedann
+ *
+ */
+@Entity
+@Table(name = "PHENO_COLLECTION", schema = Constants.PHENO_TABLE_SCHEMA)
+public class PhenotypicCollection implements Serializable{
+	
+	
+	private Long id;
+	private String name;
+	private String description;
+	private LinkSubjectStudy linkSubjectStudy;
+	private Date recordedDate;
+	private Date reviewedDate;
+	private ArkUser reviewedBy;
+	private CustomFieldGroup customFieldGroup;
+	private QuestionnaireStatus status;
+	
+	/**
+	 * Constructor
+	 */
+	public  PhenotypicCollection(){
+		
+	}
+	
+	@Id																			
+	@SequenceGenerator(name = "phenotypic_collection_generator", sequenceName = "PHENOTYPIC_COLLECTION_SEQ")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "phenotypic_collection_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", length = 255)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "DESCRIPTION", length = 500)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "LINK_SUBJECT_STUDY_ID")
+	public LinkSubjectStudy getLinkSubjectStudy() {
+		return linkSubjectStudy;
+	}
+
+	public void setLinkSubjectStudy(LinkSubjectStudy linkSubjectStudy) {
+		this.linkSubjectStudy = linkSubjectStudy;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "RECORDED_DATE", nullable = false)
+	public Date getRecordedDate() {
+		return recordedDate;
+	}
+
+	public void setRecordedDate(Date recordedDate) {
+		this.recordedDate = recordedDate;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "REVIEWED_DATE", nullable = false)
+	public Date getReviewedDate() {
+		return reviewedDate;
+	}
+
+	public void setReviewedDate(Date reviewedDate) {
+		this.reviewedDate = reviewedDate;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "REVIEWED_BY_ID")
+	public ArkUser getReviewedBy() {
+		return reviewedBy;
+	}
+
+	public void setReviewedBy(ArkUser reviewedBy) {
+		this.reviewedBy = reviewedBy;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CUSTOM_FIELD_GROUP_ID")
+	public CustomFieldGroup getCustomFieldGroup() {
+		return customFieldGroup;
+	}
+
+	public void setCustomFieldGroup(CustomFieldGroup customFieldGroup) {
+		this.customFieldGroup = customFieldGroup;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "QUESTIONNAIRE_STATUS_ID")
+	public QuestionnaireStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(QuestionnaireStatus status) {
+		this.status = status;
+	}
+}
