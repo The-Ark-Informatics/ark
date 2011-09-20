2409352dc39cda407bc8b9cef4f00d9e668847bf
nivedann <nivedann@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-20 06:36:58 +0000

    added boolean published field.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5216 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/0.1.4/20110920_study_custom_field_group_schemaPatch.sql
+++ ark-database/db-scripts/patch/0.1.4/20110920_study_custom_field_group_schemaPatch.sql
@@ -10,5 +10,22 @@ ALTER TABLE `study`.`custom_field_group` ADD COLUMN `STUDY_ID` INT NULL  AFTER `
 
 ALTER TABLE `study`.`custom_field_group` CHANGE COLUMN `STUDY_ID` `STUDY_ID` INT(11) NOT NULL  ;
 
+/* Use this script if the FK was dropped for some reason after adding STUDY_ID not null 
+ALTER TABLE `study`.`custom_field_group` 
+  ADD CONSTRAINT `FK_CUSTOM_FIELD_GROUP_STUDY_ID`
+  FOREIGN KEY (`STUDY_ID` )
+  REFERENCES `study`.`study` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, DROP PRIMARY KEY 
+, ADD PRIMARY KEY (`ID`) ;
+
+
+*/
+
+/* We need this field to stop users from using the Questionnaire. When published field is set to true, then it should be avaialble to users to data entry */
+
+ALTER TABLE `study`.`custom_field_group` ADD COLUMN `PUBLISHED` TINYINT(1)  NULL  AFTER `STUDY_ID` ;
+
 
 
