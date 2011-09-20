1b66305ef16798563db5dd7b29116e300f23e08e
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-20 08:04:23 +0000

    removed data patch for function. Not needed will re-use the functions defined for pheno.
    git-svn-id: svn://localhost/ARK@5221 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.4/20110920_study_ark_function_dataPatch.sql
+++
@@ -1,7 +0,0 @@
-use study;
-INSERT INTO `study`.`ark_function` (`ID`, `NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) VALUES (39, 'MANAGE_QUESTIONNAIRE', 'Create,Edit and Remove Questionnaires. Allows to add Custom Fields to Custom Field Group.', 1, 'tab.module.phenotypic.manage.questionnaire');
-
-/* Associate the Ark Function 39 Manage Questionnaire with Phenotypic Module */
-INSERT INTO `study`.`ark_module_function` (`ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (36, 3, 39, 7);
-
-
