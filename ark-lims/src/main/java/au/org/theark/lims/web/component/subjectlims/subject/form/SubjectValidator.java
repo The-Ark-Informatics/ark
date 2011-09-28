210bbfc0afb10ea918f877dc2e0ed0f2ae65052a
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-28 02:40:51 +0000

    Amended warnings
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5342 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-lims/src/main/java/au/org/theark/lims/web/component/subjectlims/subject/form/SubjectValidator.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/subjectlims/subject/form/SubjectValidator.java
@@ -22,9 +22,13 @@ import java.util.Calendar;
 
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidator;
-import org.apache.wicket.validation.ValidationError;
 
 public class SubjectValidator implements IValidator<Long> {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -5915383351087650967L;
 
 	/*
 	 * (non-Javadoc)
@@ -32,13 +36,12 @@ public class SubjectValidator implements IValidator<Long> {
 	 * @see org.apache.wicket.validation.IValidator#validate(org.apache.wicket.validation.IValidatable)
 	 */
 	public void validate(IValidatable<Long> arg0) {
-		// TODO Auto-generated method stub
 		if (arg0.getValue() != null) {
 			Calendar calendar = Calendar.getInstance();
 			int calYear = calendar.get(Calendar.YEAR);
 			if (arg0.getValue() > calYear) {
 
-				ValidationError ve = new ValidationError().addMessageKey("error.found");
+				//ValidationError ve = new ValidationError().addMessageKey("error.found");
 				// ve.setVariables(vars);
 			}
 		}
