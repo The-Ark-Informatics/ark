e6c8bf7402bc4ebbb1b5cf374433e45f0ef77d35
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-28 03:06:53 +0000

    Refactored to more simpler pattern (ie not search/detail/results)
    git-svn-id: svn://localhost/ARK@5345 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/summaryModule/SummaryContainerPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/summaryModule/SummaryContainerPanel.java
@@ -18,85 +18,18 @@
  ******************************************************************************/
 package au.org.theark.phenotypic.web.component.summaryModule;
 
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.list.PageableListView;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
 
-import au.org.theark.core.model.pheno.entity.PhenoCollection;
-import au.org.theark.core.web.component.AbstractContainerPanel;
-import au.org.theark.phenotypic.model.vo.PhenoCollectionVO;
-import au.org.theark.phenotypic.web.component.summaryModule.form.ContainerForm;
-
-public class SummaryContainerPanel extends AbstractContainerPanel<PhenoCollectionVO> {
-	private static final long						serialVersionUID	= 1L;
-
-	// Panels
-	private SearchPanel								searchComponentPanel;
-	private SearchResultListPanel					searchResultPanel;
-	private DetailPanel								detailPanel;
-	private PageableListView<PhenoCollection>	listView;
-	private ContainerForm							containerForm;
+public class SummaryContainerPanel extends Panel {
+	private static final long	serialVersionUID	= 1L;
+	private FeedbackPanel		feedBackPanel;
+	private SummaryPanel			summaryPanel;
 
 	public SummaryContainerPanel(String id) {
 		super(id);
-
-		/* Initialise the CPM */
-		cpModel = new CompoundPropertyModel<PhenoCollectionVO>(new PhenoCollectionVO());
-
-		initialiseMarkupContainers();
-
-		/* Bind the CPM to the Form */
-		containerForm = new ContainerForm("containerForm", cpModel);
-		containerForm.add(initialiseFeedBackPanel());
-		containerForm.add(initialiseDetailPanel());
-		containerForm.add(initialiseSearchResults());
-		containerForm.add(initialiseSearchPanel());
-		add(containerForm);
-	}
-
-	protected WebMarkupContainer initialiseSearchResults() {
-		searchResultPanel = new SearchResultListPanel("searchResults", detailPanelContainer, searchPanelContainer, containerForm, searchResultPanelContainer, detailPanel, viewButtonContainer,
-				editButtonContainer, detailPanelFormContainer);
-
-		iModel = new LoadableDetachableModel<Object>() {
-			private static final long	serialVersionUID	= 1L;
-
-			@Override
-			protected Object load() {
-				return containerForm.getModelObject().getPhenoCollectionCollection();
-			}
-		};
-
-		listView = searchResultPanel.buildPageableListView(iModel);
-		listView.setReuseItems(true);
-		PagingNavigator pageNavigator = new PagingNavigator("navigator", listView);
-		searchResultPanel.add(pageNavigator);
-		searchResultPanel.add(listView);
-		searchResultPanelContainer.add(searchResultPanel);
-
-		// for Summary Module, disable search result list
-		searchResultPanel.setVisible(false);
-
-		return searchResultPanelContainer;
-	}
-
-	protected WebMarkupContainer initialiseDetailPanel() {
-		detailPanel = new DetailPanel("detailPanel", searchResultPanelContainer, feedBackPanel, detailPanelContainer, searchPanelContainer, containerForm, viewButtonContainer, editButtonContainer,
-				detailPanelFormContainer);
-		detailPanel.initialisePanel();
-		detailPanelContainer.add(detailPanel);
-		return detailPanelContainer;
-	}
-
-	protected WebMarkupContainer initialiseSearchPanel() {
-		// For Summary Module, no results ever displayed
-		searchComponentPanel = new SearchPanel("searchPanel", feedBackPanel, searchPanelContainer, listView, searchResultPanelContainer, detailPanelContainer, detailPanel, containerForm,
-				viewButtonContainer, editButtonContainer, detailPanelFormContainer);
-		searchComponentPanel.initialisePanel();
-
-		searchPanelContainer.add(searchComponentPanel);
-		return searchPanelContainer;
+		summaryPanel = new SummaryPanel("summaryPanel", feedBackPanel);
+		summaryPanel.initialisePanel();
+		add(summaryPanel);
 	}
 }
