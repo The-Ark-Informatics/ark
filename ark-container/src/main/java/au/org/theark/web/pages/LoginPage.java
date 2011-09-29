cc82ef8912fe35fd5bc542528c923fdf73342f6a
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-29 09:43:51 +0000

    Refactored to separate out the forms from within the page classes (added properties files accordingly)
    git-svn-id: svn://localhost/ARK@5418 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-container/src/main/java/au/org/theark/web/pages/LoginPage.java
+++ ark-container/src/main/java/au/org/theark/web/pages/LoginPage.java
@@ -18,85 +18,52 @@
  ******************************************************************************/
 package au.org.theark.web.pages;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.StatelessForm;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.cycle.RequestCycle;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.util.visit.IVisitor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import au.org.theark.core.model.study.entity.ArkFunction;
-import au.org.theark.core.model.study.entity.ArkModule;
-import au.org.theark.core.service.IArkCommonService;
-import au.org.theark.core.vo.ArkUserVO;
-import au.org.theark.core.web.component.button.ArkIndicatingAjaxButton;
-import au.org.theark.core.web.form.ArkFormVisitor;
-
-public class LoginPage<T> extends WebPage
-{
+/**
+ * <p>
+ * The <code>LoginPage</code> class that extends the {@link au.org.theark.web.pages.WebPage WebPage} class. It provides the implementation of the
+ * login page of The Ark application.
+ * 
+ * @author nivedann
+ * @author cellis
+ * 
+ * @param <T>
+ */
+public class LoginPage<T> extends WebPage {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-
-	private transient Logger log = LoggerFactory.getLogger(LoginPage.class);
-
-	private FeedbackPanel feedBackPanel	= new FeedbackPanel("feedbackMessage");
-
-	// Add a visitor class for required field marking/validation/highlighting
-	private IVisitor	formVisitor		= new ArkFormVisitor();
-
-	@SpringBean( name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService iArkCommonService;
+	private static final long			serialVersionUID	= -985615571643703296L;
+	private FeedbackPanel				feedbackPanel		= new FeedbackPanel("feedbackMessage");
 	
-	@SuppressWarnings("unchecked")
-	public void onBeforeRender()
-	{
-		super.onBeforeRender();
-		visitChildren();
-	}
-
 	/**
-	 * Page Constructor
+	 * LoginPage Constructor
 	 */
-	@SuppressWarnings( { "serial", "unchecked" })
-	public LoginPage()
-	{
-		//log.info("LoginPage() constructor");
+	public LoginPage() {
+		feedbackPanel.setOutputMarkupId(true);
 
-		feedBackPanel.setOutputMarkupId(true);
-
-		LoginForm form = new LoginForm("loginForm");
+		LoginForm form = new LoginForm("loginForm", feedbackPanel);
 
 		this.add(form);
-		this.add(feedBackPanel);
+		this.add(feedbackPanel);
 
-		//AjaxFormValidatingBehavior.addToAllFormComponents(form, "onKeyup", Duration.seconds(2));
 		ContextImage hostedByImage = new ContextImage("hostedByImage", new Model<String>("images/" + Constants.HOSTED_BY_IMAGE));
 		ContextImage productImage = new ContextImage("productImage", new Model<String>("images/" + Constants.PRODUCT_IMAGE));
 
-		Link resetPasswordLink = new Link("resetPasswordLink")
-		{
+		Link<Void> resetPasswordLink = new Link<Void>("resetPasswordLink") {
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
 			@Override
-			public void onClick()
-			{
+			public void onClick() {
 				setResponsePage(ResetPage.class);
 			}
 		};
@@ -111,100 +78,17 @@ public class LoginPage<T> extends WebPage
 	}
 
 	@Override
-	protected void configureResponse(final org.apache.wicket.request.http.WebResponse webResponse)
-	{
-		super.configureResponse((org.apache.wicket.request.http.WebResponse)RequestCycle.get().getResponse());
-		org.apache.wicket.request.http.WebResponse response = (org.apache.wicket.request.http.WebResponse)RequestCycle.get().getResponse();
+	public void onBeforeRender() {
+		super.onBeforeRender();
+		visitChildren();
+	}
+
+	@Override
+	protected void configureResponse(final org.apache.wicket.request.http.WebResponse webResponse) {
+		super.configureResponse((org.apache.wicket.request.http.WebResponse) RequestCycle.get().getResponse());
+		org.apache.wicket.request.http.WebResponse response = (org.apache.wicket.request.http.WebResponse) RequestCycle.get().getResponse();
 		response.setHeader("Cache-Control", "no-cache, max-age=0,must-revalidate, no-store");
 		response.setHeader("Expires", "-1");
 		response.setHeader("Pragma", "no-cache");
-	}
-
-	/**
-	 * The form class that the LoginPage will use to capture input.
-	 */
-	@SuppressWarnings("serial")
-	class LoginForm extends StatelessForm<ArkUserVO>
-	{
-		TextField<String>	userNameTxtFld	= new TextField<String>("userName");
-		PasswordTextField	passwordTxtFld	= new PasswordTextField("password");
-		AjaxButton			submitButton;
-
-	
-
-		private void addComponentsToForm()
-		{
-			add(userNameTxtFld.setRequired(true));
-			add(passwordTxtFld.setRequired(true));
-			add(submitButton);
-		}
-
-		/* Constructor */
-		public LoginForm(String id)
-		{
-			// Pass in the Model to the Form so the IFormSubmitListener can set the Model Object with values that were submitted.
-			super(id, new CompoundPropertyModel<ArkUserVO>(new ArkUserVO()));
-
-			submitButton = new ArkIndicatingAjaxButton("submitBtn")
-			{
-				@Override
-				protected void onSubmit(AjaxRequestTarget target, Form<?> form)
-				{
-					ArkUserVO user = (ArkUserVO) getForm().getModelObject();
-					if (authenticate(target, user))
-					{
-						ArkFunction  arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_STUDY); //Place a default use case into session
-						ArkModule arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_STUDY); //Place a default module into session
-						SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_FUNCTION_KEY, arkFunction.getId());
-						SecurityUtils.getSubject().getSession().setAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY, arkModule.getId());
-						setResponsePage(HomePage.class);
-						
-					}
-				}
-				
-				@Override
-				protected void onError(AjaxRequestTarget target, Form<?> form)
-				{
-					target.add(feedBackPanel);
-				}
-			};
-			addComponentsToForm();
-		}
-		
-
-		public final boolean authenticate(AjaxRequestTarget target, ArkUserVO user)
-		{
-			Subject subject = SecurityUtils.getSubject();
-			UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(user.getUserName(), user.getPassword(), false);// Disable Remember me
-
-			try
-			{
-				subject.login(usernamePasswordToken);// this will propagate to the Realm
-				return true;
-			}
-			catch (IncorrectCredentialsException ice)
-			{
-				String errMessage = getLocalizer().getString("page.incorrect.password", LoginPage.this, "Password is incorrect");
-				error(errMessage);
-			}
-			catch (UnknownAccountException uae)
-			{
-				String errMessage = getLocalizer().getString("page.account.notfound", LoginPage.this, "User account not found.");
-				error(errMessage);
-			}
-			catch (AuthenticationException ae)
-			{
-				String errMessage = getLocalizer().getString("page.invalid.username.password", LoginPage.this, "Invalid username and/or password.");
-				error(errMessage);
-			}
-			catch (Exception ex)
-			{
-				String errMessage = getLocalizer().getString("page.login.failed", LoginPage.this, "Login Failed.");
-				error(errMessage);
-			}
-
-			target.add(feedBackPanel);
-			return false;
-		}
 	}
 }
--- ark-container/src/main/java/au/org/theark/web/pages/ResetPage.java
+++ ark-container/src/main/java/au/org/theark/web/pages/ResetPage.java
@@ -19,25 +19,56 @@
 package au.org.theark.web.pages;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.StatelessForm;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.image.ContextImage;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.org.theark.core.vo.ArkUserVO;
+/**
+ * <p>
+ * The <code>ResetPage</code> class that extends the {@link au.org.theark.web.pages.BasePage BasePage} class. It provides the implementation of the
+ * reset username page of The Ark application.
+ * </p>
+ * 
+ * @author nivedann
+ * @author cellis
+ */
+public class ResetPage<T> extends WebPage {
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -8767984428141993995L;
+	private transient Logger	log					= LoggerFactory.getLogger(ResetPage.class);
 
-public class ResetPage<T> extends WebPage{
+	private String					userId;
+	private String					emailAddress;
+
+	public ResetPage() {
+		log.info("ResetPage() constructor");
+		ContextImage hostedByImage = new ContextImage("hostedByImage", new Model<String>("images/" + Constants.HOSTED_BY_IMAGE));
+		ContextImage productImage = new ContextImage("productImage", new Model<String>("images/" + Constants.PRODUCT_IMAGE));
+
+		add(hostedByImage);
+		add(productImage);
+
+		ResetForm resetForm = new ResetForm("resetForm") {
+
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			protected void onCancel() {
+				setResponsePage(LoginPage.class);
+			}
+
+			protected void onReset() {
+				log.info("onReset() invoked on Reset Password page");
+			}
+		};
+		add(resetForm);
+	}
 	
-	private transient Logger log = LoggerFactory.getLogger(ResetPage.class);
-	
-
-	private String userId;
-	private String emailAddress;
-
 	public String getUserId() {
 		return userId;
 	}
@@ -46,70 +77,11 @@ public class ResetPage<T> extends WebPage{
 		this.userId = userId;
 	}
 
-
 	public String getEmailAddress() {
 		return emailAddress;
 	}
 
-
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
-	}
-
-	@SuppressWarnings("serial")
-	public ResetPage(){
-		log.info("ResetPage() constructor");
-		ContextImage hostedByImage = new ContextImage("hostedByImage",new Model<String>("images/"+Constants.HOSTED_BY_IMAGE));
-		ContextImage productImage = new ContextImage("productImage", new Model<String>("images/"+Constants.PRODUCT_IMAGE));
-		
-		add(hostedByImage);
-		add(productImage);
-		
-		ResetForm resetForm = new ResetForm("resetForm"){
-			
-			protected void onCancel(){
-				setResponsePage(LoginPage.class);
-			}
-			
-			protected void onReset(){
-				log.info("onReset() invoked on Reset Password page");
-			}
-		};
-		add(resetForm);
-	}
-	
-	@SuppressWarnings("serial")
-	class ResetForm extends StatelessForm<T>{
-		@SuppressWarnings("unchecked")
-		public ResetForm(String id) {
-			super(id, new CompoundPropertyModel(new ArkUserVO()));
-			
-			this.add(emailAddressId);
-			this.add(resetButton);
-			this.add(cancelButton);
-		}
-
-		protected void onCancel(){};
-		protected void onReset(){};
-
-		TextField<String> emailAddressId = new TextField<String>("email");
-
-		Button resetButton =  new Button("resetButton")
-		{
-			public void onSubmit()
-			{
-				// Reset user password
-				onReset();
-			}
-		};
-		
-		Button cancelButton = new Button("cancelButton")
-		{
-			public void onSubmit()
-			{
-				// Go to Login page
-				onCancel();
-			}		
-		};
 	}
 }
