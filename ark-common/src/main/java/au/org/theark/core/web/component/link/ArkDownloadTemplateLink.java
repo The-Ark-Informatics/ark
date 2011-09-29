c0a33954db404df6d8199db8d4095d2b82aa1383
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-29 03:54:10 +0000

    Moved another class into the appropriate package location for ark-common links.
    git-svn-id: svn://localhost/ARK@5407 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/component/link/ArkDownloadTemplateLink.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/link/ArkDownloadTemplateLink.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package au.org.theark.core.web.component;
+package au.org.theark.core.web.component.link;
 
 import jxl.Workbook;
 import jxl.write.WritableSheet;
