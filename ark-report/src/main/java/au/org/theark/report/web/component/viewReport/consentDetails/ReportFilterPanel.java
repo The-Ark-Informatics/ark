85ac5cafb7f70da0de6b21c0a365ddb0e358eb01
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-09-30 05:19:43 +0000

    Tidied code, removed warnings
    git-svn-id: svn://localhost/ARK@5453 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-report/src/main/java/au/org/theark/report/web/component/viewReport/consentDetails/ReportFilterPanel.java
+++ ark-report/src/main/java/au/org/theark/report/web/component/viewReport/consentDetails/ReportFilterPanel.java
@@ -18,27 +18,26 @@
  ******************************************************************************/
 package au.org.theark.report.web.component.viewReport.consentDetails;
 
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.report.model.vo.ConsentDetailsReportVO;
-import au.org.theark.report.model.vo.GenericReportViewVO;
 import au.org.theark.report.web.component.viewReport.consentDetails.filterForm.ConsentDetailsFilterForm;
 
+/**
+ * 
+ * @author elam
+ *
+ */
 public class ReportFilterPanel extends Panel {
-
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 1L;
-
-	AjaxButton						generateButton;
+	private static final long	serialVersionUID	= 3615896498822855545L;
 
 	public ReportFilterPanel(String id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 
 	public void initialisePanel(CompoundPropertyModel<ConsentDetailsReportVO> cpModel, FeedbackPanel feedbackPanel, au.org.theark.report.web.component.viewReport.ReportOutputPanel reportOutputPanel) {
