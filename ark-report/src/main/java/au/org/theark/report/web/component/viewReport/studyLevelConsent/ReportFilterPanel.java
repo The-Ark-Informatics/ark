83177a9fd36a98161cfac57d1d1f66fcf1f09a67
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-30 05:14:20 +0000

    Tidied code, removed warnings
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5451 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-report/src/main/java/au/org/theark/report/web/component/viewReport/studyLevelConsent/ReportFilterPanel.java
+++ ark-report/src/main/java/au/org/theark/report/web/component/viewReport/studyLevelConsent/ReportFilterPanel.java
@@ -18,28 +18,25 @@
  ******************************************************************************/
 package au.org.theark.report.web.component.viewReport.studyLevelConsent;
 
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 
 import au.org.theark.report.model.vo.ConsentDetailsReportVO;
-import au.org.theark.report.model.vo.GenericReportViewVO;
-import au.org.theark.report.web.component.viewReport.consentDetails.filterForm.ConsentDetailsFilterForm;
 import au.org.theark.report.web.component.viewReport.studyLevelConsent.filterForm.StudyLevelConsentDetailsFilterForm;
 
+/**
+ * 
+ * @author elam
+ */
 public class ReportFilterPanel extends Panel {
-
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 1L;
-
-	AjaxButton						generateButton;
-
+	private static final long	serialVersionUID	= 5654177575562262548L;
+	
 	public ReportFilterPanel(String id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 
 	public void initialisePanel(CompoundPropertyModel<ConsentDetailsReportVO> cpModel, FeedbackPanel feedbackPanel, au.org.theark.report.web.component.viewReport.ReportOutputPanel reportOutputPanel) {
