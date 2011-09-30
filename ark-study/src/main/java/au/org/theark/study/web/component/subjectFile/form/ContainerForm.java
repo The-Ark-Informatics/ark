09b566ed0e248d9f371ef3b7b1f39d3e0fa9e424
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-09-30 07:35:15 +0000

    Now uses DownloadAjaxLinkPanel and DeleteAjaxLinkPanel instead of buttons.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5461 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-study/src/main/java/au/org/theark/study/web/component/subjectFile/form/ContainerForm.java
+++ ark-study/src/main/java/au/org/theark/study/web/component/subjectFile/form/ContainerForm.java
@@ -40,6 +40,9 @@ public class ContainerForm extends AbstractContainerForm<SubjectVO> {
 	 */
 	public ContainerForm(String id, CompoundPropertyModel<SubjectVO> cpmModel) {
 		super(id, cpmModel);
+		//TODO: Seems like this multiPart(true) is required to fix a problem after upgrading to Wicket 1.5.0...
+		//Problem is with trying to create a new Attachment for the 2nd time without moving to another function
+		setMultiPart(true);
 	}
 
 }
