5ebc62e1c46eb93010ab7969c80f16e6ba74e422
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-04 06:40:29 +0000

    Modified AttributeModifier for odd/even rows to removed deprecated call
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5514 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/web/component/customfield/SearchResultListPanel.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/customfield/SearchResultListPanel.java
@@ -133,7 +133,7 @@ public class SearchResultListPanel extends Panel {
 				}
 
 				/* For the alternative stripes */
-				item.add(new AttributeModifier("class", true, new AbstractReadOnlyModel() {
+				item.add(new AttributeModifier("class", new AbstractReadOnlyModel() {
 					@Override
 					public String getObject() {
 						return (item.getIndex() % 2 == 1) ? "even" : "odd";
