8d20c4d5189cab40bd8a0b863c01a6e8636a010e
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-05 15:15:54 +0000

    Added new utility class
    git-svn-id: svn://localhost/ARK@5549 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/util/ArkUserPasswordGenerator.java
@@ -0,0 +1,21 @@
+package au.org.theark.core.util;
+
+/**
+ * Generate a new password consisting of a random 8 characters, and 2 random digits
+ * @author arkadmin
+ *
+ */
+public class ArkUserPasswordGenerator {
+	/**
+	 * Generate a new password consisting of a random 8 characters, and 2 random digits
+	 * @return
+	 */
+	public synchronized static String generateNewPassword() {
+		StringBuffer password = new StringBuffer();
+		String random8Chars = new String(org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(8));
+		String random2Digits = new String(org.apache.commons.lang.RandomStringUtils.randomNumeric(2));
+		password.append(random8Chars);
+		password.append(random2Digits);
+		return password.toString();
+	}
+}
