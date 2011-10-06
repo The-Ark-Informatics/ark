cf4193214ca34502b1f8c12944af3f2abb6fac03
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-06 04:28:16 +0000

    Partial progress check-in: Implemented PhenoData entry, but it is hard-coded to load a specific test PhenotypicCollection.  Thus still need to implement the PhenotypicCollection management.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5569 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenodataentry/PhenoDataDataViewPanel.java
@@ -0,0 +1,170 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.phenotypic.web.component.phenodataentry;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.core.model.pheno.entity.PhenoData;
+import au.org.theark.core.model.pheno.entity.PhenotypicCollection;
+import au.org.theark.core.model.study.entity.ArkFunction;
+import au.org.theark.core.model.study.entity.CustomField;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+import au.org.theark.core.model.study.entity.SubjectCustomFieldData;
+import au.org.theark.core.security.ArkPermissionHelper;
+import au.org.theark.core.web.component.ArkDataProvider2;
+import au.org.theark.core.web.component.customfield.dataentry.CustomDataEditorDataView;
+import au.org.theark.phenotypic.service.Constants;
+import au.org.theark.phenotypic.service.IPhenotypicService;
+
+/**
+ * @author elam
+ * 
+ */
+@SuppressWarnings({ "serial" })
+public class PhenoDataDataViewPanel extends Panel {
+
+	private static final long		serialVersionUID	= -1L;
+	private static final Logger	log					= LoggerFactory.getLogger(PhenoDataDataViewPanel.class);
+
+	private CompoundPropertyModel<PhenoCollectionDataVO>			cpModel;
+
+	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
+	private IPhenotypicService			iPhenotypicService;
+	
+	protected ArkDataProvider2<PhenoCollectionDataVO, PhenoData> scdDataProvider;
+	protected DataView<PhenoData> dataView;
+
+	public PhenoDataDataViewPanel(String id, CompoundPropertyModel<PhenoCollectionDataVO> cpModel) {
+		super(id);
+		this.cpModel = cpModel;
+		
+		this.setOutputMarkupPlaceholderTag(true);
+	}
+	
+	public PhenoDataDataViewPanel initialisePanel(Integer numRowsPerPage) {	
+		initialiseDataView();
+		if (numRowsPerPage != null) {
+			dataView.setItemsPerPage(numRowsPerPage);	// au.org.theark.core.Constants.ROWS_PER_PAGE);
+		}
+		
+		this.add(dataView);
+		return this;
+	}
+
+	private void initialiseDataView() {
+		// TODO fix for READ permission check
+		if (ArkPermissionHelper.isActionPermitted(au.org.theark.core.Constants.SEARCH)) {
+		// Data provider to get pageable results from backend
+			scdDataProvider = new ArkDataProvider2<PhenoCollectionDataVO, PhenoData>() {
+				
+				public int size() {
+					PhenotypicCollection phenoCollection = criteriaModel.getObject().getPhenotypicCollection();
+					ArkFunction arkFunction = criteriaModel.getObject().getArkFunction();
+	
+					return iPhenotypicService.getPhenoDataCount(phenoCollection);
+				}
+	
+				public Iterator<PhenoData> iterator(int first, int count) {
+					PhenotypicCollection phenoCollection = criteriaModel.getObject().getPhenotypicCollection();
+					ArkFunction arkFunction = criteriaModel.getObject().getArkFunction();
+	
+					List<PhenoData> phenoDataList = iPhenotypicService.getPhenoDataList(phenoCollection, first, count);
+					cpModel.getObject().setCustomFieldDataList(phenoDataList);
+					return cpModel.getObject().getCustomFieldDataList().iterator();
+				}
+			};
+			// Set the criteria for the data provider
+			scdDataProvider.setCriteriaModel(cpModel);
+		}
+		else {
+			// Since module is not accessible, create a dummy dataProvider that returns nothing
+			scdDataProvider = new ArkDataProvider2<PhenoCollectionDataVO, PhenoData>() {
+				
+				public Iterator<? extends PhenoData> iterator(int first, int count) {
+					return null;
+				}
+
+				public int size() {
+					return 0;
+				}
+			};
+		}
+		
+		dataView = this.buildDataView(scdDataProvider);
+	}
+	
+	public DataView<PhenoData> buildDataView(ArkDataProvider2<PhenoCollectionDataVO, PhenoData> scdDataProvider2) {
+
+		DataView<PhenoData> subjectCFDataDataView = new CustomDataEditorDataView<PhenoData>("customDataList", scdDataProvider2) {
+
+			@Override
+			protected void populateItem(final Item<PhenoData> item) {
+				PhenoData phenoData = item.getModelObject();
+				// Ensure we tie Subject in context to the item if that link isn't there already
+				if (phenoData.getPhenotypicCollection() == null) {
+					phenoData.setPhenotypicCollection(cpModel.getObject().getPhenotypicCollection());
+				}
+				super.populateItem(item);
+			}
+
+			@Override
+			protected WebMarkupContainer getParentContainer() {
+				return this;
+			}
+
+			@Override
+			protected Logger getLog() {
+				return log;
+			}
+		};
+		return subjectCFDataDataView;
+	}
+	
+	public DataView<PhenoData> getDataView() {
+		return dataView;
+	}
+	
+	public void saveCustomData() {
+		if (ArkPermissionHelper.isActionPermitted(au.org.theark.core.Constants.SAVE)) {
+			List<PhenoData> errorList = iPhenotypicService.createOrUpdatePhenoData(cpModel.getObject().getCustomFieldDataList());
+			if (errorList.size() > 0) {
+				for (PhenoData phenoData : errorList) {
+					CustomField cf = phenoData.getCustomFieldDisplay().getCustomField();
+					String fieldType = cf.getFieldType().getName();
+					if (fieldType.equals(au.org.theark.core.web.component.customfield.Constants.DATE_FIELD_TYPE_NAME)) {
+						this.error("Unable to save this data: " + cf.getFieldLabel() + " = " + phenoData.getDateDataValue());
+					}
+					else {
+						this.error("Unable to save this data: " + cf.getFieldLabel() + " = " + phenoData.getTextDataValue());					
+					}
+				}
+			}
+		}
+	}
+}
---
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/phenodataentry/PhenoDataEntryContainerPanel.java
@@ -0,0 +1,128 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.phenotypic.web.component.phenodataentry;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import au.org.theark.core.model.pheno.entity.PhenotypicCollection;
+import au.org.theark.core.security.ArkPermissionHelper;
+import au.org.theark.core.security.PermissionConstants;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.phenotypic.service.Constants;
+import au.org.theark.phenotypic.service.IPhenotypicService;
+/**
+ * @author elam
+ *
+ */
+public class PhenoDataEntryContainerPanel extends Panel {
+	private static final long	serialVersionUID	= 1L;
+	
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	protected IArkCommonService<Void>					iArkCommonService;
+	
+	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
+	private IPhenotypicService			iPhenotypicService;
+	
+	protected CompoundPropertyModel<PhenoCollectionDataVO> cpModel;
+
+	protected FeedbackPanel feedbackPanel;
+	protected WebMarkupContainer customDataEditorWMC;
+
+	/**
+	 * Constructor requires the phenoCollection to be passed in
+	 * @param id
+	 * @param phenoCollection
+	 */
+	public PhenoDataEntryContainerPanel(String id, CompoundPropertyModel<PhenoCollectionDataVO> phenoCollectionModel) {
+		super(id, phenoCollectionModel);
+		this.cpModel = phenoCollectionModel;
+	}
+
+	public PhenoDataEntryContainerPanel initialisePanel() {
+		add(initialiseFeedbackPanel());
+		add(initialiseCustomDataEditorWMC());
+		if (!ArkPermissionHelper.isModuleFunctionAccessPermitted()) {
+			this.error(au.org.theark.core.Constants.MODULE_NOT_ACCESSIBLE_MESSAGE);
+			customDataEditorWMC.setVisible(false);
+		}
+		return this;
+	}
+	
+	protected WebMarkupContainer initialiseCustomDataEditorWMC() {
+		customDataEditorWMC = new WebMarkupContainer("customDataEditorWMC");
+		Panel dataEditorPanel;
+		boolean contextLoaded = prerenderContextCheck();
+		if (contextLoaded && isActionPermitted()) {
+			dataEditorPanel = new PhenoDataEditorPanel("customDataEditorPanel", cpModel, feedbackPanel).initialisePanel();;
+		}
+		else if (!contextLoaded) {
+			dataEditorPanel = new EmptyPanel("customDataEditorPanel");
+			this.error("A study and subject in context are required to proceed.");
+		}
+		else {
+			dataEditorPanel = new EmptyPanel("customDataEditorPanel");
+			this.error("You do not have sufficient permissions to access this function");
+		}
+		customDataEditorWMC.add(dataEditorPanel);
+		return customDataEditorWMC;
+	}
+	
+	protected WebMarkupContainer initialiseFeedbackPanel() {
+		/* Feedback Panel doesn't have to sit within a form */
+		feedbackPanel = new FeedbackPanel("feedbackMessage");
+		feedbackPanel.setOutputMarkupId(true);
+		return feedbackPanel;
+	}
+	
+	protected boolean isActionPermitted() {
+		boolean flag = false;
+		SecurityManager securityManager = ThreadContext.getSecurityManager();
+		Subject currentUser = SecurityUtils.getSubject();
+		if (securityManager.isPermitted(currentUser.getPrincipals(), PermissionConstants.READ)) {
+			flag = true;
+		}
+		else {
+			flag = false;
+		}
+		return flag;
+	}
+	
+	protected boolean prerenderContextCheck() {
+		
+		//TODO: Remove this test code later
+		PhenotypicCollection aPhenoCollection = iPhenotypicService.getPhenotypicCollection(new Long(1));
+		
+		boolean contextLoaded = false;
+		if (aPhenoCollection != null) {
+			cpModel.getObject().setPhenotypicCollection(aPhenoCollection);
+			contextLoaded = true;
+		}
+		return contextLoaded;
+	}
+
+}
