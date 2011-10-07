5ba0a16d0e6e4d0258f49fbb1e3e1b48285600d6
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-07 08:41:48 +0000

    Amended to ensure pattern was correctly built
    git-svn-id: svn://localhost/ARK@5601 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/util/ArkUserPasswordGenerator.java
+++ ark-common/src/main/java/au/org/theark/core/util/ArkUserPasswordGenerator.java
@@ -16,7 +16,20 @@ import java.util.Random;
  */
 public class ArkUserPasswordGenerator {
 	protected static final int		DEFAULT_PASSWORD_LENGTH	= 8;
-	protected static final char[]	SPECIAL_CHAR				= { '@', '#', '$', '%' };
+	protected static final char[]	SPECIAL_CHARS				= { '@', '#', '$', '%' };
+	protected static final char[] CHAR_LIST = {
+      'A','B','C','D','E','F','G','H',
+      'I','J','K','L','M','N','O','P',
+      'Q','R','S','T','U','V','W','X',
+      'Y','Z','a','b','c','d','e','f',
+      'g','h','i','j','k','l','m','n',
+      'o','p','q','r','s','t','u','v',
+      'w','x','y','z','0','1','2','3',
+      '4','5','6','7','8','9',
+	};
+	
+	// 1 digit, 1 lower, 1 upper, 1 symbol "@#$%", from 6 to 20
+	public static final String			PASSWORD_PATTERN	= "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
 
 	/**
 	 * Generate a new password, of the pattern:
@@ -31,22 +44,31 @@ public class ArkUserPasswordGenerator {
 	 * @return A new random password for the ArkUser
 	 */
 	public synchronized static String generate() {
+		String password = generatePassword();
+		return password;
+	}
+
+	protected static String generatePassword() {
 		StringBuffer password = new StringBuffer();
 
-		// Randomly generate the "pieces" of the password
-		String randomSpecialChar = new String(org.apache.commons.lang.RandomStringUtils.random(1, SPECIAL_CHAR));
-		String randomChars = new String(org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(DEFAULT_PASSWORD_LENGTH));
+		// A random lowercase letter
+		String randomLowercaseChar = org.apache.commons.lang.RandomStringUtils.randomAlphabetic(1).toLowerCase();
+		// A random string from the base set of chars
+		String randomString = org.apache.commons.lang.RandomStringUtils.random(DEFAULT_PASSWORD_LENGTH, 0, CHAR_LIST.length, true, true, CHAR_LIST);
+		// A special character
+		String randomSpecialChar = new String(org.apache.commons.lang.RandomStringUtils.random(1, SPECIAL_CHARS));
+		// A random number (enforced)
 		String randomNumeric = new String(org.apache.commons.lang.RandomStringUtils.randomNumeric(1));
 
 		// Randomly replace one of the randomChars with a special character
 		Random rand = new Random();
 		int index = rand.nextInt(DEFAULT_PASSWORD_LENGTH);
-		char randomChar = randomChars.charAt(index);
-		String replacedChars = randomChars.replace(Character.toString(randomChar), randomSpecialChar);
+		char randomChar = randomString.charAt(index);
+		String replacedChars = randomString.replace(Character.toString(randomChar), randomSpecialChar);
 
+		password.append(randomLowercaseChar);
 		password.append(replacedChars);
 		password.append(randomNumeric);
-
 		return password.toString();
 	}
 }
