5104090e242cc3062753360efeffa50279387f47
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-07 04:30:35 +0000

    Detail panel implementation added
    git-svn-id: svn://localhost/ARK@5589 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/CustomFieldGroupDetailPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/CustomFieldGroupDetailPanel.java
@@ -1,9 +1,44 @@
 package au.org.theark.phenotypic.web.component.customfieldgroup;
 
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+import au.org.theark.core.vo.ArkCrudContainerVO;
+import au.org.theark.core.vo.CustomFieldGroupVO;
+import au.org.theark.phenotypic.web.component.customfieldgroup.form.DetailForm;
+
 /**
  * @author nivedann
  *
  */
-public class CustomFieldGroupDetailPanel {
+public class CustomFieldGroupDetailPanel extends Panel {
+	
+	private FeedbackPanel		feedBackPanel;
+	private ArkCrudContainerVO	arkCrudContainerVO;
+	private CompoundPropertyModel<CustomFieldGroupVO> cpmModel;
+	private DetailForm detailForm;
+
+	
+	/**
+	 * 
+	 * @param id
+	 * @param feedBackPanel
+	 * @param arkCrudContainerVO
+	 * @param containerForm
+	 */
+	public CustomFieldGroupDetailPanel(String id, FeedbackPanel feedBackPanel, ArkCrudContainerVO arkCrudContainerVO,CompoundPropertyModel<CustomFieldGroupVO> cpmModel){
+		super(id);
+		this.arkCrudContainerVO = arkCrudContainerVO;
+		this.feedBackPanel = feedBackPanel;
+		this.cpmModel = cpmModel;
+		initialisePanel();
+	}
+	
+	public void initialisePanel() {
+		detailForm = new DetailForm("detailsForm",feedBackPanel,cpmModel,arkCrudContainerVO);
+		detailForm.initialiseDetailForm();
+		add(detailForm);
+	}
 
 }
