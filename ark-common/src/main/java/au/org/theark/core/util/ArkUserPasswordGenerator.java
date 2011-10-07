76ec73f3e6189d3b6587584ea0e40f751acd2636
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-07 03:53:44 +0000

    Amended class to confirm to ARK password pattern
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5578 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/util/ArkUserPasswordGenerator.java
+++ ark-common/src/main/java/au/org/theark/core/util/ArkUserPasswordGenerator.java
@@ -1,21 +1,52 @@
 package au.org.theark.core.util;
 
+import java.util.Random;
+
 /**
- * Generate a new password consisting of a random 8 characters, and 2 random digits
- * @author arkadmin
- *
+ * Generate a new password, of the pattern:
+	 *         <ul>
+	 *         <li>at least 1 digit</li>
+	 *         <li>a lower case letter</li>
+	 *         <li>an upper case letter</li>
+	 *         <li>a special character (@#$%) and</li>
+	 *         <li>length of 10 characters.</li>
+	 *         </ul>
+ * @author cellis
+ * 
  */
 public class ArkUserPasswordGenerator {
+	protected static final int		DEFAULT_PASSWORD_LENGTH	= 8;
+	protected static final char[]	SPECIAL_CHAR				= { '@', '#', '$', '%' };
+
 	/**
-	 * Generate a new password consisting of a random 8 characters, and 2 random digits
+	 * Generate a new password, of the pattern:
+	 *         <ul>
+	 *         <li>at least 1 digit</li>
+	 *         <li>a lower case letter</li>
+	 *         <li>an upper case letter</li>
+	 *         <li>a special character (@#$%) and</li>
+	 *         <li>length of 10 characters.</li>
+	 *         </ul>
+	 * 
 	 * @return A new random password for the ArkUser
 	 */
-	public synchronized static String generateNewPassword() {
+	public synchronized static String generate() {
 		StringBuffer password = new StringBuffer();
-		String random8Chars = new String(org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(8));
-		String random2Digits = new String(org.apache.commons.lang.RandomStringUtils.randomNumeric(2));
-		password.append(random8Chars);
-		password.append(random2Digits);
+
+		// Randomly generate the "pieces" of the password
+		String randomSpecialChar = new String(org.apache.commons.lang.RandomStringUtils.random(1, SPECIAL_CHAR));
+		String randomChars = new String(org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(DEFAULT_PASSWORD_LENGTH));
+		String randomNumeric = new String(org.apache.commons.lang.RandomStringUtils.randomNumeric(1));
+
+		// Randomly replace one of the randomChars with a special character
+		Random rand = new Random();
+		int index = rand.nextInt(DEFAULT_PASSWORD_LENGTH);
+		char randomChar = randomChars.charAt(index);
+		String replacedChars = randomChars.replace(Character.toString(randomChar), randomSpecialChar);
+
+		password.append(replacedChars);
+		password.append(randomNumeric);
+
 		return password.toString();
 	}
 }
