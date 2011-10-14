99a72afba141ad9ca8a5b285b3d848629f8401f8
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-14 05:20:19 +0000

    Added Barcode service and dao layers
    git-svn-id: svn://localhost/ARK@5638 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-lims/src/main/java/au/org/theark/lims/model/dao/BarcodeDao.java
@@ -0,0 +1,139 @@
+package au.org.theark.lims.model.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import au.org.theark.core.dao.HibernateSessionDao;
+import au.org.theark.core.model.lims.entity.BarcodeLabel;
+import au.org.theark.core.model.lims.entity.BarcodeLabelData;
+import au.org.theark.core.model.lims.entity.BarcodePrinter;
+
+@Repository("barcodeDao")
+public class BarcodeDao extends HibernateSessionDao implements IBarcodeDao {
+	private static final Logger	log	= LoggerFactory.getLogger(BarcodeDao.class);
+
+	public void createBarcodeLabel(BarcodeLabel barcodeLabel) {
+		getSession().save(barcodeLabel);
+	}
+	
+	public void createBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		getSession().save(barcodeLabelData);
+	}
+
+	public void createBarcodePrinter(BarcodePrinter barcodePrinter) {
+		getSession().save(barcodePrinter);
+	}
+
+	public void deleteBarcodeLabel(BarcodeLabel barcodeLabel) {
+		getSession().delete(barcodeLabel);
+	}
+
+	public void deleteBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		getSession().delete(barcodeLabelData);
+	}
+
+	public void deleteBarcodePrinter(BarcodePrinter barcodePrinter) {
+		getSession().delete(barcodePrinter);
+	}
+
+	public void updateBarcodeLabel(BarcodeLabel barcodeLabel) {
+		getSession().update(barcodeLabel);
+	}
+
+	public void updateBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		getSession().update(barcodeLabelData);
+	}
+
+	public void updateBarcodePrinter(BarcodePrinter barcodePrinter) {
+		getSession().update(barcodePrinter);
+	}
+
+	@SuppressWarnings("unchecked")
+	public BarcodeLabel searchBarcodeLabel(BarcodeLabel barcodeLabel) {
+		Criteria criteria = getSession().createCriteria(BarcodeLabel.class);
+		if (barcodeLabel.getId() != null) {
+			criteria.add(Restrictions.eq("id", barcodeLabel.getId()));
+		}
+		
+		if (barcodeLabel.getStudy() != null) {
+			criteria.add(Restrictions.eq("study", barcodeLabel.getStudy()));
+		}
+		
+		if (barcodeLabel.getBarcodePrinter() != null) {
+			criteria.add(Restrictions.eq("barcodePrinter", barcodeLabel.getBarcodePrinter()));
+		}
+		
+		List<BarcodeLabel> list = criteria.list();
+		if (list != null && list.size() > 0) {
+			barcodeLabel = list.get(0);
+		}
+		else {
+			log.error("The entity with id" + barcodeLabel.getId().toString() + " cannot be found.");
+		}
+
+		return barcodeLabel;
+	}
+
+	@SuppressWarnings("unchecked")
+	public BarcodeLabelData searchBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		Criteria criteria = getSession().createCriteria(BarcodeLabelData.class);
+		if (barcodeLabelData.getId() != null) {
+			criteria.add(Restrictions.eq("id", barcodeLabelData.getId()));
+		}
+		
+		List<BarcodeLabelData> list = criteria.list();
+		if (list != null && list.size() > 0) {
+			barcodeLabelData = list.get(0);
+		}
+		else {
+			log.error("The entity with id" + barcodeLabelData.getId().toString() + " cannot be found.");
+		}
+
+		return barcodeLabelData;
+	}
+
+	@SuppressWarnings("unchecked")
+	public BarcodePrinter searchBarcodePrinter(BarcodePrinter barcodePrinter) {
+		Criteria criteria = getSession().createCriteria(BarcodePrinter.class);
+		
+		if (barcodePrinter.getId() != null) {
+			criteria.add(Restrictions.eq("id", barcodePrinter.getId()));
+		}
+		
+		if (barcodePrinter.getStudy() != null) {
+			criteria.add(Restrictions.eq("study", barcodePrinter.getStudy()));
+		}
+		
+		if(barcodePrinter.getName() != null) {
+			criteria.add(Restrictions.eq("name", barcodePrinter.getName()));
+		}
+		
+		if (barcodePrinter.getDescription() != null) {
+			criteria.add(Restrictions.ilike("description", barcodePrinter.getDescription(), MatchMode.ANYWHERE));
+		}
+		
+		if(barcodePrinter.getLocation() != null) {
+			criteria.add(Restrictions.eq("location", barcodePrinter.getLocation()));
+		}
+		
+		if(barcodePrinter.getHost() != null) {
+			criteria.add(Restrictions.eq("host", barcodePrinter.getHost()));
+		}
+
+		List<BarcodePrinter> list = criteria.list();
+		if (list != null && list.size() > 0) {
+			barcodePrinter = list.get(0);
+		}
+		else {
+			log.error("The entity with id" + barcodePrinter.getId().toString() + " cannot be found.");
+		}
+
+		return barcodePrinter;
+	}
+}
---
+++ ark-lims/src/main/java/au/org/theark/lims/service/IBarcodeService.java
@@ -0,0 +1,118 @@
+package au.org.theark.lims.service;
+
+import au.org.theark.core.model.lims.entity.BarcodeLabel;
+import au.org.theark.core.model.lims.entity.BarcodeLabelData;
+import au.org.theark.core.model.lims.entity.BarcodePrinter;
+import au.org.theark.core.model.lims.entity.BioCollection;
+import au.org.theark.core.model.lims.entity.Biospecimen;
+
+public interface IBarcodeService {
+	/**
+	 * Gets the barcodePrinter from the database
+	 * @param barcodePrinter
+	 * @return
+	 */
+	public BarcodePrinter searchBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Gets the barcodeLabel from the database
+	 * @param barcodeLabel
+	 * @return
+	 */
+	public BarcodeLabel searchBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Gets the barcodeLabelData from the database
+	 * @param barcodeLabelData
+	 * @return
+	 */
+	public BarcodeLabelData searchBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Create a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void createBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Update a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void updateBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Delete a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void deleteBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Create a barcode Label based on the supplied barcodeLabel
+	 * 
+	 * @param barcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void createBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Update a barcode label based on the supplied BarcodeLabel
+	 * 
+	 * @param BarcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void updateBarcodeLabel(BarcodeLabel BarcodeLabel);
+	
+	/**
+	 * Delete a barcode label based on the supplied BarcodeLabel
+	 * 
+	 * @param barcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void deleteBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Create a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void createBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Update a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void updateBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Delete a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void deleteBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Create the template string to send to the barcode printer for the specified bioCollection and barcodeLabel
+	 * @param bioCollection
+	 * @param barcodeLabel
+	 * @return
+	 */
+	public String createBioCollectionLabelTemplate(BioCollection bioCollection, BarcodeLabel barcodeLabel);
+
+	/**
+	 * Create the template string to send to the barcode printer for the specified biospecimen and barcodeLabel
+	 * @param biospecimen
+	 * @param barcodeLabel
+	 * @return
+	 */
+	public String createBiospecimenLabelTemplate(Biospecimen biospecimen, BarcodeLabel barcodeLabel);
+}
