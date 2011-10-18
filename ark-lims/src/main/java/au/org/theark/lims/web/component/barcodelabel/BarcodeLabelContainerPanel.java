d740db7afa302171c509cd9eadf39accd1bac162
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-18 06:13:14 +0000

    Added new components barcodePrinter and barcodeLabel
    git-svn-id: svn://localhost/ARK@5686 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/barcodelabel/BarcodeLabelContainerPanel.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.lims.web.component.barcodelabel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.core.model.lims.entity.BarcodeLabel;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.security.ArkPermissionHelper;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.ArkCrudContainerVO;
+import au.org.theark.core.web.component.AbstractContainerPanel;
+import au.org.theark.core.web.component.ArkDataProvider;
+import au.org.theark.lims.service.IBarcodeService;
+import au.org.theark.lims.web.component.barcodelabel.form.ContainerForm;
+
+/**
+ * 
+ * @author cellis
+ * 
+ */
+public class BarcodeLabelContainerPanel extends AbstractContainerPanel<BarcodeLabel> {
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 2114933695455527870L;
+
+	private static final Logger										log					= LoggerFactory.getLogger(BarcodeLabelContainerPanel.class);
+
+	protected CompoundPropertyModel<BarcodeLabel>				cpModel;
+
+	protected ArkCrudContainerVO										arkCrudContainerVO;
+
+	protected WebMarkupContainer										arkContextMarkup;
+	protected ContainerForm												containerForm;
+	protected Panel														resultsListPanel;
+
+	private ArkDataProvider<BarcodeLabel, IBarcodeService>	dataProvider;
+	private DataView<BarcodeLabel>									dataView;
+	
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>									iArkCommonService;
+
+	@SpringBean(name = au.org.theark.lims.web.Constants.LIMS_BARCODE_SERVICE)
+	private IBarcodeService												iBarcodeService;
+
+	public BarcodeLabelContainerPanel(String id, WebMarkupContainer arkContextMarkup) {
+		super(id);
+		this.arkContextMarkup = arkContextMarkup;
+		BarcodeLabel barcodeLabel = new BarcodeLabel();
+		Long sessionStudyId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.SESSION_STUDY_KEY);
+		if(sessionStudyId != null) {
+			Study study = iArkCommonService.getStudy(sessionStudyId);
+			barcodeLabel.setStudy(study);
+		}
+		cpModel = new CompoundPropertyModel<BarcodeLabel>(barcodeLabel);
+		arkCrudContainerVO = new ArkCrudContainerVO();
+
+		initialisePanel();
+	}
+
+	public void initialisePanel() {
+		containerForm = new ContainerForm("containerForm", cpModel);
+
+		containerForm.add(initialiseFeedBackPanel());
+		containerForm.add(initialiseSearchPanel());
+		containerForm.add(initialiseSearchResults());
+		containerForm.add(initialiseDetailPanel());
+		this.add(containerForm);
+	}
+
+	protected WebMarkupContainer initialiseSearchPanel() {
+		WebMarkupContainer searchPanelContainer = arkCrudContainerVO.getSearchPanelContainer();
+		SearchPanel searchPanel = new SearchPanel("searchPanel", feedBackPanel, containerForm, arkCrudContainerVO);
+		searchPanel.initialisePanel();
+		searchPanelContainer.add(searchPanel);
+		return searchPanelContainer;
+	}
+
+	protected WebMarkupContainer initialiseSearchResults() {
+		WebMarkupContainer resultListContainer = arkCrudContainerVO.getSearchResultPanelContainer();
+		resultListContainer.setOutputMarkupPlaceholderTag(true);
+
+		SearchResultsPanel searchResultsPanel = new SearchResultsPanel("resultListPanel", containerForm, arkCrudContainerVO);
+
+		initialiseDataView();
+		dataView = searchResultsPanel.buildDataView(dataProvider);
+		dataView.setItemsPerPage(au.org.theark.core.Constants.ROWS_PER_PAGE);
+		PagingNavigator pageNavigator = new PagingNavigator("navigator", dataView);
+		searchResultsPanel.add(pageNavigator);
+		searchResultsPanel.add(dataView);
+		arkCrudContainerVO.getSearchResultPanelContainer().add(searchResultsPanel);
+		return arkCrudContainerVO.getSearchResultPanelContainer();
+	}
+
+	@Override
+	protected WebMarkupContainer initialiseDetailPanel() {
+		DetailPanel detailPanel = new DetailPanel("detailPanel", feedBackPanel, arkCrudContainerVO, containerForm);
+		detailPanel.initialisePanel();
+		arkCrudContainerVO.getDetailPanelContainer().add(detailPanel);
+		return arkCrudContainerVO.getDetailPanelContainer();
+	}
+
+	private void initialiseDataView() {
+		// Data provider to paginate resultList
+		dataProvider = new ArkDataProvider<BarcodeLabel, IBarcodeService>(iBarcodeService) {
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			public int size() {
+				return service.getBarcodeLabelCount(model.getObject());
+			}
+
+			public Iterator<BarcodeLabel> iterator(int first, int count) {
+				List<BarcodeLabel> listCollection = new ArrayList<BarcodeLabel>();
+				if (ArkPermissionHelper.isActionPermitted(au.org.theark.core.Constants.SEARCH)) {
+					listCollection = service.searchPageableBarcodeLabels(model.getObject(), first, count);
+				}
+				return listCollection.iterator();
+			}
+		};
+		// Set the criteria into the data provider's model
+		dataProvider.setModel(new LoadableDetachableModel<BarcodeLabel>() {
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected BarcodeLabel load() {
+				return cpModel.getObject();
+			}
+		});
+	}
+
+	/**
+	 * @return the log
+	 */
+	public static Logger getLog() {
+		return log;
+	}
+}
---
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/barcodeprinter/form/DetailForm.java
@@ -0,0 +1,222 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.lims.web.component.barcodeprinter.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.validator.StringValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.model.lims.entity.BarcodePrinter;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.model.study.entity.ArkUser;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.ArkCrudContainerVO;
+import au.org.theark.core.vo.ArkUserVO;
+import au.org.theark.core.web.form.AbstractDetailForm;
+import au.org.theark.lims.service.IBarcodeService;
+import au.org.theark.lims.web.Constants;
+
+/**
+ * @author cellis
+ * 
+ */
+public class DetailForm extends AbstractDetailForm<BarcodePrinter> {
+	/**
+	 * 
+	 */
+	private static final long				serialVersionUID	= -9040147188276890390L;
+	protected static final Logger				log					= LoggerFactory.getLogger(DetailForm.class);
+
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>			iArkCommonService;
+
+	@SpringBean(name = au.org.theark.lims.web.Constants.LIMS_BARCODE_SERVICE)
+	private IBarcodeService					iBarcodeService;
+
+	private TextField<String>				idTxtFld;
+	private DropDownChoice<Study>			studyDdc;
+	private TextField<String>				nameTxtFld;
+	private TextField<String>				locationTxtFld;
+	private TextField<String>				hostTxtFld;
+	private TextField<String>				portTxtFld;
+	private TextArea<String>				descriptionTxtArea;
+
+	/**
+	 * 
+	 * @param id
+	 * @param feedBackPanel
+	 * @param arkCrudContainerVO
+	 * @param containerForm
+	 */
+	public DetailForm(String id, FeedbackPanel feedBackPanel, ArkCrudContainerVO arkCrudContainerVO, ContainerForm containerForm) {
+		super(id, feedBackPanel, containerForm, arkCrudContainerVO);
+		this.feedBackPanel = feedBackPanel;
+	}
+
+	public void initialiseDetailForm() {
+		idTxtFld = new TextField<String>("id");
+		nameTxtFld = new TextField<String>("name");
+		descriptionTxtArea = new TextArea<String>("description");
+		locationTxtFld = new TextField<String>("location");
+		hostTxtFld = new TextField<String>("host");
+		portTxtFld = new TextField<String>("port");
+		
+		initStudyDdc();
+		
+		addDetailFormComponents();
+		attachValidators();
+	}
+	
+	private void initStudyDdc() {
+		List<Study> studyListForUser = new ArrayList<Study>(0);
+		studyListForUser = getStudyListForUser();
+		ChoiceRenderer<Study> choiceRenderer = new ChoiceRenderer<Study>(Constants.NAME, Constants.ID);
+		studyDdc = new DropDownChoice<Study>("study", studyListForUser, choiceRenderer) {
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onBeforeRender() {
+				super.onBeforeRender();
+				this.setChoices(getStudyListForUser());
+			}
+		};
+	}
+
+	public void addDetailFormComponents() {
+		arkCrudContainerVO.getDetailPanelFormContainer().add(idTxtFld.setEnabled(false));
+		arkCrudContainerVO.getDetailPanelFormContainer().add(nameTxtFld);
+		arkCrudContainerVO.getDetailPanelFormContainer().add(studyDdc);
+		arkCrudContainerVO.getDetailPanelFormContainer().add(descriptionTxtArea);
+		arkCrudContainerVO.getDetailPanelFormContainer().add(locationTxtFld);
+		arkCrudContainerVO.getDetailPanelFormContainer().add(hostTxtFld);
+		arkCrudContainerVO.getDetailPanelFormContainer().add(portTxtFld);
+		add(arkCrudContainerVO.getDetailPanelFormContainer());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#attachValidators()
+	 */
+	@Override
+	protected void attachValidators() {
+		nameTxtFld.setRequired(true).setLabel(new StringResourceModel("error.name.required", this, new Model<String>("Name")));
+		studyDdc.setRequired(true).setLabel(new StringResourceModel("error.study.required", this, new Model<String>("Study")));
+		hostTxtFld.setRequired(true).setLabel(new StringResourceModel("error.host.required", this, new Model<String>("Host")));
+		portTxtFld.setRequired(true).setLabel(new StringResourceModel("error.port.required", this, new Model<String>("Port")));
+		portTxtFld.add(StringValidator.maximumLength(4));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#onCancel(org.apache.wicket.ajax.AjaxRequestTarget)
+	 */
+	@Override
+	protected void onCancel(AjaxRequestTarget target) {
+		containerForm.setModelObject(new BarcodePrinter());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#onDeleteConfirmed(org.apache.wicket.ajax.AjaxRequestTarget, java.lang.String,
+	 * org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow)
+	 */
+	@Override
+	protected void onDeleteConfirmed(AjaxRequestTarget target, String selection) {
+		// studyService.delete(containerForm.getModelObject().getPhone());
+		iBarcodeService.deleteBarcodePrinter(containerForm.getModelObject());
+		containerForm.info("The Barcode Printer record was deleted successfully.");
+		editCancelProcess(target);
+		onCancel(target);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see au.org.theark.core.web.form.AbstractDetailForm#processErrors(org.apache.wicket.ajax.AjaxRequestTarget)
+	 */
+	@Override
+	protected void processErrors(AjaxRequestTarget target) {
+		target.add(feedBackPanel);
+	}
+
+	@Override
+	protected void onSave(Form<BarcodePrinter> containerForm, AjaxRequestTarget target) {
+		if(containerForm.getModelObject().getId() == null) {
+			iBarcodeService.createBarcodePrinter(containerForm.getModelObject());	
+		}
+		else {
+			iBarcodeService.updateBarcodePrinter(containerForm.getModelObject());
+		}
+		this.info("Barcode printer: " + containerForm.getModelObject().getName() + " was created/updated successfully.");
+		target.add(feedBackPanel);
+	}
+
+	@Override
+	protected boolean isNew() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/**
+	 * Returns a list of Studies the user is permitted to access
+	 * 
+	 * @return
+	 */
+	private List<Study> getStudyListForUser() {
+		List<Study> studyListForUser = new ArrayList<Study>(0);
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			ArkUser arkUser = iArkCommonService.getArkUser(currentUser.getPrincipal().toString());
+			ArkUserVO arkUserVo = new ArkUserVO();
+			arkUserVo.setArkUserEntity(arkUser);
+
+			Long sessionArkModuleId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY);
+			ArkModule arkModule = null;
+			arkModule = iArkCommonService.getArkModuleById(sessionArkModuleId);
+			studyListForUser = iArkCommonService.getStudyListForUserAndModule(arkUserVo, arkModule);
+		}
+		catch (EntityNotFoundException e) {
+			log.error(e.getMessage());
+		}
+		return studyListForUser;
+	}
+}
