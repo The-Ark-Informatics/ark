f7dcaf6d4e0579ae9a0ae56c6b2fa319260f2311
elam <elam@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-18 05:03:38 +0000

    Fixes ARK-425:: Image will be generated with the correct biospecimentUid.
    However, side-affect of the fix is that it makes the image consistent in behaviour to the study-logo image within the Manage Study form.  So if the parent form is disabled, then the image will not show.
    
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5682 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-lims/src/main/java/au/org/theark/lims/util/barcode/DataMatrixBarcodeImage.java
+++ ark-lims/src/main/java/au/org/theark/lims/util/barcode/DataMatrixBarcodeImage.java
@@ -18,27 +18,15 @@
  ******************************************************************************/
 package au.org.theark.lims.util.barcode;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.font.FontRenderContext;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import org.apache.wicket.markup.html.image.NonCachingImage;
-import org.apache.wicket.markup.html.image.resource.BufferedDynamicImageResource;
-import org.apache.wicket.request.resource.IResource;
-import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.resource.DynamicImageResource;
 import org.krysalis.barcode4j.impl.datamatrix.DataMatrixBean;
 import org.krysalis.barcode4j.impl.datamatrix.SymbolShapeHint;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
-import org.krysalis.barcode4j.output.bitmap.BitmapEncoder;
-import org.krysalis.barcode4j.output.bitmap.BitmapEncoderRegistry;
 import org.krysalis.barcode4j.tools.UnitConv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,10 +52,10 @@ public class DataMatrixBarcodeImage extends NonCachingImage {
 	 * @param id
 	 * @param barcodeString
 	 */
-	public DataMatrixBarcodeImage(String id, final String barcodeString) {
+	public DataMatrixBarcodeImage(String id, final IModel<String> barcodeStringModel) {
 		super(id);
 		setOutputMarkupPlaceholderTag(true);
-		setImageResourceReference(getDataMatrixBarcodeImageResource(barcodeString));
+		setImageResource(getDataMatrixBarcodeImageResource(barcodeStringModel));
 	}
 
 	/**
@@ -90,19 +78,16 @@ public class DataMatrixBarcodeImage extends NonCachingImage {
 	/**
 	 * @return Gets shared image component
 	 */
-	public static ResourceReference getDataMatrixBarcodeImageResource(final String barcodeString) {
-		return new ResourceReference(DataMatrixBarcodeImage.class, "dataMatrixBarcodeImage") {
-			/**
-			 * 
-			 */
+	public static DynamicImageResource getDataMatrixBarcodeImageResource(final IModel<String> barcodeStringModel) {
+		return new DynamicImageResource() {
+
 			private static final long	serialVersionUID	= 1L;
 
 			@Override
-			public IResource getResource() {
-				final BufferedDynamicImageResource resource = new BufferedDynamicImageResource();
+			protected byte[] getImageData(Attributes attributes) {
+				String barcodeString = barcodeStringModel.getObject();
 				final BufferedImage image = generateBufferedImage(barcodeString);
-				resource.setImage(image);
-				return resource;
+				return toImageData(image);
 			}
 		};
 	}
@@ -144,90 +129,4 @@ public class DataMatrixBarcodeImage extends NonCachingImage {
 		return bufferedImage;
 	}
 
-	/**
-	 * Creates a File of the specified string as a 2D DataMatrix barcode
-	 * 
-	 * @param barcodeString
-	 * @param outputFile
-	 * @throws IOException
-	 */
-	public void generateToFile(String barcodeString, File outputFile) {
-		String[] paramArr = new String[] { "Information 1", "Information 2", "Barcode4J is cool!" };
-		// Create the barcode bean
-		DataMatrixBean bean = new DataMatrixBean();
-
-		// Configure the barcode generator
-		bean.setModuleWidth(UnitConv.in2mm(10.0f / dpi)); // makes a dot/module exactly ten pixels
-		bean.doQuietZone(false);
-		bean.setShape(SymbolShapeHint.FORCE_SQUARE);
-
-		// Set up the canvas provider to create a monochrome bitmap
-		BitmapCanvasProvider canvas = new BitmapCanvasProvider(dpi, BufferedImage.TYPE_BYTE_BINARY, antiAlias, orientation);
-
-		// Generate the barcode
-		bean.generateBarcode(canvas, barcodeString);
-
-		// Signal end of generation
-		try {
-			canvas.finish();
-		}
-		catch (IOException e) {
-			log.error(e.getMessage());
-		}
-
-		// Get generated bitmap
-		BufferedImage symbol = canvas.getBufferedImage();
-
-		int fontSize = 10; // pixels
-		int lineHeight = (int) (fontSize * 1.2);
-		Font font = new Font("Arial", Font.PLAIN, fontSize);
-		int width = symbol.getWidth();
-		int height = symbol.getHeight();
-		FontRenderContext frc = new FontRenderContext(new AffineTransform(), antiAlias, true);
-		for (int i = 0; i < paramArr.length; i++) {
-			String line = paramArr[i];
-			Rectangle2D bounds = font.getStringBounds(line, frc);
-			width = (int) Math.ceil(Math.max(width, bounds.getWidth()));
-			height += lineHeight;
-		}
-
-		// Add padding
-		int padding = 1;
-		width += 2 * padding;
-		height += 3 * padding;
-
-		BufferedImage bitmap = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
-		Graphics2D g2d = (Graphics2D) bitmap.getGraphics();
-		g2d.setBackground(Color.white);
-		g2d.setColor(Color.black);
-		g2d.clearRect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-		g2d.setFont(font);
-
-		// Place the barcode symbol
-		AffineTransform symbolPlacement = new AffineTransform();
-		symbolPlacement.translate(padding, padding);
-		g2d.drawRenderedImage(symbol, symbolPlacement);
-
-		// Add text lines (or anything else you might want to add)
-		int y = padding + symbol.getHeight() + padding;
-		for (int i = 0; i < paramArr.length; i++) {
-			String line = paramArr[i];
-			y += lineHeight;
-			g2d.drawString(line, padding, y);
-		}
-		g2d.dispose();
-
-		// Encode bitmap as file
-		String mime = "image/png";
-		OutputStream out = null;
-		try {
-			out = new FileOutputStream(outputFile);
-			final BitmapEncoder encoder = BitmapEncoderRegistry.getInstance(mime);
-			encoder.encode(bitmap, out, mime, dpi);
-			out.close();
-		}
-		catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
 }
