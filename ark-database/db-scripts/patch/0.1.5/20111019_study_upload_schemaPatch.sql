34223b7d12ed4d75772eb7c5670c37ad9878a173
nivedann <nivedann@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-19 06:01:45 +0000

    added ark_function_id FK,Not Null dropped update_user_id and update_time
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5708 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-database/db-scripts/patch/0.1.5/20111019_study_upload_schemaPatch.sql
@@ -0,0 +1,23 @@
+use study;
+ALTER TABLE `study`.`upload` DROP COLUMN `UPDATE_TIME` , DROP COLUMN `UPDATE_USER_ID` ;
+
+ALTER TABLE `study`.`upload` ADD COLUMN `ARK_FUNCTION_ID` INT NULL  AFTER `UPLOAD_REPORT` , 
+  ADD CONSTRAINT `fk_upload_ark_function_id`
+  FOREIGN KEY (`ARK_FUNCTION_ID` )
+  REFERENCES `study`.`ark_function` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_upload_ark_function_id` (`ARK_FUNCTION_ID` ASC) ;
+
+/*Update the ark_function_ud with value 10 */
+update upload set ark_function_id = 10 where  id >= 1
+
+/* Set the ark_function_id to not null */
+ALTER TABLE `study`.`upload` DROP FOREIGN KEY `fk_upload_ark_function_id` ;
+ALTER TABLE `study`.`upload` CHANGE COLUMN `ARK_FUNCTION_ID` `ARK_FUNCTION_ID` INT(11) NOT NULL  , 
+  ADD CONSTRAINT `fk_upload_ark_function_id`
+  FOREIGN KEY (`ARK_FUNCTION_ID` )
+  REFERENCES `study`.`ark_function` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
