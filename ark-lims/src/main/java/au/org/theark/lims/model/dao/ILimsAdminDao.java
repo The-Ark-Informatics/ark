07bd51a0d92679eeb326568c6a6cfeb291bb9c39
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-24 06:06:19 +0000

    Refactored, added moew methods
    git-svn-id: svn://localhost/ARK@5770 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-lims/src/main/java/au/org/theark/lims/model/dao/ILimsAdminDao.java
@@ -0,0 +1,174 @@
+package au.org.theark.lims.model.dao;
+
+import java.util.List;
+
+import au.org.theark.core.model.lims.entity.BarcodeLabel;
+import au.org.theark.core.model.lims.entity.BarcodeLabelData;
+import au.org.theark.core.model.lims.entity.BarcodePrinter;
+import au.org.theark.core.model.lims.entity.BiospecimenUidTemplate;
+import au.org.theark.core.model.study.entity.Study;
+
+public interface ILimsAdminDao {
+	/**
+	 * Gets the barcodePrinter from the database
+	 * @param barcodePrinter
+	 * @return
+	 */
+	public BarcodePrinter searchBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Gets the barcodeLabel from the database
+	 * @param barcodeLabel
+	 * @return
+	 */
+	public BarcodeLabel searchBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Gets the barcodeLabelData from the database
+	 * @param barcodeLabelData
+	 * @return
+	 */
+	public BarcodeLabelData searchBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Create a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void createBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Update a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void updateBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Delete a barcode printer based on the supplied BarcodePrinter
+	 * 
+	 * @param barcodePrinter
+	 *           the BarcodePrinter object
+	 */
+	public void deleteBarcodePrinter(BarcodePrinter barcodePrinter);
+	
+	/**
+	 * Create a barcode Label based on the supplied barcodeLabel
+	 * 
+	 * @param barcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void createBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Update a barcode label based on the supplied BarcodeLabel
+	 * 
+	 * @param BarcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void updateBarcodeLabel(BarcodeLabel BarcodeLabel);
+	
+	/**
+	 * Delete a barcode label based on the supplied BarcodeLabel
+	 * 
+	 * @param barcodeLabel
+	 *           the BarcodeLabel object
+	 */
+	public void deleteBarcodeLabel(BarcodeLabel barcodeLabel);
+	
+	/**
+	 * Create a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void createBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Update a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void updateBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Delete a barcode label data based on the supplied BarcodeLabelData
+	 * 
+	 * @param barcodeLabelData
+	 *           the BarcodeLabelData object
+	 */
+	public void deleteBarcodeLabelData(BarcodeLabelData barcodeLabelData);
+	
+	/**
+	 * Get the total count of the entities
+	 * @param object
+	 * @return
+	 */
+	public int getBarcodePrinterCount(BarcodePrinter object);
+
+	/**
+	 * Search the entities, restricted by a pageable count
+	 * @param object
+	 * @param first
+	 * @param count
+	 * @return
+	 */
+	public List<BarcodePrinter> searchPageableBarcodePrinters(BarcodePrinter object, int first, int count);
+	
+	/**
+	 * Get the total count of the entities
+	 * @param object
+	 * @return
+	 */
+	public int getBarcodeLabelCount(BarcodeLabel object);
+
+	/**
+	 * Search the entities, restricted by a pageable count
+	 * @param object
+	 * @param first
+	 * @param count
+	 * @return
+	 */
+	public List<BarcodeLabel> searchPageableBarcodeLabels(BarcodeLabel object, int first, int count);
+	
+	/**
+	 * Gets the list of barcodePrinters for the spcified list of studies
+	 * @param studyListForUser
+	 * @return
+	 */
+	public List<BarcodePrinter> getBarcodePrinters(List<Study> studyListForUser);
+	
+	/**
+	 * Get the BiospeciemenUidTemplate for the specifies Study
+	 * @param study
+	 * @return
+	 */
+	public BiospecimenUidTemplate getBiospecimenUidTemplate(Study study);
+	
+	/**
+	 * Create a biospecimenUid template based on the supplied BiospecimenUidTemplate
+	 * 
+	 * @param BiospecimenUidTemplate
+	 *           the BiospecimenUidTemplate object
+	 */
+	public void createBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate);
+	
+	/**
+	 * Update a biospecimenUid template based on the supplied BiospecimenUidTemplate
+	 * 
+	 * @param BiospecimenUidTemplate
+	 *           the BiospecimenUidTemplate object
+	 */
+	public void updateBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate);
+	
+	/**
+	 * Delete a biospecimenUid template based on the supplied BiospecimenUidTemplate
+	 * 
+	 * @param BiospecimenUidTemplate
+	 *           the BiospecimenUidTemplate object
+	 */
+	public void deleteBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate);
+}
---
+++ ark-lims/src/main/java/au/org/theark/lims/service/LimsAdminServiceImpl.java
@@ -0,0 +1,365 @@
+package au.org.theark.lims.service;
+
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.model.lims.entity.BarcodeLabel;
+import au.org.theark.core.model.lims.entity.BarcodeLabelData;
+import au.org.theark.core.model.lims.entity.BarcodePrinter;
+import au.org.theark.core.model.lims.entity.BioCollection;
+import au.org.theark.core.model.lims.entity.Biospecimen;
+import au.org.theark.core.model.lims.entity.BiospecimenUidTemplate;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.lims.model.dao.ILimsAdminDao;
+
+/**
+ * The implementation of IBarcodeService. We want to auto-wire and hence use the @Service annotation.
+ * 
+ * @author cellis
+ * 
+ */
+@Transactional
+@Service(au.org.theark.lims.web.Constants.LIMS_BARCODE_SERVICE)
+public class LimsAdminServiceImpl implements ILimsAdminService {
+	private static final Logger	log			= LoggerFactory.getLogger(LimsAdminServiceImpl.class);
+	@SuppressWarnings("unchecked")
+	private IArkCommonService		iArkCommonService;
+	private ILimsAdminDao				iBarcodeDao;
+	private VelocityEngine			velocityEngine;
+	static private final String	REAL_NUMBER	= "^[-+]?\\d+(\\.\\d+)?$";
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(au.org.theark.core.Constants.DD_MM_YYYY);
+	
+	/**
+	 * @param iArkCommonService
+	 *           the iArkCommonService to set
+	 */
+	@SuppressWarnings("unchecked")
+	@Autowired
+	public void setiArkCommonService(IArkCommonService iArkCommonService) {
+		this.iArkCommonService = iArkCommonService;
+	}
+
+	public ILimsAdminDao getiBarcodeDao() {
+		return iBarcodeDao;
+	}
+
+	@Autowired
+	public void setiBarcodeDao(ILimsAdminDao iBarcodeDao) {
+		this.iBarcodeDao = iBarcodeDao;
+	}
+
+	public VelocityEngine getVelocityEngine() {
+		return velocityEngine;
+	}
+
+	@Autowired
+	public void setVelocityEngine(VelocityEngine velocityEngine) {
+		this.velocityEngine = velocityEngine;
+	}
+
+	public void createBarcodePrinter(BarcodePrinter barcodePrinter) {
+		iBarcodeDao.createBarcodePrinter(barcodePrinter);
+	}
+
+	public void deleteBarcodePrinter(BarcodePrinter barcodePrinter) {
+		iBarcodeDao.createBarcodePrinter(barcodePrinter);
+	}
+
+	public void updateBarcodePrinter(BarcodePrinter barcodePrinter) {
+		iBarcodeDao.updateBarcodePrinter(barcodePrinter);
+	}
+
+	public void createBarcodeLabel(BarcodeLabel barcodeLabel) {
+		iBarcodeDao.createBarcodeLabel(barcodeLabel);
+	}
+
+	public void createBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		iBarcodeDao.createBarcodeLabelData(barcodeLabelData);
+	}
+
+	public void deleteBarcodeLabel(BarcodeLabel barcodeLabel) {
+		iBarcodeDao.deleteBarcodeLabel(barcodeLabel);
+	}
+
+	public void deleteBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		iBarcodeDao.deleteBarcodeLabelData(barcodeLabelData);
+	}
+
+	public void updateBarcodeLabel(BarcodeLabel BarcodeLabel) {
+		iBarcodeDao.updateBarcodeLabel(BarcodeLabel);
+	}
+
+	public void updateBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		iBarcodeDao.updateBarcodeLabelData(barcodeLabelData);
+	}
+
+	public BarcodeLabel searchBarcodeLabel(BarcodeLabel barcodeLabel) {
+		return iBarcodeDao.searchBarcodeLabel(barcodeLabel);
+	}
+
+	public BarcodeLabelData searchBarcodeLabelData(BarcodeLabelData barcodeLabelData) {
+		return iBarcodeDao.searchBarcodeLabelData(barcodeLabelData);
+	}
+
+	public BarcodePrinter searchBarcodePrinter(BarcodePrinter barcodePrinter) {
+		return iBarcodeDao.searchBarcodePrinter(barcodePrinter);
+	}
+
+	public String createBioCollectionLabelTemplate(BioCollection bioCollection, BarcodeLabel barcodeLabel) {
+		/* lets make a Context and put data into it */
+		VelocityContext context = getBioCollectionLabelContext(bioCollection);
+
+		/* lets render a template */
+		StringWriter stringWriter = new StringWriter();
+
+		/* lets make our own string to render */
+		String template = getBarcodeLabelTemplate(barcodeLabel);
+
+		stringWriter = new StringWriter();
+
+		Velocity.evaluate(context, stringWriter, "bioCollectionLabelTemplate", template);
+		log.info("Created bioCollectionLabelTemplate: " + stringWriter);
+		return stringWriter.toString();
+	}
+
+	public String createBiospecimenLabelTemplate(Biospecimen biospecimen, BarcodeLabel barcodeLabel) {
+		/* lets make a Context and put data into it */
+		VelocityContext context = getBiospecimenLabelContext(biospecimen);
+		
+		/* lets render a template */
+		StringWriter stringWriter = new StringWriter();
+
+		/* lets make our own string to render */
+		String template = getBarcodeLabelTemplate(barcodeLabel);
+
+		stringWriter = new StringWriter();
+
+		Velocity.evaluate(context, stringWriter, "biospecimenLabelTemplate", template);
+		log.info("Created biospecimenLabelTemplate: " + stringWriter);
+		return stringWriter.toString();
+	}
+
+	public VelocityContext getBiospecimenLabelContext(Biospecimen biospecimen) {
+		/* lets make a Context and put data into it */
+		VelocityContext velocityContext = new VelocityContext();
+		String lastLineOfCircle = "";
+		String secondLineOfCircle = "";
+		String biospecimenUid = biospecimen.getBiospecimenUid();
+		
+		String dateOfBirth = "";
+		if (biospecimen.getLinkSubjectStudy().getPerson().getDateOfBirth() != null) {
+			dateOfBirth = simpleDateFormat.format(biospecimen.getLinkSubjectStudy().getPerson().getDateOfBirth());
+		}
+
+		// Ok first check and see if last character is a number.
+		String lastChar = biospecimenUid.substring(biospecimenUid.length() - 1);
+		// Need to find out where the study code ends
+		// Trim off the year component
+		String withoutYear = biospecimenUid.substring(2);
+		Pattern pat = Pattern.compile("\\d");
+		Matcher matcher = pat.matcher(withoutYear);
+		boolean gotMatch = matcher.find();
+		int indexOfnum = -1;
+		if (gotMatch) {
+			indexOfnum = matcher.start() + 2;
+		}
+		if (lastChar.matches(REAL_NUMBER)) {
+			// We have a clone, therefore go one backwards.
+			lastLineOfCircle = biospecimenUid.substring(biospecimenUid.length() - 2);
+			secondLineOfCircle = biospecimenUid.substring(indexOfnum, biospecimenUid.length() - 2);
+		}
+		else {
+			lastLineOfCircle = lastChar;
+			secondLineOfCircle = biospecimenUid.substring(indexOfnum, biospecimenUid.length() - 1);
+		}
+		String secondLine = secondLineOfCircle;
+
+		try {
+			secondLine = new Integer(secondLineOfCircle).toString();
+		}
+		catch (NumberFormatException e) {
+			log.error(e.getMessage());
+		}
+
+		velocityContext.put("biospecimenUid", biospecimenUid);
+		velocityContext.put("firstLineOfCircle", biospecimenUid.substring(0, indexOfnum));
+		velocityContext.put("secondLineOfCircle", secondLine);
+		velocityContext.put("lastLineOfCircle", lastLineOfCircle);
+		if (dateOfBirth != null) {
+			velocityContext.put("dateOfBirth", dateOfBirth);
+		}
+		return velocityContext;
+	}
+	
+	public VelocityContext getBioCollectionLabelContext(BioCollection bioCollection) {
+		VelocityContext velocityContext = new VelocityContext();
+		LinkSubjectStudy linkSubjectStudy = null;
+		try {
+			linkSubjectStudy = iArkCommonService.getSubjectByUID(bioCollection.getLinkSubjectStudy().getSubjectUID(), bioCollection.getStudy());
+		}
+		catch (EntityNotFoundException e) {
+			log.error(e.getMessage());
+		}
+		String subjectUid = linkSubjectStudy.getSubjectUID();
+		//TODO: need to get custom field data
+		String familyId = "FAMILYID";
+		//TODO: need to get custom field data
+		String asrbno = "ASRBNO";
+		String collectionDate = new String();
+		if(bioCollection.getCollectionDate() != null) {
+			collectionDate = simpleDateFormat.format(bioCollection.getCollectionDate());
+		}
+		String refDoctor = new String();
+		if(bioCollection.getRefDoctor() != null) {
+			refDoctor = bioCollection.getRefDoctor();
+		}
+		String dateOfBirth = new String();
+		if(bioCollection.getLinkSubjectStudy().getPerson().getDateOfBirth() != null) {
+			dateOfBirth= simpleDateFormat.format(bioCollection.getLinkSubjectStudy().getPerson().getDateOfBirth());
+		}
+		String sex = new String();
+		if(bioCollection.getLinkSubjectStudy().getPerson().getGenderType() != null) {
+			bioCollection.getLinkSubjectStudy().getPerson().getGenderType().getName();
+		}
+		
+		velocityContext.put("subjectUid", subjectUid);
+		velocityContext.put("familyId", familyId);
+		velocityContext.put("asrbno", asrbno);
+		velocityContext.put("collectionDate", collectionDate);
+		velocityContext.put("refDoctor", refDoctor);
+		velocityContext.put("dateOfBirth", dateOfBirth);
+		velocityContext.put("sex", sex);
+		
+		return velocityContext;
+	}
+	
+	/**
+	 * Build the barcode label commands into a template string for merging via Velocity
+	 * @param barcodeLabel
+	 * @return
+	 */
+	public String getBarcodeLabelTemplate(BarcodeLabel barcodeLabel) {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append(barcodeLabel.getLabelPrefix());
+		
+		List<BarcodeLabelData> data = barcodeLabel.getBarcodeLabelData();
+		for (Iterator<BarcodeLabelData> iterator = data.iterator(); iterator.hasNext();) {
+			BarcodeLabelData barcodeLabelData = (BarcodeLabelData) iterator.next();
+			
+			sb.append(barcodeLabelData.getCommand());
+			sb.append(barcodeLabelData.getXCoord());
+			sb.append(",");
+			sb.append(barcodeLabelData.getYCoord());
+			sb.append(",");
+			
+			if(barcodeLabelData.getP1() != null) {
+				sb.append(barcodeLabelData.getP1());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP2() != null) {
+				sb.append(barcodeLabelData.getP2());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP3() != null) {
+				sb.append(barcodeLabelData.getP3());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP4() != null) {
+				sb.append(barcodeLabelData.getP4());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP5() != null) {
+				sb.append(barcodeLabelData.getP5());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP6() != null) {
+				sb.append(barcodeLabelData.getP6());
+				sb.append(",");
+			}
+			
+			if(barcodeLabelData.getP7() != null) {
+				sb.append(barcodeLabelData.getP7());
+				sb.append(",");
+			}
+			
+			// Quote the data
+			sb.append(barcodeLabelData.getQuoteLeft());
+			
+			// Add the data/text
+			sb.append(barcodeLabelData.getData());
+			
+			// End quote the data
+			sb.append(barcodeLabelData.getQuoteRight());
+			
+			// Add a line feed
+			sb.append(barcodeLabelData.getLineFeed());
+		}
+		
+		// add label suffix
+		sb.append(barcodeLabel.getLabelSuffix());
+
+		return sb.toString();	
+	}
+
+	public int getBarcodeLabelCount(BarcodeLabel object) {
+		return iBarcodeDao.getBarcodeLabelCount(object);
+	}
+
+	public int getBarcodePrinterCount(BarcodePrinter object) {
+		return iBarcodeDao.getBarcodePrinterCount(object);
+	}
+
+	public List<BarcodeLabel> searchPageableBarcodeLabels(BarcodeLabel object, int first, int count) {
+		return iBarcodeDao.searchPageableBarcodeLabels(object, first, count);
+	}
+
+	public List<BarcodePrinter> searchPageableBarcodePrinters(BarcodePrinter object, int first, int count) {
+		return iBarcodeDao.searchPageableBarcodePrinters(object, first, count);
+	}
+
+	public List<BarcodePrinter> getBarcodePrinters(List<Study> studyListForUser) {
+		return iBarcodeDao.getBarcodePrinters(studyListForUser);
+	}
+
+	public void createBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public BiospecimenUidTemplate getBiospecimenUidTemplate(Study study) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateBiospecimenUidTemplate(BiospecimenUidTemplate biospecimenUidTemplate) {
+		// TODO Auto-generated method stub
+		
+	}
+}
