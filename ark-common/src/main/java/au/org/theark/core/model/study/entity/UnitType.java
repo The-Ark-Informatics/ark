b9b3c4a9af11f0fad22063eb8090de747f99d7c5
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-25 01:38:30 +0000

    added equals() and hash()
    git-svn-id: svn://localhost/ARK@5784 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/UnitType.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/UnitType.java
@@ -85,5 +85,43 @@ public class UnitType implements Serializable{
 		this.description = description;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((arkFunction == null) ? 0 : arkFunction.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnitType other = (UnitType) obj;
+		if (arkFunction == null) {
+			if (other.arkFunction != null)
+				return false;
+		} else if (!arkFunction.equals(other.arkFunction))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 
 }
