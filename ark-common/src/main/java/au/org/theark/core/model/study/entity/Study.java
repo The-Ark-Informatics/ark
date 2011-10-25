bed4162335e8a7c9ff5e0ef41ebdd14880a1b320
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-25 01:38:50 +0000

    added equals and hashCode
    git-svn-id: svn://localhost/ARK@5785 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/Study.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/Study.java
@@ -403,4 +403,36 @@ public class Study implements java.io.Serializable {
 		this.subjectUidPadChar = subjectUidPadChar;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((filename == null) ? 0 : filename.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Study other = (Study) obj;
+		if (filename == null) {
+			if (other.filename != null)
+				return false;
+		} else if (!filename.equals(other.filename))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
