aa83b70aa0429bb4f5f4269878eaaba1b9de9c6e
nivedann <nivedann@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-10-27 04:30:18 +0000

    added public Field getFieldByNameAndStudy(String fieldName, Study study) throws EntityNotFoundException Refactored this from Phenotypic Module.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5822 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
+++ ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
@@ -31,6 +31,7 @@ import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.exception.ArkUniqueException;
 import au.org.theark.core.exception.EntityCannotBeRemoved;
 import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.model.pheno.entity.Field;
 import au.org.theark.core.model.pheno.entity.FieldData;
 import au.org.theark.core.model.study.entity.AddressStatus;
 import au.org.theark.core.model.study.entity.AddressType;
@@ -559,5 +560,7 @@ public interface IArkCommonService<T> {
 	public String getDelimiterTypeByDelimiterChar(char phenotypicDelimChr);
 	
 	public Collection<FieldData> searchFieldDataBySubjectAndDateCollected(LinkSubjectStudy linkSubjectStudy, java.util.Date dateCollected);
+	
+	public Field getFieldByNameAndStudy(String fieldName, Study study) throws EntityNotFoundException;
 	
 }
