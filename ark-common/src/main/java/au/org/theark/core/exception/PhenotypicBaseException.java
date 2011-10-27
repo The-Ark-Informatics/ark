cb5147d5de3e20fb7727570a06f58bb5f970b5d4
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-10-27 04:13:19 +0000

    These exception classes were earlier under ark-phenotypic module. Created them here so ark-common does not have dependency on sub-modules.
    git-svn-id: svn://localhost/ARK@5815 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/exception/PhenotypicBaseException.java
@@ -0,0 +1,16 @@
+package au.org.theark.core.exception;
+
+/**
+ * @author nivedann
+ *
+ */
+public class PhenotypicBaseException  extends ArkBaseException {
+
+	public PhenotypicBaseException() {
+		super();
+	}
+
+	public PhenotypicBaseException(String message) {
+		super(message);
+	}
+}
