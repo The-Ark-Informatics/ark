cd6606595e1d592c24f44bc6e1a0298b367240f3
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-11-02 10:14:59 +0000

    Modified due to certain ID/FKs needing correction.
    git-svn-id: svn://localhost/ARK@5855 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.5/20111018_study_ark_function_schemaPatch.sql
+++ ark-database/db-scripts/patch/0.1.5/20111018_study_ark_function_schemaPatch.sql
@@ -1,16 +1,22 @@
 -- Add new barcode printer/label functions
+
+-- NB: BARCODE_PRINTER function after being inserted by the following SQL should have ID = 39
 INSERT INTO `study`.`ark_function` (`NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) VALUES ('BARCODE_PRINTER', 'LIMS Admin function to ad/edit barcode printers.', 1, 'tab.module.lims.barcodeprinter');
+
+INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (5, 39, 8);
+
+INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 39, 1);
+INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 39, 2);
+INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 39, 3);
+INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 39, 4);
+
+-- NB: BARCODE_LABEL function after being inserted by the following SQL should have ID = 40
 INSERT INTO `study`.`ark_function` (`NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) VALUES ('BARCODE_LABEL', 'LIMS Admin function to ad/edit barcode labels.', 1, 'tab.module.lims.barcodelabel');
 
-INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (5, 40, 8);
-INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (5, 41, 9);
+INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (5, 40, 9);
 
 INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 40, 1);
 INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 40, 2);
 INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 40, 3);
 INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 40, 4);
 
-INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 41, 1);
-INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 41, 2);
-INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 41, 3);
-INSERT INTO `study`.`ark_role_policy_template` (`ARK_ROLE_ID`, `ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `ARK_PERMISSION_ID`) VALUES (12, 5, 41, 4);
