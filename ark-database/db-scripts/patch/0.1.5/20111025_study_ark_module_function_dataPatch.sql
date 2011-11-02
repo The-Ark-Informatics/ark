c93a60bb7c60629526bda3edf08d47b16a6d6f39
elam <elam@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-11-02 07:48:57 +0000

    Updated a script to also disable the Pheno Summary tab for now (due to rework required on converting it to reference CustomField tables).
    git-svn-id: svn://localhost/ARK@5849 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.5/20111025_study_ark_module_function_dataPatch.sql
+++ ark-database/db-scripts/patch/0.1.5/20111025_study_ark_module_function_dataPatch.sql
@@ -2,14 +2,43 @@ USE study;
 
 -- Remove the Pheno Data Uploader/Importer function from Pheno module
 -- (Decision on 24 Oct 2011: disable the pheno data uploader for 0.1.6 till it can be fully converted)
-DELETE amf FROM ark_module_function amf
-INNER JOIN ark_function af
+DELETE amf 
+FROM study.ark_module_function amf
+INNER JOIN study.ark_function af
 ON amf.ark_function_id = af.id
-WHERE af.name = 'FIELD_DATA_UPLOAD';
+INNER JOIN study.ark_module am
+ON amf.ark_module_id = am.id
+WHERE af.name = 'FIELD_DATA_UPLOAD'
+AND am.name = 'phenotypic';
 
 -- Confirm that the following SQL should return nothing
-SELECT * FROM study.ark_module_function amf,
-study.ark_function af
+SELECT * 
+FROM study.ark_module_function amf,
+study.ark_function af,
+study.ark_module am
 WHERE amf.ark_function_id = af.id
-AND af.name = 'FIELD_DATA_UPLOAD';
+AND amf.ark_module_id = am.id
+AND af.name = 'FIELD_DATA_UPLOAD'
+AND am.name = 'phenotypic';
+
+-- Remove the Pheno Summary Tab function from Pheno module
+-- (Decision on 2 Nov 2011: disable the pheno summary stats in 0.1.6 till it can be fully converted)
+DELETE amf 
+FROM study.ark_module_function amf
+INNER JOIN study.ark_function af
+ON amf.ark_function_id = af.id
+INNER JOIN study.ark_module am
+ON amf.ark_module_id = am.id
+WHERE af.name = 'SUMMARY'
+AND am.name = 'phenotypic';
+
+-- Confirm that the following SQL should return nothing
+SELECT * 
+FROM study.ark_module_function amf,
+study.ark_function af,
+study.ark_module am
+WHERE amf.ark_function_id = af.id
+AND amf.ark_module_id = am.id
+AND af.name = 'SUMMARY'
+AND am.name = 'phenotypic';
 
