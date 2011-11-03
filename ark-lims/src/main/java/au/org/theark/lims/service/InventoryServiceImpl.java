308de119883816e6c290049706230b74b9b725bd
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-11-03 04:58:48 +0000

    Added audit history to CRUD methods
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5862 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-lims/src/main/java/au/org/theark/lims/service/InventoryServiceImpl.java
+++ ark-lims/src/main/java/au/org/theark/lims/service/InventoryServiceImpl.java
@@ -15,6 +15,8 @@ import au.org.theark.core.model.lims.entity.InvColRowType;
 import au.org.theark.core.model.lims.entity.InvFreezer;
 import au.org.theark.core.model.lims.entity.InvRack;
 import au.org.theark.core.model.lims.entity.InvSite;
+import au.org.theark.core.model.study.entity.AuditHistory;
+import au.org.theark.core.service.IArkCommonService;
 import au.org.theark.lims.model.dao.IInventoryDao;
 import au.org.theark.lims.model.vo.BiospecimenLocationVO;
 import au.org.theark.lims.model.vo.LimsVO;
@@ -23,11 +25,13 @@ import au.org.theark.lims.model.vo.LimsVO;
  * @author cellis
  * 
  */
+@SuppressWarnings("unchecked")
 @Transactional
 @Service(au.org.theark.lims.web.Constants.LIMS_INVENTORY_SERVICE)
 public class InventoryServiceImpl implements IInventoryService {
 
-	private IInventoryDao	iInventoryDao;
+	private IArkCommonService	arkCommonService;
+	private IInventoryDao		iInventoryDao;
 
 	/**
 	 * @param iInventoryDao
@@ -36,6 +40,11 @@ public class InventoryServiceImpl implements IInventoryService {
 	@Autowired
 	public void setiInventoryDao(IInventoryDao iInventoryDao) {
 		this.iInventoryDao = iInventoryDao;
+	}
+
+	@Autowired
+	public void setArkCommonService(IArkCommonService arkCommonService) {
+		this.arkCommonService = arkCommonService;
 	}
 
 	public void createInvBox(LimsVO modelObject) {
@@ -48,14 +57,22 @@ public class InventoryServiceImpl implements IInventoryService {
 		iInventoryDao.createInvBox(invBox);
 
 		createCellsForBox(invBox);
-		
+
 		// update available of parent
 		invBox.getInvRack().setAvailable(invBox.getInvRack().getAvailable() - 1);
 		iInventoryDao.updateInvRack(invBox.getInvRack());
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_CREATED);
+		ah.setComment("Created InvBox " + invBox.getName());
+		ah.setEntityId(invBox.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_BOX);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	/**
 	 * Create cells for the box in question
+	 * 
 	 * @param invBox
 	 */
 	private void createCellsForBox(InvBox invBox) {
@@ -74,15 +91,30 @@ public class InventoryServiceImpl implements IInventoryService {
 	}
 
 	public void createInvSite(LimsVO modelObject) {
-		iInventoryDao.createInvSite(modelObject.getInvSite());
+		InvSite invSite = modelObject.getInvSite();
+		iInventoryDao.createInvSite(invSite);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_CREATED);
+		ah.setComment("Created InvSite " + invSite.getName());
+		ah.setEntityId(invSite.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_SITE);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void createInvFreezer(LimsVO modelObject) {
 		InvFreezer invFreezer = modelObject.getInvFreezer();
 		int capacity = invFreezer.getCapacity();
 		invFreezer.setAvailable(capacity);
-		
-		iInventoryDao.createInvFreezer(modelObject.getInvFreezer());
+
+		iInventoryDao.createInvFreezer(invFreezer);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_CREATED);
+		ah.setComment("Created InvFreezer " + invFreezer.getName());
+		ah.setEntityId(invFreezer.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_FREEZER);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void createInvRack(LimsVO modelObject) {
@@ -90,10 +122,17 @@ public class InventoryServiceImpl implements IInventoryService {
 		int capacity = invRack.getCapacity();
 		invRack.setAvailable(capacity);
 		iInventoryDao.createInvRack(invRack);
-		
+
 		// update available of parent
 		invRack.getInvFreezer().setAvailable(invRack.getInvFreezer().getAvailable() - 1);
 		iInventoryDao.updateInvFreezer(invRack.getInvFreezer());
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_CREATED);
+		ah.setComment("Created InvRack " + invRack.getName());
+		ah.setEntityId(invRack.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_RACK);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void createInvCell(InvCell invCell) {
@@ -101,19 +140,59 @@ public class InventoryServiceImpl implements IInventoryService {
 	}
 
 	public void deleteInvBox(LimsVO modelObject) {
-		iInventoryDao.deleteInvBox(modelObject.getInvBox());
+		InvBox invBox = modelObject.getInvBox();
+		// update available of parent
+		invBox.getInvRack().setAvailable(invBox.getInvRack().getAvailable() + 1);
+		iInventoryDao.updateInvRack(invBox.getInvRack());
+
+		iInventoryDao.deleteInvBox(invBox);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_DELETED);
+		ah.setComment("Deleted InvBox " + invBox.getName());
+		ah.setEntityId(invBox.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_BOX);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void deleteInvSite(LimsVO modelObject) {
-		iInventoryDao.deleteInvSite(modelObject.getInvSite());
+		InvSite invSite = modelObject.getInvSite();
+		iInventoryDao.deleteInvSite(invSite);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_DELETED);
+		ah.setComment("Deleted InvSite " + invSite.getName());
+		ah.setEntityId(invSite.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_SITE);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void deleteInvFreezer(LimsVO modelObject) {
-		iInventoryDao.deleteInvFreezer(modelObject.getInvFreezer());
+		InvFreezer invFreezer = modelObject.getInvFreezer();
+		iInventoryDao.deleteInvFreezer(invFreezer);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_DELETED);
+		ah.setComment("Deleted InvFreezer " + invFreezer.getName());
+		ah.setEntityId(invFreezer.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_FREEZER);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void deleteInvRack(LimsVO modelObject) {
-		iInventoryDao.deleteInvRack(modelObject.getInvRack());
+		InvRack invRack = modelObject.getInvRack();
+		// update available of parent
+		invRack.getInvFreezer().setAvailable(invRack.getInvFreezer().getAvailable() + 1);
+		iInventoryDao.updateInvFreezer(invRack.getInvFreezer());
+
+		iInventoryDao.deleteInvRack(invRack);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_DELETED);
+		ah.setComment("Deleted InvRack " + invRack.getName());
+		ah.setEntityId(invRack.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_RACK);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void deleteInvCell(InvCell invCell) {
@@ -129,7 +208,15 @@ public class InventoryServiceImpl implements IInventoryService {
 	}
 
 	public void updateInvBox(LimsVO modelObject) {
-		iInventoryDao.updateInvBox(modelObject.getInvBox());
+		InvBox invBox = modelObject.getInvBox();
+		iInventoryDao.updateInvBox(invBox);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_UPDATED);
+		ah.setComment("Updated InvBox " + invBox.getName());
+		ah.setEntityId(invBox.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_BOX);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void updateInvCell(InvCell invCell) {
@@ -137,15 +224,39 @@ public class InventoryServiceImpl implements IInventoryService {
 	}
 
 	public void updateInvSite(LimsVO modelObject) {
-		iInventoryDao.updateInvSite(modelObject.getInvSite());
+		InvSite invSite = modelObject.getInvSite();
+		iInventoryDao.updateInvSite(invSite);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_UPDATED);
+		ah.setComment("Updated InvSite " + invSite.getName());
+		ah.setEntityId(invSite.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_SITE);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void updateInvFreezer(LimsVO modelObject) {
-		iInventoryDao.updateInvFreezer(modelObject.getInvFreezer());
+		InvFreezer invFreezer = modelObject.getInvFreezer();
+		iInventoryDao.updateInvFreezer(invFreezer);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_UPDATED);
+		ah.setComment("Updated InvFreezer " + invFreezer.getName());
+		ah.setEntityId(invFreezer.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_FREEZER);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public void updateInvRack(LimsVO modelObject) {
-		iInventoryDao.updateInvRack(modelObject.getInvRack());
+		InvRack invRack = modelObject.getInvRack();
+		iInventoryDao.updateInvRack(invRack);
+
+		AuditHistory ah = new AuditHistory();
+		ah.setActionType(au.org.theark.core.Constants.ACTION_TYPE_UPDATED);
+		ah.setComment("Updated InvRack " + invRack.getName());
+		ah.setEntityId(invRack.getId());
+		ah.setEntityType(au.org.theark.core.Constants.ENTITY_TYPE_INV_RACK);
+		arkCommonService.createAuditHistory(ah);
 	}
 
 	public InvCell getInvCell(InvBox invBox, int rowno, int colno) {
@@ -195,8 +306,8 @@ public class InventoryServiceImpl implements IInventoryService {
 	public List<InvRack> searchInvRack(InvRack invRack) throws ArkSystemException {
 		return iInventoryDao.searchInvRack(invRack);
 	}
-	
-	public BiospecimenLocationVO getBiospecimenLocation(Biospecimen biospecimen) throws ArkSystemException{
+
+	public BiospecimenLocationVO getBiospecimenLocation(Biospecimen biospecimen) throws ArkSystemException {
 		BiospecimenLocationVO biospecimenLocationVO = new BiospecimenLocationVO();
 		biospecimenLocationVO = iInventoryDao.getBiospecimenLocation(biospecimen);
 		return biospecimenLocationVO;
@@ -204,10 +315,11 @@ public class InventoryServiceImpl implements IInventoryService {
 
 	/**
 	 * Returns the current path (ie synced to database) to the node in question (box,tray,tank, or site)
+	 * 
 	 * @param node
 	 * @return List of objects (nodes) in the path order (site : freezer : rack : box)
 	 */
-	public List<Object> getInventoryPathOfNode(Object node){
+	public List<Object> getInventoryPathOfNode(Object node) {
 		List<Object> path = new ArrayList<Object>(0);
 		if (node instanceof InvSite) {
 			InvSite invSite = (InvSite) node;
