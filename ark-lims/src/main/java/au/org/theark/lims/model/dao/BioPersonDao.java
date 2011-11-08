1831890b47a671cd03a1d824398f32dc68ae59e7
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-11-08 00:29:55 +0000

    Formatted. Used criteria.uniqueResult() where possible
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@5922 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-lims/src/main/java/au/org/theark/lims/model/dao/BioPersonDao.java
+++ ark-lims/src/main/java/au/org/theark/lims/model/dao/BioPersonDao.java
@@ -18,8 +18,6 @@
  ******************************************************************************/
 package au.org.theark.lims.model.dao;
 
-import java.util.List;
-
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
@@ -29,18 +27,16 @@ import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.exception.EntityNotFoundException;
 import au.org.theark.core.model.study.entity.Person;
 
-@SuppressWarnings("unchecked")
 @Repository("bioPersonDao")
 public class BioPersonDao extends HibernateSessionDao implements IBioPersonDao {
 	public Person getPerson(Long id) throws EntityNotFoundException, ArkSystemException {
 		Criteria criteria = getSession().createCriteria(Person.class);
 		criteria.add(Restrictions.eq("id", id));
-		List<Person> listOfPerson = criteria.list();
-		if (listOfPerson != null && listOfPerson.size() > 0) {
-			return listOfPerson.get(0);
-		}
-		else {
+		
+		Person person = (Person) criteria.uniqueResult();
+		if (person.getId() == null) {
 			throw new EntityNotFoundException("The entity with id" + id.toString() + " cannot be found.");
 		}
+		return person;
 	}
 }
