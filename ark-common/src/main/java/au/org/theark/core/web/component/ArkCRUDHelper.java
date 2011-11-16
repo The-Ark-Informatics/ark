209dc7e5c43fa49d5e1a79bf83954daa3765f2b0
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-11-16 09:41:29 +0000

    Added a security check for preProcessDetailPanelOnSearchResults impl. To check if current user has delete permission and enable/disabled.
    git-svn-id: svn://localhost/ARK@6046 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/web/component/ArkCRUDHelper.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/ArkCRUDHelper.java
@@ -18,10 +18,73 @@
  ******************************************************************************/
 package au.org.theark.core.web.component;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+
+import au.org.theark.core.Constants;
+import au.org.theark.core.security.ArkPermissionHelper;
+import au.org.theark.core.vo.ArkCrudContainerVO;
+
 /**
  * @author nivedann
  * 
  */
 public class ArkCRUDHelper {
+	
+	/**
+	 * While navigating from a result list(Search result list panel) into a Detail Panel this method must be invoked
+	 * by or within the onClick event handler.
+	 * @param target
+	 * @param arkCrudContainerVO
+	 */
+	public static void preProcessDetaiPanelOnSearchResults(AjaxRequestTarget target, ArkCrudContainerVO arkCrudContainerVO){
+		// this check is required to make the delete button enabled if the user has Delete permissions. If the user had navigated from New to Detail Form first, 
+		// The delete button will be disabled. When he cancels out and then clicks on an existing record, even if the user had permissions the delete button will be disabled.
+		//The earlier code had two containers to manage this so it was possible. Now this has to be done through this method.
+		if(ArkPermissionHelper.isActionPermitted(Constants.DELETE)){
+			AjaxButton ajaxButton = (AjaxButton) arkCrudContainerVO.getEditButtonContainer().get("delete");
+			if (ajaxButton != null) {
+				ajaxButton.setEnabled(true);
+				target.add(ajaxButton);
+			}
+		}
+		
+		arkCrudContainerVO.getDetailPanelContainer().setVisible(true);
+		arkCrudContainerVO.getSearchResultPanelContainer().setVisible(false);
+		arkCrudContainerVO.getSearchPanelContainer().setVisible(false);
+		target.add(arkCrudContainerVO.getSearchPanelContainer());
+		target.add(arkCrudContainerVO.getSearchResultPanelContainer());
+		target.add(arkCrudContainerVO.getDetailPanelContainer());
+	
+	}
 
+	
+	/**
+	 * This method should be invoked while the detail form's onBeforeRender is invoked on the AbstractDetailForm class.
+	 * Based on the security it can call this method 
+	 * @param arkCrudContainerVO
+	 */
+	public static void onBeforeRenderWithCRDPermissions(ArkCrudContainerVO arkCrudContainerVO){
+		
+		arkCrudContainerVO.getDetailPanelFormContainer().setEnabled(true);
+		arkCrudContainerVO.getDetailPanelContainer().setVisible(true);
+		arkCrudContainerVO.getEditButtonContainer().setVisible(true);
+		arkCrudContainerVO.getEditButtonContainer().setEnabled(true);
+		arkCrudContainerVO.getSearchResultPanelContainer().setVisible(false);
+		arkCrudContainerVO.getSearchPanelContainer().setVisible(false);
+	}
+	
+	/**
+	 * A method that can be invoked onBeforeRender of the DetailFomr's abstract class when a user has only Read permissions.
+	 * @param arkCrudContainerVO
+	 */
+	public static void onBeforeRenderWithReadPermission(ArkCrudContainerVO arkCrudContainerVO){
+		
+		arkCrudContainerVO.getDetailPanelFormContainer().setEnabled(false);
+		arkCrudContainerVO.getDetailPanelContainer().setVisible(true);
+		arkCrudContainerVO.getEditButtonContainer().setVisible(true);
+		arkCrudContainerVO.getSearchResultPanelContainer().setVisible(false);
+		arkCrudContainerVO.getSearchPanelContainer().setVisible(false);
+		
+	}
 }
