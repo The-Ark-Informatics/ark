70931a1ecef13dc00873ea7022cc60a6d6dd4a17
nivedann <nivedann@2beaffb7-5388-1629-0768-26d37724f1fc>
2011-11-16 03:29:50 +0000

    Removed unwanted commented lines
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6011 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicSubMenuTab.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/menu/PhenotypicSubMenuTab.java
@@ -91,15 +91,13 @@ public class PhenotypicSubMenuTab extends AbstractArkTabPanel {
 		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY)) {
 			// attach the fields to this "Data Dictionary" function
 			panelToReturn = new CustomFieldContainerPanel(panelId, false, arkFunction);
-//			panelToReturn = new FieldContainerPanel(panelId);
 		}
 		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY_UPLOAD)) {
-//			panelToReturn = new FieldUploadContainerPanel(panelId);
 			ArkFunction function = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY);
 			panelToReturn = new FieldUploadContainerPanel(panelId, function);
 		}
 		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_PHENO_COLLECTION)) {
-//			panelToReturn = new PhenoCollectionContainerPanel(panelId, arkContextMarkup);
+
 			ArkFunction function = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY);
 			panelToReturn = new CustomFieldGroupContainerPanel(panelId, function);
 		}
