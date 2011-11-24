2982d8a2d47351d0e1bd5dae480abb92ac877a40
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-11-24 07:24:30 +0000

    Added minimal subjectDetailPanel / form within the Biospeimen detail panel (ARK-505)
    git-svn-id: svn://localhost/ARK@6134 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/panel/subject/SubjectDetailPanel.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.lims.web.component.panel.subject;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+
+/**
+ * @author cellis
+ * 
+ */
+public class SubjectDetailPanel extends Panel {
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 858762052753650329L;
+	private SubjectDetailForm			subjectDetailForm;
+
+	public SubjectDetailPanel(final String id, final IModel<?> model) {
+		super(id, model);
+	}
+
+	public void initialisePanel() {
+		subjectDetailForm = new SubjectDetailForm("subjectDetailForm");
+		subjectDetailForm.initialiseDetailForm();
+		add(subjectDetailForm);
+	}
+
+	public SubjectDetailForm getDetailsForm() {
+		return subjectDetailForm;
+	}
+
+	public void setDetailsForm(SubjectDetailForm detailsForm) {
+		this.subjectDetailForm = detailsForm;
+	}
+}
