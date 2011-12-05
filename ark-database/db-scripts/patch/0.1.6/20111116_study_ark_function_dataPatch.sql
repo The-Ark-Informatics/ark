0839e8daf9cde210afdda4ec25eceeb1147955e6
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-12-05 03:18:38 +0000

    Could not insert a row in the ark_function table and had to rework it.
    git-svn-id: svn://localhost/ARK@6180 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/0.1.6/20111116_study_ark_function_dataPatch.sql
+++ ark-database/db-scripts/patch/0.1.6/20111116_study_ark_function_dataPatch.sql
@@ -1,6 +1,14 @@
 USE study;
 
-INSERT INTO `study`.`ark_function` (`NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) VALUES ('MODULE_FUNCTION', 'Allows CRUD operations on the ark_module_function table for the Ark application', 1, 'tab.module.admin.modulefunction');
+
+INSERT INTO `study`.`ark_function` (`NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) VALUES ('MODULE_FUNCTION', 'Allows CRUD operations on the ark_module_function table for the Ark application', 1,
+ 'tab.module.admin.modulefunction');
+
+UPDATE `study`.`ark_function` SET `ID`=44, `NAME`='MODULE_FUNCTION', `DESCRIPTION`='Allows CRUD operations on the ark_module_function table for the Ark application' WHERE `ID`='45';
+
+INSERT INTO `study`.`ark_function` (`ID`, `NAME`, `DESCRIPTION`) VALUES (45, 'ROLE', 'Allows CRUD operations on user roles.');
+INSERT INTO `study`.`ark_function` (`ID`, `NAME`, `DESCRIPTION`) VALUES (46, 'MODULE_ROLE', 'Allows CRUD operations on ark_module_role table.');
+
 INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) VALUES (7, 44, 3);
 
 UPDATE `study`.`ark_module_function` SET `FUNCTION_SEQUENCE`=1 WHERE `ID`='28';
