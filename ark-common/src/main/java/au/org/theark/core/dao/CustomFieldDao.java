2d7bfdf953259dbb3fb33e14f73416c084fc02fe
nivedann <nivedann@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2011-12-09 06:58:22 +0000

    organised imports.
    git-svn-id: svn://localhost/ARK@6211 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/dao/CustomFieldDao.java
+++ ark-common/src/main/java/au/org/theark/core/dao/CustomFieldDao.java
@@ -1,7 +1,5 @@
 package au.org.theark.core.dao;
 
-import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -14,17 +12,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import au.org.theark.core.Constants;
 import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.exception.EntityNotFoundException;
-import au.org.theark.core.model.pheno.entity.Field;
-import au.org.theark.core.model.pheno.entity.FieldData;
 import au.org.theark.core.model.study.entity.ArkFunction;
 import au.org.theark.core.model.study.entity.CustomField;
 import au.org.theark.core.model.study.entity.CustomFieldDisplay;
 import au.org.theark.core.model.study.entity.CustomFieldGroup;
 import au.org.theark.core.model.study.entity.FieldType;
-import au.org.theark.core.model.study.entity.LinkSubjectStudy;
 import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.model.study.entity.UnitType;
 
