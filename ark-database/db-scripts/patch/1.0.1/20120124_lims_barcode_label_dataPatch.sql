acb6b86023e1d0a6583700007063722753f51d00
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-01-27 05:27:07 +0000

    Amended label data template (removed label width parameter q451)
    git-svn-id: svn://localhost/ARK@6322 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.0.1/20120124_lims_barcode_label_dataPatch.sql
+++ ark-database/db-scripts/patch/1.0.1/20120124_lims_barcode_label_dataPatch.sql
@@ -1,6 +1,5 @@
 USE lims;
 INSERT INTO `barcode_label` (`ID`,`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRIPTION`,`LABEL_PREFIX`,`LABEL_SUFFIX`,`VERSION`) VALUES (1,NULL,1,'zebra biospecimen','Generic Zebra Biospecimen Label','D14
-q457
 N','P1',1);
 INSERT INTO `barcode_label` (`ID`,`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRIPTION`,`LABEL_PREFIX`,`LABEL_SUFFIX`,`VERSION`) VALUES (2,NULL,1,'zebra bioCollection','Generic Zebra BioCollection label','D15
 N','P1',1);
@@ -8,8 +7,6 @@ INSERT INTO `barcode_label` (`ID`,`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRI
 REFERENCE 0,0
 CLS','PRINT 1,1',1);
 INSERT INTO `barcode_label` (`ID`,`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRIPTION`,`LABEL_PREFIX`,`LABEL_SUFFIX`,`VERSION`) VALUES (4,NULL,1,'zebra biospecimen v2','Generic Zebra Biospecimen Label v2','D14
-q457
 N','P1',2);
 INSERT INTO `barcode_label` (`ID`,`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRIPTION`,`LABEL_PREFIX`,`LABEL_SUFFIX`,`VERSION`) VALUES (5,NULL,1,'straw barcode','Generic Zebra Straw Label','D14
-q457
 N','P1',3);
