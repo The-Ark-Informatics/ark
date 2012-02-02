5e5b8e2be60abe54a005dd2f16cc0ff780bc5daa
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-02-02 03:57:46 +0000

    Show tick/crossses for yes/no results
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6353 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/SearchResultListPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/SearchResultListPanel.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.model.study.entity.ArkFunction;
@@ -31,26 +34,25 @@ import au.org.theark.phenotypic.service.IPhenotypicService;
 
 /**
  * @author nivedann
- *
+ * 
  */
-public class SearchResultListPanel extends Panel{
+public class SearchResultListPanel extends Panel {
 
-	
-	private CompoundPropertyModel<CustomFieldGroupVO> cpmModel;
-	private FeedbackPanel	feedbackPanel;
-	private ArkCrudContainerVO	arkCrudContainerVO;
-	private DataView<CustomFieldDisplay> cfdDataView;
-	private ArkDataProvider2<CustomFieldDisplay, CustomFieldDisplay> cfdArkDataProvider;
+	private CompoundPropertyModel<CustomFieldGroupVO>						cpmModel;
+	private FeedbackPanel															feedbackPanel;
+	private ArkCrudContainerVO														arkCrudContainerVO;
+	private DataView<CustomFieldDisplay>										cfdDataView;
+	private ArkDataProvider2<CustomFieldDisplay, CustomFieldDisplay>	cfdArkDataProvider;
 
 	/**
 	 * Service references
 	 */
 	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService		iArkCommonService;
-	
+	private IArkCommonService														iArkCommonService;
+
 	@SpringBean(name = Constants.PHENOTYPIC_SERVICE)
-	private IPhenotypicService	iPhenotypicService;
-	
+	private IPhenotypicService														iPhenotypicService;
+
 	/**
 	 * 
 	 * @param id
@@ -58,106 +60,126 @@ public class SearchResultListPanel extends Panel{
 	 * @param arkCrudContainerVO
 	 * @param feedBackPanel
 	 */
-	public SearchResultListPanel(String id, CompoundPropertyModel<CustomFieldGroupVO> cpModel, ArkCrudContainerVO arkCrudContainerVO, FeedbackPanel feedBackPanel){
-		
+	public SearchResultListPanel(String id, CompoundPropertyModel<CustomFieldGroupVO> cpModel, ArkCrudContainerVO arkCrudContainerVO, FeedbackPanel feedBackPanel) {
+
 		super(id);
-		this.cpmModel  = cpModel;
+		this.cpmModel = cpModel;
 		this.arkCrudContainerVO = arkCrudContainerVO;
 		this.feedbackPanel = feedBackPanel;
-		
+
 	}
-	
-	public DataView<CustomFieldGroup> buildDataView(ArkDataProvider2<CustomFieldGroup, CustomFieldGroup> provider){
-		
-		DataView<CustomFieldGroup> dataView = new DataView<CustomFieldGroup>("customFieldGroupList", provider){
+
+	public DataView<CustomFieldGroup> buildDataView(ArkDataProvider2<CustomFieldGroup, CustomFieldGroup> provider) {
+
+		DataView<CustomFieldGroup> dataView = new DataView<CustomFieldGroup>("customFieldGroupList", provider) {
+
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
 
 			@Override
-			protected void populateItem(Item<CustomFieldGroup> item) {
-				
+			protected void populateItem(final Item<CustomFieldGroup> item) {
+
 				CustomFieldGroup customFieldGroup = item.getModelObject();
-				
-				//ID has to be NOT null if it is coming from the backend
+
+				// ID has to be NOT null if it is coming from the backend
 				item.add(new Label("id", customFieldGroup.getId().toString()));
-				
+
 				item.add(buildLink(item));
-				//TODO Escape any special characters
-				if(customFieldGroup.getDescription() != null){
-					item.add( new Label("description", customFieldGroup.getDescription().trim()));
-				}else{
+				// TODO Escape any special characters
+				if (customFieldGroup.getDescription() != null) {
+					item.add(new Label("description", customFieldGroup.getDescription().trim()));
+				}
+				else {
 					item.add(new Label("description", ""));
 				}
-				
-				if(customFieldGroup.getPublished()){
-					item.add( new Label("published", "Yes"));
-				}else{
-					item.add( new Label("published", "No"));
+
+				if (customFieldGroup.getPublished()) {
+					item.addOrReplace(new ContextImage("published", new Model<String>("images/icons/tick.png")));
 				}
+				else {
+					item.addOrReplace(new ContextImage("published", new Model<String>("images/icons/cross.png")));
+				}
+
+				// For the alternative stripes
+				item.add(new AttributeModifier("class", new AbstractReadOnlyModel<String>() {
+					/**
+					 * 
+					 */
+					private static final long	serialVersionUID	= 1L;
+
+					@Override
+					public String getObject() {
+						return (item.getIndex() % 2 == 1) ? "even" : "odd";
+					}
+				}));
 			}
 		};
-		
+
 		return dataView;
 	}
-	
-	public WebMarkupContainer buildLink(final Item<CustomFieldGroup> item){
-		
+
+	public WebMarkupContainer buildLink(final Item<CustomFieldGroup> item) {
+
 		WebMarkupContainer linkWmc = new WebMarkupContainer("customfieldGroupLinkWMC", item.getModel());
-		
+
 		ArkBusyAjaxLink link = new ArkBusyAjaxLink("name") {
 
 			@Override
 			public void onClick(AjaxRequestTarget target) {
-				
-				final  CustomFieldGroup itemSelected = item.getModelObject();
-				
-				CustomFieldGroup cfg =  (CustomFieldGroup) (getParent().getDefaultModelObject());
-				CompoundPropertyModel<CustomFieldGroupVO> newModel = new CompoundPropertyModel<CustomFieldGroupVO>( new CustomFieldGroupVO());
-				
+
+				final CustomFieldGroup itemSelected = item.getModelObject();
+
+				CustomFieldGroup cfg = (CustomFieldGroup) (getParent().getDefaultModelObject());
+				CompoundPropertyModel<CustomFieldGroupVO> newModel = new CompoundPropertyModel<CustomFieldGroupVO>(new CustomFieldGroupVO());
+
 				CustomField customFieldCriteria = new CustomField();
-				ArkFunction arkFunction  =iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY);
+				ArkFunction arkFunction = iArkCommonService.getArkFunctionByName(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_DATA_DICTIONARY);
 				customFieldCriteria.setStudy(cfg.getStudy());
 				customFieldCriteria.setArkFunction(arkFunction);
-				
+
 				Collection<CustomField> availableList = iArkCommonService.getCustomFieldList(customFieldCriteria);
-				
-				ArrayList<CustomField> selectedList  = (ArrayList)iPhenotypicService.getCustomFieldsLinkedToCustomFieldGroup(itemSelected);
-				
+
+				ArrayList<CustomField> selectedList = (ArrayList) iPhenotypicService.getCustomFieldsLinkedToCustomFieldGroup(itemSelected);
+
 				newModel.getObject().setAvailableCustomFields(availableList);
 				newModel.getObject().setSelectedCustomFields(selectedList);
 				newModel.getObject().setCustomFieldGroup(cfg);
-				
+
 				// Data providor to paginate a list of CustomFieldDisplays linked to the CustomFieldGroup
 				cfdArkDataProvider = new ArkDataProvider2<CustomFieldDisplay, CustomFieldDisplay>() {
 
 					public int size() {
 						return iPhenotypicService.getCFDLinkedToQuestionnaireCount(itemSelected);
 					}
+
 					public Iterator<CustomFieldDisplay> iterator(int first, int count) {
-						
+
 						Collection<CustomFieldDisplay> customFieldDisplayList = new ArrayList<CustomFieldDisplay>();
 						customFieldDisplayList = iPhenotypicService.getCFDLinkedToQuestionnaire(itemSelected, first, count);
 						return customFieldDisplayList.iterator();
 					}
 				};
-				
-				
-				CustomFieldGroupDetailPanel detailPanel = new CustomFieldGroupDetailPanel("detailsPanel", feedbackPanel, arkCrudContainerVO, newModel,cfdArkDataProvider,true);
+
+				CustomFieldGroupDetailPanel detailPanel = new CustomFieldGroupDetailPanel("detailsPanel", feedbackPanel, arkCrudContainerVO, newModel, cfdArkDataProvider, true);
 				arkCrudContainerVO.getDetailPanelContainer().addOrReplace(detailPanel);
-				
-				TextField<String> questionnaireName = (TextField<String>)arkCrudContainerVO.getDetailPanelFormContainer().get("customFieldGroup.name");
+
+				TextField<String> questionnaireName = (TextField<String>) arkCrudContainerVO.getDetailPanelFormContainer().get("customFieldGroup.name");
 				questionnaireName.setEnabled(false);
 
-				//The list of CFD must be displayed on the Detail form
-				//Create a CFD List Panel here and add it to the detailForm.
+				// The list of CFD must be displayed on the Detail form
+				// Create a CFD List Panel here and add it to the detailForm.
 				ArkCRUDHelper.preProcessDetailPanelOnSearchResults(target, arkCrudContainerVO);
 			}
-			
+
 		};
-		
+
 		CustomFieldGroup customFieldGroup = item.getModelObject();
 		Label nameLinkLabel = new Label("nameLbl", customFieldGroup.getName());
 		link.add(nameLinkLabel);
 		linkWmc.add(link);
 		return linkWmc;
 	}
-	
+
 }
