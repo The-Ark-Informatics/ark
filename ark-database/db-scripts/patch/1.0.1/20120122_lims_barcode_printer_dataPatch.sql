1b72a3a917a65388bb21fde3f04540e837e25d65
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-02-02 04:17:04 +0000

    Amended to insert only for studes assigned to LIMS
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6359 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.0.1/20120122_lims_barcode_printer_dataPatch.sql
+++ ark-database/db-scripts/patch/1.0.1/20120122_lims_barcode_printer_dataPatch.sql
@@ -1,9 +1,12 @@
 USE lims;
 
+DELETE FROM barcode_printer WHERE ID > 0;
 INSERT INTO barcode_printer (`STUDY_ID`,`NAME`,`DESCRIPTION`,`LOCATION`,`HOST`,`PORT`)
 SELECT ID, 'zebra','zebra TLP 2844','Office','130.95.56.99',9100
-FROM study.study;
+FROM study.study
+WHERE ID IN (SELECT DISTINCT study_id FROM `study`.`ark_user_role` WHERE ark_module_id=5 AND study_id IS NOT NULL);
 
 INSERT INTO barcode_printer (`STUDY_ID`,`NAME`,`DESCRIPTION`,`LOCATION`,`HOST`,`PORT`)
 SELECT ID, 'brady_bbp_11','Brady BBP 11','Office','130.95.56.99',9100
-FROM study.study;
+FROM study.study
+WHERE ID IN (SELECT DISTINCT study_id FROM `study`.`ark_user_role` WHERE ark_module_id=5 AND study_id IS NOT NULL);
