1324a3097d186630c5d128f5688537050d694b79
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-02-14 02:52:06 +0000

    Added ModuleConstant class for gloabal module name referencing
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6391 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/security/ModuleConstants.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.core.security;
+
+/**
+ * These Module Constants are for the new module management. The names here should match the string one ArkModule table
+ * 
+ * @author cellis
+ * 
+ */
+public class ModuleConstants {
+	public static final String	ARK_MODULE_STUDY			= "Study";
+	public static final String	ARK_MODULE_SUBJECT		= "Subject";
+	public static final String	ARK_MODULE_PHENOTYPIC	= "Phenotypic";
+	public static final String	ARK_MODULE_GENOTYPIC		= "Genotypic";
+	public static final String	ARK_MODULE_LIMS			= "LIMS";
+	public static final String	ARK_MODULE_REPORTING		= "Reporting";
+	public static final String	ARK_MODULE_ADMIN			= "Admin";
+}
