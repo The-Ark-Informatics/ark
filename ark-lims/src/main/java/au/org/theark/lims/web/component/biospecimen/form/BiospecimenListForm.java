a7bbb5660f8af34d05a5bcd02462918f40b4fb0d
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-02-22 09:11:02 +0000

    Organised imports
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6422 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-lims/src/main/java/au/org/theark/lims/web/component/biospecimen/form/BiospecimenListForm.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/biospecimen/form/BiospecimenListForm.java
@@ -38,7 +38,6 @@ import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
