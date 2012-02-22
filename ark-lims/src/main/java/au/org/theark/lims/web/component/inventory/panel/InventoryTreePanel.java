b140302ee45358eb7da4424568890945dce14dff
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-02-22 09:05:59 +0000

    Testing wicket-tree
    git-svn-id: svn://localhost/ARK@6413 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/InventoryTreePanel.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/InventoryTreePanel.java
@@ -1,21 +1,36 @@
 package au.org.theark.lims.web.component.inventory.panel;
 
+import java.util.Set;
+
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.tree.AbstractTree;
 import org.apache.wicket.markup.html.tree.BaseTree;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IDetachable;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wickettree.NestedTree;
+import wickettree.util.ProviderSubset;
+import au.org.theark.core.model.lims.entity.Biospecimen;
 import au.org.theark.lims.model.vo.LimsVO;
+import au.org.theark.lims.service.ILimsService;
+import au.org.theark.lims.web.Constants;
 import au.org.theark.lims.web.component.inventory.form.ContainerForm;
 import au.org.theark.lims.web.component.inventory.panel.box.BoxDetailPanel;
 import au.org.theark.lims.web.component.inventory.panel.freezer.FreezerDetailPanel;
 import au.org.theark.lims.web.component.inventory.panel.rack.RackDetailPanel;
 import au.org.theark.lims.web.component.inventory.panel.site.SiteDetailPanel;
+import au.org.theark.lims.web.component.inventory.tree.BiospecimenProvidor;
 import au.org.theark.lims.web.component.inventory.tree.InventoryLinkTree;
 
 public class InventoryTreePanel extends AbstractInventoryTreePanel {
@@ -30,6 +45,11 @@ public class InventoryTreePanel extends AbstractInventoryTreePanel {
 	private FeedbackPanel			feedbackPanel;
 	private WebMarkupContainer		detailContainer;
 	private ContainerForm			containerForm;
+	
+	@SpringBean(name = Constants.LIMS_SERVICE)
+	private ILimsService						iLimsService;
+	private BiospecimenProvidor provider = new BiospecimenProvidor(iLimsService); 
+	private Set<Biospecimen> state = new ProviderSubset<Biospecimen>(provider);
 
 	public InventoryTreePanel(String id, FeedbackPanel feedbackPanel, WebMarkupContainer detailContainer, ContainerForm containerForm) {
 		super(id);
@@ -40,7 +60,51 @@ public class InventoryTreePanel extends AbstractInventoryTreePanel {
 		tree = new InventoryLinkTree("tree", createTreeModel(), feedbackPanel, detailContainer, containerForm);
 		// tree.getTreeState().collapseAll();
 		tree.setRootLess(true);
+		
+		
+		NestedTree<Biospecimen> biospecimenTree = new NestedTree<Biospecimen>("bioTree", provider){
+
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected Component newContentComponent(String id, IModel<Biospecimen> model) {
+				
+				return new Label(id, model.getObject().getBiospecimenUid());
+			}
+		};
+		
+		addOrReplace(biospecimenTree);
+		
 		addComponents();
+	}
+	
+	private IModel<Set<Biospecimen>> newStateModel()
+	{
+		return new AbstractReadOnlyModel<Set<Biospecimen>>()
+		{
+			/**
+			 * 
+			 */
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			public Set<Biospecimen> getObject()
+			{
+				return state;
+			}
+
+			/**
+			 * Super class doesn't detach - would be nice though.
+			 */
+			@Override
+			public void detach()
+			{
+				((IDetachable)state).detach();
+			}
+		};
 	}
 
 	@Override
