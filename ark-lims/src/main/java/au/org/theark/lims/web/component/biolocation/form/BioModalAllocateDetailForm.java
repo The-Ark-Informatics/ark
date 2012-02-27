97adb9eeebd838010b7dac038a5f85ffa1da6bd3
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-02-27 07:02:34 +0000

    Amended/refactored AllocatonLinkTree (simplfied/extends InventoryLinkTree)
    git-svn-id: svn://localhost/ARK@6445 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/biolocation/form/BioModalAllocateDetailForm.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/biolocation/form/BioModalAllocateDetailForm.java
@@ -21,10 +21,6 @@ package au.org.theark.lims.web.component.biolocation.form;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeModel;
-
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
@@ -35,14 +31,10 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.org.theark.core.exception.ArkSystemException;
 import au.org.theark.core.model.lims.entity.InvBox;
 import au.org.theark.core.model.lims.entity.InvSite;
-import au.org.theark.core.model.lims.entity.InvTreeNode;
 import au.org.theark.core.vo.ArkCrudContainerVO;
 import au.org.theark.core.web.component.AbstractDetailModalWindow;
-import au.org.theark.lims.model.InventoryModel;
-import au.org.theark.lims.model.TreeNodeModel;
 import au.org.theark.lims.model.vo.LimsVO;
 import au.org.theark.lims.service.IInventoryService;
 import au.org.theark.lims.web.component.inventory.panel.box.display.GridBoxPanel;
@@ -84,7 +76,7 @@ public class BioModalAllocateDetailForm extends Form<LimsVO> {
 		this.cpModel = cpModel;
 		this.modalWindow = modalWindow;
 
-		tree = new AllocationLinkTree<LimsVO>("tree", createTreeModel(), feedBackPanel, cpModel) {
+		tree = new AllocationLinkTree("tree") {
 			/**
 			 * 
 			 */
@@ -117,44 +109,5 @@ public class BioModalAllocateDetailForm extends Form<LimsVO> {
 		gridBoxPanel.setVisible(true);
 		addOrReplace(gridBoxPanel);
 		target.add(gridBoxPanel);
-	}
-
-	/**
-	 * Creates the model that feeds the tree.
-	 * 
-	 * @return New instance of tree model.
-	 */
-	protected TreeModel createTreeModel() {
-		InvSite invSite = new InvSite();
-
-		try {
-			invSites = iInventoryService.searchInvSite(invSite);
-		}
-		catch (ArkSystemException e) {
-			log.error(e.getMessage());
-		}
-		return convertToTreeModel();
-	}
-
-	private TreeModel convertToTreeModel() {
-		TreeModel model = null;
-		// Default root node (set to not show)
-		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(new TreeNodeModel("ROOT"));
-		add(rootNode, invSites);
-		model = new DefaultTreeModel(rootNode);
-		return model;
-	}
-
-	@SuppressWarnings("unchecked")
-	private void add(DefaultMutableTreeNode parentNode, List<? extends InvTreeNode> childNodes) {
-		for (InvTreeNode node : childNodes) {
-			DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new InventoryModel(node, node.getNodeType()));
-			parentNode.add(childNode);
-
-			// If no children, don't bother
-			if (node.getChildren() != null && !node.getChildren().isEmpty()) {
-				add(childNode, node.getChildren());
-			}
-		}
 	}
 }
