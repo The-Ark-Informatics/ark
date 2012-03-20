00f5778d7f585a218af73b8df64168682ec171b5
travis <travis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-03-20 04:41:42 +0000

    made allowances for case sensitive DBMS
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6478 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/Experimental/20120320_update_script_for_unit_types_and_measurement_types.sql
+++ ark-database/db-scripts/patch/Experimental/20120320_update_script_for_unit_types_and_measurement_types.sql
@@ -1,14 +1,17 @@
-#SELECT * FROM `study`.`unit_type`;
-
+/*#SELECT * FROM `study`.`unit_type`;
 #select * from study.ark_function_type; can be rport or non-report
-/*
+#select * from study.measurement_type;
+
+
+#--dev only run drop if exists
+#--drop table study.measurement_type;
+*/
 CREATE TABLE study.measurement_type (
 	ID INT primary key,
 	VALUE VARCHAR(64),
 	DESCRIPTION VARCHAR(512)
 );
 
-select * from study.measurement_type;
 
 ALTER TABLE study.unit_type
 	ADD COLUMN MEASUREMENT_TYPE_ID int;
@@ -20,22 +23,22 @@ ALTER TABLE study.unit_type
 	ADD CONSTRAINT
 	FOREIGN KEY fk_unit_type_measurement(MEASUREMENT_TYPE_ID) 
 	REFERENCES study.measurement_type(ID) ; 
-*/
 
-insert into study.measurement_type(id, value)
-values (0, 'Other');
-insert into study.measurement_type(id, value)
-values (1, 'Distance');
-insert into study.measurement_type(id, value)
-values (2, 'Volume');
-insert into study.measurement_type(id, value)
-values (3, 'Time');
-insert into study.measurement_type(id, value)
-values (4, 'Weight');
-insert into study.measurement_type(id, value)
-values (5, 'Weight per Volume');
-insert into study.measurement_type(id, value)
-values (6, 'Distance per Time');
-insert into study.measurement_type(id, value)
-values (7, 'Percentage or Fraction');
+
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (0, 'Other');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (1, 'Distance');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (2, 'Volume');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (3, 'Time');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (4, 'Weight');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (5, 'Weight per Volume');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (6, 'Distance per Time');
+INSERT INTO study.measurement_type(ID, VALUE)
+VALUES (7, 'Percentage or Fraction');
 
