3665f3981c04c237ec60eb1d318945aad737b17d
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-03-24 08:04:50 +0000

    Added new Audit DAO class/interface for access to audit schema
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6492 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/dao/IAuditDao.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.core.dao;
+
+import java.util.List;
+
+import au.org.theark.core.model.audit.entity.ConsentHistory;
+import au.org.theark.core.model.audit.entity.LssConsentHistory;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+
+
+/**
+ * Interface that provides CRUD and accessor methods to Audit entities
+ * @author cellis
+ */
+public interface IAuditDao {
+	/**
+	 * Return a list of LinkSubjectStudy ConsentHistory entities
+	 * @param linkSubjectStudy
+	 * @return
+	 */
+	public List<LssConsentHistory> getLssConsentHistoryList(LinkSubjectStudy linkSubjectStudy);
+	
+	/**
+	 * Return a list of ConsentHistory entities
+	 * @param linkSubjectStudy
+	 * @return
+	 */
+	public List<ConsentHistory> getConsentHistoryList(LinkSubjectStudy linkSubjectStudy);
+
+	/**
+	 * Creates a new audit history log
+	 * @param lssConsentHistory
+	 */
+	public void createLssConsentHistory(LssConsentHistory lssConsentHistory);
+	
+	/**
+	 * Creates a new audit history log
+	 * @param consentHistory
+	 */
+	public void createConsentHistory(ConsentHistory consentHistory);
+}
