ab96b39c9251d8dc54f32f31a7d2aca80982deea
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-03-24 08:08:29 +0000

    Added new ConsnentOption entity (ARK-561)
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6494 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/ConsentOption.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.core.model.study.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "CONSENT_OPTION", schema = Constants.STUDY_SCHEMA)
+public class ConsentOption implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -3939936220113729388L;
+	private Long	id;
+	private String	name;
+
+	public ConsentOption() {
+
+	}
+
+	@Id
+	@SequenceGenerator(name = "CONSENT_OPTION_SEQ", sequenceName = "CONSENT_OPTION_SEQUENCE")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "CONSENT_OPTION_SEQ")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
