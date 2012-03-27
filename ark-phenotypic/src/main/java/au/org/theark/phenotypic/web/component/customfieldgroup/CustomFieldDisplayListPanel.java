ce3869ae94530577c925a7270a7ec1722e1c9d9c
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-03-27 05:38:58 +0000

    Amended to show label for field, tick/cross for yes/no and tidied up class/HTML. (ARK-583)
    git-svn-id: svn://localhost/ARK@6541 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/CustomFieldDisplayListPanel.java
+++ ark-phenotypic/src/main/java/au/org/theark/phenotypic/web/component/customfieldgroup/CustomFieldDisplayListPanel.java
@@ -1,13 +1,15 @@
 package au.org.theark.phenotypic.web.component.customfieldgroup;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import au.org.theark.core.model.study.entity.CustomFieldDisplay;
@@ -18,135 +20,136 @@ import au.org.theark.core.web.component.AbstractDetailModalWindow;
 import au.org.theark.core.web.component.ArkDataProvider2;
 import au.org.theark.core.web.component.link.ArkBusyAjaxLink;
 
+/**
+ * Panel for displaying the list of custom fields
+ * 
+ * @author cellis
+ * 
+ */
 public class CustomFieldDisplayListPanel extends Panel {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long				serialVersionUID	= 1L;
 	protected AbstractDetailModalWindow	modalWindow;
-	private Panel modalContentPanel;
-	private FeedbackPanel feedbackPanel;
-	private ArkCrudContainerVO arkCrudContainerVO;
-	
+	private Panel								modalContentPanel;
+	private FeedbackPanel					feedbackPanel;
+	private ArkCrudContainerVO				arkCrudContainerVO;
+
+	@SuppressWarnings("unchecked")
 	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService iArkCommonService;
-	Boolean flag;
-	
-	
-	public CustomFieldDisplayListPanel(String id, FeedbackPanel feedbackPanel,ArkCrudContainerVO arkCrudContainerVO, Boolean flag) {
+	private IArkCommonService				iArkCommonService;
+	private Boolean							flag;
+
+	public CustomFieldDisplayListPanel(String id, FeedbackPanel feedbackPanel, ArkCrudContainerVO arkCrudContainerVO, Boolean flag) {
 		super(id);
 		this.feedbackPanel = feedbackPanel;
 		this.arkCrudContainerVO = arkCrudContainerVO;
 		this.flag = flag;
 		setOutputMarkupPlaceholderTag(true);
 	}
-	
-	public void initialisePanel(){
-	
-		modalWindow = new AbstractDetailModalWindow("detailModalWindow") {
 
+	public void initialisePanel() {
+
+		modalWindow = new AbstractDetailModalWindow("detailModalWindow") {
+			/**
+			 * 
+			 */
 			private static final long	serialVersionUID	= 1L;
+
 			@Override
 			protected void onCloseModalWindow(AjaxRequestTarget target) {
-				
+
 				target.add(arkCrudContainerVO.getWmcForCustomFieldDisplayListPanel());
 			}
 
 		};
-		
+
 		add(modalWindow);
 	}
-	
-	public DataView<CustomFieldDisplay> buildDataView(ArkDataProvider2<CustomFieldDisplay, CustomFieldDisplay> provider){
-		
-		
-		DataView<CustomFieldDisplay> dataView = new DataView<CustomFieldDisplay>("customFieldDisplayList", provider){
+
+	public DataView<CustomFieldDisplay> buildDataView(ArkDataProvider2<CustomFieldDisplay, CustomFieldDisplay> provider) {
+
+		DataView<CustomFieldDisplay> dataView = new DataView<CustomFieldDisplay>("customFieldDisplayList", provider) {
 
 			/**
 			 * 
 			 */
-			private static final long serialVersionUID = 1L;
+			private static final long	serialVersionUID	= 1L;
 
 			@Override
 			protected void populateItem(Item<CustomFieldDisplay> item) {
-				
-				CustomFieldDisplay cfd  = item.getModelObject();
-				
+
+				CustomFieldDisplay cfd = item.getModelObject();
+
+				if (cfd.getCustomField().getId() != null) {
+					item.add(new Label("id", cfd.getId().toString()));
+				}
+				else {
+					item.add(new Label("id", ""));
+				}
+
 				item.add(buildLink(item));
-				if(cfd.getCustomField() != null){
-					item.add( new Label("customField.name", cfd.getCustomField().getName()));
-				}else{
-					item.add( new Label("customField.name",""));
+
+				if (cfd.getCustomField().getFieldLabel() != null) {
+					item.add(new Label("fieldLabel", cfd.getCustomField().getFieldLabel()));
+				}
+				else {
+					item.add(new Label("fieldLabel", ""));
 				}
 				
-				if(cfd.getSequence() != null){
-					item.add( new Label("sequence",cfd.getSequence().toString()));
-				}else{
-					item.add(new Label("sequence",""));
+				if (cfd.getSequence() != null) {
+					item.add(new Label("sequence", cfd.getSequence().toString()));
+				}
+				else {
+					item.add(new Label("sequence", ""));
 				}
 				
-				if(cfd.getRequired() != null && cfd.getRequired()){
-					item.add( new Label("required", "Yes"));
-				}else{
-					item.add( new Label("required", "No"));
+				if (cfd.getRequired() != null && cfd.getRequired()) {
+					item.addOrReplace(new ContextImage("required", new Model<String>("images/icons/tick.png")));
 				}
-				
-				
-				if(cfd.getRequiredMessage() != null){
-					item.add( new Label("requiredMessage",cfd.getRequiredMessage()));
-				}else{
-					item.add(new Label("requiredMessage",""));
+				else {
+					item.addOrReplace(new ContextImage("required", new Model<String>("images/icons/cross.png")));
 				}
 			}
 		};
-		
+
 		return dataView;
 	}
-	
-	
-	public WebMarkupContainer buildLink(final Item<CustomFieldDisplay> item){
-		
-		WebMarkupContainer linkWmc = new WebMarkupContainer("cfdLinkWmc", item.getModel());
-		
-		ArkBusyAjaxLink link = new ArkBusyAjaxLink("id") {
+
+	public AjaxLink<String> buildLink(final Item<CustomFieldDisplay> item) {
+		ArkBusyAjaxLink<String> link = new ArkBusyAjaxLink<String>("link") {
 			/**
 			 * 
 			 */
-			private static final long serialVersionUID = 1L;
+			private static final long	serialVersionUID	= 1L;
 
 			@Override
 			public void onClick(AjaxRequestTarget target) {
-				
 				CustomFieldDisplay customFieldDisplayToEdit = iArkCommonService.getCustomFieldDisplay(item.getModelObject().getId());
 				CustomFieldGroupVO cfgVO = new CustomFieldGroupVO();
 				cfgVO.setCustomFieldDisplay(customFieldDisplayToEdit);
 				CompoundPropertyModel<CustomFieldGroupVO> newCpmModel = new CompoundPropertyModel<CustomFieldGroupVO>(cfgVO);
 				showModalWindow(target, newCpmModel);
-				//refresh the list
-				
 			}
 		};
-		
-		
-		CustomFieldDisplay customFieldDisplay = item.getModelObject();
-		Label idLink = new Label("idLabel", customFieldDisplay.getId().toString());
-		link.add(idLink);
-		linkWmc.add(link);
-		return linkWmc;
+
+		Label linkLabel = new Label("name", item.getModelObject().getCustomField().getName());
+		link.add(linkLabel);
+		return link;
 	}
-	
+
 	/**
+	 * Show the modal window, with specified detail panel
 	 * 
 	 * @param target
+	 * @param cpmModel
 	 */
-	protected void showModalWindow(AjaxRequestTarget target, CompoundPropertyModel<CustomFieldGroupVO> cpmModel){
-		
-		modalContentPanel = new CustomFieldDisplayModalPanel("content",modalWindow,cpmModel,feedbackPanel,flag);
+	protected void showModalWindow(AjaxRequestTarget target, CompoundPropertyModel<CustomFieldGroupVO> cpmModel) {
+		modalContentPanel = new CustomFieldDisplayModalPanel("content", modalWindow, cpmModel, feedbackPanel, flag);
 		modalWindow.setTitle("Custom Field Display Detail");
 		modalWindow.setContent(modalContentPanel);
 		modalWindow.show(target);
 	}
-	
-
 }
