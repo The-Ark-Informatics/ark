7703e3f8f29a3a8f49d826e2e5f4f136fdff610d
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-03-28 08:01:02 +0000

    amended ip address
    git-svn-id: svn://localhost/ARK@6570 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.0.1/20120122_lims_barcode_printer_dataPatch.sql
+++ ark-database/db-scripts/patch/1.0.1/20120122_lims_barcode_printer_dataPatch.sql
@@ -2,11 +2,11 @@ USE lims;
 
 DELETE FROM barcode_printer WHERE ID > 0;
 INSERT INTO barcode_printer (`STUDY_ID`,`NAME`,`DESCRIPTION`,`LOCATION`,`HOST`,`PORT`)
-SELECT ID, 'zebra','zebra TLP 2844','Office','130.95.56.99',9100
+SELECT ID, 'zebra','zebra TLP 2844','Office','127.0.0.1',9100
 FROM study.study
 WHERE ID IN (SELECT DISTINCT study_id FROM `study`.`ark_user_role` WHERE ark_module_id=5 AND study_id IS NOT NULL);
 
 INSERT INTO barcode_printer (`STUDY_ID`,`NAME`,`DESCRIPTION`,`LOCATION`,`HOST`,`PORT`)
-SELECT ID, 'brady_bbp_11','Brady BBP 11','Office','130.95.56.99',9100
+SELECT ID, 'brady_bbp_11','Brady BBP 11','Office','127.0.0.1',9100
 FROM study.study
 WHERE ID IN (SELECT DISTINCT study_id FROM `study`.`ark_user_role` WHERE ark_module_id=5 AND study_id IS NOT NULL);
