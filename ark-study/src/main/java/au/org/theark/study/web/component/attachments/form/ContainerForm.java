8346d8cf1689182d6ca80de0e1b592e2926aa546
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-03-30 04:57:24 +0000

    Refactored subjectFile package to attachments (matched function name)
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6612 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-study/src/main/java/au/org/theark/study/web/component/attachments/form/ContainerForm.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.study.web.component.attachments.form;
+
+import org.apache.wicket.model.CompoundPropertyModel;
+
+import au.org.theark.core.vo.SubjectVO;
+import au.org.theark.core.web.form.AbstractContainerForm;
+
+/**
+ * @author cellis
+ * 
+ */
+public class ContainerForm extends AbstractContainerForm<SubjectVO> {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -5497609816761774384L;
+
+	/**
+	 * @param id
+	 * @param cpmModel
+	 */
+	public ContainerForm(String id, CompoundPropertyModel<SubjectVO> cpmModel) {
+		super(id, cpmModel);
+		setMultiPart(true);
+	}
+
+}
