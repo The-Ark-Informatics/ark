49e7fafa1eb563e80279141fff2a4169c28efb0e
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-04-12 07:33:48 +0000

    Tidied up sql, made column names uppercase
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6654 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.0.2/20120330_study_upload_refactor_schemaPatch.sql
+++ ark-database/db-scripts/patch/1.0.2/20120330_study_upload_refactor_schemaPatch.sql
@@ -1,16 +1,17 @@
-alter table study.upload
-add column STATUSstudy.upload ENUM('UPLOADING', 'COMPLETE', 'ERRORS');
+USE study;
 
+alter table study.upload add column STATUS ENUM('UPLOADING', 'COMPLETE', 'ERRORS');
 
-
-create table upload_error(
-id int primary key,
-upload_id int,	
-error_msg varchar(256),
-row_number int,
-original_row_data text,
-foreign key fk_upload_error_upload 
-	(upload_id) references study.upload(id)
-)
-
+CREATE TABLE `upload_error`(
+    `ID` int(11) NOT NULL,
+    `UPLOAD_ID` int(11) DEFAULT NULL,
+    `ERROR_MSG` varchar(256) DEFAULT NULL,
+    `ROW_NUMBER` int(11) DEFAULT NULL,
+    `ORIGINAL_ROW_DATA` text,
+    PRIMARY KEY (`ID`),
+    KEY `FK_UPLOAD_ERROR_UPLOAD` (`UPLOAD_ID`),
+    CONSTRAINT `FK_UPLOAD_ERROR_UPLOAD` FOREIGN KEY (`UPLOAD_ID`)
+        REFERENCES `upload` (`ID`)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
