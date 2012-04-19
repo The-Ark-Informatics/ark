6a90d4e656ba07e82e71bba592512331b868c344
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-04-19 04:10:45 +0000

    Added vo class for custom field uploads
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6678 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/vo/CustomFieldUploadVO.java
@@ -0,0 +1,32 @@
+package au.org.theark.core.vo;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import au.org.theark.core.model.study.entity.CustomFieldUpload;
+import au.org.theark.core.vo.UploadVO;
+
+public class CustomFieldUploadVO extends UploadVO {
+
+	private static final long	serialVersionUID	= -3761087990900803427L;
+
+	private java.util.Collection<CustomFieldUpload>	customFieldUploadCollection = new ArrayList<CustomFieldUpload>();
+	private File tempFile = null;
+
+	public void setCustomFieldUploadCollection(java.util.Collection<CustomFieldUpload> customFieldUploadCollection) {
+		this.customFieldUploadCollection = customFieldUploadCollection;
+	}
+
+	public java.util.Collection<CustomFieldUpload> getCustomFieldUploadCollection() {
+		return customFieldUploadCollection;
+	}
+
+	public void setTempFile(File tempFile) {
+		this.tempFile = tempFile;
+	}
+
+	public File getTempFile() {
+		return tempFile;
+	}
+	
+}
