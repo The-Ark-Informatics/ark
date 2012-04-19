e658c0ae03a91a085438acdd98aa1313a2980459
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-04-19 05:49:05 +0000

    Added new excpetion class
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK@6681 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/exception/CustomFieldSystemException.java
@@ -0,0 +1,22 @@
+package au.org.theark.core.exception;
+
+/**
+ * @author cellis
+ *
+ */
+public class CustomFieldSystemException  extends ArkSystemException {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 8636172768000327003L;
+
+	public CustomFieldSystemException() {
+		super();
+	}
+
+	public CustomFieldSystemException(String message) {
+		super(message);
+	}
+}
+
