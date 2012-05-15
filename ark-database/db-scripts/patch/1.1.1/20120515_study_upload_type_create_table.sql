87d5191ea8187975281599cda0af69b75f556782
travis <travis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-05-15 04:10:35 +0000

    Created a reference table for UploadType to be consistent with the way we do all other similar work
    git-svn-id: svn://localhost/ARK@6794 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20120515_study_upload_type_create_table.sql
@@ -0,0 +1,7 @@
+CREATE  TABLE `study`.`upload_type` (
+  `id` INT NOT NULL ,
+  `name` VARCHAR(45) NULL ,
+  `description` VARCHAR(255) NULL )
+ENGINE = InnoDB
+COMMENT = 'Reference table to describe the type of an upload';
+
