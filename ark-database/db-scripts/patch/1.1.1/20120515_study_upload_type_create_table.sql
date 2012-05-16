ab151536be810235056cdd0d3065ff4a72fdfeed
travis <travis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-05-16 01:15:52 +0000

    several changes to allow for upload type
    git-svn-id: svn://localhost/ARK@6811 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120515_study_upload_type_create_table.sql
+++ ark-database/db-scripts/patch/1.1.1/20120515_study_upload_type_create_table.sql
@@ -1,7 +1,18 @@
 CREATE  TABLE `study`.`upload_type` (
-  `id` INT NOT NULL ,
-  `name` VARCHAR(45) NULL ,
-  `description` VARCHAR(255) NULL )
+  `ID` INT NOT NULL ,
+  `NAME` VARCHAR(45) NULL ,
+  `DESCRIPTION` VARCHAR(255) NULL )
 ENGINE = InnoDB
 COMMENT = 'Reference table to describe the type of an upload';
+
+ALTER TABLE `study`.`upload` ADD COLUMN `UPLOAD_TYPE_ID` INT(11) NULL  AFTER `STATUS` ;
+
+
+ALTER TABLE `study`.`upload` 
+  ADD CONSTRAINT `fk_upload_upload_type`
+  FOREIGN KEY (`UPLOAD_TYPE_ID` )
+  REFERENCES `study`.`upload_type` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_upload_upload_type` (`UPLOAD_TYPE_ID` ASC) ;
 
