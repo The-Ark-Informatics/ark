c14945c6e6d419e3d0756b4f5d6224b1bae6f08d
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-04 09:21:25 +0000

    Added extra session details, and updates lastAccessTime accordingly when user accesses [ARK-619]
    git-svn-id: svn://localhost/ARK@6894 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/session/SessionAttributeListener.java
+++ ark-common/src/main/java/au/org/theark/core/session/SessionAttributeListener.java
@@ -3,6 +3,7 @@ package au.org.theark.core.session;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,52 +12,58 @@ import javax.servlet.http.HttpSessionBindingEvent;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import au.org.theark.core.vo.ArkSubjectVO;
+import au.org.theark.core.vo.ArkSubjectSessionVO;
 
 /**
- * A very simple implementation of a SessionAttribute listner, that maintains a synchronised List of current users logged into the ARK 
+ * A very simple implementation of a SessionAttribute listener, that maintains a synchronised List of current users logged into the ARK
+ * 
  * @author cellis
- *
+ * 
  */
 public class SessionAttributeListener implements HttpSessionAttributeListener, Serializable {
+	private static final long						serialVersionUID	= 9115645497579631900L;
+	protected transient Logger						log					= LoggerFactory.getLogger(SessionAttributeListener.class);
+	// NOTE: listener is being called from multiple threads and ArrayList is unsynchronized
+	private static List<ArkSubjectSessionVO>	activeUsers			= Collections.synchronizedList(new ArrayList<ArkSubjectSessionVO>());
 
-	private static final long	serialVersionUID	= 9115645497579631900L;
-	//TODO: Further implement to maintain detailed session information 
-	private static List<ArkSubjectVO>	activeUsers	= Collections.synchronizedList(new ArrayList<ArkSubjectVO>()); // listener is being called from multiple
-																																		// threads and ArrayList is unsynchronized
-
-	public static List<ArkSubjectVO> getActiveUsers() {
+	public static List<ArkSubjectSessionVO> getActiveUsers() {
 		return activeUsers;
 	}
 
 	@Override
 	public void attributeAdded(HttpSessionBindingEvent event) {
-		if (event.getName().equals(au.org.theark.core.Constants.ARK_USERID))
-		{
+		if (event.getName().equals(au.org.theark.core.Constants.ARK_USERID)) {
 			Subject subject = SecurityUtils.getSubject();
 			String sessionId = subject.getSession().getId().toString();
 			String userId = subject.getPrincipal().toString();
 			String host = subject.getSession().getHost();
-			ArkSubjectVO arkSubjectVo = new ArkSubjectVO(sessionId, userId, host);
+			Date startTimestamp = subject.getSession().getStartTimestamp();
+			Date lastAccessTime = subject.getSession().getLastAccessTime();
+			String action = "Logged in";
+			ArkSubjectSessionVO arkSubjectVo = new ArkSubjectSessionVO(sessionId, userId, host, startTimestamp, lastAccessTime, action);
 			activeUsers.add(arkSubjectVo);
 		}
 	}
 
 	@Override
 	public void attributeRemoved(HttpSessionBindingEvent event) {
-		String currentUser = SecurityUtils.getSubject().getPrincipal().toString();
-		if (event.getName().equals("org.apache.shiro.subject.support.DefaultSubjectContext_PRINCIPALS_SESSION_KEY") && event.getValue().toString().equalsIgnoreCase(currentUser))
-		{
+		Subject subject = SecurityUtils.getSubject();
+		String currentUser = subject.getPrincipal().toString();
+		if (event.getName().equals("org.apache.shiro.subject.support.DefaultSubjectContext_PRINCIPALS_SESSION_KEY") && event.getValue().toString().equalsIgnoreCase(currentUser)) {
 			// actual user logging off
-			activeUsers.remove(SecurityUtils.getSubject());
+			String sessionId = subject.getSession().getId().toString();
+			ArkSubjectSessionVO arkSubjectVo = new ArkSubjectSessionVO(sessionId);
+			activeUsers.remove(arkSubjectVo);
 		}
-			
-		if(event.getName().equals(au.org.theark.core.Constants.ARK_LOGOFF_SESSION_ID)) {
+
+		if (event.getName().equals(au.org.theark.core.Constants.ARK_LOGOFF_SESSION_ID)) {
 			// Forced session kill by admin
-			for (Iterator<ArkSubjectVO> iterator = activeUsers.iterator(); iterator.hasNext();) {
-				ArkSubjectVO arkSubjectVo = (ArkSubjectVO) iterator.next();
-				if(arkSubjectVo.getSessionId().equalsIgnoreCase(event.getValue().toString())){
+			for (Iterator<ArkSubjectSessionVO> iterator = activeUsers.iterator(); iterator.hasNext();) {
+				ArkSubjectSessionVO arkSubjectVo = (ArkSubjectSessionVO) iterator.next();
+				if (arkSubjectVo.getSessionId().equalsIgnoreCase(event.getValue().toString())) {
 					activeUsers.remove(arkSubjectVo);
 				}
 			}
@@ -65,5 +72,8 @@ public class SessionAttributeListener implements HttpSessionAttributeListener, S
 
 	@Override
 	public void attributeReplaced(HttpSessionBindingEvent event) {
+		String sessionId = SecurityUtils.getSubject().getSession().getId().toString();
+		ArkSubjectSessionVO arkSubjectVo = new ArkSubjectSessionVO(sessionId);
+		activeUsers.get(activeUsers.indexOf(arkSubjectVo)).setLastAccessTime(new Date());
 	}
 }
