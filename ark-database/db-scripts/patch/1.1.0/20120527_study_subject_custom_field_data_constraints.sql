1af43939fc8636ed24bfcd00a9072497d1ce5248
cellis <cellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-08 07:02:01 +0000

    changed to comment
    git-svn-id: svn://localhost/ARK@6898 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.0/20120527_study_subject_custom_field_data_constraints.sql
+++ ark-database/db-scripts/patch/1.1.0/20120527_study_subject_custom_field_data_constraints.sql
@@ -1,3 +1,3 @@
-# one data row per CFD per subject
+-- one data row per CFD per subject
 ALTER TABLE `study`.`subject_custom_field_data` 
 ADD UNIQUE INDEX `UQ_SCFD_CFD_LSS` USING BTREE (`LINK_SUBJECT_STUDY_ID` ASC, `CUSTOM_FIELD_DISPLAY_ID` ASC) ;
