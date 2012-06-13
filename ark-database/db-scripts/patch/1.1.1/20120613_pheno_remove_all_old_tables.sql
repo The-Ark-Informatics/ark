d8a15b15da837df107fe4e7ea751ad9daa95aa4e
travis <travis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-13 04:33:05 +0000

    massive array of tables to be deleted in pheno schema
    git-svn-id: svn://localhost/ARK@6904 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120613_pheno_remove_all_old_tables.sql
+++ ark-database/db-scripts/patch/1.1.1/20120613_pheno_remove_all_old_tables.sql
@@ -1,21 +1,21 @@
-Tables_in_pheno
-collection
-collection_upload
-delimiter_type
-field
-field_collection
-field_data
-field_data_log
-field_field_group
-field_group
-field_group_upload
-field_summary
-field_type
-field_upload
-field_upload_v
-file_format
-pheno_collection
-pheno_data
-questionnaire_status
-status
-upload
+-- dropping a bunch of unused tables from pheno
+
+SET foreign_key_checks = 0;
+drop table pheno.collection;
+drop table pheno.collection_upload;
+drop table pheno.delimiter_type;
+drop table pheno.field;
+drop table pheno.field_collection;
+drop table pheno.field_data;
+drop table pheno.field_data_log;
+drop table pheno.field_field_group;
+drop table pheno.field_group;
+drop table pheno.field_group_upload;
+drop table pheno.field_summary;
+drop table pheno.field_type;
+drop table pheno.field_upload;
+drop table pheno.field_upload_v;
+drop table pheno.file_format;
+SET foreign_key_checks = 1;
+
+-- keep pheno_collection, pheno_data, questionnaire_status, status
