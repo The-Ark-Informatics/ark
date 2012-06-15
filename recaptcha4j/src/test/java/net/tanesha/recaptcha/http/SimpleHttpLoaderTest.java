865e741629a5bfb4552fb93e22d30ce18b79a997
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-06-15 08:06:36 +0000

    moved into trunk
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@6913 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ recaptcha4j/src/test/java/net/tanesha/recaptcha/http/SimpleHttpLoaderTest.java
@@ -0,0 +1,33 @@
+package net.tanesha.recaptcha.http;
+
+import junit.framework.TestCase;
+
+public class SimpleHttpLoaderTest extends TestCase {
+
+	SimpleHttpLoader l;
+	
+	protected void setUp() throws Exception {
+		l = new SimpleHttpLoader();
+	}
+
+	public void testHttpGet() {
+		// check http plain
+		assertNotNull(l.httpGet("http://www.google.com/"));
+		
+		// check https
+		assertNotNull(l.httpGet("https://mail.google.com/"));
+		
+		// check content.
+		// assertEquals("no?")
+	}
+
+	public void testHttpPost() {
+		try {
+			l.httpPost("http://www.google.com/search", "hl=da&q=bla&btnG=Google-s%C3%B8gning&meta=");
+		}
+		catch (Exception e) {
+			assertTrue(e.getMessage().indexOf("405 for URL") != -1);
+		}
+	}
+
+}
