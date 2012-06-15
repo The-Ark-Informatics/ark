15f2ee93175f52a3f582b302ee8f6fed0c073b91
cellis <cellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-06-15 08:14:01 +0000

    moved into trunk
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@6930 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-registry/src/test/java/au/org/theark/registry/TestHomePage.java
@@ -0,0 +1,27 @@
+package au.org.theark.registry;
+
+import junit.framework.TestCase;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage extends TestCase {
+// TODO:  remove class or write some tests!
+	 //private WicketTester	tester;
+
+	@Override
+	public void setUp() {
+		// tester = new WicketTester(new WicketApplication());
+	}
+
+	public void testRenderMyPage() {
+		// start and render the test page
+		// tester.startPage(HomePage.class);
+
+		// assert rendered page class
+		// tester.assertRenderedPage(HomePage.class);
+
+		// assert rendered label component
+		// tester.assertLabel("message", "If you see this message wicket is properly configured and running");
+	}
+}
