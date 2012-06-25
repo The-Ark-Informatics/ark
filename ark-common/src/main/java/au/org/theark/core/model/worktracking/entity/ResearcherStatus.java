b218dc9997d88ca7a5bf9be4bd483631d6a60521
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-25 06:35:53 +0000

    added work-tracking entities
    git-svn-id: svn://localhost/ARK/trunk@6970 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/worktracking/entity/ResearcherStatus.java
@@ -0,0 +1,82 @@
+package au.org.theark.core.model.worktracking.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "RESEARCHER_STATUS", schema = Constants.ADMIN_SCHEMA)
+public class ResearcherStatus implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Long			id;
+	private String			name;
+	private String			description;
+	private Set<Researcher>	researchers	= new HashSet<Researcher>(0);
+
+
+	public ResearcherStatus() {
+	}
+
+	public ResearcherStatus(Long id) {
+		this.id = id;
+	}
+
+	public ResearcherStatus(Long id, String name, String description, Set<Researcher> researchers) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.researchers = researchers;
+	}
+
+	public ResearcherStatus(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Id
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", length = 25)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "DESCRIPTION")
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "researcherStatus")
+	public Set<Researcher> getResearchers() {
+		return researchers;
+	}
+
+	public void setResearchers(Set<Researcher> researchers) {
+		this.researchers = researchers;
+	}
+}
