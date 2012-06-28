89d103131d35770396f09836061f22e24d2d8bf8
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-28 02:57:53 +0000

    added ark_module to upload type...so that we can differentiate pheno and subject uploads
    git-svn-id: svn://localhost/ARK/trunk@6989 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20120628_study_upload_type_add_field.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `study`.`upload_type` ADD COLUMN `ARK_MODULE_ID` INT(11) NOT NULL DEFAULT 2  AFTER `DESCRIPTION` , 
+  ADD CONSTRAINT `fk_upload_type_ark_module`
+  FOREIGN KEY (`ARK_MODULE_ID` )
+  REFERENCES `study`.`ark_module` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_upload_type_ark_module` (`ARK_MODULE_ID` ASC) ;
+
+
+
+update study.upload_type set ark_module_id = 3
+where id = 3;
