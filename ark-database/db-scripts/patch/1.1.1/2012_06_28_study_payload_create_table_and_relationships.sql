c286482e770f68d0eb0b7d961df0fbbbbb8efefa
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-06-28 07:38:19 +0000

    changed pk definition  int(11) to match other tables in schema.
    Added another field to upload to reference the payload table (and the foreign key reference)
    
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@6993 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/2012_06_28_study_payload_create_table_and_relationships.sql
+++ ark-database/db-scripts/patch/1.1.1/2012_06_28_study_payload_create_table_and_relationships.sql
@@ -1,8 +1,18 @@
+# drop table study.payload;
+
 CREATE  TABLE `study`.`payload` (
-  `id` INT NOT NULL ,
+  `id` INT(11) NOT NULL ,
   `payload` BLOB NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB
 COMMENT = 'This is a simple table for storing LObs and an id to represent them.  Hopefully pushing heavy stuff outside of tables to which logic is applied and making hibernate behave and not do too much heavy lifting until we need it too in more databases (think \"lazy loading\", hibernate doesnt always listen to lazy loading of an attribute...thus make it another entity with lazy loading).  ';
+
+ALTER TABLE `study`.`upload` ADD COLUMN `PAYLOAD_ID` INT(11) NULL  AFTER `UPLOAD_TYPE_ID` , 
+  ADD CONSTRAINT `fk_upload_payload`
+  FOREIGN KEY (`PAYLOAD_ID` )
+  REFERENCES `study`.`payload` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_upload_payload` (`PAYLOAD_ID` ASC) ;
 
 
