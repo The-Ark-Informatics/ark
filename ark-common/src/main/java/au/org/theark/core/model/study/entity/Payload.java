f878e6edbdd324d1f3c3bb3e8bfdd9b9a1643023
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-06-28 06:45:05 +0000

    Added new Payload table/entity to break away large objects to a lazy loaded entity that we get only when we need (in hopefully any database that someone may use)
    git-svn-id: svn://localhost/ARK/trunk@6992 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/Payload.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.core.model.study.entity;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import au.org.theark.core.Constants;
+
+/**
+ * Person entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "payload", schema = Constants.STUDY_SCHEMA)
+public class Payload implements java.io.Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+
+	private Long								id;
+	private Set<Upload>			uploads					= new HashSet<Upload>(0);
+
+	public Payload() {
+	}
+
+	public Payload(Long id) {
+		this.id = id;
+	}
+
+	public Payload(Set<Upload> uploads) {
+		this.uploads = uploads;
+	}
+
+	@Id
+	@SequenceGenerator(name = "payload_generator", sequenceName = "PAYLOAD_SEQUENCE")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "payload_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "payload")
+	public Set<Upload> getUploads() {
+		return this.uploads;
+	}
+
+	public void setUploads(Set<Upload> uploads) {
+		this.uploads = uploads;
+	}
+	
+}
