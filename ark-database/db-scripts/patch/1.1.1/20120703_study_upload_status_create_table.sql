17159ad56ae5ae7ca25df566a3a9ba754eef9605
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-03 03:50:15 +0000

    tweak upload status (may have been an issue with db engine selection, so slightly longwinded approach to use of statuses for uploads
    git-svn-id: svn://localhost/ARK/trunk@7006 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120703_study_upload_status_create_table.sql
+++ ark-database/db-scripts/patch/1.1.1/20120703_study_upload_status_create_table.sql
@@ -6,3 +6,36 @@ CREATE  TABLE `study`.`upload_status` (
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) );
 
+ALTER TABLE `study`.`upload` DROP COLUMN `STATUS`;
+
+ALTER TABLE `study`.`upload_status` ENGINE = InnoDB , CHANGE COLUMN `short_message` `SHORT_MESSAGE` VARCHAR(45) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)'  AFTER `NAME` , CHANGE COLUMN `id` `ID` INT(11) NOT NULL  , CHANGE COLUMN `name` `NAME` VARCHAR(45) NOT NULL  , CHANGE COLUMN `long_message` `LONG_MESSAGE` VARCHAR(45) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)'  
+, DROP PRIMARY KEY 
+, ADD PRIMARY KEY (`ID`) 
+, DROP INDEX `name_UNIQUE` 
+, ADD UNIQUE INDEX `name_UNIQUE` (`NAME` ASC);
+
+ALTER TABLE `study`.`upload` ADD COLUMN `STATUS_ID` INT(11) NOT NULL  AFTER `PAYLOAD_ID` , 
+  ADD CONSTRAINT `fk_upload_status`
+  FOREIGN KEY (`STATUS_ID` )
+  REFERENCES `study`.`upload_status` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_upload_status` (`STATUS_ID` ASC);
+
+
+
+insert upload_status(id, name, short_message, long_message)
+			values(0, 'STATUS_NOT_UPDATED', 'Status not defined', 'Status not defined.  This may predate our adding status to uploads');
+
+insert upload_status(id, name, short_message, long_message)
+			values(0, 'ERROR_IN_VALIDATION', 'Error while validating', 'Error while validating data, prior to uploading');
+
+insert upload_status(id, name, short_message, long_message)
+			values(0, 'ERROR_ON_UPLOAD', 'Error while uploading data', 'While the file passed validation, an error occured during the upload.  Please contact your system administrator.');
+
+insert upload_status(id, name, short_message, long_message)
+			values(0, 'VALIDATED', 'Successfully validatied', 'Successfully validatied.  Awaiting upload into fields');
+
+insert upload_status(id, name, short_message, long_message)
+			values(0, 'UPLOADED', 'Successfully uploaded', 'Successfully updated into fields');
+
