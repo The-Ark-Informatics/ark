2077b4519da8ec42a943c1d47840b264b9d52851
chrisellis <chrisellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-07-04 04:52:08 +0000

    added use statement
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7023 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20120628_study_custom_field_and_custom_field_group_data_patch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120628_study_custom_field_and_custom_field_group_data_patch.sql
@@ -1,4 +1,5 @@
 -- this is intended to create uniformity in how all pheno data fields and groups are seen and accessed through the pheno module
+use study;
 
 update custom_field_group set ark_function_id = 14 where ark_function_id = 12;
 
