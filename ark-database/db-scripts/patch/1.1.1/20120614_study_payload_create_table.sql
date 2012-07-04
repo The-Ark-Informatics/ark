258016ee9deb40ccf260e1451f4b27db1bcee193
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-04 04:50:54 +0000

    removed redundant patch
    git-svn-id: svn://localhost/ARK/trunk@7022 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120614_study_payload_create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE  TABLE `study`.`payload` (
-  `id` INT NOT NULL ,
-  `payload` BLOB NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-COMMENT = 'This is strictly for storing large objects, and will slightly aid in having hibernate lazy loading work accross databases';
-
