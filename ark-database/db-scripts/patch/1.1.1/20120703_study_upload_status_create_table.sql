b2f23b98538693f134bf27adc80ed17d02b66461
chrisellis <chrisellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-07-04 06:55:37 +0000

    added use statement, uppercased colnames
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7028 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20120703_study_upload_status_create_table.sql
+++ ark-database/db-scripts/patch/1.1.1/20120703_study_upload_status_create_table.sql
@@ -1,18 +1,13 @@
+USE study;
 CREATE  TABLE `study`.`upload_status` (
-  `id` INT NOT NULL ,
-  `name` VARCHAR(45) NOT NULL ,
-  `short_message` VARCHAR(128) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)' ,
-  `long_message` VARCHAR(256) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)' ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) );
+  `ID` INT NOT NULL ,
+  `NAME` VARCHAR(45) NOT NULL ,
+  `SHORT_MESSAGE` VARCHAR(128) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)' ,
+  `LONG_MESSAGE` VARCHAR(256) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)' ,
+  PRIMARY KEY (`ID`) ,
+  UNIQUE INDEX `name_UNIQUE` (`NAME` ASC) );
 
 ALTER TABLE `study`.`upload` DROP COLUMN `STATUS`;
-
-ALTER TABLE `study`.`upload_status` ENGINE = InnoDB , CHANGE COLUMN `short_message` `SHORT_MESSAGE` VARCHAR(45) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)'  AFTER `NAME` , CHANGE COLUMN `id` `ID` INT(11) NOT NULL  , CHANGE COLUMN `name` `NAME` VARCHAR(45) NOT NULL  , CHANGE COLUMN `long_message` `LONG_MESSAGE` VARCHAR(45) NOT NULL COMMENT 'evenutally messages may all be brought out to allow i18n (internationalization)'  
-, DROP PRIMARY KEY 
-, ADD PRIMARY KEY (`ID`) 
-, DROP INDEX `name_UNIQUE` 
-, ADD UNIQUE INDEX `name_UNIQUE` (`NAME` ASC);
 
 ALTER TABLE `study`.`upload` ADD COLUMN `STATUS_ID` INT(11) NOT NULL  AFTER `PAYLOAD_ID` , 
   ADD CONSTRAINT `fk_upload_status`
@@ -21,7 +16,6 @@ ALTER TABLE `study`.`upload` ADD COLUMN `STATUS_ID` INT(11) NOT NULL  AFTER `PAY
   ON DELETE NO ACTION
   ON UPDATE NO ACTION
 , ADD INDEX `fk_upload_status` (`STATUS_ID` ASC);
-
 
 INSERT INTO `study`.`upload_status` (ID,NAME,SHORT_MESSAGE,LONG_MESSAGE) VALUES (-3,'ERROR_ON_DATA_IMPORT','Error while importing data','While the file passed validation, an error occured during the import of data.  Please contact your system administrator.');
 INSERT INTO `study`.`upload_status` (ID,NAME,SHORT_MESSAGE,LONG_MESSAGE) VALUES (-2,'ERROR_IN_DATA_VALIDATION','Error while validating data','Error while validating data, prior to uploading');
