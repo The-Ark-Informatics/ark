3e054071fbd996053c596b39a6d4765a8bf7dfcc
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-04 02:51:26 +0000

    amended sql
    git-svn-id: svn://localhost/ARK/trunk@7016 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/2012_06_28_study_payload_create_table_and_relationships.sql
+++ ark-database/db-scripts/patch/1.1.1/2012_06_28_study_payload_create_table_and_relationships.sql
@@ -1,11 +1,11 @@
-# drop table study.payload;
+drop table if exists study.payload;
 
 CREATE  TABLE `study`.`payload` (
   `ID` INT(11) NOT NULL AUTO_INCREMENT,
   `PAYLOAD` LONGBLOB NOT NULL ,
   PRIMARY KEY (`ID`) )
 ENGINE = InnoDB
-COMMENT = 'This is a simple table for storing LObs and an id to represent them.  Hopefully pushing heavy stuff outside of tables to which logic is applied and making hibernate behave and not do too much heavy lifting until we need it too in more databases (think \"lazy loading\", hibernate doesnt always listen to lazy loading of an attribute...thus make it another entity with lazy loading).  ';
+COMMENT = 'This is a simple table for storing LOBs and an id to represent them.  Hopefully pushing heavy stuff outside of tables to which logic is applied and making hibernate behave and not do too much heavy lifting until we need it too in more databases (think \"lazy loading\", hibernate doesnt always listen to lazy loading of an attribute...thus make it another entity with lazy loading).  ';
 
 ALTER TABLE `study`.`upload` ADD COLUMN `PAYLOAD_ID` INT(11) NULL  AFTER `UPLOAD_TYPE_ID` , 
   ADD CONSTRAINT `fk_upload_payload`
@@ -15,13 +15,11 @@ ALTER TABLE `study`.`upload` ADD COLUMN `PAYLOAD_ID` INT(11) NULL  AFTER `UPLOAD
   ON UPDATE NO ACTION
 , ADD INDEX `fk_upload_payload` (`PAYLOAD_ID` ASC) ;
 
-
-
 -- then fix the data
-insert into payload(ID, PAYLOAD) 
+insert into study.payload(ID, PAYLOAD) 
 select up.ID, up.PAYLOAD from upload up;
 
-update upload set PAYLOAD_ID = ID ;
+update study.upload set PAYLOAD_ID = ID ;
 
 -- then drop the old blob column from 
 alter table study.upload drop column PAYLOAD;
@@ -34,5 +32,3 @@ ALTER TABLE `study`.`upload` CHANGE COLUMN `PAYLOAD_ID` `PAYLOAD_ID` INT(11) NOT
   REFERENCES `study`.`payload` (`id` )
   ON DELETE CASCADE
   ON UPDATE NO ACTION;
-
-
