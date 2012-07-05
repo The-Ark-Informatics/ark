adc4b9e99a8a53c06562f84ddbbcc84d4194e4e1
Thilina Ranaweera <tranaweera@gmail.com>
2012-07-05 07:02:31 +0000

    ARK-659 Add study id to Researcher entity
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7052 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/model/worktracking/entity/Researcher.java
+++ ark-common/src/main/java/au/org/theark/core/model/worktracking/entity/Researcher.java
@@ -40,10 +40,11 @@ public class Researcher implements Serializable {
 	private String								fax;
 	private String								comment;
 	private BillingType							billingType;
-	private Long								acccountNumber;
-	private Integer								bsb;
+	private String								accountNumber;
+	private String								bsb;
 	private String								bank;
 	private String								accountName;
+	private Long								studyId;
 	
 	public Researcher() {
 	}
@@ -56,8 +57,8 @@ public class Researcher implements Serializable {
 			String institute, String address, ResearcherRole researcherRole,
 			ResearcherStatus researcherStatus, Date lastActiveDate,
 			String officePhone, String mobile, String email, String fax,
-			String comment, BillingType billingType, Long acccountNumber,
-			Integer bsb, String bank, String accountName) {
+			String comment, BillingType billingType, String accountNumber,
+			String bsb, String bank, String accountName, Long studyId) {
 		
 		this.id = id;
 		this.firstName = firstName;
@@ -73,10 +74,11 @@ public class Researcher implements Serializable {
 		this.fax = fax;
 		this.comment = comment;
 		this.billingType = billingType;
-		this.acccountNumber = acccountNumber;
+		this.accountNumber = accountNumber;
 		this.bsb = bsb;
 		this.bank = bank;
 		this.accountName = accountName;
+		this.studyId=studyId;
 	}
 
 	@Id
@@ -213,20 +215,20 @@ public class Researcher implements Serializable {
 	}
 
 	@Column(name = "ACCOUNT_NUMBER")
-	public Long getAcccountNumber() {
-		return acccountNumber;
+	public String getAccountNumber() {
+		return accountNumber;
 	}
 	
-	public void setAcccountNumber(Long acccountNumber) {
-		this.acccountNumber = acccountNumber;
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
 	}
 
 	@Column(name = "BSB")
-	public Integer getBsb() {
+	public String getBsb() {
 		return bsb;
 	}
 
-	public void setBsb(Integer bsb) {
+	public void setBsb(String bsb) {
 		this.bsb = bsb;
 	}
 
@@ -247,4 +249,138 @@ public class Researcher implements Serializable {
 	public void setAccountName(String accountName) {
 		this.accountName = accountName;
 	}
+
+	@Column(name = "STUDY_ID")
+	public Long getStudyId() {
+		return studyId;
+	}
+
+	public void setStudyId(Long studyId) {
+		this.studyId = studyId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((accountName == null) ? 0 : accountName.hashCode());
+		result = prime * result
+				+ ((accountNumber == null) ? 0 : accountNumber.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((bank == null) ? 0 : bank.hashCode());
+		result = prime * result + ((bsb == null) ? 0 : bsb.hashCode());
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fax == null) ? 0 : fax.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((institute == null) ? 0 : institute.hashCode());
+		result = prime * result
+				+ ((lastActiveDate == null) ? 0 : lastActiveDate.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+		result = prime * result
+				+ ((officePhone == null) ? 0 : officePhone.hashCode());
+		result = prime * result + ((studyId == null) ? 0 : studyId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Researcher other = (Researcher) obj;
+		if (accountName == null) {
+			if (other.accountName != null)
+				return false;
+		} else if (!accountName.equals(other.accountName))
+			return false;
+		if (accountNumber == null) {
+			if (other.accountNumber != null)
+				return false;
+		} else if (!accountNumber.equals(other.accountNumber))
+			return false;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (bank == null) {
+			if (other.bank != null)
+				return false;
+		} else if (!bank.equals(other.bank))
+			return false;
+		if (bsb == null) {
+			if (other.bsb != null)
+				return false;
+		} else if (!bsb.equals(other.bsb))
+			return false;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fax == null) {
+			if (other.fax != null)
+				return false;
+		} else if (!fax.equals(other.fax))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (institute == null) {
+			if (other.institute != null)
+				return false;
+		} else if (!institute.equals(other.institute))
+			return false;
+		if (lastActiveDate == null) {
+			if (other.lastActiveDate != null)
+				return false;
+		} else if (!lastActiveDate.equals(other.lastActiveDate))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (mobile == null) {
+			if (other.mobile != null)
+				return false;
+		} else if (!mobile.equals(other.mobile))
+			return false;
+		if (officePhone == null) {
+			if (other.officePhone != null)
+				return false;
+		} else if (!officePhone.equals(other.officePhone))
+			return false;
+		if (studyId == null) {
+			if (other.studyId != null)
+				return false;
+		} else if (!studyId.equals(other.studyId))
+			return false;
+		return true;
+	}	
+	
+	
+	
 }
