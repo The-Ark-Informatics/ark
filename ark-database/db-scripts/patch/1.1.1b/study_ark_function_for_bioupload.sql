65ecc61c9c7b69b5c2741689f3bb813219ec533e
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-07-16 01:18:24 +0000

    added new module, ark function, menu item, etc for bio custom field uploader
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7085 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-database/db-scripts/patch/1.1.1b/study_ark_function_for_bioupload.sql
@@ -0,0 +1,3 @@
+INSERT INTO `study`.`ark_function` (`ID`, `NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) 
+VALUES (58, 'BIOSPECIMEN_AND_BIOCOLLECTION_CUSTOM_FIELD_UPLOAD', 'Uploader for both Biospecimen and Biocollection Custom Fields', 1, 'tab.module.lims.bioupload');
+
