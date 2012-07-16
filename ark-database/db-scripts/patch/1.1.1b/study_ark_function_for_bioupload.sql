77c0cd1489d72a3fb53cc55bedd7805d5ffc20ce
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-16 04:12:32 +0000

    Naming conventions (add data at start)
    git-svn-id: svn://localhost/ARK/trunk@7092 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1b/study_ark_function_for_bioupload.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `study`.`ark_function` (`ID`, `NAME`, `DESCRIPTION`, `ARK_FUNCTION_TYPE_ID`, `RESOURCE_KEY`) 
-VALUES (58, 'BIOSPECIMEN_AND_BIOCOLLECTION_CUSTOM_FIELD_UPLOAD', 'Uploader for both Biospecimen and Biocollection Custom Fields', 1, 'tab.module.lims.bioupload');
-
-
-UPDATE `study`.`upload_type` SET `DESCRIPTION`='Custom Data to be associated with a biospecimen', `ARK_MODULE_ID`=5 WHERE `ID`='0';
-
-INSERT INTO `study`.`upload_type` (`ID`, `NAME`, `DESCRIPTION`, `ARK_MODULE_ID`) VALUES (4, 'Biocollection Custom Data', 'Custom Data to be associated with a biospecimen', 5);
-
