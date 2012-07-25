b41d2524e9e7996626143914acd43ff0624748f8
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-25 07:45:57 +0000

    Amended to create table structures after reference table creation/insertion
    git-svn-id: svn://localhost/ARK/trunk@7151 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120725_Work_tracking_module_status_and_roles_insert_patch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120725_Work_tracking_module_status_and_roles_insert_patch.sql
@@ -1,70 +1,40 @@
-CREATE DATABASE  IF NOT EXISTS `admin` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `admin`;
+
 USE `admin`;
--- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (x86_64)
---
--- Host: ark-database    Database: admin
--- ------------------------------------------------------
--- Server version	5.1.63-0ubuntu0.10.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `billable_item_status`
---
 
 DROP TABLE IF EXISTS `billable_item_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `billable_item_status` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `billable_item_status`
 --
 
 LOCK TABLES `billable_item_status` WRITE;
-/*!40000 ALTER TABLE `billable_item_status` DISABLE KEYS */;
 INSERT INTO `billable_item_status` VALUES (1,'Not Commenced',''),(2,'Commenced',''),(3,'Completed','');
-/*!40000 ALTER TABLE `billable_item_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `researcher_role`
 --
-
 DROP TABLE IF EXISTS `researcher_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `researcher_role` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `researcher_role`
 --
 
 LOCK TABLES `researcher_role` WRITE;
-/*!40000 ALTER TABLE `researcher_role` DISABLE KEYS */;
 INSERT INTO `researcher_role` VALUES (1,'Chief Investigator',NULL),(2,'Assoc Investigator',NULL),(3,'Other Investigator',NULL),(4,'Research Assistant',NULL);
-/*!40000 ALTER TABLE `researcher_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -72,24 +42,19 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `billable_item_type_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `billable_item_type_status` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `billable_item_type_status`
 --
 
 LOCK TABLES `billable_item_type_status` WRITE;
-/*!40000 ALTER TABLE `billable_item_type_status` DISABLE KEYS */;
 INSERT INTO `billable_item_type_status` VALUES (1,'ACTIVE','Active record'),(2,'INACTIVE','Inactive record');
-/*!40000 ALTER TABLE `billable_item_type_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -97,24 +62,19 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `work_request_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `work_request_status` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `work_request_status`
 --
 
 LOCK TABLES `work_request_status` WRITE;
-/*!40000 ALTER TABLE `work_request_status` DISABLE KEYS */;
 INSERT INTO `work_request_status` VALUES (1,'Not Commenced',''),(2,'Commenced',''),(3,'Completed','');
-/*!40000 ALTER TABLE `work_request_status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -122,58 +82,133 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `billing_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `billing_type` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `billing_type`
 --
 
 LOCK TABLES `billing_type` WRITE;
-/*!40000 ALTER TABLE `billing_type` DISABLE KEYS */;
 INSERT INTO `billing_type` VALUES (1,'CASH',NULL),(2,'CHEQUE',NULL),(3,'DRAFT',NULL);
-/*!40000 ALTER TABLE `billing_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `researcher_status`
 --
-
 DROP TABLE IF EXISTS `researcher_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `researcher_status` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `NAME` varchar(25) NOT NULL,
   `DESCRIPTION` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `researcher_status`
 --
 
 LOCK TABLES `researcher_status` WRITE;
-/*!40000 ALTER TABLE `researcher_status` DISABLE KEYS */;
-INSERT INTO `researcher_status` VALUES (1,'Active',NULL),(5,'Inactive','');
-/*!40000 ALTER TABLE `researcher_status` ENABLE KEYS */;
+INSERT INTO `researcher_status` VALUES (1,'Active',NULL),(2,'Inactive','');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-07-25 11:49:56
+CREATE TABLE `researcher` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` varchar(30) DEFAULT NULL,
+  `LAST_NAME` varchar(45) DEFAULT NULL,
+  `INSTITUTE` varchar(50) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `ROLE_ID` int(11) DEFAULT NULL,
+  `STATUS_ID` int(11) DEFAULT NULL,
+  `CREATED_DATE` date DEFAULT NULL,
+  `OFFICE_PHONE` varchar(12) DEFAULT NULL,
+  `MOBILE` varchar(12) DEFAULT NULL,
+  `EMAIL` varchar(45) DEFAULT NULL,
+  `FAX` varchar(12) DEFAULT NULL,
+  `COMMENT` varchar(255) DEFAULT NULL,
+  `BILLING_TYPE_ID` int(11) DEFAULT NULL,
+  `ACCOUNT_NUMBER` varchar(30) DEFAULT NULL,
+  `BSB` varchar(8) DEFAULT NULL,
+  `BANK` varchar(50) DEFAULT NULL,
+  `ACCOUNT_NAME` varchar(50) DEFAULT NULL,
+  `STUDY_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_researcher_role_id` (`ROLE_ID`),
+  KEY `fk_researcher_status_id` (`STATUS_ID`),
+  KEY `fk_researcher_billing_type_id` (`BILLING_TYPE_ID`),
+  CONSTRAINT `fk_researcher_billing_type_id` FOREIGN KEY (`BILLING_TYPE_ID`) REFERENCES `billing_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_researcher_role_id` FOREIGN KEY (`ROLE_ID`) REFERENCES `researcher_role` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_researcher_status_id` FOREIGN KEY (`STATUS_ID`) REFERENCES `researcher_status` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `billable_item_type` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ITEM_NAME` varchar(45) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `QUANTITY_PER_UNIT` int(11) DEFAULT NULL,
+  `UNIT_PRICE` double DEFAULT NULL,
+  `GST` double DEFAULT NULL,
+  `STATUS_ID` int(1) DEFAULT '1',
+  `STUDY_ID` int(11) NOT NULL,
+  `TYPE` varchar(10) NOT NULL DEFAULT 'DEFAULT',
+  PRIMARY KEY (`ID`),
+  KEY `fk_billable_item_type_study_id` (`STUDY_ID`),
+  KEY `fk_billable_item_type_status_id` (`STATUS_ID`),
+  CONSTRAINT `fk_billable_item_type_status_id` FOREIGN KEY (`STATUS_ID`) REFERENCES `billable_item_type_status` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_billable_item_type_study_id` FOREIGN KEY (`STUDY_ID`) REFERENCES `study`.`study` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `work_request` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `STATUS_ID` int(11) DEFAULT NULL,
+  `RESEARCHER_ID` int(11) DEFAULT NULL,
+  `REQUESTED_DATE` date DEFAULT NULL,
+  `COMMENCED_DATE` date DEFAULT NULL,
+  `COMPLETED_DATE` date DEFAULT NULL,
+  `STUDY_ID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_work_request_researcher_id` (`RESEARCHER_ID`),
+  KEY `fk_work_request_status_id` (`STATUS_ID`),
+  CONSTRAINT `fk_work_request_researcher_id` FOREIGN KEY (`RESEARCHER_ID`) REFERENCES `researcher` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_work_request_status_id` FOREIGN KEY (`STATUS_ID`) REFERENCES `work_request_status` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `bilable_item` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `QUANTITY` int(11) DEFAULT NULL,
+  `COMMENCE_DATE` date DEFAULT NULL,
+  `TYPE` varchar(10) NOT NULL,
+  `STATUS_ID` int(11) DEFAULT NULL,
+  `REQUEST_ID` int(11) DEFAULT NULL,
+  `INVOICE` varchar(5) DEFAULT NULL,
+  `STUDY_ID` int(11) NOT NULL,
+  `BILLABLE_TYPE` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_bilable_item_billable_type` (`BILLABLE_TYPE`),
+  KEY `fk_bilable_item_request_id` (`REQUEST_ID`),
+  KEY `fk_bilable_item_status` (`STATUS_ID`),
+  CONSTRAINT `fk_bilable_item_billable_type` FOREIGN KEY (`BILLABLE_TYPE`) REFERENCES `billable_item_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bilable_item_request_id` FOREIGN KEY (`REQUEST_ID`) REFERENCES `work_request` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bilable_item_status` FOREIGN KEY (`STATUS_ID`) REFERENCES `billable_item_status` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `link_billable_item_subject` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `BILLABLE_ITEM_ID` int(11) DEFAULT NULL,
+  `SUBJECT_ID` int(11) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_link_billable_item_subject_bi_id` (`BILLABLE_ITEM_ID`),
+  CONSTRAINT `fk_link_billable_item_subject_bi_id` FOREIGN KEY (`BILLABLE_ITEM_ID`) REFERENCES `bilable_item` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
