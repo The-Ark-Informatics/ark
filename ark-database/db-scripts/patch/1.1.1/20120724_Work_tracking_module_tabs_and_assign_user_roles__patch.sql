e36bd960029ff73d50beb34516cc53441d3085d6
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-25 04:05:31 +0000

    Added CRUD permissions for Study Administrator (and removed Super Administrator from ark_module_role, since Super admin inherently sees everything)
    git-svn-id: svn://localhost/ARK/trunk@7145 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120724_Work_tracking_module_tabs_and_assign_user_roles__patch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120724_Work_tracking_module_tabs_and_assign_user_roles__patch.sql
@@ -79,14 +79,134 @@ INSERT INTO `study`.`ark_module_role`
 VALUES
 (
 (select ID from study.ark_module where LOWER(Name) = LOWER('Work')),
-(select ID from study.ark_role where LOWER(Name) = LOWER('Super Administrator'))
-),
-(
-(select ID from study.ark_module where LOWER(Name) = LOWER('Work')),
 (select ID from study.ark_role where LOWER(Name) = LOWER('Study Administrator'))
 );
 
 
+/* Insert CRUD permissions */
+INSERT INTO `study`.`ark_role_policy_template`
+(`ARK_ROLE_ID`,
+`ARK_MODULE_ID`,
+`ARK_FUNCTION_ID`,
+`ARK_PERMISSION_ID`)
+VALUES
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.researcher')),
+1
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.researcher')),
+2
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.researcher')),
+3
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.researcher')),
+4
+);
+
+INSERT INTO `study`.`ark_role_policy_template`
+(`ARK_ROLE_ID`,
+`ARK_MODULE_ID`,
+`ARK_FUNCTION_ID`,
+`ARK_PERMISSION_ID`)
+VALUES
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitemtype')),
+1
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitemtype')),
+2
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitemtype')),
+3
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitemtype')),
+4
+);
+
+INSERT INTO `study`.`ark_role_policy_template`
+(`ARK_ROLE_ID`,
+`ARK_MODULE_ID`,
+`ARK_FUNCTION_ID`,
+`ARK_PERMISSION_ID`)
+VALUES
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.workrequest')),
+1
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.workrequest')),
+2
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.workrequest')),
+3
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.workrequest')),
+4
+);
+
+INSERT INTO `study`.`ark_role_policy_template`
+(`ARK_ROLE_ID`,
+`ARK_MODULE_ID`,
+`ARK_FUNCTION_ID`,
+`ARK_PERMISSION_ID`)
+VALUES
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitem')),
+1
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitem')),
+2
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitem')),
+3
+),
+(
+(SELECT ID FROM study.ark_role WHERE name = 'Study Administrator'),
+(SELECT ID FROM study.ark_module WHERE LOWER(Name) = LOWER('Work')),
+(select ID from study.ark_function where LOWER(RESOURCE_KEY) = LOWER('tab.module.work.billableitem')),
+4
+);
 
 
 
