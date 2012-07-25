d6e32b3e78609c2c42f273b628d0dab86f2d8999
chrisellis <chrisellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-07-25 07:17:55 +0000

    Added use statement
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7149 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20120717_study_correspondence_schemaPatch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120717_study_correspondence_schemaPatch.sql
@@ -1,4 +1,5 @@
+USE study;
 DROP TABLE study.correspondence_audit_attachment;
-DROP TABLE link_correspondence_audit_person;
+DROP TABLE study.link_correspondence_audit_person;
 DROP TABLE study.correspondence_audit;
 DROP TABLE study.correspondence_attachment;
