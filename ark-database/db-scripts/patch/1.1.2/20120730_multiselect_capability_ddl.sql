a9c3db1d6fcd2589c3a0036feba35c435283dc60
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-07-31 08:33:24 +0000

    we want to allow the reuse of the same decoded stuff but in some cases make it multipleSelection and some cases not.  OK...this needs to go at custom_field display  instead of customfield
    git-svn-id: svn://localhost/ARK/trunk@7170 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.2/20120730_multiselect_capability_ddl.sql
+++ ark-database/db-scripts/patch/1.1.2/20120730_multiselect_capability_ddl.sql
@@ -1,3 +1,12 @@
+/**
+
+SO...we want to allow the reuse of the same encoded stuff but in some cases make it multi and some cases not.  OK...this needs to go at custom_field display  instead...
+
 ALTER TABLE `study`.`custom_field` ADD COLUMN `ALLOW_MULTIPLE_SELECTION` VARCHAR(45) NULL DEFAULT 0  AFTER `CUSTOM_FIELD_LABEL` ;
 
+ALTER TABLE `study`.`custom_field` DROP COLUMN `ALLOW_MULTIPLE_SELECTION`;
+
+*****/
+
+ALTER TABLE `study`.`custom_field_display` ADD COLUMN `ALLOW_MULTIPLE_SELECTION` TINYINT(1)  NULL DEFAULT 0  AFTER `REQUIRED_MESSAGE` ;
 
