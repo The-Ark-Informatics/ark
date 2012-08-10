20a11352121f6904cef9b54e1f0584c1461234c1
Thilina Ranaweera <tranaweera@gmail.com>
2012-08-10 03:19:22 +0000

    Modify the bilable_item table and add researcher recruitment costs - summary of account report
    git-svn-id: svn://localhost/ARK/trunk@7184 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120725_Work_tracking_module_schema_patch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120725_Work_tracking_module_schema_patch.sql
@@ -216,3 +216,37 @@ CREATE TABLE `link_billable_item_subject` (
 
 ALTER TABLE `admin`.`researcher` CHANGE COLUMN `INSTITUTE` `ORGANIZATION` VARCHAR(50) NULL DEFAULT NULL  ;
 
+-- Additional changes for the billable_item and researcher tables
+
+ALTER TABLE `admin`.`bilable_item` ADD COLUMN `TOTAL_COST` DOUBLE NULL AFTER `BILLABLE_TYPE`;
+
+ALTER TABLE `admin`.`bilable_item` ADD COLUMN `ITEM_COST` DOUBLE NULL  AFTER `TOTAL_COST` ;
+
+ALTER TABLE `admin`.`bilable_item` ADD COLUMN `ATTACHMENT_FILENAME` VARCHAR(45) NULL  AFTER `TOTAL_COST` , ADD COLUMN `ATTACHMENT_PAYLOAD` LONGBLOB NULL  AFTER `ATTACHMENT_FILENAME` ;
+
+ALTER TABLE `admin`.`researcher` ADD COLUMN `TITLE_TYPE_ID` INT(11) NULL  AFTER `STUDY_ID` ;
+
+ALTER TABLE `admin`.`bilable_item` ADD COLUMN `TOTAL_GST` DOUBLE NULL  AFTER `ATTACHMENT_PAYLOAD` ;
+
+-- Add the Researcher Recruitment Costs - Summary of Account report
+INSERT INTO `reporting`.`report_template`
+(`NAME`,
+`DESCRIPTION`,
+`TEMPLATE_PATH`,
+`MODULE_ID`,
+`FUNCTION_ID`)
+VALUES
+(
+'Work Researcher Cost Report',
+'This report lists the total billable item type costs realated to a researcher',
+'ResearcherCostReport.jrxml',
+(select ID from study.ark_module where name='Work Tracking'),
+(select ID from study.ark_function where name='BILLABLE_ITEM')
+);
+
+-- Rename the Work module to Work Tracking
+UPDATE `study`.`ark_module` SET `NAME`='Work Tracking' WHERE `NAME`='Work';
+
+
+
+
