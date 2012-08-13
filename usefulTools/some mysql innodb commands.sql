77744bca5e690e01ffd0c99fdd358015c2d95e3e
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-08-13 02:53:51 +0000

    some useful sql commands
    git-svn-id: svn://localhost/ARK/trunk@7194 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- usefulTools/some mysql innodb commands.sql
+++ usefulTools/some mysql innodb commands.sql
@@ -3,5 +3,12 @@
 # SHOW INNODB STATUS
 
 #  show lock
+-- '327769', 'SLP#67', '1', '473026', '2', '16442', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '20', '17', '4', '0', NULL, NULL, NULL, '0'
 
+-- select * from lims.biocollection where (id>16000 OR NAME ='20111118112010');
 
+select * from lims.bio_transaction where id>260000;
+
+select * from lims.biospecimen where id>327000;
+
+select * from lims.inv_cell where id>655500
