fbd0e8707bf44aada2577f325bd0018534e68e0b
Thilina Ranaweera <tranaweera@gmail.com>
2012-08-21 05:52:36 +0000

    Change the work tracking cost report search year by option to date range
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7266 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-report/src/main/java/au/org/theark/report/model/vo/StudyCostReportVo.java
+++ ark-report/src/main/java/au/org/theark/report/model/vo/StudyCostReportVo.java
@@ -1,5 +1,7 @@
 package au.org.theark.report.model.vo;
 
+import java.util.Calendar;
+import java.util.Date;
 
 public class StudyCostReportVo extends GenericReportViewVO {
 	/**
@@ -9,25 +11,61 @@ public class StudyCostReportVo extends GenericReportViewVO {
 	private Long studyId;
 	private String invoice;
 	private String year;
+	
+	private Date fromDate;
+	private Date toDate;
+	
+	public StudyCostReportVo() {
+		
+		Calendar firstDayCalendar=Calendar.getInstance();
+		firstDayCalendar.set(Calendar.DAY_OF_YEAR,1);
+		fromDate= firstDayCalendar.getTime();
+		
+		Calendar lastDayCalendar=Calendar.getInstance();
+		lastDayCalendar.set(Calendar.MONTH,11);
+		lastDayCalendar.set(Calendar.DAY_OF_MONTH,31);
+		toDate= lastDayCalendar.getTime();	
+	}
+	
+	
 	public Long getStudyId() {
 		return studyId;
 	}
+	
 	public void setStudyId(Long studyId) {
 		this.studyId = studyId;
 	}
+	
 	public String getInvoice() {
 		return invoice;
 	}
+	
 	public void setInvoice(String invoice) {
 		this.invoice = invoice;
 	}
+	
 	public String getYear() {
 		return year;
 	}
+	
 	public void setYear(String year) {
 		this.year = year;
 	}
-	
-	
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}	
 
 }
