f0d1c882477c92170214325b6a84db8f38d7b393
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-08-22 03:53:48 +0000

    Sets up studyList for user into LimsVO model (used for access permission checks)
    git-svn-id: svn://localhost/ARK/trunk@7277 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/InventoryContainerPanel.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/inventory/panel/InventoryContainerPanel.java
@@ -1,16 +1,32 @@
 package au.org.theark.lims.web.component.inventory.panel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import au.org.theark.core.exception.EntityNotFoundException;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.model.study.entity.ArkUser;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.ArkUserVO;
 import au.org.theark.lims.model.vo.LimsVO;
 import au.org.theark.lims.web.component.inventory.form.ContainerForm;
 
 public class InventoryContainerPanel extends AbstractInventoryPanel<LimsVO> {
-
 	private static final long			serialVersionUID	= -8575670114976786294L;
-
+	private static Logger				log	= LoggerFactory.getLogger(InventoryContainerPanel.class);
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>	iArkCommonService;
+	
 	private ContainerForm				containerForm;
 	private InventoryTreePanel			treePanel;
 	private EmptyPanel					detailPanel;
@@ -20,6 +36,7 @@ public class InventoryContainerPanel extends AbstractInventoryPanel<LimsVO> {
 
 		/* Initialise the CPM */
 		cpModel = new CompoundPropertyModel<LimsVO>(new LimsVO());
+		setUserStudyList();
 
 		/* Bind the CPM to the Form */
 		containerForm = new ContainerForm("containerForm", cpModel);
@@ -33,6 +50,24 @@ public class InventoryContainerPanel extends AbstractInventoryPanel<LimsVO> {
 		add(containerForm);
 	}
 	
+	private void setUserStudyList() {
+		List<Study> studyListForUser = new ArrayList<Study>(0);
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			ArkUser arkUser = iArkCommonService.getArkUser(currentUser.getPrincipal().toString());
+			ArkUserVO arkUserVo = new ArkUserVO();
+			arkUserVo.setArkUserEntity(arkUser);
+			
+			Long sessionArkModuleId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.ARK_MODULE_KEY);
+			ArkModule arkModule = iArkCommonService.getArkModuleById(sessionArkModuleId);
+			studyListForUser = iArkCommonService.getStudyListForUserAndModule(arkUserVo, arkModule);
+			cpModel.getObject().setStudyList(studyListForUser);
+		}
+		catch (EntityNotFoundException e) {
+			log.error(e.getMessage());
+		}
+	}
+
 	/**
 	 * Initialise the tree
 	 * @return
