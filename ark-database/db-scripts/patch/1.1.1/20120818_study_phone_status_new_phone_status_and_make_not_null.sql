66830356d4513b536bc530651e7dcfb63266a687
chrisellis <chrisellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-08-22 05:45:35 +0000

    renamed to .sql
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7281 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-database/db-scripts/patch/1.1.1/20120818_study_phone_status_new_phone_status_and_make_not_null.sql
@@ -0,0 +1,15 @@
+INSERT INTO `study`.`phone_status` (`ID`, `NAME`, `DESCRIPTION`) VALUES (0, 'Unknown', 'Status not known, this will be the default if no status provided');
+
+ALTER TABLE `study`.`phone` DROP FOREIGN KEY `phone_ibfk_3` ;
+ALTER TABLE `study`.`phone` CHANGE COLUMN `PHONE_STATUS_ID` `PHONE_STATUS_ID` INT(11) NOT NULL DEFAULT 0  , 
+  ADD CONSTRAINT `phone_ibfk_3`
+  FOREIGN KEY (`PHONE_STATUS_ID` )
+  REFERENCES `study`.`phone_status` (`ID` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+  
+-- just a bit over the top and unnecessary.  deleting
+ALTER TABLE `study`.`phone` 
+DROP INDEX `AREA_CODE` ;
+ 
