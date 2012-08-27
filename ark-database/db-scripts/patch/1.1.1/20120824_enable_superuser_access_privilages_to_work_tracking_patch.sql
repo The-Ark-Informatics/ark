e9f548212965c84636b1bb8935780d290340cde4
Thilina Ranaweera <tranaweera@gmail.com>
2012-08-27 06:52:16 +0000

    Modify the insert query for all ark instances
    git-svn-id: svn://localhost/ARK/trunk@7303 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120824_enable_superuser_access_privilages_to_work_tracking_patch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120824_enable_superuser_access_privilages_to_work_tracking_patch.sql
@@ -1,1 +1,6 @@
-INSERT INTO `study`.`ark_user_role` (`ARK_USER_ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`) VALUES (1, 1, 8);
+INSERT INTO `study`.`ark_user_role` 
+(`ARK_USER_ID`, `ARK_ROLE_ID`, `ARK_MODULE_ID`) 
+VALUES ((select id from study.ark_user where ldap_user_name='arksuperuser@ark.org.au'), 
+	(select id from study.ark_role where name= 'Super Administrator'), 
+	(select id from study.ark_module where name='Work Tracking')
+);
