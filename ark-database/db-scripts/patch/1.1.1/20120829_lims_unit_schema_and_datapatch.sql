69feacfd419c2ae24b71e82f0ed1ec200c9c6c75
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-08-29 07:10:08 +0000

    added patch for default biospecimen unit
    git-svn-id: svn://localhost/ARK/trunk@7311 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20120829_lims_unit_schema_and_datapatch.sql
@@ -0,0 +1,10 @@
+INSERT INTO lims.unit (ID, NAME) VALUES (0, 'unit');
+UPDATE lims.unit SET ID = 0 WHERE NAME= 'unit';
+UPDATE lims.biospecimen SET UNIT_ID = 0 where unit_id is null
+
+ALTER TABLE `lims`.`biospecimen` DROP FOREIGN KEY `fk_biospecimen_unit` ;
+ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `UNIT_ID` `UNIT_ID` INT(11) NULL DEFAULT '0'  , 
+  ADD CONSTRAINT `fk_biospecimen_unit`
+  FOREIGN KEY (`UNIT_ID` )
+  REFERENCES `lims`.`unit` (`ID` )
+  ON UPDATE CASCADE;
