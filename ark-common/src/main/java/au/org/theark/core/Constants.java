40c22b86b0e3df65bff677d12ddd79f4f5b2baef
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-09-03 07:59:39 +0000

    commit initial coding of multiselect upload validation (not necessarily complete...can't test right now).  Also commit a fix to non-null multiselect enforcement issue  in db script.
    git-svn-id: svn://localhost/ARK/trunk@7335 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/Constants.java
+++ ark-common/src/main/java/au/org/theark/core/Constants.java
@@ -151,6 +151,7 @@ public class Constants {
 	public static final String FIELD_TYPE_DATE = "DATE";
 	public static final String DISCRETE_RANGE_TOKEN = ",";
 	public static final String ENCODED_VALUES_TOKEN = ";";
+	public static final String ENCODED_VALUES_FROM_TELEFORMS_TOKEN_SPACE = " ";
 	public static final String ENCODED_VALUES_SEPARATOR = "=";
 
 	/* Button names/labels */
