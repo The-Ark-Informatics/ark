bf22f422f3f2c94703bb1281665a4d275e269bf4
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-09-10 07:21:59 +0000

    Add new fields to db and entities to require explicit use of autogeneration logic in biospecimen and biocollections as part of ARK-790
    Tweaks the existing data to use explicit boolean about autogen of bio-stuff
    
    git-svn-id: svn://localhost/ARK/trunk@7362 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120910_study_add_autogenerate_biospecimen_and_biocollection_booleans.sql
+++ ark-database/db-scripts/patch/1.1.1/20120910_study_add_autogenerate_biospecimen_and_biocollection_booleans.sql
@@ -1,2 +1,13 @@
 ALTER TABLE `study`.`study` ADD COLUMN `AUTO_GENERATE_BIOSPECIMENUID` TINYINT NOT NULL DEFAULT 0  AFTER `PARENT_ID` , ADD COLUMN `AUTO_GENERATE_BIOCOLLECTIONUID` TINYINT NOT NULL DEFAULT 0  AFTER `AUTO_GENERATE_BIOSPECIMENUID` ;
 
+-- now that we have the new structure, need to change the current data to guess autogenerate based on existing data.  So if they have the pattern, then just give them auto-generate.  It's a good enough guess for now as that seems to be how the app logic is guessing.
+update study.study s
+set auto_generate_biocollectionuid = 1
+where s.id in (select study_id from biocollection)
+and s.id in (select study_id from lims.biocollectionuid_template);
+
+update study.study s
+set auto_generate_biospecimenuid = 1
+where s.id in (select study_id from biospecimen)
+and s.id in (select study_id from lims.biospecimenuid_template);
+
