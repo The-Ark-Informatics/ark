4fcd4a64d9bce8f15f1d14bd0ac64d1c12881465
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-09-10 00:49:40 +0000

    syntax ;
    git-svn-id: svn://localhost/ARK/trunk@7353 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120829_lims_unit_schema_and_datapatch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120829_lims_unit_schema_and_datapatch.sql
@@ -1,6 +1,6 @@
 INSERT INTO lims.unit (ID, NAME) VALUES (0, 'unit');
 UPDATE lims.unit SET ID = 0 WHERE NAME= 'unit';
-UPDATE lims.biospecimen SET UNIT_ID = 0 where unit_id is null
+UPDATE lims.biospecimen SET UNIT_ID = 0 where unit_id is null;
 
 ALTER TABLE `lims`.`biospecimen` DROP FOREIGN KEY `fk_biospecimen_unit` ;
 ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `UNIT_ID` `UNIT_ID` INT(11) NULL DEFAULT '0'  , 
