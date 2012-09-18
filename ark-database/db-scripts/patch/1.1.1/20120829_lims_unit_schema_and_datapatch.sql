cd9e474b03aa74075e25fd396ab92cf4f0adb2fc
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-09-18 02:53:05 +0000

    removed quotes form default value
    git-svn-id: svn://localhost/ARK/trunk@7389 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120829_lims_unit_schema_and_datapatch.sql
+++ ark-database/db-scripts/patch/1.1.1/20120829_lims_unit_schema_and_datapatch.sql
@@ -3,7 +3,7 @@ UPDATE lims.unit SET ID = 0 WHERE NAME= 'unit';
 UPDATE lims.biospecimen SET UNIT_ID = 0 where unit_id is null;
 
 ALTER TABLE `lims`.`biospecimen` DROP FOREIGN KEY `fk_biospecimen_unit` ;
-ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `UNIT_ID` `UNIT_ID` INT(11) NULL DEFAULT '0'  , 
+ALTER TABLE `lims`.`biospecimen` CHANGE COLUMN `UNIT_ID` `UNIT_ID` INT(11) NULL DEFAULT 0  , 
   ADD CONSTRAINT `fk_biospecimen_unit`
   FOREIGN KEY (`UNIT_ID` )
   REFERENCES `lims`.`unit` (`ID` )
