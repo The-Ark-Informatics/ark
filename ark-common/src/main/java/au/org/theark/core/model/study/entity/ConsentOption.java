e39f2a05d4792dbe77350d4acc0d1e989ecfc01d
Thilina Ranaweera <tranaweera@gmail.com>
2012-09-20 06:15:03 +0000

    ARK-621 implement equals() and hashcode() methods in entity classes
    git-svn-id: svn://localhost/ARK/trunk@7397 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/ConsentOption.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/ConsentOption.java
@@ -63,4 +63,35 @@ public class ConsentOption implements Serializable {
 		this.name = name;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsentOption other = (ConsentOption) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
