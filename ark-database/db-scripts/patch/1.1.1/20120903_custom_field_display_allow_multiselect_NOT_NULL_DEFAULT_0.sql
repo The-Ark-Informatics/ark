4957747cb9778a220a441a6b7066311e7d3298cb
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-09-27 01:53:58 +0000

    Added update statement to fix any data where allow_mutliselect was in fact null
    git-svn-id: svn://localhost/ARK/trunk@7429 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20120903_custom_field_display_allow_multiselect_NOT_NULL_DEFAULT_0.sql
+++ ark-database/db-scripts/patch/1.1.1/20120903_custom_field_display_allow_multiselect_NOT_NULL_DEFAULT_0.sql
@@ -1,2 +1,5 @@
+USE study;
 ALTER TABLE `study`.`custom_field_display` CHANGE COLUMN `ALLOW_MULTIPLE_SELECTION` `ALLOW_MULTIPLE_SELECTION` TINYINT(1) NOT NULL DEFAULT 0  ;
+UPDATE study.custom_field_display SET allow_multiple_selection = 0 WHERE allow_multiple_selection is null;
+
 
