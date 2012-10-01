dfe8bfef7c4ae2d7ed8155d45c1fe7a98a6cce90
Thilina Ranaweera <tranaweera@gmail.com>
2012-10-01 07:10:13 +0000

    ARK-851 Clean the researcher role list and add Principal researcher role to top of the new researcher role list.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7454 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-database/db-scripts/patch/1.1.1/20121001_delete_the_researcher_roles_and_add_new_researcher_role_list_patch.sql
@@ -0,0 +1,7 @@
+delete from admin.researcher_role
+
+INSERT INTO `admin`.`researcher_role` (`NAME`) VALUES ('Principal Investigator');
+INSERT INTO `admin`.`researcher_role` (`NAME`) VALUES ('Chief Investigator');
+INSERT INTO `admin`.`researcher_role` (`NAME`) VALUES ('Assoc Investigator');
+INSERT INTO `admin`.`researcher_role` (`NAME`) VALUES ('Other Investigator');
+INSERT INTO `admin`.`researcher_role` (`NAME`) VALUES ('Research Assistant');
