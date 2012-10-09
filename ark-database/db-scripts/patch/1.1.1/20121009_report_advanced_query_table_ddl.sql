85c61f1ff40bfff3b0ded46c867ca599903240d4
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-09 06:10:03 +0000

    Further advancements in advance filters/queries/joins/etc, adding ddl now
    git-svn-id: svn://localhost/ARK/trunk@7506 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20121009_report_advanced_query_table_ddl.sql
+++ ark-database/db-scripts/patch/1.1.1/20121009_report_advanced_query_table_ddl.sql
@@ -21,3 +21,25 @@ CREATE  TABLE `reporting`.`search` (
 ALTER TABLE `reporting`.`search` 
 ADD INDEX `fk_search_query_grouping` (`TOP_LEVEL_GROUPING_ID` ASC) ;
 
+
+
+
+CREATE  TABLE `reporting`.`custom_field_display_search` (
+  `ID` INT NOT NULL ,
+  `CUSTOM_FIELD_DISPLAY_ID` INT NULL ,
+  `SEARCH_ID` INT NULL ,
+  PRIMARY KEY (`ID`) ,
+  INDEX `fk_cfds_custom_field_display` (`CUSTOM_FIELD_DISPLAY_ID` ASC) ,
+  INDEX `fk_cfds_search` (`SEARCH_ID` ASC) ,
+  CONSTRAINT `fk_cfds_custom_field_display`
+    FOREIGN KEY (`CUSTOM_FIELD_DISPLAY_ID` )
+    REFERENCES `study`.`custom_field_display` (`ID` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_cfds_search`
+    FOREIGN KEY (`SEARCH_ID` )
+    REFERENCES `reporting`.`search` (`ID` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = 'many to many join custom_field_display and search';
+
