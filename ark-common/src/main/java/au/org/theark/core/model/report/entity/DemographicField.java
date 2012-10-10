18df2dce3a2ab2d199106768d0d116c2e6714fb3
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-10 05:26:50 +0000

    Further advancements in advance filters/queries/joins/etc
    git-svn-id: svn://localhost/ARK/trunk@7514 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/DemographicField.java
@@ -0,0 +1,49 @@
+package au.org.theark.core.model.report.entity;
+
+import au.org.theark.core.model.study.entity.FieldType;
+
+public class DemographicField {
+	//the alternative being to hit the mysql system tables somehow with a defined list of which fields in certain tables
+	
+	private Long id;
+	private String entity;
+	private String underlyingFieldName;
+	private String publicFieldName;
+	private FieldType fieldType;
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+	public String getEntity() {
+		return entity;
+	}
+	
+	public void setUnderlyingFieldName(String underlyingFieldName) {
+		this.underlyingFieldName = underlyingFieldName;
+	}
+	public String getUnderlyingFieldName() {
+		return underlyingFieldName;
+	}
+	
+	public void setPublicFieldName(String publicFieldName) {
+		this.publicFieldName = publicFieldName;
+	}
+	public String getPublicFieldName() {
+		return publicFieldName;
+	}
+	
+	public void setFieldType(FieldType fieldType) {
+		this.fieldType = fieldType;
+	}
+	public FieldType getFieldType() {
+		return fieldType;
+	}
+	
+}
