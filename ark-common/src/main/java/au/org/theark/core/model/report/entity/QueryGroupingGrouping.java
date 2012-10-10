5afdcda9931049d50f590f10774d4eee7e892d5e
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-10 08:27:04 +0000

    Further advancements in advance filters/queries/joins/etc, adding ddl now
    git-svn-id: svn://localhost/ARK/trunk@7523 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGroupingGrouping.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGroupingGrouping.java
@@ -3,7 +3,6 @@ package au.org.theark.core.model.report.entity;
 public class QueryGroupingGrouping {
 	QueryGrouping 	parent;
 	QueryGrouping	leftGrouping;
-		
 	JoinType 		relationshipToNextFilter;
 	Long			filterPrecedence; //or order...but didnt want to confuse with order by
 
