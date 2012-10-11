59fc13ccf0f8101b099fd18f0e8cb1da9d970377
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-10-11 04:00:13 +0000

    Further advancements in advance filters/queries/joins/etc, adding ddl now
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7529 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryFilterGrouping.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryFilterGrouping.java
@@ -1,9 +1,43 @@
 package au.org.theark.core.model.report.entity;
 
 public class QueryFilterGrouping {
-	QueryGrouping 	parent;
-	QueryFilter 	leftFilter;
-	JoinType 		relationshipToNextFilter;
-	Long			filterPrecedence; //or order...but didnt want to confuse with order by
+	private Long id;
+	private QueryGrouping 	parent;
+	private QueryFilter 	leftFilter;
+	private JoinType 		joinToNextFilter;
+	private Long			filterPrecedence; //or order...but didn't want to confuse with order by
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public QueryGrouping getParent() {
+		return parent;
+	}
+	public void setParent(QueryGrouping parent) {
+		this.parent = parent;
+	}
+	public QueryFilter getLeftFilter() {
+		return leftFilter;
+	}
+	public void setLeftFilter(QueryFilter leftFilter) {
+		this.leftFilter = leftFilter;
+	}
+	public JoinType getJoinToNextFilter() {
+		return joinToNextFilter;
+	}
+	public void setRelationshipToNextFilter(JoinType joinToNextFilter) {
+		this.joinToNextFilter = joinToNextFilter;
+	}
+	public Long getFilterPrecedence() {
+		return filterPrecedence;
+	}
+	public void setFilterPrecedence(Long filterPrecedence) {
+		this.filterPrecedence = filterPrecedence;
+	}
 
 }
