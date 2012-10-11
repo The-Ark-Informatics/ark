60ecf339f38f86d6f357a40bf5836bec09fc8564
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-11 04:00:13 +0000

    Further advancements in advance filters/queries/joins/etc, adding ddl now
    git-svn-id: svn://localhost/ARK/trunk@7529 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryFilter.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryFilter.java
@@ -6,22 +6,81 @@ public class QueryFilter {
 //	QueryFilter leftQueryFilter;	//potentially link to another query to combine rules on...its more explicit than relying on brackets.
 //	QueryFilter rightQueryFilter;	//potentially link to another query to combine rules on...its more explicit than relying on brackets.
 //	JoinType filterJoin; //AND OR  
-	
+	private Long id;
 	// which contraints field, operator, value, nullableSecondValue (eg in between),
-	Prefix prefix; //eg NOT, IN, 
+	private Prefix prefix; //eg NOT, IN, 
 //	int numberOfLeftBracketsBeforePrecursor;
 //	int numberOfLeftBracketsAfterPrecursor;
 //	int numberOfRightBracketsBeforeJoin;
 //	int numberOfRightBracketsAfterJoin;
 	
-	DemographicField demographicField;	
-	CustomFieldDisplay customFieldDisplay;
-	String value;
-	Operator operator;
-	String secondValue; // for between and similar operators
+	private DemographicField demographicField;	
+	private CustomFieldDisplay customFieldDisplay;
+	private String value;
+	private Operator operator;
+	private String secondValue; // for between and similar operators
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 	public QueryFilter(){
 //		join.
 	}
+
+
+	public DemographicField getDemographicField() {
+		return demographicField;
+	}
+
+
+	public void setDemographicField(DemographicField demographicField) {
+		this.demographicField = demographicField;
+	}
+
+	public Prefix getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(Prefix prefix) {
+		this.prefix = prefix;
+	}
+
+	public CustomFieldDisplay getCustomFieldDisplay() {
+		return customFieldDisplay;
+	}
+
+	public void setCustomFieldDisplay(CustomFieldDisplay customFieldDisplay) {
+		this.customFieldDisplay = customFieldDisplay;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Operator getOperator() {
+		return operator;
+	}
+
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+
+	public String getSecondValue() {
+		return secondValue;
+	}
+
+	public void setSecondValue(String secondValue) {
+		this.secondValue = secondValue;
+	}
+
+
 }
