c85e1a5e69ab5f78ffb01429e45ca383a10aa08c
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-11 06:29:47 +0000

    Further advancements in advance filters/queries/joins/etc, adding ddl now
    git-svn-id: svn://localhost/ARK/trunk@7539 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGrouping.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGrouping.java
@@ -1,21 +1,40 @@
 package au.org.theark.core.model.report.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import au.org.theark.core.model.Constants;
+
+@Entity
+@Table(name = "guery_grouping", schema = Constants.REPORT_SCHEMA)
 public class QueryGrouping {
 	private Long id;
 	private String name;
 
+	@Id
+	@SequenceGenerator(name = "query_grouping_generator", sequenceName = "QUERY_GROUPING_SEQ")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "query_grouping_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return id;
+	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Long getId() {
-		return id;
+
+
+	@Column(name = "NAME", length = 45)
+	public String getName() {
+		return name;
 	}
 	
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getName() {
-		return name;
-	}
-	
 }
