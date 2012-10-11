5c7d0d58778fc166a38c096e925088c960d4bafe
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-11 04:46:30 +0000

    Further advancements in advance filters/queries/joins/etc
    git-svn-id: svn://localhost/ARK/trunk@7531 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGroupingGrouping.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/QueryGroupingGrouping.java
@@ -1,9 +1,40 @@
 package au.org.theark.core.model.report.entity;
 
 public class QueryGroupingGrouping {
-	QueryGrouping 	parent;
-	QueryGrouping	leftGrouping;
-	JoinType 		relationshipToNextFilter;
-	Long			filterPrecedence; //or order...but didnt want to confuse with order by
+	private	Long id;
+	private QueryGrouping 	parent;
+	private QueryGrouping	leftGrouping;
+	private JoinType 		relationshipToNextFilter;
+	private Long			filterPrecedence; //or order...but didn't want to confuse with order by
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public QueryGrouping getParent() {
+		return parent;
+	}
+	public void setParent(QueryGrouping parent) {
+		this.parent = parent;
+	}
+	public QueryGrouping getLeftGrouping() {
+		return leftGrouping;
+	}
+	public void setLeftGrouping(QueryGrouping leftGrouping) {
+		this.leftGrouping = leftGrouping;
+	}
+	public JoinType getRelationshipToNextFilter() {
+		return relationshipToNextFilter;
+	}
+	public void setRelationshipToNextFilter(JoinType relationshipToNextFilter) {
+		this.relationshipToNextFilter = relationshipToNextFilter;
+	}
+	public Long getFilterPrecedence() {
+		return filterPrecedence;
+	}
+	public void setFilterPrecedence(Long filterPrecedence) {
+		this.filterPrecedence = filterPrecedence;
+	}
 
 }
