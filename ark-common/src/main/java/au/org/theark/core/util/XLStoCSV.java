9f150b023b1bae96229364d9ab53a178474ef061
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-12 04:59:55 +0000

    Amended CustomFieldImportValidator to correctly check for incorrect fieldtype (exception thrown when getting, but was never caught/handled, thus appeared all ok to user Moved convertXlsToCsv into common accessible class (reduces lots of repeated code) Added XLStoCSV as common class
    git-svn-id: svn://localhost/ARK/trunk@7547 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/util/XLStoCSV.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.core.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
+import org.apache.wicket.util.io.ByteArrayOutputStream;
+
+public class XLStoCSV {
+	char delimiterCharacter = ',';
+	
+	public XLStoCSV(char delimiterCharacter){
+		this.delimiterCharacter = delimiterCharacter;
+	}
+	
+	/**
+	 * Return the inputstream of the converted workbook as csv
+	 * 
+	 * @return inputstream of the converted workbook as csv
+	 */
+	public InputStream convertXlsToCsv(Workbook w) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			OutputStreamWriter osw = new OutputStreamWriter(out);
+			// Gets first sheet from workbook
+			Sheet s = w.getSheet(0);
+			Cell[] row = null;
+
+			for (int i = 0; i < s.getRows(); i++) {
+				row = s.getRow(i);
+
+				if (row.length > 0) {
+					osw.write(row[0].getContents());
+					for (int j = 1; j < row.length; j++) {
+						osw.write(delimiterCharacter);
+						
+						if (row[j].getContents().contains(",")) {  
+							osw.write('"');  
+							osw.write(row[j].getContents());  
+							osw.write('"');  
+						} else {  
+               	   osw.write(row[j].getContents());  
+						}
+					}
+				}
+				osw.write("\n");
+			}
+
+			osw.flush();
+			osw.close();
+		}
+		catch (UnsupportedEncodingException e) {
+			System.err.println(e.toString());
+		}
+		catch (IOException e) {
+			System.err.println(e.toString());
+		}
+		catch (Exception e) {
+			System.err.println(e.toString());
+		}
+		return new ByteArrayInputStream(out.toByteArray());
+	}
+}
