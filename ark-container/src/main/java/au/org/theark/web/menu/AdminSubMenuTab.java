8dff957118a7d1f900b311ef4b830f4733bb8aad
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-10-23 05:28:03 +0000

    Refactored all menus into the one menu package in ark-container. Allows moving/cloning of functions between modules, for improved useability ARK-848
    git-svn-id: svn://localhost/ARK/trunk@7637 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-container/src/main/java/au/org/theark/web/menu/AdminSubMenuTab.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.web.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.extensions.markup.html.tabs.ITab;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import au.org.theark.admin.web.component.activitymonitor.ActivityMonitorContainerPanel;
+import au.org.theark.admin.web.component.function.FunctionContainerPanel;
+import au.org.theark.admin.web.component.module.ModuleContainerPanel;
+import au.org.theark.admin.web.component.modulefunction.ModuleFunctionContainerPanel;
+import au.org.theark.admin.web.component.modulerole.ModuleRoleContainerPanel;
+import au.org.theark.admin.web.component.role.RoleContainerPanel;
+import au.org.theark.admin.web.component.rolePolicy.RolePolicyContainerPanel;
+import au.org.theark.core.model.study.entity.ArkFunction;
+import au.org.theark.core.model.study.entity.ArkModule;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.web.component.menu.AbstractArkTabPanel;
+import au.org.theark.core.web.component.tabbedPanel.ArkAjaxTabbedPanel;
+
+public class AdminSubMenuTab extends AbstractArkTabPanel {
+
+	private static final long			serialVersionUID	= 2808674930679468072L;
+	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService<Void>	iArkCommonService;
+	private List<ITab>					moduleSubTabsList	= new ArrayList<ITab>();
+
+	public AdminSubMenuTab(String id) {
+		super(id);
+		buildTabs();
+	}
+
+	public void buildTabs() {
+		ArkModule arkModule = iArkCommonService.getArkModuleByName(au.org.theark.core.Constants.ARK_MODULE_ADMIN);
+		List<ArkFunction> arkFunctionList = iArkCommonService.getModuleFunction(arkModule);
+
+		// TODO: Shoud admin sub-menus really access the database to determine their visibility?
+		for (final ArkFunction arkFunction : arkFunctionList) {
+			AbstractTab tab = new AbstractTab(new StringResourceModel(arkFunction.getResourceKey(), this, null)) {
+				/**
+				 * 
+				 */
+				private static final long	serialVersionUID	= 5972079308171290188L;
+
+				@Override
+				public Panel getPanel(final String panelId) {
+					return panelToReturn(arkFunction, panelId);
+				}
+			};
+			moduleSubTabsList.add(tab);
+		}
+		
+		// Add a new tab for activity monitoring
+		AbstractTab tab = new AbstractTab(new Model<String>("Activity Monitoring")){
+
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			public Panel getPanel(String panelId) {
+				Panel panel = new ActivityMonitorContainerPanel(panelId);
+				return panel;
+			}
+			
+		};
+		moduleSubTabsList.add(tab);
+
+		ArkAjaxTabbedPanel moduleTabbedPanel = new ArkAjaxTabbedPanel("adminSubMenus", moduleSubTabsList);
+		add(moduleTabbedPanel);
+	}
+
+	protected Panel panelToReturn(ArkFunction arkFunction, String panelId) {
+		Panel panelToReturn = null;
+
+		// Clear cache to determine permissions
+		processAuthorizationCache(au.org.theark.core.Constants.ARK_MODULE_ADMIN, arkFunction);
+
+		if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MODULE)) {
+			ModuleContainerPanel containerPanel = new ModuleContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_FUNCTION)) {
+			FunctionContainerPanel containerPanel = new FunctionContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MODULE_FUNCTION)) {
+			ModuleFunctionContainerPanel containerPanel = new ModuleFunctionContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_ROLE)) {
+			RoleContainerPanel containerPanel = new RoleContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_MODULE_ROLE)) {
+			ModuleRoleContainerPanel containerPanel = new ModuleRoleContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+		else if (arkFunction.getName().equalsIgnoreCase(au.org.theark.core.Constants.FUNCTION_KEY_VALUE_ROLE_POLICY_TEMPLATE)) {
+			RolePolicyContainerPanel containerPanel = new RolePolicyContainerPanel(panelId);
+			panelToReturn = containerPanel;
+		}
+
+		return panelToReturn;
+	}
+}
---
+++ ark-container/src/main/java/au/org/theark/web/menu/ReportSubMenuTab.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.web.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.extensions.markup.html.tabs.ITab;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+import au.org.theark.core.vo.MenuModule;
+import au.org.theark.core.web.component.tabbedPanel.ArkAjaxTabbedPanel;
+import au.org.theark.report.web.Constants;
+import au.org.theark.report.web.component.viewReport.ReportContainerPanel;
+
+public class ReportSubMenuTab extends Panel {
+
+	private static final long	serialVersionUID	= -3695404298701886701L;
+	private List<ITab>			moduleSubTabsList;
+
+	public ReportSubMenuTab(String id) {
+		super(id);
+		moduleSubTabsList = new ArrayList<ITab>();
+		buildTabs();
+	}
+
+	public void buildTabs() {
+		List<MenuModule> moduleTabs = new ArrayList<MenuModule>();
+
+		// This way we can get the menus from the back-end. We should source this data from a table in the backend and wrap it up in a class like this
+		MenuModule menuModule = new MenuModule();
+		menuModule.setModuleName(Constants.REPORT_DETAIL);
+		menuModule.setResourceKey(Constants.TAB_MODULE_REPORT_DETAIL);
+		moduleTabs.add(menuModule);
+
+		for (final MenuModule moduleName : moduleTabs) {
+			moduleSubTabsList.add(new AbstractTab(new Model<String>(getLocalizer().getString(moduleName.getResourceKey(), ReportSubMenuTab.this, moduleName.getModuleName()))) {
+				/**
+				 * 
+				 */
+				private static final long	serialVersionUID	= -7414890128705025350L;
+
+				public boolean isVisible() {
+					// Reporting tab always visible
+					return true;
+				}
+
+				@Override
+				public Panel getPanel(String panelId) {
+					Panel panelToReturn = null;// Set up a common tab that will be accessible for all users
+
+					if (moduleName.getModuleName().equalsIgnoreCase(Constants.REPORT_DETAIL)) {
+						ReportContainerPanel reportContainerPanel = new ReportContainerPanel(panelId);
+						reportContainerPanel.initialisePanel();
+						panelToReturn = reportContainerPanel;
+					}
+
+					return panelToReturn;
+				};
+			});
+		}
+
+		ArkAjaxTabbedPanel moduleTabbedPanel = new ArkAjaxTabbedPanel(Constants.REPORT_SUBMENU, moduleSubTabsList);
+		add(moduleTabbedPanel);
+	}
+}
