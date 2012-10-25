c1f38812da7fb2165c46effdab6a3176e35bf8be
chrisellis <chrisellis@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-10-25 09:02:19 +0000

    altered hashCode to prevent exception during save of module_function
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7661 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/model/study/entity/ArkFunction.java
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/ArkFunction.java
@@ -99,8 +99,6 @@ public class ArkFunction implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((arkFunctionType == null) ? 0 : arkFunctionType.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
