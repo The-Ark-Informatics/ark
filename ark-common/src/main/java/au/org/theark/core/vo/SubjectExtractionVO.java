71997a3bc4a568ece608cdda7a5d1253993ac815
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-11-07 07:19:58 +0000

    improved the data extraction and also having it storing data exteraction to the VO I created...theory from here is that we can use that standard VO to convert to any format and it has nothing to do the extraction code
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7701 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/vo/SubjectExtractionVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/SubjectExtractionVO.java
@@ -1,9 +1,11 @@
 package au.org.theark.core.vo;
 
+import java.util.HashMap;
+
 public class SubjectExtractionVO {
 //	private String subjectId;//keeping it a bit stupid and ignorant of entities for rapid moves to other formats
 	//private String studyId;
-	private java.util.HashMap<String, String> keyValues;
+	private HashMap<String, String> keyValues = new HashMap<String, String>();
 	
 	/*public String getSubjectId() {
 		return subjectId;
