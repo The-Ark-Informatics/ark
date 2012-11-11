02cb4481a32c015ca6de9b1b8e1fadd0147af4e5
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2012-11-11 03:57:05 +0000

    added significant code to deal with filters.  Compiles and doesn't break anything else, but the filter logic is still incomplete and not being called
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@7724 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-report/src/main/java/au/org/theark/report/web/component/dataextraction/filter/form/QueryFilterForm.java
@@ -0,0 +1,534 @@
+/*******************************************************************************
+ * Copyright (c) 2011  University of Western Australia. All rights reserved.
+ * 
+ * This file is part of The Ark.
+ * 
+ * The Ark is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The Ark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package au.org.theark.report.web.component.dataextraction.filter.form;
+
+import java.lang.reflect.InvocationTargetException;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converter.DoubleConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.org.theark.core.Constants;
+import au.org.theark.core.model.lims.entity.BioTransaction;
+import au.org.theark.core.model.lims.entity.BioTransactionStatus;
+import au.org.theark.core.model.lims.entity.Biospecimen;
+import au.org.theark.core.model.lims.entity.TreatmentType;
+import au.org.theark.core.model.report.entity.DemographicField;
+import au.org.theark.core.model.report.entity.FieldCategory;
+import au.org.theark.core.model.report.entity.QueryFilter;
+import au.org.theark.core.model.study.entity.ArkFunction;
+import au.org.theark.core.model.study.entity.CustomFieldDisplay;
+import au.org.theark.core.model.study.entity.Study;
+import au.org.theark.core.service.IArkCommonService;
+import au.org.theark.core.vo.QueryFilterListVO;
+import au.org.theark.core.vo.QueryFilterVO;
+import au.org.theark.core.web.component.listeditor.AbstractListEditor;
+import au.org.theark.core.web.component.listeditor.AjaxEditorButton;
+import au.org.theark.core.web.component.listeditor.ListItem;
+import au.org.theark.core.web.form.ArkFormVisitor;
+
+/**
+ * @author cellis
+ * 
+ */
+@SuppressWarnings( { "unchecked" })
+public class QueryFilterForm extends Form<QueryFilterListVO> {
+
+	private static final long								serialVersionUID	= 1L;
+	private static final Logger							log					= LoggerFactory.getLogger(QueryFilterForm.class);
+
+	@SpringBean(name = Constants.ARK_COMMON_SERVICE)
+	private IArkCommonService										iArkCommonService;
+
+	// Add a visitor class for required field marking/validation/highlighting
+	protected ArkFormVisitor		formVisitor			= new ArkFormVisitor();
+	
+	protected FeedbackPanel								feedbackPanel;
+	private AbstractListEditor<QueryFilterVO>	listEditor;
+	
+/*	private Label										parentQtyLbl;
+	private TextField<String>							biospecimenUidTxtFld;
+	private TextField<Number>							numberToCreateTxtFld;
+	private TextField<Double>							quantityTxtFld;
+	*/private DropDownChoice<FieldCategory>				fieldCategoryDdc;
+	private DropDownChoice								fieldDdc;
+	//private TextField<Number>							concentrationTxtFld;
+	
+	protected ModalWindow 									modalWindow;
+
+	public QueryFilterForm(String id, IModel<QueryFilterListVO> model, ModalWindow modalWindow) {
+		super(id, model);
+		Long studySessionId = (Long) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.STUDY_CONTEXT_ID);
+		final Study study = iArkCommonService.getStudy(studySessionId);
+		model.getObject().setStudy(study);
+		this.feedbackPanel = new FeedbackPanel("feedback");
+		feedbackPanel.setOutputMarkupId(true);
+		setMultiPart(true);
+		this.modalWindow = modalWindow;
+		add(feedbackPanel);
+	}
+
+	public void initialiseForm() {
+/*		numberToCreateTxtFld = new TextField<Number>("numberToCreate", new PropertyModel(getModelObject(), "numberToCreate")){
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			public boolean isVisible() {
+				// Only visible on first instantation of form, once entered and saved, hidden by enclosure
+				return getModelObject() == null;
+			}
+		};*/
+		/*
+		add(numberToCreateTxtFld);
+		add(new AjaxButton("numberToCreateButton") {
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				int numberToCreate = ((Integer) numberToCreateTxtFld.getDefaultModelObject());
+				for (int i = 0; i < numberToCreate; i++) {
+					Biospecimen biospecimen= new Biospecimen();
+					listEditor.addItem(biospecimen);
+					listEditor.updateModel();
+					target.add(form);	
+				}	
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+		});
+		*/
+		add(buildListEditor());
+		/*
+		add(new Label("parentBiospecimen.biospecimenUid", new PropertyModel(getModelObject(), "parentBiospecimen.biospecimenUid")));
+		parentQtyLbl = new Label("parentBiospecimen.quantity", new PropertyModel(getModelObject(), "parentBiospecimen.quantity")){
+			
+			private static final long	serialVersionUID	= 1L;
+		};
+		add(parentQtyLbl);
+		add(new Label("parentBiospecimen.unit.name", new PropertyModel(getModelObject(), "parentBiospecimen.unit.name")));
+		*/
+		add(new AjaxEditorButton(Constants.NEW) {
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				QueryFilterVO filter= new QueryFilterVO();
+				listEditor.addItem(filter);
+				target.add(form);
+			}
+		}.setDefaultFormProcessing(false));
+		
+		add(new AjaxButton(Constants.SAVE) {
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				onSave(target);
+				target.add(feedbackPanel);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+		});
+		
+		add(new AjaxButton(Constants.SAVEANDCLOSE) {
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				if(onSave(target)) {
+					modalWindow.close(target);
+				}
+				target.add(feedbackPanel);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+		});
+		
+		add(new AjaxButton(Constants.CANCEL) {
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				modalWindow.close(target);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+		}.setDefaultFormProcessing(false));
+	}
+
+	/**
+	 * 
+	 * @return the listEditor of Biospecimens to aliquot
+	 */
+	public AbstractListEditor<QueryFilterVO> buildListEditor() {
+		listEditor = new AbstractListEditor<QueryFilterVO>("queryFilterVOs", new PropertyModel(getModelObject(), "queryFilterVOs")) {
+
+			private static final long	serialVersionUID	= 1L;
+
+			@SuppressWarnings("serial")
+			@Override
+			protected void onPopulateItem(final ListItem<QueryFilterVO> item) {
+				item.setOutputMarkupId(true);
+				
+				initFieldCategoryDdc(item);
+				initFieldDdc(item);
+				
+		/*		
+				Biospecimen parentBiospecimen = QueryFilterForm.this.getModelObject().getParentBiospecimen();
+				try {
+					PropertyUtils.copyProperties(item.getModelObject(), parentBiospecimen);
+				}
+				catch (IllegalAccessException e1) {
+					log.error(e1.getMessage());
+				}
+				catch (InvocationTargetException e1) {
+					log.error(e1.getMessage());
+				}
+				catch (NoSuchMethodException e1) {
+					log.error(e1.getMessage());
+				}
+				item.getModelObject().setParent(parentBiospecimen);
+				item.getModelObject().setParentUid(parentBiospecimen.getBiospecimenUid());
+				item.getModelObject().setQuantity(null);
+				item.getModelObject().setConcentration(null);
+				
+				
+				biospecimenUidTxtFld = new TextField<String>("biospecimenUid", new PropertyModel(item.getModelObject(), "biospecimenUid"));
+				if(parentBiospecimen.getStudy().getAutoGenerateBiospecimenUid()) {
+					biospecimenUidTxtFld.setEnabled(false);
+					biospecimenUidTxtFld.setModelObject(Constants.AUTO_GENERATED);
+				}
+				else {
+					biospecimenUidTxtFld.setEnabled(true);
+					biospecimenUidTxtFld.setModelObject(null);
+				}
+
+				quantityTxtFld = new TextField<Double>("quantity", new PropertyModel(item.getModelObject(), "quantity")) {
+					private static final long	serialVersionUID	= 1L;
+
+					@Override
+					public <C> IConverter<C> getConverter(Class<C> type) {
+						DoubleConverter doubleConverter = new DoubleConverter();
+						NumberFormat numberFormat = NumberFormat.getInstance();
+						numberFormat.setMinimumFractionDigits(1);
+						doubleConverter.setNumberFormat(getLocale(), numberFormat);
+						return (IConverter<C>) doubleConverter;
+					}
+				};
+				*/
+			//	initTreatmentTypeDdc(item);
+				/*concentrationTxtFld = new TextField<Number>("concentration", new PropertyModel(item.getModelObject(), "concentration"));
+
+				item.add(biospecimenUidTxtFld.add(new AjaxFormComponentUpdatingBehavior("onchange"){
+				    @Override
+				    protected void onUpdate(AjaxRequestTarget target) {
+				   	if(!item.getModelObject().getStudy().getAutoGenerateBiospecimenUid()) {
+					   	 // Check BiospecimenUID is unique
+							String biospecimenUid = (getComponent().getDefaultModelObject().toString() != null ? getComponent().getDefaultModelObject().toString() : new String());
+							Biospecimen biospecimen = iArkCommonService.getBiospecimenByUid(biospecimenUid, item.getModelObject().getStudy());
+							if (biospecimen != null && biospecimen.getId() != null) {
+								error("Biospecimen UID must be unique. Please try again.");
+								target.focusComponent(getComponent());
+							}
+				   	}
+				   	target.add(feedbackPanel);
+				    } 
+				}));
+				
+				item.add(quantityTxtFld.add(new AjaxFormComponentUpdatingBehavior("onchange"){
+				    @Override
+				    protected void onUpdate(AjaxRequestTarget target) {
+				   	 if(!totalQuantityLessThanParentQuantity()) {
+				   		 target.focusComponent(getComponent());
+				   	 }
+				   	 target.add(feedbackPanel);
+				    } 
+				    
+				    @Override
+				   protected void onError(AjaxRequestTarget target, RuntimeException e) {
+				   	target.add(feedbackPanel);
+				   }
+				}));
+				item.add(treatmentTypeDdc.add(new AjaxFormComponentUpdatingBehavior("onchange"){
+				    @Override
+				    protected void onUpdate(AjaxRequestTarget target) {
+				    } 
+				}));
+				item.add(concentrationTxtFld.add(new AjaxFormComponentUpdatingBehavior("onchange"){
+				    @Override
+				    protected void onUpdate(AjaxRequestTarget target) {
+				    } 
+				}));
+
+				*/
+				// Copy button allows entire row details to be copied
+				item.add(new AjaxEditorButton(Constants.COPY) {
+					private static final long	serialVersionUID	= 1L;
+
+					@Override
+					protected void onError(AjaxRequestTarget target, Form<?> form) {
+						target.add(feedbackPanel);
+					}
+
+					@Override
+					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+						QueryFilterVO queryFilterVO = new QueryFilterVO();
+						
+						try {
+							PropertyUtils.copyProperties(queryFilterVO, item.getModelObject());
+							listEditor.addItem(queryFilterVO);
+							target.add(form);
+						}
+						catch (IllegalAccessException e) {
+							e.printStackTrace();
+						}
+						catch (InvocationTargetException e) {
+							e.printStackTrace();
+						}
+						catch (NoSuchMethodException e) {
+							e.printStackTrace();
+						}
+					}
+				}.setDefaultFormProcessing(false));
+				item.add(new AjaxEditorButton(Constants.DELETE) {
+					private static final long	serialVersionUID	= 1L;
+
+					@Override
+					protected void onError(AjaxRequestTarget target, Form<?> form) {
+						target.add(feedbackPanel);
+					}
+
+					@Override
+					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+						listEditor.removeItem(item);
+						target.add(form);
+					}
+				}.setDefaultFormProcessing(false).setVisible(item.getIndex()>0));
+				
+				item.add(new AttributeModifier(Constants.CLASS, new AbstractReadOnlyModel() {
+
+					private static final long	serialVersionUID	= 1L;
+
+					@Override
+					public String getObject() {
+						return (item.getIndex() % 2 == 1) ? Constants.EVEN : Constants.ODD;
+					}
+				}));
+			
+				}
+
+		};
+		return listEditor;
+	}
+	
+	
+	
+	/**
+	 * perform this after initfieldcategoryddc
+	 * @param item
+	 */
+	private void initFieldDdc(ListItem item){
+		Collection<DemographicField> demographicFieldCategoryList = iArkCommonService.getAllDemographicFields();
+		ChoiceRenderer<DemographicField> choiceRenderer = new ChoiceRenderer<DemographicField>(Constants.PUBLIC_FIELD_NAME, Constants.ID);
+		fieldDdc = new DropDownChoice<DemographicField>("fieldCategory", 
+				new PropertyModel(item.getModelObject(), "queryFilter.demographicField"), 
+				(List<DemographicField>) demographicFieldCategoryList, choiceRenderer);
+	}
+	
+	private void initFieldCategoryDdc(final ListItem<QueryFilterVO> item) {
+		
+		List<FieldCategory> fieldCategoryList = Arrays.asList(FieldCategory.values()); //iArkCommonService.getFieldCategories();
+		ChoiceRenderer<FieldCategory> choiceRenderer = new ChoiceRenderer<FieldCategory>(Constants.NAME, Constants.ID);
+		fieldCategoryDdc = new DropDownChoice<FieldCategory>("fieldCategory", 
+				new PropertyModel(item.getModelObject(), "fieldCategory"), 
+				(List<FieldCategory>) fieldCategoryList, choiceRenderer);
+		fieldCategoryDdc.setNullValid(false);
+		fieldCategoryDdc.setDefaultModelObject(FieldCategory.DEMOGRAPHIC_FIELD);
+		fieldCategoryDdc.add(new AjaxFormComponentUpdatingBehavior("onchange"){
+			private static final long serialVersionUID = 1L;
+
+			@Override
+		    protected void onUpdate(AjaxRequestTarget target) {
+				FieldCategory catFromDDC = item.getModelObject().getFieldCategory();
+				
+				switch (catFromDDC){
+
+					case BIOCOLLECTION_CFD:{
+						ArkFunction arkFunction = iArkCommonService.getArkFunctionByName(Constants.FUNCTION_KEY_VALUE_LIMS_COLLECTION);
+						
+						List<CustomFieldDisplay> fieldCategoryList = iArkCommonService.getCustomFieldDisplaysIn(getModelObject().getStudy(), arkFunction);
+						ChoiceRenderer<CustomFieldDisplay> choiceRenderer = new ChoiceRenderer<CustomFieldDisplay>(Constants.CUSTOM_FIELD_DOT_NAME, Constants.ID);
+						fieldDdc = new DropDownChoice<CustomFieldDisplay>("fieldCategory", 
+								new PropertyModel(item.getModelObject(), "queryFilter.customFieldDisplay"), 
+								(List<CustomFieldDisplay>) fieldCategoryList, choiceRenderer);
+						break;
+					}
+					case DEMOGRAPHIC_FIELD:{					
+						Collection<DemographicField> demographicFieldCategoryList = iArkCommonService.getAllDemographicFields();
+						ChoiceRenderer<DemographicField> choiceRenderer = new ChoiceRenderer<DemographicField>(Constants.PUBLIC_FIELD_NAME, Constants.ID);
+						fieldDdc = new DropDownChoice<DemographicField>("fieldCategory", 
+								new PropertyModel(item.getModelObject(), "queryFilter.demographicField"), 
+								(List<DemographicField>) demographicFieldCategoryList, choiceRenderer);
+						break;
+					}
+					/*
+					DEMOGRAPHIC_FIELD, 
+					BIOSPECIMEN_FIELD, 
+					BIOCOLLECTION_FIELD, 
+					BIOCOLLECTION_CFD, 
+					BIOSPECIMEN_CFD, 
+					SUBJECT_CFD, 
+					PHENO_CFD, */
+				}
+				addOrReplace(fieldDdc);
+			
+		    } 
+		});
+				
+	}
+	
+	private boolean onSave(AjaxRequestTarget target) {
+		List<QueryFilter> filterList = new ArrayList<QueryFilter>(0);
+		if(validatedList()) {
+		
+			// Loop through entire list
+			for (QueryFilterVO queryFilterVO: getModelObject().getQueryFilterVOs()) {
+
+				QueryFilter queryfilter = queryFilterVO.getQueryFilter();
+
+				filterList.add(queryfilter);
+/*
+				QueryFilter queryfilter = queryFilterVO.getQueryFilter();
+				queryfilter.setOperator(getModelObject().getQueryFilterVOs().getBioCollection());
+				queryFilterVO.setSampleType(getModelObject().getParentBiospecimen().getSampleType());
+				queryFilterVO.setSampleDate(new Date());
+				queryFilterVO.setStudy(getModelObject().getParentBiospecimen().getStudy());
+				queryFilterVO.setLinkSubjectStudy(getModelObject().getParentBiospecimen().getLinkSubjectStudy());
+				
+				filterList.add(queryFilterVO);
+		
+				
+				DO I NEED ANY OF THIS< OR DO I JUST TAKE  IT DIRECTLY...THE ONLY CRAP THING IS CHOSING WHICH FIELD TO FILL OUT
+						(given it is a choice of one out of 4)
+				
+				
+				*/
+			}
+		
+			StringBuffer message = new StringBuffer();
+			message.append("Created ");
+			//message.append(getModelObject().getNumberToCreate());
+			message.append(" simple filters ");
+			
+			if(!filterList.isEmpty()) {
+				info("Batch aliquots created:");
+				log.info("Attempting to create " + getModelObject().getQueryFilterVOs().size() + " filters");
+			//TODO ASAP	iArkCommonService.createFilters(filterList);
+			}
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	private boolean validatedList() {
+		boolean ok = true;/*
+		boolean biospecimenUidError = false;
+		boolean quantityError = false;
+		boolean treatmentTypeError = false;
+		boolean concentrationError = false;
+		
+		// Check for any empty required fields in list
+		for (Biospecimen biospecimen: getModelObject().getAliquots()) {
+			biospecimenUidError = (biospecimen.getBiospecimenUid() == null);
+			quantityError = (biospecimen.getQuantity() == null);
+			treatmentTypeError = (biospecimen.getTreatmentType() == null);
+			// Concentration required?
+			//concentrationError = (biospecimen.getConcentration() == null);
+			
+			if(biospecimenUidError || quantityError || treatmentTypeError || concentrationError) {
+				break;
+			}
+		}
+		
+		if(biospecimenUidError) {
+			error("Field 'Biospecimen UID' is required.");
+			ok = false;
+		}
+		if(quantityError) {
+			error("Field 'Quantity' is required.");
+			ok = false;
+		}
+		if(treatmentTypeError) {
+			error("Field 'Treatment' is required.");
+			ok = false;
+		}
+		if(concentrationError) {
+			error("Field 'Concentration' is required.");
+			ok = false;
+		}*/
+		return ok;
+	}
+}
