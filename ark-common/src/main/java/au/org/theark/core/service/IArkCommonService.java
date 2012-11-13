d22c2b25391e307a6759a32f7f773122cdadcdd3
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-11-13 07:42:58 +0000

    now savign the query filters.  few things to sort out still (field is cahnging on operator change)
    git-svn-id: svn://localhost/ARK/trunk@7739 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
+++ ark-common/src/main/java/au/org/theark/core/service/IArkCommonService.java
@@ -44,6 +44,7 @@ import au.org.theark.core.model.lims.entity.BiospecimenUidToken;
 import au.org.theark.core.model.report.entity.BiocollectionField;
 import au.org.theark.core.model.report.entity.BiospecimenField;
 import au.org.theark.core.model.report.entity.DemographicField;
+import au.org.theark.core.model.report.entity.QueryFilter;
 import au.org.theark.core.model.report.entity.Search;
 import au.org.theark.core.model.study.entity.AddressStatus;
 import au.org.theark.core.model.study.entity.AddressType;
@@ -775,6 +776,8 @@ public interface IArkCommonService<T> {
 	public Collection<CustomFieldDisplay> getSelectedBiocollectionCustomFieldDisplaysForSearch(Search search);
 
 	public void runSearch(Long searchId);
+
+	public void createQueryFilters(List filterList) throws ArkSystemException;
 
 	
 	
