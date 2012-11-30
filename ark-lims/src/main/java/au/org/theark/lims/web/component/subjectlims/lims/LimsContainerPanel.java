710faeba4e66f1139da481b1b813cae69ab88bad
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-11-30 07:50:53 +0000

    Tidied code, removed redundant method calls
    git-svn-id: svn://localhost/ARK/trunk@7800 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-lims/src/main/java/au/org/theark/lims/web/component/subjectlims/lims/LimsContainerPanel.java
+++ ark-lims/src/main/java/au/org/theark/lims/web/component/subjectlims/lims/LimsContainerPanel.java
@@ -20,40 +20,27 @@ package au.org.theark.lims.web.component.subjectlims.lims;
 
 import java.util.Set;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IDetachable;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import wickettree.AbstractTree;
 import wickettree.ITreeProvider;
-import wickettree.NestedTree;
-import wickettree.content.StyledLinkLabel;
 import wickettree.util.ProviderSubset;
-import au.org.theark.core.exception.EntityNotFoundException;
-import au.org.theark.core.model.lims.entity.BioCollection;
-import au.org.theark.core.model.lims.entity.Biospecimen;
-import au.org.theark.core.model.study.entity.LinkSubjectStudy;
 import au.org.theark.core.web.component.AbstractDetailModalWindow;
 import au.org.theark.lims.model.vo.LimsVO;
 import au.org.theark.lims.service.ILimsService;
 import au.org.theark.lims.web.component.inventory.tree.nestedtree.BiospecimenNestedTreePanel;
 import au.org.theark.lims.web.component.inventory.tree.nestedtree.BiospecimenTreeProvidor;
 import au.org.theark.lims.web.component.subjectlims.lims.biocollection.BioCollectionListPanel;
-import au.org.theark.lims.web.component.subjectlims.lims.biocollection.BioCollectionModalDetailPanel;
 import au.org.theark.lims.web.component.subjectlims.lims.biospecimen.BiospecimenListPanel;
-import au.org.theark.lims.web.component.subjectlims.lims.biospecimen.BiospecimenModalDetailPanel;
 import au.org.theark.lims.web.component.subjectlims.lims.form.ContainerForm;
 
 /**
@@ -64,7 +51,6 @@ import au.org.theark.lims.web.component.subjectlims.lims.form.ContainerForm;
 public class LimsContainerPanel extends Panel {
 
 	private static final long						serialVersionUID	= -1L;
-	private static final Logger							log					= LoggerFactory.getLogger(LimsContainerPanel.class);
 	
 	@SpringBean(name = au.org.theark.lims.web.Constants.LIMS_SERVICE)
 	private ILimsService											iLimsService;
@@ -146,133 +132,11 @@ public class LimsContainerPanel extends Panel {
 		state = new ProviderSubset<Object>(provider);
 		bioTreePanel = new BiospecimenNestedTreePanel("tree", cpModel, provider, newStateModel());
 		
-		
-		//tree = createTree();
-		
 		containerForm.add(bioTreePanel);
 		containerForm.add(modalWindow);
 		containerForm.add(biospecimenListPanel);
 		
 		this.add(containerForm);
-	}
-
-	@SuppressWarnings("unchecked")
-	private AbstractTree<Object> createTree() {
-		return new NestedTree("tree", provider, newStateModel()){
-			private static final long	serialVersionUID	= 1L;
-
-			@Override
-			protected Component newContentComponent(String id, IModel model) {
-				if(model.getObject() instanceof LinkSubjectStudy) {
-					LinkSubjectStudy lss = (LinkSubjectStudy)model.getObject();
-					return new Label(id, lss.getSubjectUID());
-				}
-				if(model.getObject() instanceof BioCollection) {
-					final BioCollection bc = (BioCollection) model.getObject();
-					StyledLinkLabel<String> styledLink = new StyledLinkLabel<String>(id, new Model<String>(bc.getBiocollectionUid())) {
-						private static final long	serialVersionUID	= 1L;
-
-						@Override
-						protected String getStyleClass() {
-							// TODO Auto-generated method stub
-							return null;
-						}
-						@Override
-						protected void onClick(AjaxRequestTarget target) {
-							log.info("BiospecimenUID: " + getModelObject().toString());
-							CompoundPropertyModel<LimsVO> newModel = new CompoundPropertyModel<LimsVO>(new LimsVO());
-							try {
-								BioCollection biocollectionFromDB = iLimsService.getBioCollection(bc.getId());
-								newModel.getObject().setBioCollection(biocollectionFromDB);
-								newModel.getObject().setTreeModel(cpModel.getObject().getTreeModel());
-								
-								BioCollectionModalDetailPanel modalContentPanel = new BioCollectionModalDetailPanel("content", modalWindow, newModel);
-
-								// Set the modalWindow title and content
-								modalWindow.setTitle("Biocollection Detail");
-								modalWindow.setContent(modalContentPanel);
-								modalWindow.show(target);
-								modalWindow.setWindowClosedCallback(new ModalWindow.WindowClosedCallback()
-								{
-									/**
-									 * 
-									 */
-									private static final long	serialVersionUID	= 1L;
-
-									public void onClose(AjaxRequestTarget target)
-						         {
-						         }
-								});
-								
-							}
-							catch (EntityNotFoundException e) {
-								log.error(e.getMessage());
-							}
-						}
-						
-						@Override
-						protected boolean isClickable() {
-							return true;
-						}
-					};
-					
-					return styledLink;
-				}
-				else if (model.getObject() instanceof Biospecimen) {
-					final Biospecimen b = (Biospecimen) model.getObject();
-					StyledLinkLabel<String> styledLink = new StyledLinkLabel<String>(id, new Model<String>(b.getBiospecimenUid())) {
-						private static final long	serialVersionUID	= 1L;
-
-						@Override
-						protected String getStyleClass() {
-							// TODO Auto-generated method stub
-							return null;
-						}
-						@Override
-						protected void onClick(AjaxRequestTarget target) {
-							log.info("BiospecimenUID: " + getModelObject().toString());
-							CompoundPropertyModel<LimsVO> newModel = new CompoundPropertyModel<LimsVO>(new LimsVO());
-							try {
-								Biospecimen biospecimenFromDB = iLimsService.getBiospecimen(b.getId());
-								newModel.getObject().setBiospecimen(biospecimenFromDB);
-								newModel.getObject().setTreeModel(cpModel.getObject().getTreeModel());
-								
-								BiospecimenModalDetailPanel modalContentPanel = new BiospecimenModalDetailPanel("content", modalWindow, newModel);
-
-								// Set the modalWindow title and content
-								modalWindow.setTitle("Biospecimen Detail");
-								modalWindow.setContent(modalContentPanel);
-								modalWindow.show(target);
-								modalWindow.setWindowClosedCallback(new ModalWindow.WindowClosedCallback()
-								{
-									/**
-									 * 
-									 */
-									private static final long	serialVersionUID	= 1L;
-
-									public void onClose(AjaxRequestTarget target)
-						         {
-						         }
-								});
-								
-							}
-							catch (EntityNotFoundException e) {
-								log.error(e.getMessage());
-							}
-						}
-						
-						@Override
-						protected boolean isClickable() {
-							return true;
-						}
-					};
-					
-					return styledLink;
-				}
-				return null;
-			}
-			
-		};
 	}
 
 	private IModel<Set<Object>> newStateModel() {
