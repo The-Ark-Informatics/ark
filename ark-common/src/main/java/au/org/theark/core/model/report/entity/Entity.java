940897851e8573ba3bd3aad647c411f0beb903bd
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2012-12-07 00:13:58 +0000

    Added extra Entity references
    git-svn-id: svn://localhost/ARK/trunk@7811 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/report/entity/Entity.java
+++ ark-common/src/main/java/au/org/theark/core/model/report/entity/Entity.java
@@ -1,6 +1,6 @@
 package au.org.theark.core.model.report.entity;
 
 public enum Entity {
-	Person, LinkSubjectStudy, Address, Email, Phone
+	Person, LinkSubjectStudy, Address, Email, Phone, Biospecimen, BioCollection
 }
 
