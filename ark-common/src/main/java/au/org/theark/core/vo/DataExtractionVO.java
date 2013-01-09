9b136c6ce77b4657df9f14925114a02d2616a867
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-01-09 08:05:35 +0000

    implemented biospecimen filtering and population added to data vo.  This step also uses filtering to decrease the lss.IDs we keep
    git-svn-id: svn://localhost/ARK/trunk@7852 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/vo/DataExtractionVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/DataExtractionVO.java
@@ -1,9 +1,15 @@
 package au.org.theark.core.vo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import au.org.theark.core.model.lims.entity.Biospecimen;
 
 public class DataExtractionVO {
 	private HashMap<String, ExtractionVO> demographicData = new HashMap<String, ExtractionVO>();
+	private List<Biospecimen> biospecimens = new ArrayList<Biospecimen>();//maybe it needs something more like above
+																//but if we just order/broup by subject uid its should be fine
 
 	public HashMap<String, ExtractionVO> getDemographicData() {
 		return demographicData;
@@ -23,7 +29,14 @@ public class DataExtractionVO {
 	public void setSubjectCustomData(HashMap<String, ExtractionVO> subjectCustomData) {
 		this.subjectCustomData = subjectCustomData;
 	}
-	
+
+	public void setBiospecimens(List<Biospecimen> biospecimens) {
+		this.biospecimens = biospecimens;
+	}
+
+	public List<Biospecimen> getBiospecimens() {
+		return this.biospecimens;
+	}
 	
 	
 }
