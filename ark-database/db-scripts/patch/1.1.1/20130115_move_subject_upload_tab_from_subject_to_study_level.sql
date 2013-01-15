891fa0a40f2c02c7268e76f0c512f473aef68b65
Thilina Ranaweera <tranaweera@gmail.com>
2013-01-15 04:31:28 +0000

    ARK-912 Move Subject upload tab
    git-svn-id: svn://localhost/ARK/trunk@7857 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20130115_move_subject_upload_tab_from_subject_to_study_level.sql
@@ -0,0 +1,12 @@
+DELETE FROM study.ark_module_function 
+where ark_module_id = (SELECT id from study.ark_module where name='subject')
+        AND ark_function_id = (select id from study.ark_function where name='SUBJECT_UPLOAD');
+
+INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) 
+VALUES ((SELECT id from study.ark_module where name='study'), 
+        (select id from study.ark_function where name='SUBJECT_UPLOAD'), 
+        (SELECT max(amf.function_sequence)+1 
+            FROM study.ark_module_function amf
+            	inner join study.ark_module am on am.id=amf.ark_module_id   
+            where am.name='study')
+);
