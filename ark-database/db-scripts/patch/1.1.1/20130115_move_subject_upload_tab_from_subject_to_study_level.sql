a7da3ae66d50e0b8614c59f2ef92398ebbbfff9c
Thilina Ranaweera <tranaweera@gmail.com>
2013-01-16 04:06:49 +0000

    ARK-947 Add Senetor as title type and Modify the  move subject upload tab from subject to study level script
    git-svn-id: svn://localhost/ARK/trunk@7860 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20130115_move_subject_upload_tab_from_subject_to_study_level.sql
+++ ark-database/db-scripts/patch/1.1.1/20130115_move_subject_upload_tab_from_subject_to_study_level.sql
@@ -2,11 +2,14 @@ DELETE FROM study.ark_module_function
 where ark_module_id = (SELECT id from study.ark_module where name='subject')
         AND ark_function_id = (select id from study.ark_function where name='SUBJECT_UPLOAD');
 
+SET @SEQ_ID = 1;
+
+SELECT @SEQ_ID := max(amf.function_sequence)+1 
+FROM study.ark_module_function amf
+     	inner join study.ark_module am on am.id=amf.ark_module_id   
+where am.name='study';
+
 INSERT INTO `study`.`ark_module_function` (`ARK_MODULE_ID`, `ARK_FUNCTION_ID`, `FUNCTION_SEQUENCE`) 
 VALUES ((SELECT id from study.ark_module where name='study'), 
         (select id from study.ark_function where name='SUBJECT_UPLOAD'), 
-        (SELECT max(amf.function_sequence)+1 
-            FROM study.ark_module_function amf
-            	inner join study.ark_module am on am.id=amf.ark_module_id   
-            where am.name='study')
-);
+        @SEQ_ID );
