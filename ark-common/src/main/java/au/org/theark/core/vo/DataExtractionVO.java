79906b86334bf71d7b6199024e6850e2e2b55873
Thilina Ranaweera <tranaweera@gmail.com>
2013-02-11 08:01:37 +0000

    ARK-950 Biocollection Data extraction
    git-svn-id: svn://localhost/ARK/trunk@7896 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/vo/DataExtractionVO.java
+++ ark-common/src/main/java/au/org/theark/core/vo/DataExtractionVO.java
@@ -9,16 +9,19 @@ import au.org.theark.core.model.lims.entity.Biospecimen;
 
 public class DataExtractionVO {
 	private HashMap<String, ExtractionVO> demographicData = new HashMap<String, ExtractionVO>();
+	private HashMap<String, ExtractionVO> biocollectionData = new HashMap<String, ExtractionVO>();
+	private HashMap<String, ExtractionVO> biospecimenData = new HashMap<String, ExtractionVO>();
 	private List<BioCollection> biocollections = new ArrayList<BioCollection>();
-	private List<Biospecimen> biospecimens = new ArrayList<Biospecimen>();//maybe it needs something more like above
+	private List<Biospecimen> biospecimens = new ArrayList<Biospecimen>();
+	//maybe it needs something more like above
 																//but if we just order/group by subject uid its should be fine
 
 	public HashMap<String, ExtractionVO> getDemographicData() {
-		return demographicData;
+		return biospecimenData;
 	}
 
 	public void setDemographicData(HashMap<String, ExtractionVO> subjectAndData) {
-		this.demographicData = subjectAndData;
+		this.biospecimenData = subjectAndData;
 	}
 	
 
@@ -49,6 +52,21 @@ public class DataExtractionVO {
 		return this.biocollections;
 	}
 
+	public HashMap<String, ExtractionVO> getBiocollectionData() {
+		return biocollectionData;
+	}
+
+	public void setBiocollectionData(HashMap<String, ExtractionVO> biocollectionData) {
+		this.biocollectionData = biocollectionData;
+	}
+
+	public HashMap<String, ExtractionVO> getBiospecimenData() {
+		return biospecimenData;
+	}
+
+	public void setBiospecimenData(HashMap<String, ExtractionVO> biospecimenData) {
+		this.biospecimenData = biospecimenData;
+	}
 
 	/* I guess the key to each of these is infact a biospecimen uid and not a subject uid, and subjectUID is just a key value pair?  i am open to suggestions */
 	private HashMap<String, ExtractionVO> biospecimenCustomData = new HashMap<String, ExtractionVO>();
