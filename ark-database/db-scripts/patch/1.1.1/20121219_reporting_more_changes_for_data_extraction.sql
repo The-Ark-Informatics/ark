1cfe48681f229a94a5b1df049fd5ec1d7e13a5c8
Thilina Ranaweera <tranaweera@gmail.com>
2013-02-25 06:51:05 +0000

    Auto increment id for non auto increment PK tables
    git-svn-id: svn://localhost/ARK/trunk@7908 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1/20121219_reporting_more_changes_for_data_extraction.sql
+++ ark-database/db-scripts/patch/1.1.1/20121219_reporting_more_changes_for_data_extraction.sql
@@ -1,2 +1,9 @@
 
-INSERT INTO study.field_type(id, name) values (4,'COMPLEX');
+use study;
+
+set @id = 0;
+
+SELECT @id := max(id)+1 from field_type;
+
+INSERT INTO field_type(id, name) values (@id,'COMPLEX');
+
