27a2b3d66bfbb34bc6e89f5d3c96429c517d76fe
travisendersby <travisendersby@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-03-22 02:39:57 +0000

    more relationships in the hash are break due to the serialized nature of wicket.   We may now need to go and make the hashes more meaningful again
    git-svn-id: svn://localhost/ARK/trunk@7964 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/lims/entity/Biospecimen.java
+++ ark-common/src/main/java/au/org/theark/core/model/lims/entity/Biospecimen.java
@@ -505,7 +505,7 @@ public class Biospecimen implements java.io.Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((bioCollection == null) ? 0 : bioCollection.hashCode());
+//		result = prime * result + ((bioCollection == null) ? 0 : bioCollection.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
