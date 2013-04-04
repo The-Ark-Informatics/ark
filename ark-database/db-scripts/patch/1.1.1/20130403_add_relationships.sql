8a1c611e83a6fe24d8e937ddad955ccb5840a3ef
Thilina Ranaweera <tranaweera@gmail.com>
2013-04-04 02:50:27 +0000

    ARK-830 add relatioship types and create pedigree relationships table
    git-svn-id: svn://localhost/ARK/trunk@7997 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20130403_add_relationships.sql
@@ -0,0 +1,2 @@
+INSERT INTO `study`.`relationship` (`ID`, `NAME`, `DESCRIPTION`) VALUES (0, 'Father', 'Father');
+INSERT INTO `study`.`relationship` (`ID`, `NAME`, `DESCRIPTION`) VALUES (1, 'Mother', 'Mother');
