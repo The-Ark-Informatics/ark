0f90e0a0e383fb31af0474f5274bbbfd0e6fba3c
chrisellis <chrisellis@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-04-04 03:05:28 +0000

    modified mehtods to take in custom field references
    git-svn-id: svn://localhost/ARK/trunk@8000 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/dao/IDataExtractionDao.java
+++ ark-common/src/main/java/au/org/theark/core/dao/IDataExtractionDao.java
@@ -1,15 +1,18 @@
 package au.org.theark.core.dao;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.HashMap;
 
 import au.org.theark.core.model.report.entity.FieldCategory;
 import au.org.theark.core.model.report.entity.Search;
+import au.org.theark.core.model.study.entity.CustomFieldDisplay;
+import au.org.theark.core.vo.DataExtractionVO;
 import au.org.theark.core.vo.ExtractionVO;
 
 public interface IDataExtractionDao {
-	public File createBiocollectionCSV(Search search, HashMap<String, ExtractionVO> map, FieldCategory fieldCategory);
-	public File createBiospecimenCSV(Search search, HashMap<String, ExtractionVO> map, FieldCategory fieldCategory);
-	public File createSubjectDemographicCSV(Search search, HashMap<String, ExtractionVO> map, FieldCategory fieldCategory);
-	public File createBiospecimenDataCustomCSV(Search search, HashMap<String, ExtractionVO> hashOfBiospecimenCustomData, FieldCategory fieldCategory);
+	public File createBiocollectionCSV(Search search, DataExtractionVO devo, Collection<CustomFieldDisplay> cfds, FieldCategory fieldCategory);
+	public File createBiospecimenCSV(Search search, DataExtractionVO devo, Collection<CustomFieldDisplay> cfds, FieldCategory fieldCategory);
+	public File createSubjectDemographicCSV(Search search, DataExtractionVO devo, Collection<CustomFieldDisplay> cfds, FieldCategory fieldCategory);
+	public File createBiospecimenDataCustomCSV(Search search, DataExtractionVO devo, Collection<CustomFieldDisplay> cfds, FieldCategory fieldCategory);
 }
