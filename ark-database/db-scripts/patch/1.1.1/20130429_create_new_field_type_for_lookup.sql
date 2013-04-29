3e111b565dfc4d5ff09ad1448acd1fc8d84cb772
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-04-29 09:33:42 +0000

    got the first instance of lookup filters working
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8099 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20130429_create_new_field_type_for_lookup.sql
+++ ark-database/db-scripts/patch/1.1.1/20130429_create_new_field_type_for_lookup.sql
@@ -6,3 +6,5 @@ UPDATE `study`.`field_type` SET `VISIBILE`='0' WHERE `ID`='4';
 
 ALTER TABLE `study`.`field_type` CHANGE COLUMN `VISIBILE` `VISIBLE` TINYINT(4) NOT NULL DEFAULT '1'  ;
 
+UPDATE `reporting`.`demographic_field` SET `FIELD_TYPE_ID`='4' WHERE `ID`='10';
+
