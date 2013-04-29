00afb989f22da9ab3208e4b16b798dbf83fee32a
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-04-29 09:01:03 +0000

    implemented initial code to deal with new fields called LOOKUP
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8097 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-common/src/main/java/au/org/theark/core/Constants.java
+++ ark-common/src/main/java/au/org/theark/core/Constants.java
@@ -112,6 +112,7 @@ public class Constants {
 	public static final String FIELD_TYPE_NUMBER = "NUMBER";
 	public static final String FIELD_TYPE_CHARACTER = "CHARACTER";
 	public static final String FIELD_TYPE_DATE = "DATE";
+	public static final String FIELD_TYPE_LOOKUP = "LOOKUP";
 	public static final String DISCRETE_RANGE_TOKEN = ",";
 	public static final String ENCODED_VALUES_TOKEN = ";";
 	public static final String ENCODED_VALUES_FROM_TELEFORMS_TOKEN_SPACE = " ";
