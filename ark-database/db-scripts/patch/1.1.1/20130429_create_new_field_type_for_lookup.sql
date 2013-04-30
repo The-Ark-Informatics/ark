9e244531421246b709658a0916b5e1e0bd4f317a
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-04-30 14:33:57 +0000

    tweak some ugly sql
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8108 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20130429_create_new_field_type_for_lookup.sql
+++ ark-database/db-scripts/patch/1.1.1/20130429_create_new_field_type_for_lookup.sql
@@ -2,9 +2,9 @@ INSERT INTO `study`.`field_type` (`ID`, `NAME`) VALUES ('4', 'LOOKUP');
 
 ALTER TABLE `study`.`field_type` ADD COLUMN `VISIBLE` TINYINT NOT NULL DEFAULT '1'  ;
 
-UPDATE `study`.`field_type` SET `VISIBILE`='0' WHERE `ID`='4';
+UPDATE `study`.`field_type` SET `VISIBLE`='0' WHERE `ID`='4';
 
-ALTER TABLE `study`.`field_type` CHANGE COLUMN `VISIBILE` `VISIBLE` TINYINT(4) NOT NULL DEFAULT '1'  ;
+-- ALTER TABLE `study`.`field_type` CHANGE COLUMN `VISIBILE` `VISIBLE` TINYINT(4) NOT NULL DEFAULT '1'  ;
 
 /* change all of these to lookup fields */
 UPDATE `reporting`.`demographic_field` SET `FIELD_TYPE_ID`='4' WHERE `ID`='10';
