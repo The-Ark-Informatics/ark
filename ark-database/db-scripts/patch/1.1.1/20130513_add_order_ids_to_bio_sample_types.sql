48b23bb80f3b63a85e2bf518bda53137bd1d3bae
Thilina Ranaweera <tranaweera@gmail.com>
2013-05-13 05:08:55 +0000

    Ark-999 Ordering bio sample types
    git-svn-id: svn://localhost/ARK/trunk@8126 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.1.1/20130513_add_order_ids_to_bio_sample_types.sql
@@ -0,0 +1,23 @@
+use lims;
+
+select @orderId := 0;
+
+update bio_sampletype
+set order_id = (select @orderId := @orderId + 1)
+where name = 'Blood / Whole Blood';
+
+update bio_sampletype
+set order_id = (select @orderId := @orderId + 1)
+where name = 'Blood / Peripheral Blood Lysed';
+
+update bio_sampletype
+set order_id = (select @orderId := @orderId + 1)
+where name = 'Blood / Peripheral Blood DNA';
+
+update 
+(select bs.* 
+ from bio_sampletype bs
+ where bs.order_id=0
+ order by bs.name)p, bio_sampletype pp
+set pp.order_id = (select @orderId := @orderId + 1)
+where p.id=pp.id;
