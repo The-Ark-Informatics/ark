944ac3603e1e4922f3a6cb5de4b9969f12e75f65
Thilina Ranaweera <tranaweera@gmail.com>
2013-07-24 01:34:31 +0000

    Lifepool lims inventory setup scripts
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8275 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ usefulTools/lifepool/src/create_inventory_80.py
@@ -0,0 +1,65 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", port=3306, user="XXXXX", passwd="YYYYY")
+
+cursor = db.cursor()
+
+rowTypeId=0
+colTypeId=0
+
+cursor.execute("""select id,name 
+                    from lims.inv_col_row_type """)
+
+data = cursor.fetchall()
+for row in data :
+    name=row[1]
+    if "Numeric" == name:
+        colTypeId = row[0]
+    else:
+        rowTypeId = row[0]
+
+
+cursor.execute("""select id from lims.inv_site where name = %s """,
+                              ("Peter MacCallum"))
+row = cursor.fetchone()
+siteId = row[0]
+
+
+cursor.execute("""insert into lims.inv_freezer (SITE_ID,CAPACITY,NAME,AVAILABLE) values(%s,%s,%s,%s) """,
+                              (siteId,30,"-80",24))
+
+lnFreezerId = db.insert_id()
+
+db.commit()
+
+for r in range(1, 7, 1):
+        print "Rack %d" % (r)
+        cursor.execute("""insert into lims.inv_rack (FREEZER_ID,CAPACITY,NAME,AVAILABLE) values(%s,%s,%s,%s) """,
+                       (lnFreezerId, 100, ("Rack " + str(r)), 75))
+        rackId = db.insert_id()
+        db.commit()
+        if r < 2:
+                for b in range(1, 26, 1):
+                        print "Box" + str(b)
+                        cursor.execute("""insert into lims.inv_box (rack_ID,CAPACITY,NAME,AVAILABLE,COLNOTYPE_ID,ROWNOTYPE_ID,NOOFROW,NOOFCOL) values(%s,%s,%s,%s,%s,%s,%s,%s) """,
+                                       (rackId, 100, ("Box " + str(b)), 100, colTypeId, rowTypeId,10,10))
+                        boxId = db.insert_id()
+                        db.commit()
+                        for x in range(1, 11, 1):
+                                for y in range (1, 11, 1):
+                                        print "cell " + str(x) + " " + str(y)
+                                        cursor.execute("""insert into lims.inv_cell (box_ID,colno,rowno) values(%s,%s,%s) """, (boxId, x, y))
+                                        db.commit()
+        else:
+                for b in range(1, 26, 1):
+                        print "Box" + str(b)
+                        cursor.execute("""insert into lims.inv_box (rack_ID,CAPACITY,NAME,AVAILABLE,COLNOTYPE_ID,ROWNOTYPE_ID,NOOFROW,NOOFCOL) values(%s,%s,%s,%s,%s,%s,%s,%s) """,
+                                       (rackId, 169, ("Box " + str(b)), 169, colTypeId, rowTypeId,13,13))
+                        boxId = db.insert_id()
+                        db.commit()
+                        for x in range(1, 14, 1):
+                                for y in range (1, 14, 1):
+                                        print "cell " + str(x) + " " + str(y)
+                                        cursor.execute("""insert into lims.inv_cell (box_ID,colno,rowno) values(%s,%s,%s) """, (boxId, x, y))
+                                        db.commit()
+
