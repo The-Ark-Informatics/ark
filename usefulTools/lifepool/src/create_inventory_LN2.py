457e80a1d1d6553f46b19ea0e985ed3eafcec4fc
Thilina Ranaweera <tranaweera@gmail.com>
2013-07-24 01:34:31 +0000

    Lifepool lims inventory setup scripts
    git-svn-id: svn://localhost/ARK/trunk@8275 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ usefulTools/lifepool/src/create_inventory_LN2.py
@@ -0,0 +1,51 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", port=3306, user="XXXXX", passwd="YYYYYY")
+
+cursor = db.cursor()
+
+rowTypeId=0
+colTypeId=0
+
+cursor.execute("""select id,name 
+                    from lims.inv_col_row_type """)
+
+data = cursor.fetchall()
+for row in data :
+    name=row[1]
+    if "Numeric" == name:
+        colTypeId = row[0]
+    else:
+        rowTypeId = row[0]
+
+
+cursor.execute("""select id from lims.inv_site where name = %s """,
+                              ("University of Melbourne"))
+row = cursor.fetchone()
+siteId = row[0]
+
+
+cursor.execute("""insert into lims.inv_freezer (SITE_ID,CAPACITY,NAME,AVAILABLE) values(%s,%s,%s,%s) """,
+                              (siteId,30,"LN 2",10))
+
+lnFreezerId = db.insert_id()
+
+db.commit()
+
+for r in range(1, 21, 1):
+        print "Rack %d" % (r)
+        cursor.execute("""insert into lims.inv_rack (FREEZER_ID,CAPACITY,NAME,AVAILABLE) values(%s,%s,%s,%s) """,
+                       (lnFreezerId, 100, ("Rack " + str(r)), 87))
+        rackId = db.insert_id()
+        db.commit()
+        for b in range(2, 28, 2):
+                print "Box" + str(b)
+                cursor.execute("""insert into lims.inv_box (rack_ID,CAPACITY,NAME,AVAILABLE,COLNOTYPE_ID,ROWNOTYPE_ID,NOOFROW,NOOFCOL) values(%s,%s,%s,%s,%s,%s,%s,%s) """,
+                               (rackId, 100, ("Box " + str(b)), 100, colTypeId, rowTypeId,10,10))
+                boxId = db.insert_id()
+                db.commit()
+                for x in range(1, 11, 1):
+                        for y in range (1, 11, 1):
+                                print "cell " + str(x) + " " + str(y)
+                                cursor.execute("""insert into lims.inv_cell (box_ID,colno,rowno) values(%s,%s,%s) """, (boxId, x, y))
+                                db.commit()
---
+++ usefulTools/lifepool/src/create_inventory_PeterMac_site.py
@@ -0,0 +1,31 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", port=3306, user="XXXXX", passwd="YYYYY")
+
+siteId=0
+studyId=0
+lnFreezerId=0
+
+cursor = db.cursor()
+
+cursor.execute("""select id 
+		  from study.study 
+		  where name =  %s """,
+               ("Lifepool"))
+
+row = cursor.fetchone()
+
+studyId =  row[0]
+
+cursor.execute("""insert into lims.inv_site (NAME) values (%s) """,
+               ("Peter MacCallum"))
+
+siteId=db.insert_id()
+
+db.commit()
+
+
+cursor.execute("""insert into lims.study_inv_site (STUDY_ID, INV_SITE_ID) values(%s,%s) """,
+               (studyId,siteId))
+
+db.commit()
