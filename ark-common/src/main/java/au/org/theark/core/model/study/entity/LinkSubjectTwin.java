a41a65601a9e79c80c1551bf92842cc896165740
Thilina Ranaweera <tranaweera@gmail.com>
2013-07-26 09:21:13 +0000

    Ark pedigree link subject twin entity
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8281 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/LinkSubjectTwin.java
@@ -0,0 +1,86 @@
+package au.org.theark.core.model.study.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "LINK_SUBJECT_TWIN", schema = Constants.STUDY_SCHEMA)
+public class LinkSubjectTwin implements Serializable {
+
+	/**
+    *
+    */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private LinkSubjectStudy firstSubject;
+	private LinkSubjectStudy secondSubject;
+	private Relationship relationship;
+
+	public LinkSubjectTwin() {
+	}
+
+	public LinkSubjectTwin(Integer id, LinkSubjectStudy firstSubject,
+			LinkSubjectStudy secondSubject, Relationship relationship) {
+		this.id = id;
+		this.firstSubject = firstSubject;
+		this.secondSubject = secondSubject;
+		this.relationship = relationship;
+	}
+
+	@Id
+    @SequenceGenerator(name = "link_subject_twin_generator", sequenceName = "LINK_SUBJECT_TWIN_SEQUENCE")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "link_subject_twin_generator")
+    @Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "FIRST_SUBJECT")
+	public LinkSubjectStudy getFirstSubject() {
+		return firstSubject;
+	}
+
+	public void setFirstSubject(LinkSubjectStudy firstSubject) {
+		this.firstSubject = firstSubject;
+	}
+
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SECOND_SUBJECT")
+	public LinkSubjectStudy getSecondSubject() {
+		return secondSubject;
+	}
+
+	public void setSecondSubject(LinkSubjectStudy secondSubject) {
+		this.secondSubject = secondSubject;
+	}
+
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RELATIONSHIP_ID")
+	public Relationship getRelationship() {
+		return relationship;
+	}
+
+	public void setRelationship(Relationship relationship) {
+		this.relationship = relationship;
+	}
+	
+	
+
+}
