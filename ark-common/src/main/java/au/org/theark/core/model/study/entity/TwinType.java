4a5eef59151f6c0b0aee477b2093226c448589f5
Thilina Ranaweera <tranaweera@gmail.com>
2013-07-31 04:41:33 +0000

    Add Pedigree Type entity
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8289 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/model/study/entity/TwinType.java
@@ -0,0 +1,48 @@
+package au.org.theark.core.model.study.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "TWIN_TYPE", schema = Constants.STUDY_SCHEMA, uniqueConstraints = @UniqueConstraint(columnNames = "NAME"))
+public class TwinType implements Serializable {
+	
+	private Long id;
+	private String name;
+	private String description;
+	
+	@Id
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", unique = true, length = 20)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "DESCRIPTION")
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
