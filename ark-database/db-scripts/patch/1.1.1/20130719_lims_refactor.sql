324cfc550af568417e5a1e327a641a716d521377
travisendersby <travisendersby@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-07-31 06:17:48 +0000

    added clean base dump from which 1.1.1c tag should work.  Essentially a prod dump minus all user data.  Contains lookup data and a base test study right now.
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8290 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/20130719_lims_refactor.sql
+++ ark-database/db-scripts/patch/1.1.1/20130719_lims_refactor.sql
@@ -21,4 +21,42 @@ drop table if exists lims.samplecode	;
 drop table if exists lims.biodata_group_criteria	;
 drop table if exists lims.biodata_criteria	;
 drop table if exists lims.biodata_group	;
-/* don't delete even though empty...this links to biotransaction...may be used?  drop table if exists lims.access_request; */
+#  don't delete even though empty...this links to biotransaction...may be used?  drop table if exists lims.access_request;
+
+
+
+ALTER TABLE `lims`.`unit` 
+ADD COLUMN `FACTOR` DOUBLE NOT NULL DEFAULT 1  AFTER `DESCRIPTION` , 
+ADD COLUMN `ORDER` BIGINT NOT NULL DEFAULT 1  AFTER `FACTOR` , 
+ADD COLUMN `TYPE` ENUM('VOLUME', 'MASS', 'DISTANCE', 'TIME','TEMPERATURE','UNKNOWN') NOT NULL DEFAULT 'UNKNOWN'  AFTER `ORDER` ;
+
+ALTER TABLE `lims`.`unit` CHANGE COLUMN `TYPE` `TYPE` ENUM('VOLUME','MASS','TIME','TEMPERATURE','DISTANCE','UNKNOWN') NOT NULL DEFAULT 'UNKNOWN'  ;
+
+
+UPDATE `lims`.`unit` SET `NAME`='ul', `ORDER`='3', `TYPE`='VOLUME' WHERE `ID`='2';
+UPDATE `lims`.`unit` SET `NAME`='nl', `FACTOR`='0.001', `ORDER`='2', `TYPE`='VOLUME' WHERE `ID`='3';
+UPDATE `lims`.`unit` SET `TYPE`='DISTANCE' WHERE `ID`='1';
+UPDATE `lims`.`unit` SET `FACTOR`='1000000', `ORDER`='5', `TYPE`='VOLUME' WHERE `ID`='38';
+UPDATE `lims`.`unit` SET `FACTOR`='1000', `ORDER`='4' WHERE `ID`='17';
+
+
+UPDATE `lims`.`unit` SET `NAME`='g', `ORDER`='104', `TYPE`='MASS' WHERE `ID`='12';
+UPDATE `lims`.`unit` SET `NAME`='mg', `ORDER`='103', `TYPE`='MASS' WHERE `ID`='15';
+UPDATE `lims`.`unit` SET `ORDER`='105', `TYPE`='MASS' WHERE `ID`='10';
+UPDATE `lims`.`unit` SET `NAME`='ug', `ORDER`='102', `TYPE`='MASS' WHERE `ID`='14';
+UPDATE `lims`.`unit` SET `ORDER`='301', `TYPE`='DISTANCE' WHERE `ID`='16';
+UPDATE `lims`.`unit` SET `TYPE`='VOLUME' WHERE `ID`='17';
+UPDATE `lims`.`unit` SET `ORDER`='301', `TYPE`='DISTANCE' WHERE `ID`='1';
+UPDATE `lims`.`unit` SET `ORDER`='-1' WHERE `ID`='0';
+
+UPDATE `lims`.`unit` SET `FACTOR`='1000000' WHERE `ID`='10';
+UPDATE `lims`.`unit` SET `FACTOR`='1000' WHERE `ID`='12';
+UPDATE `lims`.`unit` SET `FACTOR`='0.001' WHERE `ID`='14';
+UPDATE `lims`.`unit` SET `FACTOR`='.001' WHERE `ID`='1';
+
+
+
+
+
+
+
