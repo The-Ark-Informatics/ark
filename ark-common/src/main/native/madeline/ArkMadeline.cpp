e9bba4e9d96f1acdb94e7eeb0f89833df92f9457
Thilina Ranaweera <tranaweera@gmail.com>
2013-08-17 02:40:14 +0000

    Handle unexpected madeline exceptions
    git-svn-id: svn://localhost/ARK/trunk@8348 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/native/madeline/ArkMadeline.cpp
+++ ark-common/src/main/native/madeline/ArkMadeline.cpp
@@ -19,43 +19,47 @@ using namespace std;
 void generatePedigree (const std::string &filename){
 	cout << "Start Generating pedigree!" << endl;
 
-	std::vector<std::string> showColumns; // vector containing field labels that need to be displayed on the pedigree
-	CLP clp;
-	Parser dataTableParser;
-	dataTableParser.readFile(filename);
+	try {
+		std::vector<std::string> showColumns; // vector containing field labels that need to be displayed on the pedigree
+		CLP clp;
+		Parser dataTableParser;
+		dataTableParser.readFile(filename);
 
-	DataTable *dataTable;
-	for( int j=0 ; j < dataTableParser.getNumberOfTables() ; j++ ){
+		DataTable *dataTable;
+		for( int j=0 ; j < dataTableParser.getNumberOfTables() ; j++ ){
 
-		PedigreeSet pedigreeSet;
+			PedigreeSet pedigreeSet;
 
-		dataTable = dataTableParser.getTable(j);
+			dataTable = dataTableParser.getTable(j);
 
-		// DEBUG:
-		//dataTable->display();
-		//
-		// Tell user the type of table:
-		//
-		std::cout << "Table " << (j+1) << " is a " << dataTable->getTableTypeAsString() << " table." << std::endl;
+			// DEBUG:
+			//dataTable->display();
+			//
+			// Tell user the type of table:
+			//
+			std::cout << "Table " << (j+1) << " is a " << dataTable->getTableTypeAsString() << " table." << std::endl;
 
-		//
-		// Draw pedigrees
-		//
-		if( dataTable->getTableType() == DataTable::PEDIGREE ){
+			//
+			// Draw pedigrees
+			//
+			if( dataTable->getTableType() == DataTable::PEDIGREE ){
 
-			// Pedigree table
-			dataTable->toggleColumnsForPedigree(showColumns);
+				// Pedigree table
+				dataTable->toggleColumnsForPedigree(showColumns);
 
 
-			std::string sortField = "";
+				std::string sortField = "";
 
-			if(clp.hasSwitchSet("-s")){
-				sortField = clp.getSwitchArgument("-s",1);
+				if(clp.hasSwitchSet("-s")){
+					sortField = clp.getSwitchArgument("-s",1);
+				}
+
+				pedigreeSet.addPedigreesFromDataTable(dataTable,j,sortField);
+				pedigreeSet.draw(dataTable);
 			}
-
-			pedigreeSet.addPedigreesFromDataTable(dataTable,j,sortField);
-			pedigreeSet.draw(dataTable);
 		}
+	}catch(...){
+		cout << "Pedigree Generation Error" << endl;
 	}
 
 	return;
