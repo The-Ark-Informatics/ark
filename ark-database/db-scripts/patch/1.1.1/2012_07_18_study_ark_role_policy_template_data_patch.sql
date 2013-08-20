d584c5a6f233a2a1d5438fccd76f510755c2f8e3
Thilina Ranaweera <tranaweera@gmail.com>
2013-08-20 02:53:44 +0000

    Automate the id generation
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/tags/1.1.1c@8350 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1/2012_07_18_study_ark_role_policy_template_data_patch.sql
+++ ark-database/db-scripts/patch/1.1.1/2012_07_18_study_ark_role_policy_template_data_patch.sql
@@ -1,10 +1,33 @@
 USE study;
 
+set @role_id :=0;
+set @module_id :=0;
+set @function_id :=0;
+set @permission_create :=0;
+set @permission_read :=0;
+set @permission_update :=0;
+set @permission_delete :=0;
+
+select @role_id:= id from ark_role where name = 'LIMS Administrator';
+
+select @module_id:= id from ark_module where name = 'LIMS';
+
+select @function_id:= id from ark_function where name = 'BIOSPECIMEN_AND_BIOCOLLECTION_CUSTOM_FIELD_UPLOAD';
+
+select @permission_create:=id from ark_permission where name ='CREATE';
+
+select @permission_read:=id from ark_permission where name ='READ';
+
+select @permission_update:=id from ark_permission where name ='UPDATE';
+
+select @permission_delete:=id from ark_permission where name ='DELETE'; 
+
+
 insert into ark_role_policy_template (ark_role_id, ark_module_id, ark_function_id, ark_permission_id)
-values(12, 5, 58, 1);
+values(@role_id, @module_id, @function_id, @permission_create);
 insert into ark_role_policy_template (ark_role_id, ark_module_id, ark_function_id, ark_permission_id)
-values(12, 5, 58, 2);
+values(@role_id, @module_id, @function_id, @permission_read);
 insert into ark_role_policy_template (ark_role_id, ark_module_id, ark_function_id, ark_permission_id)
-values(12, 5, 58, 3);
+values(@role_id, @module_id, @function_id, @permission_update);
 insert into ark_role_policy_template (ark_role_id, ark_module_id, ark_function_id, ark_permission_id)
-values(12, 5, 58, 4);
+values(@role_id, @module_id, @function_id, @permission_delete);
