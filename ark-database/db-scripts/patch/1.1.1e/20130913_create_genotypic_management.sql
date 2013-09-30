df8e16aa98ab9ba8caf4608422361a353a8ef72e
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-09-30 06:16:22 +0000

    all tables innodb force. for genotypic module
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8397 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1e/20130913_create_genotypic_management.sql
+++ ark-database/db-scripts/patch/1.1.1e/20130913_create_genotypic_management.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `geno`.`command`;
 CREATE  TABLE `geno`.`command` (
   `ID` INT NOT NULL ,
   `NAME` VARCHAR(255) NULL ,
@@ -5,8 +6,9 @@ CREATE  TABLE `geno`.`command` (
   `SERVER_URL` VARCHAR(255) NULL ,
   `INPUT_FILE_FORMAT` VARCHAR(255) NULL ,
   `OUTPUT_FILE_FORMAT` VARCHAR(255) NULL COMMENT 'may need error output too i guess or alt output' ,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) )  ENGINE=InnoDB ;
 
+DROP TABLE IF EXISTS `geno`.`process`;
 CREATE  TABLE `geno`.`process` (
   `ID` INT NOT NULL ,
   `NAME` VARCHAR(255) NULL ,
@@ -22,33 +24,37 @@ CREATE  TABLE `geno`.`process` (
   `OUTPUT_KEPT` BOOLEAN NULL ,
   `OUTPUT_SERVER` VARCHAR(255) NULL ,
   `COMMAND_ID` INT NULL COMMENT 'The command is the task/program that will perform the process/transform' ,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) ) ENGINE=InnoDB ;
 
+DROP TABLE IF EXISTS `geno`.`transformation_template`;
 CREATE  TABLE `geno`.`transformation_template` (
   `ID` INT NOT NULL ,
   `NAME` VARCHAR(255) NOT NULL ,
   `DESCRIPTION` VARCHAR(4096) NULL ,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) ) ENGINE=InnoDB ;
 
 /**where do I link a person**/
+DROP TABLE IF EXISTS `geno`.`transformation`;
 CREATE  TABLE `geno`.`transformation` (
   `ID` INT NOT NULL ,
   `NAME` VARCHAR(255) NOT NULL ,
   `DESCRIPTION` VARCHAR(4096) NULL ,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) ) ENGINE=InnoDB ;
 
+DROP TABLE IF EXISTS `geno`.`transformation_process`;
 CREATE  TABLE `geno`.`transformation_process` (
   `ID` INT NOT NULL ,
   `TRANSFORMATION_ID` INT NOT NULL,
   `PROCESS_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) ) ENGINE=InnoDB ;
 
+DROP TABLE IF EXISTS `geno`.`lss_process`;
 /* really could be person*/
 CREATE  TABLE `geno`.`lss_process` (
   `ID` INT NOT NULL ,
   `TRANSFORMATION_ID` INT NOT NULL,
   `LSS_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`) );
+  PRIMARY KEY (`ID`) ) ENGINE=InnoDB ;
 
 
 
