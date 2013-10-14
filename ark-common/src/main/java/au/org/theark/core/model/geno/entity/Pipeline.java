ab40e0a81124d09813730db6fda6b89f3dd09a79
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-10-14 06:13:57 +0000

    added new command entity for geno...skeleton
    git-svn-id: svn://localhost/ARK/trunk@8424 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/model/geno/entity/Pipeline.java
+++ ark-common/src/main/java/au/org/theark/core/model/geno/entity/Pipeline.java
@@ -1,18 +1,18 @@
 package au.org.theark.core.model.geno.entity;
 
-import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import au.org.theark.core.Constants;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
 
 
 @Entity
@@ -23,9 +23,8 @@ public class Pipeline implements java.io.Serializable {
 	private Long id;
 	private String name;
 	private String description;
-	private Command command;
-	private Date startTime;//TODO: 
-	private Date endTime;
+	private Set<Process> processes = new HashSet<Process>(0);
+	private Set<LinkSubjectStudy> linkSubjectStudies = new HashSet<LinkSubjectStudy>(0);
 
 	@Id
 	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
@@ -55,65 +54,24 @@ public class Pipeline implements java.io.Serializable {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "COMMAND_ID")
-	public Command getCommand() {
-		return command;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "person")
+	public Set<Process> getProcesses() {
+		return processes;
 	}
 
-	public void setCommand(Command command) {
-		this.command = command;
-	}
-	
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "START_DATE", nullable = false, length = 19)
-	public Date getStartTime() {
-		return this.startTime;
+	public void setProcesses(Set<Process> processes) {
+		this.processes = processes;
 	}
 
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}	
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "START_DATE", nullable = false, length = 19)
-	public Date getEndTime() {
-		return this.endTime;
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "person")
+	public Set<LinkSubjectStudy> getLinkSubjectStudies() {
+		return this.linkSubjectStudies;
 	}
 
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
+	public void setLinkSubjectStudies(Set<LinkSubjectStudy> linkSubjectStudies) {
+		this.linkSubjectStudies = linkSubjectStudies;
 	}
-/*
-	@Column(name = "NAME", length = 100)
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Column(name = "NAME", length = 100)
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Column(name = "NAME", length = 100)
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-*/
 
 }
