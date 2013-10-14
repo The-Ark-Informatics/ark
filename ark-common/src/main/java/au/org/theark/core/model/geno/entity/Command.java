4fe047eb0d33eff8817c4212d6260a5f8e6f2748
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-10-14 05:56:40 +0000

    added new command entity for geno...skeleton
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8421 2beaffb7-5388-1629-0768-26d37724f1fc

---
+++ ark-common/src/main/java/au/org/theark/core/model/geno/entity/Command.java
@@ -0,0 +1,70 @@
+package au.org.theark.core.model.geno.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+
+@Entity
+@Table(name = "COMMAND", schema = Constants.STUDY_SCHEMA)
+public class Command implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String serverURL;
+	private String location;
+	private String inputFileFormat;//TODO: 
+	private String outputFileFormat;
+
+	@Id
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+/*
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+*/
+
+}
