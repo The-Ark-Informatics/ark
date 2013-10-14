4784633f2e7761a5a33071f94ed008d20e79c48c
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-10-14 06:09:08 +0000

    added new command entity for geno...skeleton
    git-svn-id: svn://localhost/ARK/trunk@8423 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/geno/entity/Process.java
@@ -0,0 +1,91 @@
+package au.org.theark.core.model.geno.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import au.org.theark.core.Constants;
+
+
+@Entity
+@Table(name = "PROCESS", schema = Constants.STUDY_SCHEMA)
+public class Process implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String description;
+	private Command command;
+	private Date startTime;//TODO: 
+	private Date endTime;
+
+	@Id
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	@Column(name = "DESCRIPTION", length = 100)
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "COMMAND_ID")
+	public Command getCommand() {
+		return command;
+	}
+
+	public void setCommand(Command command) {
+		this.command = command;
+	}
+	
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "START_DATE", nullable = false, length = 19)
+	public Date getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}	
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "START_DATE", nullable = false, length = 19)
+	public Date getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+}
