3b9ef05b6411f962e83bedb5e0a09c37421e275d
chrisandcaz@gmail.com <chrisandcaz@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-11-04 04:08:51 +0000

    basic form for pipeline data
    git-svn-id: svn://localhost/ARK/trunk@8453 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-registry/src/main/java/au/org/theark/registry/web/component/invoice/GenoContainerPanel.java
+++ ark-registry/src/main/java/au/org/theark/registry/web/component/invoice/GenoContainerPanel.java
@@ -6,6 +6,7 @@
  */
 package au.org.theark.registry.web.component.invoice;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -27,6 +28,13 @@ public class GenoContainerPanel extends AbstractContainerPanel<Pipeline> {
 
 	// Panels
 	private DetailPanel						detailPanel;
+	
+	@Override
+	protected void onBeforeRender() {
+		//arkCrudContainerVO.showDetailPanelInEditMode(AjaxRequestTarget.get());
+		super.onBeforeRender();
+	}
+	
 	/**
 	 * @param id
 	 */
@@ -36,13 +44,18 @@ public class GenoContainerPanel extends AbstractContainerPanel<Pipeline> {
 		containerForm = new ContainerForm("containerForm", cpModel);
 		containerForm.add(initialiseFeedBackPanel());
 		containerForm.add(initialiseDetailPanel());
-		add(containerForm);	}
+			
+		add(containerForm);	
+			
+	}
 	
 	@Override
 	protected WebMarkupContainer initialiseDetailPanel() {
-		detailPanel = new DetailPanel("detailPanel", feedBackPanel, arkCrudContainerVO, containerForm);
+		detailPanel = new DetailPanel("detailsPanel", feedBackPanel, arkCrudContainerVO, containerForm);
 		detailPanel.initialisePanel();
 		arkCrudContainerVO.getDetailPanelContainer().add(detailPanel);
+		arkCrudContainerVO.getDetailPanelContainer().setVisible(true);
+		arkCrudContainerVO.getDetailPanelFormContainer().setEnabled(true);
 		return arkCrudContainerVO.getDetailPanelContainer();
 	}
 	@Override
