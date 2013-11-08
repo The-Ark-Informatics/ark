d257dcafec7c17d9de06a037a03f2a65add2a45a
chrisandcaz@gmail.com <chrisandcaz@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2013-11-08 06:28:18 +0000

    Added service/dao layer methods for search results
    git-svn-id: svn://localhost/ARK/trunk@8471 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-common/src/main/java/au/org/theark/core/dao/GenoDao.java
+++ ark-common/src/main/java/au/org/theark/core/dao/GenoDao.java
@@ -6,6 +6,11 @@
  */
 package au.org.theark.core.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import au.org.theark.core.model.geno.entity.Pipeline;
@@ -29,8 +34,49 @@ public class GenoDao extends HibernateSessionDao implements IGenoDao {
 		getSession().delete(p);
 	}
 
-	public long getPipelineCount(Study study){
-		return study.getPipelines().size();//TODO  woefully inappropriate speed/efficiency FIX
+	public int getPipelineCount(Pipeline p) {
+		// Handle for study not in context
+		if (p.getStudy() == null) {
+			return 0;
+		}
+
+		Criteria criteria = buildGeneralPipelineCriteria(p);
+		criteria.setProjection(Projections.rowCount());
+		Long totalCount = (Long) criteria.uniqueResult();
+		return totalCount.intValue();
 	}
+
+	public List<Pipeline> searchPageablePipelines(Pipeline p, int first,
+			int count) {
+		Criteria criteria = buildGeneralPipelineCriteria(p);
+		criteria.setFirstResult(first);
+		criteria.setMaxResults(count);
+		List<Pipeline> list = criteria.list();
+		return list;
+	}
+
+	private Criteria buildGeneralPipelineCriteria(Pipeline p) {
+		Criteria criteria = getSession().createCriteria(Pipeline.class);
+		if (p.getId() != null){ 
+			criteria.add(Restrictions.eq("id", p.getId()));
+		}
+		
+		if(p.getName() != null){
+			criteria.add(Restrictions.eq("name", p.getName()));
+		}
 	
+		if(p.getDescription()!=null) {
+			criteria.add(Restrictions.eq("description", p.getDescription()));
+		}
+		
+		if(p.getStudy() != null) {
+			criteria.add(Restrictions.eq("study", p.getStudy()));
+		}
+		
+		return criteria;
+	}
+
+	public long getPipelineCount(Study study) {
+		return 0;
+	}
 }
