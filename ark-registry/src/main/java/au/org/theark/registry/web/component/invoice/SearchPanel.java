060e5e77ff3e45c8e36e78fe309481a10cbb9cdb
chrisandcaz@gmail.com <chrisandcaz@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2013-11-08 01:23:21 +0000

    Updated to include search and results forms
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8466 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-registry/src/main/java/au/org/theark/registry/web/component/invoice/SearchPanel.java
+++ ark-registry/src/main/java/au/org/theark/registry/web/component/invoice/SearchPanel.java
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 /*******************************************************************************
  * Copyright (c) 2011  University of Western Australia. All rights reserved.
  * 
@@ -55,49 +54,3 @@ public class SearchPanel extends Panel {
 		add(searchForm);
 	}
 }
-=======
-package au.org.theark.registry.web.component.invoice;
-
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
-
-import au.org.theark.core.model.geno.entity.Pipeline;
-import au.org.theark.core.vo.ArkCrudContainerVO;
-import au.org.theark.registry.web.component.invoice.form.SearchForm;
-
-/**
- * @author nivedann
- *
- */
-public class SearchPanel extends Panel{
-
-
-	private static final long serialVersionUID = 1L;
-	private FeedbackPanel	feedbackPanel;
-	private ArkCrudContainerVO	arkCrudContainerVO;
-	private CompoundPropertyModel<Pipeline> cpModel;
-	
-	/**
-	 * Constructor
-	 * @param id
-	 */
-	public SearchPanel(String id, CompoundPropertyModel<Pipeline> cpModel,ArkCrudContainerVO arkCrudContainerVO, FeedbackPanel feedBackPanel) {
-		super(id);
-		this.feedbackPanel = feedBackPanel;
-		this.arkCrudContainerVO = arkCrudContainerVO;
-		this.cpModel = cpModel;
-	}
-	
-	/**
-	 * Instantiate the SearchForm and link it with the Panel
-	 */
-	public void initialisePanel() {
-		
-		SearchForm searchForm = new SearchForm(au.org.theark.core.Constants.SEARCH_FORM,cpModel,feedbackPanel,arkCrudContainerVO);//,feedbackPanel,arkCrudContainerVO);
-		add(searchForm);
-		
-	}
-
-}
->>>>>>> .r8461
