c67204607a3ec0c3a93886febfc09033d28aaf97
Thilina Ranaweera <tranaweera@gmail.com>
2013-12-12 05:24:14 +0000

    Ark remove the registry tab
    git-svn-id: svn://localhost/ARK/branches/ark-informatics-korea@8527 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-build-pwd.sh
+++ ark-build-pwd.sh
@@ -23,12 +23,6 @@ if [ "$?" != "0" ]; then
 exit 1
 fi
 
-cd $WORKSPACE_DIR/ark-registry
-mvn clean install
-if [ "$?" != "0" ]; then
-exit 1
-fi
-
 cd $WORKSPACE_DIR/ark-phenotypic
 mvn clean install
 if [ "$?" != "0" ]; then
