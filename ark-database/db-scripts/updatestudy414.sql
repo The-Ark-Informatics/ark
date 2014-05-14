ae0e4a257639cdd4d50dc94159810de45fd4ced6
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-14 07:23:37 +0000

    update 414 study to clear way for the real 414.
    git-svn-id: svn://localhost/ARK/trunk@8601 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/updatestudy414.sql
+++ ark-database/db-scripts/updatestudy414.sql
@@ -1,4 +1,4 @@
-INSERT INTO `study` (`ID`,`NAME`,`DESCRIPTION`,`DATE_OF_APPLICATION`,`ESTIMATED_YEAR_OF_COMPLETION`,`CHIEF_INVESTIGATOR`,`CO_INVESTIGATOR`,`AUTO_GENERATE_SUBJECTUID`,`SUBJECTUID_START`,`STUDY_STATUS_ID`,`SUBJECTUID_PREFIX`,`CONTACT_PERSON`,`CONTACT_PERSON_PHONE`,`LDAP_GROUP_NAME`,`AUTO_CONSENT`,`SUB_STUDY_BIOSPECIMEN_PREFIX`,`STUDY_LOGO`,`FILENAME`,`SUBJECTUID_TOKEN_ID`,`SUBJECTUID_PADCHAR_ID`,`SUBJECT_KEY_PREFIX`,`SUBJECT_KEY_START`,`PARENT_ID`,`AUTO_GENERATE_BIOSPECIMENUID`,`AUTO_GENERATE_BIOCOLLECTIONUID`) 
+INSERT INTO study.`study` (`ID`,`NAME`,`DESCRIPTION`,`DATE_OF_APPLICATION`,`ESTIMATED_YEAR_OF_COMPLETION`,`CHIEF_INVESTIGATOR`,`CO_INVESTIGATOR`,`AUTO_GENERATE_SUBJECTUID`,`SUBJECTUID_START`,`STUDY_STATUS_ID`,`SUBJECTUID_PREFIX`,`CONTACT_PERSON`,`CONTACT_PERSON_PHONE`,`LDAP_GROUP_NAME`,`AUTO_CONSENT`,`SUB_STUDY_BIOSPECIMEN_PREFIX`,`STUDY_LOGO`,`FILENAME`,`SUBJECTUID_TOKEN_ID`,`SUBJECTUID_PADCHAR_ID`,`SUBJECT_KEY_PREFIX`,`SUBJECT_KEY_START`,`PARENT_ID`,`AUTO_GENERATE_BIOSPECIMENUID`,`AUTO_GENERATE_BIOCOLLECTIONUID`) 
 VALUES (415,'Australian Ovarian Cancer Study',NULL,NULL,NULL,'Dr Nikolajs Zeps',NULL,1,NULL,1,'WTN','Dr Nikolajs Zeps',NULL,'WARTN',1,NULL,NULL,NULL,1,8,NULL,NULL,274,1,1);
 
 update study.study set parent_id = 415 where id =415;
@@ -8,23 +8,20 @@ update	lims.BioCollectionUid_Template	SET study_id = 415 where study_id =414;
 update	lims.Biospecimen	SET study_id = 415 where study_id =414;
 update	lims.BiospecimenUid_Template	SET study_id = 415 where study_id =414;
 update	lims.Study_Inv_Site	SET study_id = 415 where study_id =414;
-update	report	SET study_id = 415 where study_id =414;
-update	entity	SET study_id = 415 where study_id =414;
 update	reporting.Search	SET study_id = 415 where study_id =414;
-update	entity	SET study_id = 415 where study_id =414;
-update	Ark_User_Role	SET study_id = 415 where study_id =414;
-update	Consent	SET study_id = 415 where study_id =414;
-update	Correspondences	SET study_id = 415 where study_id =414;
-update	Custom_Field	SET study_id = 415 where study_id =414;
-update	Custom_Field_Group	SET study_id = 415 where study_id =414;
-update	Link_Study_ArkModule	SET study_id = 415 where study_id =414;
-update	Link_Study_Studycomp	SET study_id = 415 where study_id =414;
-update	Link_Study_Studysite	SET study_id = 415 where study_id =414;
-update	Link_Study_Substudy	SET study_id = 415 where study_id =414;
-update	Link_Subject_Contact	SET study_id = 415 where study_id =414;
-update	Link_Subject_Study	SET study_id = 415 where study_id =414;
-update	Link_Subject_Studycomp	SET study_id = 415 where study_id =414;
-update	Study_Comp	SET study_id = 415 where study_id =414;
-update	Upload	SET study_id = 415 where study_id =414;
+update	study.Ark_User_Role	SET study_id = 415 where study_id =414;
+update	study.Consent	SET study_id = 415 where study_id =414;
+update	study.Correspondences	SET study_id = 415 where study_id =414;
+update	study.Custom_Field	SET study_id = 415 where study_id =414;
+update	study.Custom_Field_Group	SET study_id = 415 where study_id =414;
+update	study.Link_Study_ArkModule	SET study_id = 415 where study_id =414;
+update	study.Link_Study_Studycomp	SET study_id = 415 where study_id =414;
+update	study.Link_Study_Studysite	SET study_id = 415 where study_id =414;
+update	study.Link_Study_Substudy	SET study_id = 415 where study_id =414;
+update	study.Link_Subject_Contact	SET study_id = 415 where study_id =414;
+update	study.Link_Subject_Study	SET study_id = 415 where study_id =414;
+update	study.Link_Subject_Studycomp	SET study_id = 415 where study_id =414;
+update	study.Study_Comp	SET study_id = 415 where study_id =414;
+update	study.Upload	SET study_id = 415 where study_id =414;
 DELETE FROM `study`.`sTUDY` WHERE `ID`='414';
 
