4908ec92ccfa4f96787b939a98bb9e455cd227e6
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-05-15 07:11:44 +0000

    more improvements to migration
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8611 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/migration/Step_2_Inventory.sql
+++ ark-database/db-scripts/migration/Step_2_Inventory.sql
@@ -1,5 +1,28 @@
--- INVENTORY SQL
+/* WASOS */
+SET @STUDYKEY = 414;
+SET @STUDYNAME= 'WASOS';
+SET @AUTOGEN_SUBJECT = 1;
+SET @AUTOGEN_BIOSPECIMEN = 1;
+SET @AUTOGEN_BIOCOLLECTION = 1;
+-- before setting each of these params check that this can work...ie; that there is not some weird multiple prefix for a given study.
 
+-- SET @SUBJECT_PADCHAR = 8; -- no of chars to pad out
+-- apparently subject prefix comes from wager
+-- SET @SUBJECT_PREFIX = 'RAV';
+
+
+SET @BIOCOLLECTIONUID_PREFIX = 'WAS';
+-- SET @BIOCOLLECTIONUID_TOKEN_ID = 1;
+SET @BIOCOLLECTIONUID_TOKEN_DASH = '';
+SET @BIOCOLLECTIONUID_PADCHAR_ID = 5;
+
+SET @BIOSPECIMEN_UID_PREFIX = 'WAS';
+-- SET @BIOSPECIMENUID_TOKEN_ID = 1;
+SET @BIOSPECIMENUID_PADCHAR_ID = 6;
+
+
+/* Vit A
+-- INVENTORY SQL
 SET @STUDYKEY = 22;
 SET @STUDYNAME= 'Vitamin A';
 SET @AUTOGEN_SUBJECT = 1;
@@ -17,9 +40,8 @@ SET @BIOCOLLECTIONUID_PADCHAR_ID = 5;
 SET @BIOSPECIMENUID_PREFIX = 'VTA';
 -- SET @BIOSPECIMENUID_TOKEN_ID = 1;
 SET @BIOSPECIMENUID_PADCHAR_ID = 5;
-
 SET @SITE_PERMITTED = 'WADB (SCGH)' ; 
-
+*/
 
 
 
@@ -103,6 +125,10 @@ and b.studykey=@STUDYKEY) ct
 where t.traykey = ct.traykey)
 )
 ))) s;
+
+select id, name from lims.inv_site;  -- ensure sites match wager vs prod for any of this to work!!! 
+select id, name from lims.inv_site;
+
 
 -- FREEZERS
 INSERT INTO `lims`.`inv_freezer`
