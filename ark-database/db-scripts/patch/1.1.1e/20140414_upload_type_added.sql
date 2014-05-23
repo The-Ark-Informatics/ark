8b18e539c66a7420c04854dd8c78785cd9758fa6
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-05-23 04:02:59 +0000

    commit several db imprtvements
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8631 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1e/20140414_upload_type_added.sql
+++ ark-database/db-scripts/patch/1.1.1e/20140414_upload_type_added.sql
@@ -4,3 +4,13 @@
 -- add a new uploader type;
 
 INSERT INTO `study`.`upload_type` (`NAME`, `DESCRIPTION`, `ARK_MODULE_ID`) VALUES ('Biospecimen Location Updater', 'Upload the locations of Biospecimen only', '5');
+
+
+
+select * from lims.unit where name = 'ml';
+
+select * from lims.biospecimen where unit_id = 17;
+
+select * from lims.bio_transaction where unit_id = 17;
+
+
