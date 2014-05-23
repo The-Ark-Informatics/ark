aae53f677024244ca324d59fb06618a661a18ec1
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-23 04:22:40 +0000

    add file
    git-svn-id: svn://localhost/ARK/trunk@8638 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ oldarkbuildpw.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Define global variables
+#export DEV_USER=$USER   #assumes the same as current user
+export WORKSPACE_DIR=`pwd` #relative to current user HOME dir
+
+# Maven build/install/package the source 
+cd $WORKSPACE_DIR/ark-common
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-admin
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-study
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-registry
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-phenotypic
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+#cd $WORKSPACE_DIR/ark-geno
+#mvn clean install
+#if [ "$?" != "0" ]; then
+#exit 1
+#fi
+
+cd $WORKSPACE_DIR/ark-report
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-lims
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-work-tracking
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+cd $WORKSPACE_DIR/ark-container
+mvn clean package
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
+
+SCRIPTPATH=$(dirname $0)
+echo "Ark war file built"
