05572185b458eaeeda2319e545939c22c75db208
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-27 08:44:23 +0000

    start of new megacleanup script.  Committing stepwise so steps are obvious...Step 12:  Fix order within scripts more...BUT NOW THE BIG STEP IS FIGURING OUT WHAT WE DON'T want to delete...ie; what is useful look up tables to us
    git-svn-id: svn://localhost/ARK/trunk@8676 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/base/1.1.1d/deleteFromAllAdminTables.sql
+++ ark-database/db-scripts/base/1.1.1d/deleteFromAllAdminTables.sql
@@ -1,6 +1,5 @@
 
 delete from  admin.bilable_item  ;
-delete from  admin.billable_item_type  ;
 delete from  admin.billable_item_type_status  ;
 delete from  admin.work_request  ;
 delete from  admin.work_request_status  ;
@@ -8,4 +7,6 @@ delete from  admin.researcher  ;
 delete from  admin.researcher_role  ;
 delete from  admin.researcher_status  ;
 
-delete from  admin.billing_type  ;
+-- LOOK UP TABLES
+-- delete from  admin.billing_type  ;
+-- delete from  admin.billable_item_type  ;
