b61a3f189b61529f20812a0832ba106e1d7bd4f1
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-27 06:32:46 +0000

    start of new megacleanup script.  Committing stepwise so steps are obvious...Step 7:  Fix order of running scripts
    git-svn-id: svn://localhost/ARK/trunk@8667 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/base/1.1.1d/bigBaseDatabaseCleanupScript_IF_YOU_RUN_IN_PROD_NOAH_WILL_KILL_YOU.sql
+++ ark-database/db-scripts/base/1.1.1d/bigBaseDatabaseCleanupScript_IF_YOU_RUN_IN_PROD_NOAH_WILL_KILL_YOU.sql
@@ -12,16 +12,11 @@ show tables in reporting;
 /*******************
 step 2...do all the delete froms in order that they are permitted.
 *******************/
+-- show tables in audit;
+-- show tables in admin;
+-- show tables in reporting;
+-- show tables in geno;
+-- show tables in pheno;
+-- show tables in lims;
 -- show tables in study;
 
--- show tables in lims;
-
--- show tables in geno;
-
--- show tables in pheno;
-
--- show tables in admin;
-
--- show tables in audit;
-
--- show tables in reporting;
