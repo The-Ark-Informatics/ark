4b54abc211366aa784e702b57ff86ca9d34ac46b
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-27 06:04:39 +0000

    start of new megacleanup script.  Committing stepwise so steps are obvious...Step 1:  get all tables
    git-svn-id: svn://localhost/ARK/trunk@8661 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/base/1.1.1d/bigBaseDatabaseCleanupScript_IF_YOU_RUN_IN_PROD_NOAH_WILL_KILL_YOU.sql
@@ -0,0 +1,27 @@
+/*******************
+step one.  Get all tables to generate the sql down the line.
+*******************/
+show tables in study;
+show tables in lims;
+show tables in geno;
+show tables in pheno;
+show tables in admin;
+show tables in audit;
+show tables in reporting;
+
+/*******************
+step 2...do all the delete froms in order that they are permitted.
+*******************/
+-- show tables in study;
+
+-- show tables in lims;
+
+-- show tables in geno;
+
+-- show tables in pheno;
+
+-- show tables in admin;
+
+-- show tables in audit;
+
+-- show tables in reporting;
