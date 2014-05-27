96a63b979bf2b995723a69186a5148db8133fad1
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-27 06:21:10 +0000

    start of new megacleanup script.  Committing stepwise so steps are obvious...Step 5:  rename to sql
    git-svn-id: svn://localhost/ARK/trunk@8665 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/base/1.1.1d/deleteFromAllAdminTables.sql
@@ -0,0 +1,10 @@
+
+delete from ,admin.,bilable_item,,;
+delete from ,admin.,billable_item_type,,;
+delete from ,admin.,billable_item_type_status,,;
+delete from ,admin.,billing_type,,;
+delete from ,admin.,researcher,,;
+delete from ,admin.,researcher_role,,;
+delete from ,admin.,researcher_status,,;
+delete from ,admin.,work_request,,;
+delete from ,admin.,work_request_status,,;
---
+++ ark-database/db-scripts/base/1.1.1d/deleteFromAllReportingTables.sql
@@ -0,0 +1,20 @@
+
+delete from ,reporting.,biocollection_field,;
+delete from ,reporting.,biocollection_field_search,;
+delete from ,reporting.,biospecimen_field,;
+delete from ,reporting.,biospecimen_field_search,;
+delete from ,reporting.,consent_status_field,;
+delete from ,reporting.,consent_status_field_search,;
+delete from ,reporting.,custom_field_display_search,;
+delete from ,reporting.,demographic_field,;
+delete from ,reporting.,demographic_field_search,;
+delete from ,reporting.,query_filter,;
+delete from ,reporting.,query_filter_grouping,;
+delete from ,reporting.,query_grouping,;
+delete from ,reporting.,query_grouping_grouping,;
+delete from ,reporting.,report_output_format,;
+delete from ,reporting.,report_template,;
+delete from ,reporting.,search,;
+delete from ,reporting.,search_payload,;
+delete from ,reporting.,search_result,;
+delete from ,reporting.,search_subject,;
