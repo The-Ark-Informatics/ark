5858d2078110f2f60bfcd47a2fb0209b24aa098d
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-05-30 02:36:26 +0000

    many improvements to base script
    git-svn-id: svn://localhost/ARK/trunk@8681 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/base/1.1.1d/deleteFromAllAdminTables.sql
+++ ark-database/db-scripts/base/1.1.1d/deleteFromAllAdminTables.sql
@@ -1,12 +1,18 @@
+select * from admin.billable_item_type;
 
 delete from  admin.bilable_item  ;
-delete from  admin.billable_item_type_status  ;
 delete from  admin.work_request  ;
-delete from  admin.work_request_status  ;
 delete from  admin.researcher  ;
-delete from  admin.researcher_role  ;
-delete from  admin.researcher_status  ;
+delete from  admin.billable_item_type  ; -- not actually a look up...is study specific
 
--- LOOK UP TABLES
+
+
+/*******************
+ LOOK UP TABLES DO NOT DELETE
 -- delete from  admin.billing_type  ;
 -- delete from  admin.billable_item_type  ;
+-- delete from  admin.billable_item_type_status  ;
+-- delete from  admin.researcher_role  ;
+-- delete from  admin.researcher_status  ;
+-- delete from  admin.work_request_status  ;
+*******************/
