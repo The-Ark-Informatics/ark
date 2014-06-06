5467d9840386f693d7767ff2299f7e2770d98a0a
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-06-06 04:54:31 +0000

    IRD
    git-svn-id: svn://localhost/ARK/trunk@8699 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/migration/Step_1_WagerMigration_upToButNotIncludingInventory.sql
+++ ark-database/db-scripts/migration/Step_1_WagerMigration_upToButNotIncludingInventory.sql
@@ -1,7 +1,7 @@
-SET @STUDY_GROUP_NAME = 'WAFSS';
-SET @STUDYKEY = 17;
-SET @STUDYNAME= 'WAFSS';
-SET @AUTOGEN_SUBJECT = 0;
+SET @STUDY_GROUP_NAME = 'IRD';
+SET @STUDYKEY = 18;
+SET @STUDYNAME= 'IRD';
+SET @AUTOGEN_SUBJECT = 1;
 SET @AUTOGEN_BIOSPECIMEN = 1;
 SET @AUTOGEN_BIOCOLLECTION = 1;
 -- before setting each of these params check that this can work...ie; that there is not some weird multiple prefix for a given study.
@@ -10,14 +10,15 @@ SET @AUTOGEN_BIOCOLLECTION = 1;
 -- apparently subject prefix comes from wager
 -- SET @SUBJECT_PREFIX = 'RAV';
 
-SET @BIOCOLLECTIONUID_PREFIX = 'WFC';
+SET @BIOCOLLECTIONUID_PREFIX = 'IRD';
 -- SET @BIOCOLLECTIONUID_TOKEN_ID = 1;
 SET @BIOCOLLECTIONUID_TOKEN_DASH = '';
 SET @BIOCOLLECTIONUID_PADCHAR_ID = 5;
 	
-SET @BIOSPECIMENUID_PREFIX = 'WFB';
+SET @BIOSPECIMENUID_PREFIX = 'IRD';
 -- SET @BIOSPECIMENUID_TOKEN_ID = 1;
 SET @BIOSPECIMENUID_PADCHAR_ID = 6;
+
 
 
 -- SET @SITE_PERMITTED = 'WADB (SCGH)' ;  -- IF MORE THAN ONE FIX THIS 
@@ -405,9 +406,6 @@ GROUP BY bt.biospecimenkey
 ) bt ON b.biospecimenkey = bt.biospecimenkey
 SET b.units = bt.units;
 */
-
-select * from  wagerlab.IX_BIOSPECIMEN b 
-where b.studykey=414 and b.units is null;
 
 /*
 -- Trav : is this not done already?
