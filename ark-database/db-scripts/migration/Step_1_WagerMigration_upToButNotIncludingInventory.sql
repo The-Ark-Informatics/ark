aff79e9fff0d2677e577e1c30ee1b1e91a68299c
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-07-03 08:42:14 +0000

    improvements plus finalization of wafss
    git-svn-id: svn://localhost/ARK/trunk@8758 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/migration/Step_1_WagerMigration_upToButNotIncludingInventory.sql
+++ ark-database/db-scripts/migration/Step_1_WagerMigration_upToButNotIncludingInventory.sql
@@ -1,3 +1,31 @@
+-- VUS PARAMETERS
+-- latest
+SET @STUDY_GROUP_NAME = 'VUS';
+SET @STUDYKEY = 24;
+SET @STUDYNAME= 'VUS';
+SET @AUTOGEN_SUBJECT = 0;
+SET @AUTOGEN_BIOSPECIMEN = 1;
+SET @AUTOGEN_BIOCOLLECTION = 1;
+-- before setting each of these params check that this can work...ie; that there is not some weird multiple prefix for a given study.
+
+-- SET @SUBJECT_PADCHAR = 8; -- no of chars to pad out
+-- apparently subject prefix comes from wager
+-- SET @SUBJECT_PREFIX = 'RAV';
+
+SET @BIOCOLLECTIONUID_PREFIX = 'VUC';
+-- SET @BIOCOLLECTIONUID_TOKEN_ID = 1;
+SET @BIOCOLLECTIONUID_TOKEN_DASH = '';
+SET @BIOCOLLECTIONUID_PADCHAR_ID = 5;
+	
+SET @BIOSPECIMENUID_PREFIX = 'VUB';
+-- SET @BIOSPECIMENUID_TOKEN_ID = 1;
+SET @BIOSPECIMENUID_PADCHAR_ID = 6;
+
+select * from zeus.study where studyname = 'VUS';
+-- '24', 'VUS', 'Venous Ulcer Study', '2013-04-02 15:52:43', '2007-12-21 14:20:32', 'Hilary Wallace', 'WAGERLAB', 'Deep Vein Thrombosis', NULL, '0', '1', 'DVT', NULL, '2'
+
+
+/*
 SET @STUDY_GROUP_NAME = 'WAFSS';
 SET @STUDYKEY = 17;
 SET @STUDYNAME= 'WAFSS';
@@ -18,9 +46,8 @@ SET @BIOCOLLECTIONUID_PADCHAR_ID = 5;
 SET @BIOSPECIMENUID_PREFIX = 'WFB';
 -- SET @BIOSPECIMENUID_TOKEN_ID = 1;
 SET @BIOSPECIMENUID_PADCHAR_ID = 6;
-
-
-/*SET @STUDY_GROUP_NAME = 'IRD';
+------------------------------
+SET @STUDY_GROUP_NAME = 'IRD';
 SET @STUDYKEY = 18;
 SET @STUDYNAME= 'IRD';
 SET @AUTOGEN_SUBJECT = 1;
@@ -180,7 +207,7 @@ ORDER BY ID;
 
 UPDATE study.study set parent_id = @STUDYKEY where id = @STUDYKEY;
 
-select * from study.study;
+select * from study.study where id = @STUDYKEY;
 
 
 
