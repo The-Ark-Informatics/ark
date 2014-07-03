3c32d3e11c403190fc1a687f0dd3d94c17067918
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-07-03 06:22:20 +0000

    fixed multiple pre-existing queries upon running in production - and running a review on what had been run
    git-svn-id: svn://localhost/ARK/trunk@8755 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1e/20140617_add_mS_to_pheno_unit_types.sql
+++ ark-database/db-scripts/patch/1.1.1e/20140617_add_mS_to_pheno_unit_types.sql
@@ -1,3 +1,4 @@
 INSERT INTO `study`.`unit_type` (`ARK_FUNCTION_ID`, `NAME`, `DESCRIPTION`, `MEASUREMENT_TYPE_ID`, `DISPLAY_ORDER`) VALUES ('14', 'mS', 'mS', '999', '2');
 
-select * from 
+select * from  `study`.`unit_type` -- note in testing - u need it for the corect arkfunctionid
+
