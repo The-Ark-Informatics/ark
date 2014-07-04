cc21e2a83d7f26b876fa39f6795ecd1e7b1ad57f
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-07-04 05:25:32 +0000

    adding a unique constraint that we need and naming it properly...still yet to be run because data was not constraint...
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8764 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.1.1e/201406_05_unique_constraint_inv_cell_biospecimin_only_one_location.sql
+++ ark-database/db-scripts/patch/1.1.1e/201406_05_unique_constraint_inv_cell_biospecimin_only_one_location.sql
@@ -1,4 +1,8 @@
 
 -- stops repeat locations for a given biospecimen
-alter table lims.inv_cell 
-add unique index (biospecimen_id);
+-- alter table lims.inv_cell 
+-- add unique index (biospecimen_id);
+-- of course this can't run yet in prod because it is not allowing it because WE DIDNT HAVE THIS...so logically there are some duplications;
+
+ALTER TABLE `lims`.`inv_cell` 
+ADD UNIQUE INDEX `uq_inv_cell_biospecimen` (`BIOSPECIMEN_ID` ASC) ;
