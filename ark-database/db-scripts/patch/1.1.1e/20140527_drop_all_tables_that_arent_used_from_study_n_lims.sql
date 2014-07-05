cc777269039a158ef4ab721e1e6e07bc5c343cba
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-07-05 08:01:38 +0000

    applying fixes to prod
    git-svn-id: svn://localhost/ARK/trunk@8769 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1e/20140527_drop_all_tables_that_arent_used_from_study_n_lims.sql
+++ ark-database/db-scripts/patch/1.1.1e/20140527_drop_all_tables_that_arent_used_from_study_n_lims.sql
@@ -4,14 +4,38 @@ this is a script to delete any tables we don't see being useful anymore
 
 */
 
-/* drop tables that should not have data - STUDY*/
+/* select * from tables that should not have data - STUDY*/
+select * from  study.temp ;
+select * from  study.study_user_role_permission_view ;
+select * from  study.role_policy ;-- delete from  study.role_policy ;  -- THIS IS A VIEW...CAN IT BE DELETED
+select * from   study.question_answer ;  -- CAN THIS BE DELETED?
+select * from    study.registration_status ; -- CAN THIS BE DELETED?
+select * from     study.csv_blob ;  -- TODO CHECK IF STILL NEEDED - maybe select * from and all entities
+select * from     study.link_site_contact ; -- TODO CHECK IF STILL NEEDED - select * from and all entities
 drop table study.temp ;
-drop view study.study_user_role_permission_view ;
-drop view study.role_policy ;-- delete from  study.role_policy ;  -- THIS IS A VIEW...CAN IT BE DELETED
+-- not convinced this is a good idea!!!!!!  drop view study.study_user_role_permission_view ;
+-- not convinced this is a good idea!!!!!!  drop view study.role_policy ;-- delete from  study.role_policy ;  -- THIS IS A VIEW...CAN IT BE DELETED
 drop table  study.question_answer ;  -- CAN THIS BE DELETED?
 drop table   study.registration_status ; -- CAN THIS BE DELETED?
 drop table    study.csv_blob ;  -- TODO CHECK IF STILL NEEDED - maybe drop and all entities
 drop table    study.link_site_contact ; -- TODO CHECK IF STILL NEEDED - drop and all entities
+
+
+/* select * from tables that should not have data - LIMS*/
+select * from  lims.biodata ;
+select * from  lims.biodata_field_group ;
+select * from  lims.biodata_field_lov ;
+select * from  lims.biodata_unit ;
+select * from  lims.biodata_type ;
+select * from  lims.biodata_lov_list ;
+select * from  lims.biodata_group_criteria ;
+select * from  lims.biodata_group ;
+select * from  lims.biodata_field ;
+select * from  lims.biodata_criteria ;
+select * from  lims.tmp_bio_transaction ;
+select * from   lims.tmp_biospecimen ;
+select * from   lims.tmp_biospecimen_custom_field_data ;
+
 
 
 /* drop tables that should not have data - LIMS*/
