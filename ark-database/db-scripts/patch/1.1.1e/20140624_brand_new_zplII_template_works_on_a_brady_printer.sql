17161b8bf37f49ecd36f779cfe22046e81de11ce
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-08-18 07:40:45 +0000

    lots of goodies for zebra printing
    git-svn-id: svn://localhost/ARK/trunk@8819 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1e/20140624_brand_new_zplII_template_works_on_a_brady_printer.sql
+++ ark-database/db-scripts/patch/1.1.1e/20140624_brand_new_zplII_template_works_on_a_brady_printer.sql
@@ -3,8 +3,35 @@
 -- Trav's made a biospecimen printer label using zebra languge that is tested working on brady bbp33.  Hopefully works everywhere.
 INSERT INTO `lims`.`barcode_label` (`NAME`, `DESCRIPTION`, `LABEL_PREFIX`, `LABEL_SUFFIX`, `VERSION`) 
 VALUES ('zebra or brady biospecimen', 'Generic biospecimen label using ZPL - tested on brady BBP33', '', '', '1');
+UPDATE `lims`.`barcode_label` SET `LABEL_PREFIX`='^XA\\n', `LABEL_SUFFIX`='^XZ\\n' WHERE `ID`='26';
 
 select * from lims.barcode_label;
+
+select * from lims.barcode_label where id = 4;
+
+
+INSERT INTO lims.`barcode_label` (`STUDY_ID`,`BARCODE_PRINTER_ID`,`NAME`,`DESCRIPTION`,`LABEL_PREFIX`,`LABEL_SUFFIX`,`VERSION`,`BARCODE_PRINTER_NAME`) 
+VALUES (NULL,1,'zebra biospecimen v3','Generic Zebra Biospecimen Label v3','','',3,NULL);
+-- created 26
+
+select * from lims.barcode_label_data where barcode_label_id = 26;
+
+INSERT INTO lims.`barcode_label_data` (`BARCODE_LABEL_ID`,`COMMAND`,`X_COORD`,`Y_COORD`,`P1`,`P2`,`P3`,`P4`,`P5`,`P6`,`P7`,`P8`,`QUOTE_LEFT`,`DATA`,`QUOTE_RIGHT`,`LINE_FEED`) 
+							VALUES (26,				'^FO',		-10,		20,'^BXN','10','100','\n',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'\n');
+
+INSERT INTO lims.`barcode_label_data` (`BARCODE_LABEL_ID`,`COMMAND`,`X_COORD`,`Y_COORD`,`P1`,`P2`,`P3`,`P4`,`P5`,`P6`,`P7`,`P8`,`QUOTE_LEFT`,`DATA`,`QUOTE_RIGHT`,`LINE_FEED`) 
+							VALUES (26,						'^FD',		'',		'',		NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'\"','${biospecimenUid}','\"','^FS\r\n');
+
+INSERT INTO `barcode_label_data` (`ID`,`BARCODE_LABEL_ID`,`COMMAND`,`X_COORD`,`Y_COORD`,`P1`,`P2`,`P3`,`P4`,`P5`,`P6`,`P7`,`P8`,`QUOTE_LEFT`,`DATA`,`QUOTE_RIGHT`,`LINE_FEED`)
+							VALUES (26,				'b',		105,5,'D','h3','c16','r16',NULL,NULL,NULL,NULL,'\"','${biospecimenUid}','\"','\r\n');
+INSERT INTO `barcode_label_data` (`ID`, `BARCODE_LABEL_ID`,`COMMAND`,`X_COORD`,`Y_COORD`,`P1`,`P2`,`P3`,`P4`,`P5`,`P6`,`P7`,`P8`,`QUOTE_LEFT`,`DATA`,`QUOTE_RIGHT`,`LINE_FEED`)
+							VALUES (26,				'A',		250,5,'0','1','1','1','N',NULL,NULL,NULL,'\"','${biospecimenUid}','\"','\r\n');
+INSERT INTO `barcode_label_data` (`ID`,`BARCODE_LABEL_ID`,`COMMAND`,`X_COORD`,`Y_COORD`,`P1`,`P2`,`P3`,`P4`,`P5`,`P6`,`P7`,`P8`,`QUOTE_LEFT`,`DATA`,`QUOTE_RIGHT`,`LINE_FEED`)
+							VALUES (26,				'A',		250,35,'0','1','1','2','N',NULL,NULL,NULL,'\"','${dateOfBirth}','\"','\r\n');
+
+
+select * from lims.barcode_label_data where BARCODE_LABEL_ID = 4;
+
 
 USE `lims`;
 
