0325ca296052f1ec5e96403ac3518de8a792e854
George Gooden <gecgooden@gmail.com>
2014-08-25 02:10:27 +0000

    Adding missing classes from previous commit
    git-svn-id: svn://localhost/ARK/trunk@8834 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-common/src/main/java/au/org/theark/core/model/disease/entity/AffectionCustomFieldData.java
@@ -0,0 +1,109 @@
+package au.org.theark.core.model.disease.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import au.org.theark.core.Constants;
+import au.org.theark.core.model.study.entity.CustomFieldDisplay;
+import au.org.theark.core.model.study.entity.ICustomFieldData;
+import au.org.theark.core.model.study.entity.LinkSubjectStudy;
+
+@Entity
+@Table(name = "AFFECTION_CUSTOM_FIELD_DATA", schema = Constants.DISEASE_SCHEMA)
+public class AffectionCustomFieldData implements ICustomFieldData {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private Affection affection;
+	private CustomFieldDisplay customFieldDisplay;
+	private String textDataValue;
+	private Date dateDataValue;
+	private String errorDataValue;
+	private Double numberDataValue;
+	
+	public AffectionCustomFieldData() {
+		
+	}
+
+	@Id
+	@SequenceGenerator(name = "affection_custom_field_data_generator", sequenceName = "AFFECTION_CUSTOM_FIELD_DATA_SEQ")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "affection_custom_field_data_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "AFFECTION_ID")
+	public Affection getAffection() {
+		return this.affection;
+	}
+
+	public void setAffection(Affection affection) {
+		this.affection = affection;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CUSTOM_FIELD_DISPLAY_ID")
+	public CustomFieldDisplay getCustomFieldDisplay() {
+		return customFieldDisplay;
+	}
+
+	public void setCustomFieldDisplay(CustomFieldDisplay customFieldDisplay) {
+		this.customFieldDisplay = customFieldDisplay;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "DATE_DATA_VALUE")
+	public Date getDateDataValue() {
+		return dateDataValue;
+	}
+
+	public void setDateDataValue(Date dateDataValue) {
+		this.dateDataValue = dateDataValue;
+	}
+
+	@Column(name = "ERROR_DATA_VALUE")
+	public String getErrorDataValue() {
+		return errorDataValue;
+	}
+
+	public void setErrorDataValue(String errorDataValue) {
+		this.errorDataValue = errorDataValue;
+	}
+
+	@Column(name = "NUMBER_DATA_VALUE")
+	public Double getNumberDataValue() {
+		return numberDataValue;
+	}
+
+	public void setNumberDataValue(Double numberDataValue) {
+		this.numberDataValue = numberDataValue;
+	}
+
+	@Column(name = "TEXT_DATA_VALUE")
+	public String getTextDataValue() {
+		return textDataValue;
+	}
+
+	public void setTextDataValue(String textDataValue) {
+		this.textDataValue = textDataValue;
+	}
+
+}
---
+++ ark-common/src/main/java/au/org/theark/core/model/disease/entity/Position.java
@@ -0,0 +1,67 @@
+package au.org.theark.core.model.disease.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import au.org.theark.core.Constants;
+
+@Entity
+@Table(name = "POSITION", schema = Constants.DISEASE_SCHEMA)
+public class Position implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Gene gene; //not sure if needed. Maybe just for easier navigation
+	
+	public Position() {
+		
+	}
+	
+	public Position(Long id) {
+		this.id = id;
+	}
+	
+	@Id
+	@SequenceGenerator(name = "position_generator", sequenceName = "POSITION_SEQUENCE")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "position_generator")
+	@Column(name = "ID", unique = true, nullable = false, precision = 22, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "NAME", length = 100)
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "GENE_ID")
+	public Gene getGene(){
+		return this.gene;
+	}
+	
+	public void setGene(Gene gene) {
+		this.gene = gene;
+	}
+	
+}
