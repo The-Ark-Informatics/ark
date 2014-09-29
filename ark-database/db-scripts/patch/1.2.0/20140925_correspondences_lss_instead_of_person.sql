a019d3659f2c7c49261b8e5e21dcab28e5bc796a
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-09-29 07:34:09 +0000

    commit correspondence changes and update script for correspondence which needs thorough testing on a subset before hitting prod.  I have only tested on a small subset (with success)
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8858 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.2.0/20140925_correspondences_lss_instead_of_person.sql
+++ ark-database/db-scripts/patch/1.2.0/20140925_correspondences_lss_instead_of_person.sql
@@ -6,3 +6,18 @@ CHANGE COLUMN PERSON_ID PERSON_ID INT(11) NULL ,
 	REFERENCES study.person (ID)
 	ON DELETE NO ACTION 
 	ON UPDATE NO ACTION;
+
+
+ALTER TABLE `study`.`correspondences` ADD COLUMN `LINK_SUBJECT_STUDY_ID` INT(11) NULL  AFTER `BILLABLE_ITEM_ID` ;
+
+
+UPDATE study.correspondences a
+SET LINK_SUBJECT_STUDY_ID = 
+	(select id from study.LINK_SUBJECT_STUDY lss
+	where lss.person_id = a.person_id
+	and lss.study_id = a.study_id)
+where study_id is not null
+and person_id is not null;
+
+/*after thorough testing remove person_id column*/
+
