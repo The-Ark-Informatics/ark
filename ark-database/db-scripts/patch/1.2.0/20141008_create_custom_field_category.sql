9004f5f0bfffaa8c9444ebcb2c0d1bc3a679652a
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-10-09 01:09:04 +0000

    added custom field category concept
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8869 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.2.0/20141008_create_custom_field_category.sql
+++ ark-database/db-scripts/patch/1.2.0/20141008_create_custom_field_category.sql
@@ -2,13 +2,26 @@
 * Creating the capability for a custom field to belong to a category.
 *  So far it has been requested the field belongs to one category and one custom field only
 *****/
+-- drop table study.custom_field_category;
 
-CREATE TABLE study.`custom_field_category` (
+CREATE TABLE study.`category` (
 	`ID` int(11) NOT NULL AUTO_INCREMENT,
 	`PARENT_ID` int(11) NULL,
 	`STUDY_ID` int(11) NULL,
 	`NAME` varchar(255) NOT NULL,
 	`DESCRIPTION` text NULL,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB charset=latin1;
+
+/********
+* The concept of one to one can't just be added to the custom field because it is one to one
+*	PER Study
+******/
+    
+CREATE TABLE study.`custom_field_category` (
+	`ID` int(11) NOT NULL AUTO_INCREMENT,
+	`CUSTOM_FIELD_ID` int(11) NULL,
+	`CATEGORY_ID` int(11) NULL,
 	PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB charset=latin1;
 
