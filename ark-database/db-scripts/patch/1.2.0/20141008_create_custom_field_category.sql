535f5399968c77efa8be0ed4c51dc356195a5688
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-10-09 05:30:59 +0000

    correcting original db structure after a rethought
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8870 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.2.0/20141008_create_custom_field_category.sql
+++ ark-database/db-scripts/patch/1.2.0/20141008_create_custom_field_category.sql
@@ -11,18 +11,25 @@ CREATE TABLE study.`category` (
 	`NAME` varchar(255) NOT NULL,
 	`DESCRIPTION` text NULL,
 	PRIMARY KEY (`ID`)
-) ENGINE=InnoDB charset=latin1;
+) ENGINE=InnoDB charset=latin1; -- or shall we go utf 8?
 
 /********
 * The concept of one to one can't just be added to the custom field because it is one to one
 *	PER Study
-******/
-    
+* Correction - the custom field is per study...so one to one at that level is fine
+*
+drop table study.custom_field_category
 CREATE TABLE study.`custom_field_category` (
 	`ID` int(11) NOT NULL AUTO_INCREMENT,
 	`CUSTOM_FIELD_ID` int(11) NULL,
 	`CATEGORY_ID` int(11) NULL,
 	PRIMARY KEY (`ID`)
-) ENGINE=InnoDB charset=latin1;
+) ENGINE=InnoDB charset=latin1; -- or shall we go utf 8?
+******/
+    
+ALTER TABLE `study`.`custom_field_display` 
+ADD COLUMN `CATEGORY_ID` INT(11) NULL AFTER `ALLOW_MULTIPLE_SELECTION`;
+
+
 
 
