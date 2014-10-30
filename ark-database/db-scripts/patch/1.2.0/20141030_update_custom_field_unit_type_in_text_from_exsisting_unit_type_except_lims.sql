ea620f16b38a1936520db8e40652e18c51f80825
sanjayamaddu@gmail.com <sanjayamaddu@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-10-30 06:58:18 +0000

    SQL script to rectify the change in ARK-1280.
    git-svn-id: svn://localhost/ARK/trunk@8893 35ee9b83-dd2c-47f4-99a2-c706670c48ce

---
+++ ark-database/db-scripts/patch/1.2.0/20141030_update_custom_field_unit_type_in_text_from_exsisting_unit_type_except_lims.sql
@@ -0,0 +1,7 @@
+UPDATE study.custom_field 	c inner join unit_type u on c.UNIT_TYPE_ID=u.id  
+set c.UNIT_TYPE_IN_TEXT=u.name  
+where c.ARK_FUNCTION_ID not in 
+(select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
+
+Update study.custom_field c set UNIT_TYPE_ID=null where c.ARK_FUNCTION_ID not in
+(select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
