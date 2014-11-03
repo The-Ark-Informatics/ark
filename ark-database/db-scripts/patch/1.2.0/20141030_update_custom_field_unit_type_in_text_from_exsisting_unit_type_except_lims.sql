3240fe6a6e3532b535b63d30b0653021e3e636c2
Thilina Ranaweera <tranaweera@gmail.com>
2014-11-03 03:24:31 +0000

    Add schema to the database scripts
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8897 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-database/db-scripts/patch/1.2.0/20141030_update_custom_field_unit_type_in_text_from_exsisting_unit_type_except_lims.sql
+++ ark-database/db-scripts/patch/1.2.0/20141030_update_custom_field_unit_type_in_text_from_exsisting_unit_type_except_lims.sql
@@ -1,7 +1,10 @@
-UPDATE study.custom_field 	c inner join unit_type u on c.UNIT_TYPE_ID=u.id  
-set c.UNIT_TYPE_IN_TEXT=u.name  
-where c.ARK_FUNCTION_ID not in 
-(select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
+use study;
 
-Update study.custom_field c set UNIT_TYPE_ID=null where c.ARK_FUNCTION_ID not in
-(select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
+UPDATE custom_field c 
+	inner join unit_type u on c.UNIT_TYPE_ID=u.id  
+set c.UNIT_TYPE_IN_TEXT=u.name  
+where c.ARK_FUNCTION_ID not in (select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
+
+Update custom_field c 
+set UNIT_TYPE_ID=null 
+where c.ARK_FUNCTION_ID not in (select id from ark_function a where  a.name='LIMS_COLLECTION' or a.name='BIOSPECIMEN');
