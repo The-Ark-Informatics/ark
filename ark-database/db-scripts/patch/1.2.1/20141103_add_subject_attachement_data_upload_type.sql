427fd4db0479bda8c3b5fbd658d2e918df9cd2e0
Thilina Ranaweera <tranaweera@gmail.com>
2014-11-06 01:01:50 +0000

    Correct the Ark module id selection as a foreign key
    git-svn-id: svn://localhost/ARK/trunk@8914 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.2.1/20141103_add_subject_attachement_data_upload_type.sql
+++ ark-database/db-scripts/patch/1.2.1/20141103_add_subject_attachement_data_upload_type.sql
@@ -4,7 +4,7 @@ set @module_id = 0;
 
 SELECT @id := max(id)+1 from `study`.`upload_type`;
 
-SELECT @module_id := max(id)+1 from `study`.`ark_module`;
+SELECT @module_id := id from `study`.`ark_module` where name='Subject';
 
 INSERT INTO `study`.`upload_type` (`ID`, `NAME`, `DESCRIPTION`, `ARK_MODULE_ID`) VALUES (@id, 'Subject Attachment Data', 'Upload the subject attachments and consent attachments', @module_id);
 
