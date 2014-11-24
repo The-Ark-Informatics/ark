317b7dd9ba0973fcf300ce2a511f98a552cda3ab
travis.endersby@gmail.com <travis.endersby@gmail.com@2beaffb7-5388-1629-0768-26d37724f1fc>
2014-11-24 16:40:43 +0000

    build script not complete - committed to allow prod build
    git-svn-id: https://ark-informatics.googlecode.com/svn/ARK/trunk@8927 2beaffb7-5388-1629-0768-26d37724f1fc

--- ark-build-pwd.sh
+++ ark-build-pwd.sh
@@ -59,6 +59,13 @@ if [ "$?" != "0" ]; then
 exit 1
 fi
 
+
+cd $WORKSPACE_DIR/ark-disease
+mvn clean install
+if [ "$?" != "0" ]; then
+exit 1
+fi
+
 cd $WORKSPACE_DIR/ark-container
 mvn clean package
 if [ "$?" != "0" ]; then
