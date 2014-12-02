6d87e45aaa08644cf949e81ce195aca4621bd917
travis.endersby@gmail.com <travis.endersby@gmail.com@35ee9b83-dd2c-47f4-99a2-c706670c48ce>
2014-12-02 05:26:15 +0000

    fix and add some testing to existing db scripts
    git-svn-id: svn://localhost/ARK/trunk@8949 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- ark-database/db-scripts/patch/1.1.1e/20140617_add_constraint_forbidding_duplicate_biospecimen_custom_data.sql
+++ ark-database/db-scripts/patch/1.1.1e/20140617_add_constraint_forbidding_duplicate_biospecimen_custom_data.sql
@@ -2,3 +2,7 @@ ALTER TABLE `lims`.`biospecimen_custom_field_data`
 DROP INDEX `FK_BIOSPECFDATA_BIOSPECIMEN_ID` 
 , ADD UNIQUE INDEX `FK_BIOSPECFDATA_BIOSPECIMENID_CFDID` (`BIOSPECIMEN_ID` ASC, `CUSTOM_FIELD_DISPLAY_ID` ASC) ;
 
+select * from `lims`.`biospecimen_custom_field_data`  a,  `lims`.`biospecimen_custom_field_data`  b
+where a.biospecimen_id = b.biospecimen_id
+and a.id<>b.ID
+and a.CUSTOM_FIELD_DISPLAY_ID = b.CUSTOM_FIELD_DISPLAY_ID
