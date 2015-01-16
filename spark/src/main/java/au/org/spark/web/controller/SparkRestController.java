9f2c1b322bdc9defe6b347f7ffed90dfc009b1a1
Thilina Ranaweera <tranaweera@gmail.com>
2015-01-16 06:43:29 +0000

    SPARK spring-joss integration
    git-svn-id: svn://localhost/ARK/trunk@8984 35ee9b83-dd2c-47f4-99a2-c706670c48ce

--- spark/src/main/java/au/org/spark/web/controller/SparkRestController.java
+++ spark/src/main/java/au/org/spark/web/controller/SparkRestController.java
@@ -1,19 +1,33 @@
 package au.org.spark.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
 
+import au.org.spark.service.OpenStackService;
 import au.org.spark.web.view.JavaBean;
+
 
 @RestController
 public class SparkRestController {
+	
+	@Autowired
+	OpenStackService openStackService;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public @ResponseBody JavaBean getJSONJavaBean() {
+    	openStackService.listContainers();
         return new JavaBean();
+    }
+    
+    @RequestMapping(value = "/listContainers", method = RequestMethod.GET)
+    public @ResponseBody List<String> listContainers() {
+    	return openStackService.listContainers();
     }
     
     @RequestMapping(value="/mapping/producesjson", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
@@ -25,5 +39,5 @@ public class SparkRestController {
 	public @ResponseBody JavaBean byProducesXml() {
 		return new JavaBean();
 	}
-
+    
 }
