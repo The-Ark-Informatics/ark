a7f02951a49b8f8c1e23c092fa5d5a03c6b52288
thilina <tranaweera@gmail.com>
2015-08-10 11:42:40 +1000

    Remove 20150807 script to avoid duplicte the changes applied in 20150629 and 20150622

--- ark-database/db-scripts/patch/1.2.0b/20150807_add_family_id_and_inbreed_allowed_to_study_pedigree_config.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `study`.`study_pedigree_config` ADD `family_id` INT(11)  NULL  DEFAULT NULL;
-ALTER TABLE `study`.`study_pedigree_config` ADD `inbreed_allowed` TINYINT(4)  NULL  DEFAULT NULL;
-
