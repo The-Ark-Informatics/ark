a9cc9633cc062f118b8c0cebdc4ffaee28e0bf7a
George Gooden <gecgooden@gmail.com>
2015-09-11 09:25:24 +0800

    Fixed incorrect database name in sql

--- ark-database/db-scripts/patch/1.2.0b/20150910_add_parent_uid_to_data_extraction.sql
+++ ark-database/db-scripts/patch/1.2.0b/20150910_add_parent_uid_to_data_extraction.sql
@@ -1,4 +1,4 @@
-INSERT INTO `lims`.`biospecimen_field` (`ENTITY`, `FIELD_NAME`, `PUBLIC_FIELD_NAME`, `FIELD_TYPE_ID`, `FILTERABLE`)
+INSERT INTO `reporting`.`biospecimen_field` (`ENTITY`, `FIELD_NAME`, `PUBLIC_FIELD_NAME`, `FIELD_TYPE_ID`, `FILTERABLE`)
 VALUES
 	('Biospecimen', 'parentUID', 'Parent UID', 1, 1);
 
