91676fa89178a930bb394fc7ed5cb04ebe93a437
sanjayamaddu <m.sanjaya@yahoo.co.uk>
2015-09-17 11:44:43 +1000

    Change to the sql to auto get the subject and family form custom field type table.

--- ark-database/db-scripts/patch/1.2.0b/20150814_add_column_ark_module_id_to_custom_field_type.sql
+++ ark-database/db-scripts/patch/1.2.0b/20150814_add_column_ark_module_id_to_custom_field_type.sql
@@ -8,13 +8,17 @@ ADD CONSTRAINT `fk_custom_field_type_1`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-UPDATE `study`.`custom_field_type` 
-SET `ARK_MODULE_ID`=(SELECT id FROM study.ark_module where name='Study') 
-WHERE ID =1;
+set @subject_id=(SELECT ID FROM `study`.`custom_field_type` where name='SUBJECT');
+
+set @family_id=(SELECT ID FROM `study`.`custom_field_type` where name='FAMILY');
 
 UPDATE `study`.`custom_field_type` 
 SET `ARK_MODULE_ID`=(SELECT id FROM study.ark_module where name='Study') 
-WHERE ID =2;
+WHERE ID =@subject_id;
+
+UPDATE `study`.`custom_field_type` 
+SET `ARK_MODULE_ID`=(SELECT id FROM study.ark_module where name='Study') 
+WHERE ID =@family_id;
 
 
 INSERT INTO `study`.`custom_field_type` (`ARK_MODULE_ID`, `NAME`, `DESCRIPTION`) 
