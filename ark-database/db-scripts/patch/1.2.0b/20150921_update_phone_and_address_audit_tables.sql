282d0465e965d0fa72ea002d32b4a9be20f67e7b
thilina <tranaweera@gmail.com>
2015-09-21 12:05:43 +1000

    Fix subject contact save issue

---
+++ ark-database/db-scripts/patch/1.2.0b/20150921_update_phone_and_address_audit_tables.sql
@@ -0,0 +1,6 @@
+alter table audit.aud_phone add column PHONE_STATUS_ID bigint(20);
+
+alter table audit.aud_phone add column silent bigint(20);
+
+alter table audit.aud_address add column person_id bigint(20);
+
