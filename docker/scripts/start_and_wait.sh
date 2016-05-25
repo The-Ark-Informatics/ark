c764ba8c2721effb0fea0abd55c982fb01405666
George Gooden <gecgooden@gmail.com>
2016-05-25 14:33:31 +0800

    Removed logging from start and wait script

--- docker/scripts/start_and_wait.sh
+++ docker/scripts/start_and_wait.sh
@@ -11,12 +11,7 @@ if [ -n "$DOCKER_HOST" ]; then
 	IP=$(echo $DOCKER_HOST | cut -f2 -d: | cut -f3 -d/)
 fi
 
-docker-compose -f $YAML logs -f &
-
 while [ $(curl --write-out %{http_code} --silent --output /dev/null ${IP}:8080/ark) -eq "000" ]; do
 	echo "Not ready, sleeping"
 	sleep 5;
 done
-
-kill $! 
-
