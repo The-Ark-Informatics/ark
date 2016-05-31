478462637474329ea749e97e7413d5193938b6eb
George Gooden <gecgooden@gmail.com>
2016-05-31 12:25:19 +0800

    Updated docker-mvn start procedure to create correct testing properties

--- docker/mvn/start.sh
+++ docker/mvn/start.sh
@@ -15,22 +15,36 @@ if [ "$(ls -A .)" ]; then
 	touch ark-user-account/.completed
 
 	#Replace passwords and user names in ark-container/.../applicationContext.xml
-	directories=( main test )
-	for DIR in "${directories[@]}"; do
-		APP_CONTEXT=ark-container/src/${DIR}/resources/applicationContext.xml
-		cat ${APP_CONTEXT}.example |\
-			sed "s/ark-database/mysql/g" |\
-			sed "s/arkadmin/root/g" |\
-			sed "s/##MYSQL_PASSWORD##/${MYSQL_ROOT_PASSWORD}/g" |\
-			sed "s/ark-ldap\:/slapd\:/g" |\
-			sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
-			$APP_CONTEXT
+	APP_CONTEXT=ark-container/src/main/resources/applicationContext.xml
+	cat ${APP_CONTEXT}.example |\
+		sed "s/ark-database/mysql/g" |\
+		sed "s/arkadmin/root/g" |\
+		sed "s/##MYSQL_PASSWORD##/${MYSQL_ROOT_PASSWORD}/g" |\
+		sed "s/ark-ldap\:/slapd\:/g" |\
+		sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
+		$APP_CONTEXT
 
-		APP_PROP=ark-container/src/${DIR}/resources/application.properties
-		cat ${APP_PROP}.example |\
-			sed "s/\~\/arkFileData/\/opt\/ark-datastore/g" >\
-			${APP_PROP}
-	done
+	APP_PROP=ark-container/src/main/resources/application.properties
+	cat ${APP_PROP}.example |\
+		sed "s/\~\/arkFileData/\/opt\/ark-datastore/g" >\
+		${APP_PROP}
+	
+	
+	MYSQL_HOST="127.0.0.1"
+
+	APP_CONTEXT=ark-container/src/test/resources/applicationContext.xml
+	cat ${APP_CONTEXT}.example |\
+		sed "s/ark-database/${MYSQL_HOST}/g" |\
+		sed "s/arkadmin/root/g" |\
+		sed "s/##MYSQL_PASSWORD##/${MYSQL_ROOT_PASSWORD}/g" |\
+		sed "s/ark-ldap\:/slapd\:/g" |\
+		sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
+		$APP_CONTEXT
+
+	APP_PROP=ark-container/src/test/resources/application.properties
+	cat ${APP_PROP}.example |\
+		sed "s/\~\/arkFileData/\/opt\/ark-datastore/g" >\
+		${APP_PROP}
 
 	if [ $SKIPTEST -eq 0 ]; then
 		mvn package 
