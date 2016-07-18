cd39af275d8f15d321ccd9283efbbf958e5416c4
George Gooden <gecgooden@gmail.com>
2016-07-18 13:08:31 +0800

    Added Global search section to test case

--- ark-container/src/test/java/au/org/theark/test/integration/global/ITestGlobal.java
+++ ark-container/src/test/java/au/org/theark/test/integration/global/ITestGlobal.java
@@ -8,17 +8,20 @@ import au.org.theark.core.model.study.entity.Study;
 import au.org.theark.core.selenium.utilities.WicketBy;
 import au.org.theark.core.vo.ArkUserVO;
 import au.org.theark.test.integration.BaseIntegrationTest;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.eclipse.jetty.util.ArrayUtil;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -50,17 +53,8 @@ public class ITestGlobal extends BaseIntegrationTest {
         waitForElement(By.partialLinkText(studyName)).click();
         waitForElement(By.partialLinkText("Subject")).click();
 
-        String[] subjectUIDsToAdd = {"1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
-                "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic05", "pic 5",
-                "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-                "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8"}; //examples taken from NaturalOrderComparator.java
+        String[] subjectUIDsToAdd = {"C-1358920", "C-132494", "C-5001", "C-15921", "C-00001", "C-500"};
 
-        System.out.println("subjects to add length:" + subjectUIDsToAdd.length);
-        List scrambled = Arrays.asList(subjectUIDsToAdd);
-        Collections.shuffle(scrambled); //scramble the subjectUIDs to they are added in a random order
-        System.out.println("scrambled subjects to add length:" + scrambled.size());
-
-        int counter = 0;
         for(String subjectUID : subjectUIDsToAdd) {
             log.info("Creating Subject: " + subjectUID);
 
@@ -73,18 +67,9 @@ public class ITestGlobal extends BaseIntegrationTest {
             waitForElement(By.name("detailContainer:detailPanel:detailsForm:editButtonContainer:save")).click();
             waitForElement(By.className("feedbackPanelINFO"));
             waitForElement(By.name("detailContainer:detailPanel:detailsForm:editButtonContainer:cancel")).click();
-
-            counter++;
         }
 
-        System.out.println("Counter: " + counter);
-
-        String[] sortedSubjectUIDs = {"1-02", "1-2", "1-20", "10-20", "fred", "jane", "pic01",
-                "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic05", "pic 5",
-                "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-                "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8"};
-
-        System.out.println("sorted subjects length:" + sortedSubjectUIDs.length);
+        String[] sortedSubjectUIDs = {"C-00001", "C-500", "C-5001", "C-15921", "C-132494", "C-1358920"};
 
         waitForElement(By.name("searchContainer:searchComponentPanel:searchForm:new"));
 
@@ -104,6 +89,75 @@ public class ITestGlobal extends BaseIntegrationTest {
         assertEquals(sortedSubjectUIDs.length, tableSubjectUIDs.size());
 
         Assert.assertArrayEquals(sortedSubjectUIDs, tableSubjectUIDs.toArray());
+
+        //Add second study to test global search
+        waitForElement(By.partialLinkText("Study")).click();
+
+        studyName+="2";
+
+        waitForElement(By.name("searchContainer:searchStudyPanel:searchForm:new")).click();
+        waitForElement(By.name("detailContainer:detailPanel:detailForm:detailFormContainer:study.name")).sendKeys(studyName);
+        select = new Select(driver.findElement(By.name("detailContainer:detailPanel:detailForm:detailFormContainer:study.studyStatus")));
+        select.selectByVisibleText(studyStatus);
+        driver.findElement(By.name("detailContainer:detailPanel:detailForm:detailFormContainer:study.chiefInvestigator")).sendKeys(chiefInvestigator);
+
+        driver.findElement(By.name("detailContainer:detailPanel:detailForm:editButtonContainer:save")).click();
+
+        waitForElement(By.className("feedbackPanelINFO"));
+        driver.findElement(By.name("detailContainer:detailPanel:detailForm:editButtonContainer:cancel")).click();
+        waitForElement(By.partialLinkText(studyName)).click();
+        waitForElement(By.partialLinkText("Subject")).click();
+
+        String[] subjectUIDsToAdd2 = {"C-1358921", "C-133494", "C-501", "C-15921", "C-00001", "C-500"};
+
+        for(String subjectUID : subjectUIDsToAdd) {
+            log.info("Creating Subject: " + subjectUID);
+
+            waitForElement(By.name("searchContainer:searchComponentPanel:searchForm:new")).click();
+            waitForElement(By.name("detailContainer:detailPanel:detailsForm:detailFormContainer:linkSubjectStudy.subjectUID")).sendKeys(subjectUID);
+
+            select = new Select(driver.findElement(By.name("detailContainer:detailPanel:detailsForm:detailFormContainer:linkSubjectStudy.subjectStatus")));
+            select.selectByVisibleText("Subject");
+
+            waitForElement(By.name("detailContainer:detailPanel:detailsForm:editButtonContainer:save")).click();
+            waitForElement(By.className("feedbackPanelINFO"));
+            waitForElement(By.name("detailContainer:detailPanel:detailsForm:editButtonContainer:cancel")).click();
+        }
+
+        waitForElement(By.name("searchContainer:searchComponentPanel:searchForm:new"));
+        WebElement we = driver.findElement(WicketBy.wicketPath("moduleTabsList_tabs-container_tabs_5_link"));
+        log.info(we.getText());
+        log.info(we.toString());
+        log.info(we.getTagName());
+        we.click();
+
+        String[] sortedSubjectUIDs2 = {"C-00001", "C-500", "C-5001", "C-15921", "C-132494", "C-1358920",
+                                        "C-00001", "C-500", "C-501", "C-15921", "C-133494", "C-1358921" };
+
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // Now you can do whatever you need to do with it, for example copy somewhere
+        try {
+            FileUtils.copyFile(scrFile, new File("/tmp/screenshot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        table_element = waitForElement(By.className("dataview"));
+        tr_collection = table_element.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+        tr_collection.remove(0); //Removing the header
+
+        tableSubjectUIDs = new ArrayList<>();
+        for(WebElement trElement : tr_collection)
+        {
+            List<WebElement> td_collection = trElement.findElements(By.tagName("td"));
+
+            String subjectUID = td_collection.get(1).getText();
+            tableSubjectUIDs.add(subjectUID);
+        }
+
+        assertEquals(sortedSubjectUIDs2.length, tableSubjectUIDs.size());
+
+        Assert.assertArrayEquals(sortedSubjectUIDs2, tableSubjectUIDs.toArray());
 
         log.info("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
     }
