8ef705ecb84f5eaa04170e4937e132ebe66c3e8b
sanjayamaddu <m.sanjaya@yahoo.co.uk>
2016-08-25 13:54:31 +1000

    Last commit correction sql scripts.

--- ark-database/db-scripts/patch/1.2.0b/20160816_add_unique_key_to_subject_file_table_link_subject_study_and_study_component.sql
+++ ark-database/db-scripts/patch/1.2.0b/20160816_add_unique_key_to_subject_file_table_link_subject_study_and_study_component.sql
@@ -1,14 +1,7 @@
-
 ALTER TABLE `study`.`subject_file` 
-CHANGE COLUMN `STUDY_COMP_ID` `STUDY_COMP_ID` INT(11) NOT NULL ;
-ALTER TABLE `study`.`subject_file` 
-ADD CONSTRAINT `fk_subject_file_study_component`
-  FOREIGN KEY (`STUDY_COMP_ID`)
-  REFERENCES `study`.`study_comp` (`ID`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-
-ALTER TABLE `study`.`subject_file` 
+CHANGE COLUMN `STUDY_COMP_ID` `STUDY_COMP_ID` INT(11) NOT NULL ,
+DROP INDEX `fk_subject_file_study_comp_idx`,
+ADD INDEX `fk_subject_file_study_comp_idx` (`STUDY_COMP_ID` ASC),
+DROP INDEX `uk_link_subject_study_comp`,
 ADD UNIQUE INDEX `uk_link_subject_study_comp` (`LINK_SUBJECT_STUDY_ID` ASC, `STUDY_COMP_ID` ASC);
 
