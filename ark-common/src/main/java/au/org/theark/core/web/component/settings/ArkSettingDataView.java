78e18ea7d4ed3344b4726b625895ed1fca696509
George Gooden <gecgooden@gmail.com>
2017-02-15 15:38:56 +0800

    Adds fix for validation failing when removing a numeric field

--- ark-common/src/main/java/au/org/theark/core/web/component/settings/ArkSettingDataView.java
+++ ark-common/src/main/java/au/org/theark/core/web/component/settings/ArkSettingDataView.java
@@ -171,7 +171,7 @@ public class ArkSettingDataView<T extends Setting> extends DataView<Setting> {
     private boolean validateInput(Panel finalValuePanel) {
         switch(finalValuePanel.getClass().getSimpleName()) {
             case "SettingNumberPanel":
-                return StringUtils.isNumeric(((TextField) finalValuePanel.get("propertyValue")).getValue()) || ((TextField) finalValuePanel.get("propertyValue")).getValue().isEmpty();
+                return StringUtils.isNumericSpace(((TextField) finalValuePanel.get("propertyValue")).getValue());
             case "SettingCharacterPanel":
                 return true;
             default:
