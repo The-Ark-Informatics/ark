83fce43254bda66885f75bca5e6b1b0f60d8c6ef
George Gooden <gecgooden@gmail.com>
2017-02-16 13:24:19 +0800

    Revert "Removed file attachment dir change from docker init"
    This reverts commit daa340674d8b31873f0469e089da3ab081c336eb.

--- docker/base/ark-container-config.sh
+++ docker/base/ark-container-config.sh
@@ -9,6 +9,11 @@ cat ${APP_CONTEXT}.example |\
 	sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
 	$APP_CONTEXT
 
+APP_PROP=ark-container/src/main/resources/application.properties
+cat ${APP_PROP}.example |\
+	sed "s/\~\/arkFileData/\/opt\/ark-datastore/g" >\
+	${APP_PROP}
+
 APP_CONTEXT=ark-container/src/test/resources/applicationContext.xml
 cat ${APP_CONTEXT}.example |\
 	sed "s/ark-database/mysql/g" |\
@@ -18,3 +23,7 @@ cat ${APP_CONTEXT}.example |\
 	sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
 	$APP_CONTEXT
 
+APP_PROP=ark-container/src/test/resources/application.properties
+cat ${APP_PROP}.example |\
+	sed "s/\~\/arkFileData/\/opt\/ark-datastore/g" >\
+	${APP_PROP}
