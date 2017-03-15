ade24ba4d5f70f729eee691af6bd53bbaac3b700
George Gooden <gecgooden@gmail.com>
2017-03-15 16:21:09 +0800

    Added another missing reference to mysql_password...

--- docker/base/ark-container-config.sh
+++ docker/base/ark-container-config.sh
@@ -3,8 +3,8 @@
 APP_CONTEXT=ark-container/src/main/resources/applicationContext.xml
 cat ${APP_CONTEXT}.example |\
 	sed "s/ark-database/mysql/g" |\
-	sed "s/arkadmin/root/g" |\
-	sed "s/##MYSQL_PASSWORD##/${MYSQL_ROOT_PASSWORD}/g" |\
+	sed "s/arkadmin/${MYSQL_USER}/g" |\
+	sed "s/##MYSQL_PASSWORD##/${MYSQL_PASSWORD}/g" |\
 	sed "s/ark-ldap\:/slapd\:/g" |\
 	sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
 	$APP_CONTEXT
@@ -17,8 +17,8 @@ cat ${APP_PROP}.example |\
 APP_CONTEXT=ark-container/src/test/resources/applicationContext.xml
 cat ${APP_CONTEXT}.example |\
 	sed "s/ark-database/mysql/g" |\
-	sed "s/arkadmin/root/g" |\
-	sed "s/##MYSQL_PASSWORD##/${MYSQL_ROOT_PASSWORD}/g" |\
+	sed "s/arkadmin/${MYSQL_USER}/g" |\
+	sed "s/##MYSQL_PASSWORD##/${MYSQL_PASSWORD}/g" |\
 	sed "s/ark-ldap\:/slapd\:/g" |\
 	sed "s/##SLAPD_PASSWORD##/${SLAPD_PASSWORD}/g" >\
 	$APP_CONTEXT
