dfd70a1da836cabd40d1bf8e5d50eb72c03a3df9
George Gooden <gecgooden@gmail.com>
2017-03-15 15:30:54 +0800

    Changed mysql container to use custom mysql user instead of root.

--- docker/db/init/import_base.sh
+++ docker/db/init/import_base.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-sed "s/##ARK_USERNAME##/${ARK_USERNAME}/g" /docker-entrypoint-initdb.d/data_to_import.sql.example |
+sed "s/##ARK_USERNAME##/${ARK_USERNAME}/g" /docker-entrypoint-initdb.d/data_to_import.sql.example | \
 	mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD}
+
+for schema in $(mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD} -s -r -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != \"INFORMATION_SCHEMA\" AND SCHEMA_NAME != \"MYSQL\" AND SCHEMA_NAME != \"PERFORMANCE_SCHEMA\" AND SCHEMA_NAME != \"SYS\";" | tail -n +3); do
+	echo "GRANT ALL PRIVILEGES ON ${schema}.* TO '${MYSQL_USER}'@'%';" |
+	mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD}
+done
