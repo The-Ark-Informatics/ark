4e157ce9395197d6c48895657e2a62d81e531002
George Gooden <gecgooden@gmail.com>
2017-03-16 16:10:25 +0800

    Changed tail to remove only first line - needed 3 in testing but now only needs one...

--- docker/db/init/import_base.sh
+++ docker/db/init/import_base.sh
@@ -3,7 +3,7 @@
 sed "s/##ARK_USERNAME##/${ARK_USERNAME}/g" /docker-entrypoint-initdb.d/data_to_import.sql.example | \
 	mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD}
 
-for schema in $(mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD} -s -r -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != \"INFORMATION_SCHEMA\" AND SCHEMA_NAME != \"MYSQL\" AND SCHEMA_NAME != \"PERFORMANCE_SCHEMA\" AND SCHEMA_NAME != \"SYS\";" | tail -n +3); do
+for schema in $(mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD} -s -r -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != \"INFORMATION_SCHEMA\" AND SCHEMA_NAME != \"MYSQL\" AND SCHEMA_NAME != \"PERFORMANCE_SCHEMA\" AND SCHEMA_NAME != \"SYS\";" | tail -n +1); do
 	echo "GRANT ALL PRIVILEGES ON ${schema}.* TO '${MYSQL_USER}'@'%';" |
 	mysql --protocol=socket -uroot -p${MYSQL_ROOT_PASSWORD}
 done
