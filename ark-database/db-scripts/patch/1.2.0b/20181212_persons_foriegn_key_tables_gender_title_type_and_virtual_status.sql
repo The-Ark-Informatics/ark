4421854e103aa087b3b451b74b68fda89772386f
sanjayamaddu <m.sanjaya@yahoo.co.uk>
2019-02-25 13:45:34 +1100

    deleting renamed file and make gender type table id to the joinable to person table.

--- ark-database/db-scripts/patch/1.2.0b/20181212_persons_foriegn_key_tables_gender_title_type_and_virtual_status.sql
+++ ark-database/db-scripts/patch/1.2.0b/20181212_persons_foriegn_key_tables_gender_title_type_and_virtual_status.sql
@@ -1,3 +1,11 @@
+INSERT INTO `study`.`gender_type` (`ID`, `NAME`) VALUES ('3', 'Transgender');
+INSERT INTO `study`.`gender_type` (`ID`, `NAME`) VALUES ('4', 'Intersex');
+
+ALTER TABLE `study`.`person` 
+DROP FOREIGN KEY `fk_person_gender_type`;
+
+DELETE FROM `study`.`gender_type` WHERE `ID`='0';
+
 ALTER TABLE `study`.`gender_type` 
 CHANGE COLUMN `ID` `ID` INT(11) NOT NULL AUTO_INCREMENT ;
 
@@ -5,5 +13,11 @@ INSERT INTO `study`.`gender_type` (`ID`, `NAME`) VALUES ('5', 'Unknown');
 
 UPDATE study.person set GENDER_TYPE_ID=5 where GENDER_TYPE_ID=0;
 
-DELETE FROM `study`.`gender_type` WHERE `ID`='0';
+ALTER TABLE `study`.`person` 
+ADD CONSTRAINT `fk_person_gender_type`
+  FOREIGN KEY (`GENDER_TYPE_ID`)
+  REFERENCES `study`.`gender_type` (`ID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
 
