4feef34bc9287018bc286b7459d602147fa39470
George Gooden <gecgooden@gmail.com>
2019-09-07 14:37:56 +0800

    ARK-1945 Clean up activity monitoring panel code and fix issue where it always shows (#41)
    name of logged in user

--- ark-admin/src/main/java/au/org/theark/admin/web/component/activitymonitor/SearchResultsPanel.java
+++ ark-admin/src/main/java/au/org/theark/admin/web/component/activitymonitor/SearchResultsPanel.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright (c) 2011  University of Western Australia. All rights reserved.
- * 
+ *
  * This file is part of The Ark.
- * 
+ *
  * The Ark is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The Ark is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -21,7 +21,6 @@ package au.org.theark.admin.web.component.activitymonitor;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.InvalidSessionException;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
@@ -42,13 +41,12 @@ import au.org.theark.core.vo.ArkSubjectSessionVO;
 import au.org.theark.core.vo.ArkUserVO;
 
 public class SearchResultsPanel extends Panel {
-
-	private static final long	serialVersionUID	= -2656326926648860189L;
-	protected transient Logger		log					= LoggerFactory.getLogger(SearchResultsPanel.class);
+	private static final long serialVersionUID = -2656326926648860189L;
+	protected transient Logger logger = LoggerFactory.getLogger(SearchResultsPanel.class);
 	private FeedbackPanel feedbackPanel;
-	
+
 	@SpringBean(name = au.org.theark.core.Constants.ARK_COMMON_SERVICE)
-	private IArkCommonService		iArkCommonService;
+	private IArkCommonService iArkCommonService;
 
 	public SearchResultsPanel(String id, FeedbackPanel feedbackPanel) {
 		super(id);
@@ -58,36 +56,38 @@ public class SearchResultsPanel extends Panel {
 
 	@SuppressWarnings("unchecked")
 	public PageableListView<ArkSubjectSessionVO> buildPageableListView(IModel iModel) {
-		PageableListView<ArkSubjectSessionVO> pageableListView = new PageableListView<ArkSubjectSessionVO>("list", iModel, iArkCommonService.getRowsPerPage()) {
-
-
-			private static final long	serialVersionUID	= 1L;
+		return new PageableListView<ArkSubjectSessionVO>("list", iModel, iArkCommonService.getRowsPerPage()) {
+			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void populateItem(final ListItem<ArkSubjectSessionVO> item) {
-				final ArkSubjectSessionVO subject  = (ArkSubjectSessionVO) item.getModelObject();
+				final ArkSubjectSessionVO subject = item.getModelObject();
 				final String sessionId = subject.getSessionId();
 				final String userId = subject.getUserId();
 				final String host = subject.getHost();
 				final Date startTimestamp = subject.getStartTimestamp();
 				final Date lastAccessTime = subject.getLastAccessTime();
-				ArkUserVO arkUser=null;
+				ArkUserVO arkUser = null;
 				SimpleDateFormat sdf = new SimpleDateFormat(au.org.theark.core.Constants.DD_MM_YYYY_HH_MM_SS);
 				try {
-					arkUser=iArkCommonService.getUser((String) SecurityUtils.getSubject().getSession().getAttribute(au.org.theark.core.Constants.ARK_USERID));
-				} catch (InvalidSessionException | ArkSystemException
-						| EntityNotFoundException e) {
-					e.printStackTrace();
+					arkUser = iArkCommonService.getUser(userId);
+				} catch (InvalidSessionException | ArkSystemException | EntityNotFoundException e) {
+					logger.error("Failed to fetch Ark User", e);
 				}
+
 				item.add(new Label(au.org.theark.core.Constants.ARK_SESSION_ID, sessionId));
 				item.add(new Label(au.org.theark.core.Constants.ARK_USERID, userId));
-				item.add(new Label(au.org.theark.core.Constants.ARK_USER_NAME, arkUser.getFirstName()+" "+arkUser.getLastName()));
+				String userName = "";
+				if (arkUser != null) {
+					userName = arkUser.getFirstName() + " " + arkUser.getLastName();
+				}
+				item.add(new Label(au.org.theark.core.Constants.ARK_USER_NAME, userName));
 				item.add(new Label(au.org.theark.core.Constants.ARK_HOST, host));
 				item.add(new Label(au.org.theark.core.Constants.ARK_SESSION_START_TIMESTAMP, sdf.format(startTimestamp)));
 				item.add(new Label(au.org.theark.core.Constants.ARK_SESSION_LAST_ACCESS_TIME, sdf.format(lastAccessTime)));
 
 				item.add(new AttributeModifier("class", new AbstractReadOnlyModel<String>() {
-					private static final long	serialVersionUID	= 1938679383897533820L;
+					private static final long serialVersionUID = 1938679383897533820L;
 
 					@Override
 					public String getObject() {
@@ -96,7 +96,6 @@ public class SearchResultsPanel extends Panel {
 				}));
 			}
 		};
-		return pageableListView;
 	}
 
 	public void setFeedbackPanel(FeedbackPanel feedbackPanel) {
